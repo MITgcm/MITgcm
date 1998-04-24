e945ef9dd3439c8630af78bd2b613e34e84183aa
Chris Hill <cnh@mit.edu>
1998-04-24 15:28:21 +0000

    Added very terse install instructions Need to flesh out later

--- model/inc/SIZE.h
+++ model/inc/SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/SIZE.h,v 1.2 1998/04/24 02:11:37 cnh Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/SIZE.h,v 1.3 1998/04/24 15:28:21 cnh Exp $
 C
 C     /==========================================================\
 C     | SIZE.h Declare size of underlying computational grid.    |
@@ -35,12 +35,12 @@ C     Nz  - No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nz
       PARAMETER (
-     &           sNx =  60, 
-     &           sNy =  60,
+     &           sNx =  30, 
+     &           sNy =  30,
      &           OLx =   3,
      &           OLy =   3,
-     &           nSx =   1,
-     &           nSy =   1,
+     &           nSx =   2,
+     &           nSy =   2,
      &           nPx =   1,
      &           nPy =   1,
      &           Nx  = sNx*nSx*nPx,
