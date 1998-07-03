3dfee1b737f7f3c847738541fd674ed181fc9e6e
Alistair Adcroft <aadcroft@princeton.edu>
1998-07-03 18:42:11 +0000

    Added #includes for PARAMS.h, GRID.h and AVER.h in THE_MODEL_COMMON_BLOCKS.h

--- model/inc/THE_MODEL_COMMON_BLOCKS.h
+++ model/inc/THE_MODEL_COMMON_BLOCKS.h
@@ -1,5 +1,8 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.2 1998/04/24 02:11:37 cnh Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.3 1998/07/03 18:42:11 adcroft Exp $
 C
+#include "PARAMS.h"
+#include "GRID.h"
 #include "DYNVARS.h"
 #include "FFIELDS.h"
 #include "CG2D.h"
+#include "AVER.h"
