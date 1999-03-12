0ed81908da347bbc08c2c384e88f8a6ccc781b19
Alistair Adcroft <aadcroft@princeton.edu>
1999-03-12 16:44:04 +0000

    Some simple fixes that save a bit of memory.  o AVER.h was included even if not be used.  o gW, gWnm1 and cg3d_nm1 existed in DYNVARS.h and are only    needed in the non-hydrostatic version (which this isn't).

--- model/inc/DYNVARS.h
+++ model/inc/DYNVARS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/DYNVARS.h,v 1.7 1998/11/06 22:44:43 cnh Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/DYNVARS.h,v 1.8 1999/03/12 16:44:04 adcroft Exp $
 C
 C     /==========================================================\
 C     | DYNVARS.h                                                |
@@ -17,8 +17,9 @@ C     uVelD  - D grid zonal velocity
 C     vVelD  - D grid meridional velocity
       COMMON /DYNVARS_R/ 
      &                   uVel,vVel,theta,salt,
-     &                   gu,gv,gt,gs,guNm1,gvNm1,gtNm1,gsNm1,
-     &                   gw, gwnm1
+     &                   gu,gv,gt,gs,guNm1,gvNm1,gtNm1,gsNm1
+Caja &                   gu,gv,gt,gs,guNm1,gvNm1,gtNm1,gsNm1,
+Caja &                   gw, gwnm1
       _RL  uVel (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  vVel (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  theta(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
@@ -31,8 +32,8 @@ C     vVelD  - D grid meridional velocity
       _RL  gvNm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  gtNm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  gsNm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL  gw(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL  gwNm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+Caja  _RL  gw(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+Caja  _RL  gwNm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 #ifdef INCLUDE_CD_CODE
       COMMON /DYNVARS_CD/ 
      &                   uVelD, vVelD,
@@ -42,7 +43,7 @@ C     vVelD  - D grid meridional velocity
       _RL  uVeld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  vVeld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  cg2d_xNM1 (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL  cg3d_xNm1 (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+Caja  _RL  cg3d_xNm1 (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  uNm1  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  vNm1  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  guCD  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
--- model/inc/THE_MODEL_COMMON_BLOCKS.h
+++ model/inc/THE_MODEL_COMMON_BLOCKS.h
@@ -1,8 +1,10 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.3 1998/07/03 18:42:11 adcroft Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.4 1999/03/12 16:44:04 adcroft Exp $
 C
 #include "PARAMS.h"
 #include "GRID.h"
 #include "DYNVARS.h"
 #include "FFIELDS.h"
 #include "CG2D.h"
+#ifdef INCLUDE_DIAGNOSTICS_INTERFACE_CODE
 #include "AVER.h"
+#endif
