fc0c9d876245c8158b3cdda5aba19a90c2d82717
Alistair Adcroft <aadcroft@princeton.edu>
2000-06-29 19:32:00 +0000

    Added new example for testing OBCs.

---
+++ verification/internal_wave/code/CPP_OPTIONS.h
@@ -0,0 +1,142 @@
+C $Header: /u/gcmpack/MITgcm/verification/internal_wave/code/CPP_OPTIONS.h,v 1.1 2000/06/29 19:29:04 adcroft Exp $
+C
+
+C 
+C CPP flags controlling which code in included in the files that
+C will be compiled.
+C
+
+C o Include/exclude code for GM/Redi parameterization
+#undef  ALLOW_GMREDI
+
+C o Include/exclude code for KPP mixing scheme
+#undef  ALLOW_KPP
+
+C o Include/exclude code for C-D grid method of integrating the 
+C   coriolis terms
+#undef   INCLUDE_CD_CODE
+
+C o Include/exclude code for open-boundary conditions
+#define  ALLOW_OBCS
+
+C o Include/exclude diagnostics package interface code
+#define  INCLUDE_DIAGNOSTICS_INTERFACE_CODE
+
+C o Include/exclude latitude circle FFT filter
+#undef  INCLUDE_LAT_CIRC_FFT_FILTER_CODE
+
+C o Include/exclude temperature advection code
+#define  INCLUDE_T_ADVECTION_CODE
+#ifdef   INCLUDE_T_ADVECTION_CODE
+#define  _ADT(a)a
+#endif
+#ifndef  INCLUDE_T_ADVECTION_CODE
+#define  _ADT(a)
+#endif
+
+C o Include/exclude temperature diffusion code
+#define  INCLUDE_T_DIFFUSION_CODE
+#ifdef   INCLUDE_T_DIFFUSION_CODE
+#define  _LPT(a)a
+#define  _BHT(a)a
+#endif
+#ifndef  INCLUDE_T_DIFFUSION_CODE
+#define  _LPT(a)
+#define  _BHT(a)
+#endif
+
+C o Include/exclude temperature forcing code
+#define  INCLUDE_T_FORCING_CODE
+
+C o Include/exclude momentum advection code
+#define  INCLUDE_MOMENTUM_ADVECTION_CODE
+#ifdef   INCLUDE_MOMENTUM_ADVECTION_CODE
+#define  _ADM(a)a
+#endif
+#ifndef  INCLUDE_MOMENTUM_ADVECTION_CODE
+#define  _ADM(a)
+#endif
+
+C o Include/exclude laplacian viscosity code
+#define  INCLUDE_LP_MOMENTUM_DIFFUSION_CODE
+#ifdef   INCLUDE_LP_MOMENTUM_DIFFUSION_CODE
+#define  _LPM(a)a
+#endif
+#ifndef  INCLUDE_LP_MOMENTUM_DIFFUSION_CODE
+#define  _LPM(a)
+#endif
+
+C o Include/exclude biharmonic viscosity code
+#define  INCLUDE_BH_MOMENTUM_DIFFUSION_CODE
+#ifdef   INCLUDE_BH_MOMENTUM_DIFFUSION_CODE
+#define  _BHM(a)a
+#endif
+#ifndef  INCLUDE_BH_MOMENTUM_DIFFUSION_CODE
+#define  _BHM(a)
+#endif
+
+C o Include/exclude gradient of phy_hyd code
+#define INCLUDE_GRADPH_CODE
+#ifdef  INCLUDE_GRADPH_CODE
+#define _PHM(a)a
+#endif
+#ifndef INCLUDE_GRADPH_CODE
+#define _PHM(a)
+#endif
+
+C o Include/exclude momentum forcing code
+#define INCLUDE_MOMENTUM_FORCING_CODE
+
+C o Include/exclude momentum eqn metric terms code
+#define INCLUDE_MOMENTUM_METRIC_TERM_CODE
+
+C o Include/exclude phi_hyd calculation code
+#define INCLUDE_PHIHYD_CALCULATION_CODE
+
+C o Include/exclude prognostic variable shapiro filter code
+C   Note - Shapiro filter of prognostics variables requires the
+C          three steps "step forward including edges", filter, 
+C          "communicate edges".
+C           If the filtering code is included then we do not use the 
+C          pipelined "step forward including edges" in S/R DYNAMICS. 
+C          Instead the three steps are performed before DYNAMICS one 
+C          after another in an un-pipelined fashion.
+#undef  INCLUDE_SHAPIRO_FILTER_CODE
+#ifdef  INCLUDE_SHAPIRO_FILTER_CODE
+#undef  DO_PIPELINED_CORRECTION_STEP
+#endif
+#ifndef INCLUDE_SHAPIRO_FILTER_CODE
+#define DO_PIPELINED_CORRECTION_STEP
+#endif
+
+C o Include/exclude call to S/R FIND_RHO
+#define INCLUDE_FIND_RHO_CALL
+
+C o Include/exclude call to S/R CONVECT
+#define INCLUDE_CONVECT_CALL
+
+C o Include/exclude call to S/R CALC_ISOSLOPES
+#define INCLUDE_CALC_ISOSLOPES_CALL
+
+C o Include/exclude call to S/R CALC_DIFFUSIVITY
+#define INCLUDE_CALC_DIFFUSIVITY_CALL
+
+C o Allow nonHydrostatic code
+#define ALLOW_NONHYDROSTATIC
+
+C o Use "natural" boundary conditions for salinity
+C   instead of the "virtual salt flux"
+#undef  USE_NATURAL_BCS
+
+C o Use "OLD" UV discretisation near boundaries (*not* recommended)
+C   Note - only works with  #undef NO_SLIP_LATERAL  in calc_mom_rhs.F
+C          because the old code did not have no-slip BCs
+#undef  OLD_ADV_BCS
+
+C o Use "OLD" UV geometry on sphere (definately *NOT* recommended)
+C   Note - only works with  #undef NO_SLIP_LATERAL  in calc_mom_rhs.F
+C          because the old code did not have no-slip BCs
+#undef  OLD_UV_GEOMETRY
+
+C o Execution environment support options
+#include "CPP_EEOPTIONS.h"
---
+++ verification/internal_wave/input/gendata.m
@@ -0,0 +1,132 @@
+% This is a matlab script that generates the input data
+% variable x resolution
+prec='real*8';
+ieee='b';
+
+
+% Dimensions of grid
+nx=60;
+ny=3;
+nz=20;
+% Nominal depth of model (meters)
+H=200.0;
+% Size of domain
+Lx=13.3e3;
+
+% Horizontal resolution (m)
+dx=zeros(nx,1);
+for i=1:nx
+dx(i) = Lx/(nx+1);
+end
+
+dy = Lx/nx
+% Stratification
+gravity=9.81;
+talpha=2.0e-4;
+N2=1e-6;
+
+Tz=N2/(gravity*talpha);
+
+dz=H/nz;
+sprintf('delZ = %d * %7.6g,',nz,dz)
+
+x=zeros(nx,1);
+x(1) = dx(1);
+for i=2:nx
+x(i)=x(i-1) + dx(i);
+end
+z=-dz/2:-dz:-H;
+
+%[Y,X]=meshgrid(y,x);
+
+% Temperature profile
+Tref=Tz*z-mean(Tz*z);
+[sprintf('Tref =') sprintf(' %8.6g,',Tref)]
+t=0.0*rand([nx,ny,nz]);
+for k=1:nz
+t(:,:,k) = t(:,:,k) + Tref(k);
+end
+fid=fopen('T.init','w',ieee); fwrite(fid,t,prec); fclose(fid);
+
+% Sloping channel 
+slope=0.0
+offset=2.5e3;
+dmax=-40.0;
+d=0.0*rand([nx,ny]);
+for i=1:nx
+for j=1:ny
+d(i,j) = -H;
+d(i,j) = d(i,j) + slope*(x(i) - offset);
+if d(i,j) < -H ;
+d(i,j) = -H;
+end
+if d(i,j) > dmax;
+d(i,j) = dmax;
+end
+end
+end
+%d(nx,:)=0.0;
+fid=fopen('topog.slope','w',ieee); fwrite(fid,d,prec); fclose(fid);
+plot(x,d(:,2))
+
+fid=fopen('delXvar','w',ieee); fwrite(fid,dx,prec); fclose(fid);
+
+%convex slope
+nxslope=(dmax + H)/(slope)
+d1=zeros(nx,ny);
+hamp=(H-dmax)/5.0
+pi=4.0*atan(1.0)
+for i=1:nx
+for j=1:ny
+if x(i) < (offset + nxslope)
+if x(i) < offset
+d1(i,j) = d(i,j);
+else
+d1(i,j) = -H;
+d1(i,j) = d(i,j) + hamp*sin(pi*(x(i)-offset)/nxslope);
+if d1(i,j) < -H ;
+d1(i,j) = -H;
+end
+if d1(i,j) > dmax;
+d1(i,j) = dmax;
+end
+end
+else
+d1(i,j) = d(i,j);
+end
+end
+end
+%d1(end-1:end,:)=d1(1:2,:); % debug by aja
+fid=fopen('topog.convex','w',ieee); fwrite(fid,d1,prec); fclose(fid);
+hold on
+plot(x,d1(:,2),'g')
+
+%convex slope
+d2=zeros(nx,ny);
+for i=1:nx
+for j=1:ny
+if x(i) < (offset + nxslope)
+if x(i) < offset
+d2(i,j) = d(i,j);
+else
+d2(i,j) = -H;
+d2(i,j) = d(i,j) - hamp*sin(pi*(x(i)-offset)/nxslope);
+if d2(i,j) < -H ;
+d2(i,j) = -H;
+end
+if d2(i,j) > dmax;
+d2(i,j) = dmax;
+end
+end
+else
+d2(i,j) = d(i,j);
+end
+end
+end
+%d2(end-1:end,:)=d2(1:2,:); % debug by aja
+fid=fopen('topog.concave','w',ieee); fwrite(fid,d2,prec); fclose(fid);
+hold on
+plot(x,d2(:,2),'r')
+
+
+fid=fopen('delXvar','w',ieee); fwrite(fid,dx,prec); fclose(fid);
