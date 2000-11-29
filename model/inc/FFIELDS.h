b04996eab799de903823476d1e4e762d5983d7a2
Alistair Adcroft <aadcroft@princeton.edu>
2000-11-29 22:31:47 +0000

    Fixed confusion about units of forcing arrays in FFIELDS.h namely Fu,Fv,Qnet,Qsw,EmPmR:   - Removed verification/natl_box/code/external_fields_scale.F         (did not differ from that in model/src)   - Changed units of fu,fv,Qnet,Qsw,EmPmR back to proper units      (see FFIELDS.h for description)   - Scale fu,fv,Qnet,Qsw,EmPmR when used in external_forcing_surf.F,     kpp_calc.F and kpp_transport_t.F   - Removed model/src/external_fields_scale.F and calls to it   - verification/natl_box uses flux data with "atmospheric" sign so     a special version of external_fields_load.F is used to     change the data as it's read in. This way, the arrays     have the right units and signs at all times tha a user could     possibly use them.

--- model/inc/FFIELDS.h
+++ model/inc/FFIELDS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/FFIELDS.h,v 1.8 2000/11/13 16:32:57 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/FFIELDS.h,v 1.9 2000/11/29 22:29:23 adcroft Exp $
 C
 C     /==========================================================\
 C     | FFIELDS.h                                                |
@@ -10,30 +10,20 @@ C     \==========================================================/
 C
 C--   For a classical "gyre" type experiment just one term is needed.
 C
-C     fu     - Zonal velocity tendency term
-C                -> read assumes     N/m^2 (>0 from East to West)
-C                -> transformed to   m/s^2
-C                   via              fu   ->   -fu/(rhoNil*dR)
+C     fu     - Zonal surface wind stress
+C                Units are           N/m^2 (>0 from East to West)
 C
-C     fv     - Meridional velocity tendency term
-C                -> read assumes     N/m^2 (>0 from North to South))
-C                -> transformed to   m/s^2
-C                   via              fv   ->   -fv/(rhoNil*dR)
+C     fv     - Meridional surface wind stress
+C                Units are           N/m^2 (>0 from North to South))
 C
 C     EmPmR  - Evaporation - Precipitation - Runoff
-C                -> read assumes     m/s (>0 for ocean salting)
-C                -> transformed to   psu/s
-C                   via              empmr -> empmr*35./dR
+C                Units are           m/s (>0 for ocean salting)
 C
-C     Qnet   - Surface heat flux
-C                -> read assumes     W/m^2=kg/s^3 (>0 for ocean cooling)
-C                -> transformed to   K/s
-C                   via              Qnet -> -Qnet/(rhonil*Cp*dR)
+C     Qnet   - Upward surface heat flux
+C                Units are           W/m^2=kg/s^3 (>0 for ocean cooling)
 C
-C     Qsw    - Short-wave surface heat flux
-C                -> read assumes     W/m^2=kg/s^3 (>0 for ocean cooling)
-C                -> transformed to   K/s
-C                   via              Qsw -> -Qsw/(rhonil*Cp*dR)
+C     Qsw    - Upward short-wave surface heat flux
+C                Units are           W/m^2=kg/s^3 (>0 for ocean cooling)
 C
 C     SST    - Sea surface temperature (degrees) for relaxation
 C     SSS    - Sea surface salinity (psu) for relaxation
@@ -54,18 +44,18 @@ C     SSS    - Sea surface salinity (psu) for relaxation
       _RS  SSS      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  Qsw      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
-C     surfaceTendencyU
+C     surfaceTendencyU       (units are  m/s^2)
 C                -> usage in gU:     gU = gU + surfaceTendencyU[m/s^2]
 C
-C     surfaceTendencyV
+C     surfaceTendencyV       (units are  m/s^2)
 C                -> usage in gV:     gV = gV + surfaceTendencyV[m/s^2]
 C
-C     surfaceTendencyS   
+C     surfaceTendencyS       (units are  psu/s)
 C            - EmPmR plus salinity relaxation term
 C                -> calculate        -lambda*(S(model)-S(clim))
 C                -> usage in gS:     gS = gS + surfaceTendencyS[psu/s]
 C
-C     surfaceTendencyT
+C     surfaceTendencyT       (units are  degrees/s)
 C            - Qnet plus temp. relaxation
 C                -> calculate        -lambda*(T(model)-T(clim))
 C            >>> Qnet assumed to be total flux minus s/w rad. <<<
