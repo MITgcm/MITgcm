7c5fa2f731b73a640be5ba41ddaf25ff1d90bd0e
Jean-Michel Campin <jmc@ocean.mit.edu>
2001-03-06 17:27:08 +0000

    remove "include CG2D.h" ; add "SURFACE.h"

--- model/inc/THE_MODEL_COMMON_BLOCKS.h
+++ model/inc/THE_MODEL_COMMON_BLOCKS.h
@@ -1,11 +1,11 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.5 2001/02/04 14:38:45 cnh Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.6 2001/03/06 17:27:08 jmc Exp $
 C $Name:  $
 C
 #include "PARAMS.h"
 #include "GRID.h"
 #include "DYNVARS.h"
 #include "FFIELDS.h"
-#include "CG2D.h"
-#ifdef INCLUDE_DIAGNOSTICS_INTERFACE_CODE
-#include "AVER.h"
+#include "SURFACE.h"
+#ifdef ALLOW_TIMEAVE
+#include "TIMEAVE_STATV.h"
 #endif
