c565cc7e157cb290b53516cb489cc160a0f232c9
Daniel Jamous <jamous@fas.harvard.edu>
2001-06-21 14:34:33 +0000

    Wrote real constants using the "_d" macro notation in order to be consistent with the _RL declaration of Limiter.

--- pkg/generic_advdiff/GAD_FLUX_LIMITER.h
+++ pkg/generic_advdiff/GAD_FLUX_LIMITER.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/generic_advdiff/GAD_FLUX_LIMITER.h,v 1.2 2001/06/20 16:15:07 jamous Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/generic_advdiff/GAD_FLUX_LIMITER.h,v 1.3 2001/06/21 14:34:33 jamous Exp $
 C $Name:  $
 
 C Statement function to describe flux limiter
@@ -9,4 +9,4 @@ C Suberbee      Limiter(Cr)=max(0.,max(min(1.,2*Cr),min(2.,Cr)))
 
 c     Limiter(Cr)=0.
 c     Limiter(Cr)=1.
-      Limiter(Cr)=max(0.d0,max(min(1.d0,2.d0*Cr),min(2.d0,Cr)))
+      Limiter(Cr)=max(0. _d 0,max(min(1. _d 0,2. _d 0*Cr),min(2. _d 0,Cr))) 
