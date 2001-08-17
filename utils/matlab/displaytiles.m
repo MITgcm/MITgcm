83ce4d2aa6374bd99fec78e28d29a8ae9540e3a1
Alistair Adcroft <aadcroft@princeton.edu>
2001-08-17 18:07:08 +0000

    Two useful matlab scripts for cube.

---
+++ utils/matlab/displaytiles.m
@@ -0,0 +1,45 @@
+function [] = displaytiles(A)
+% Display tiled field.
+%
+% Dimensions of A must be (n,n,6)
+
+global cmin cmax
+clf
+
+cmin=min(min(min(A)));
+cmax=max(max(max(A)));
+if isnan(cmin)
+ cmin = 0;
+end
+if isnan(cmax)
+ cmax = 1;
+end
+if cmin==cmax
+ cmax = cmin+1;
+end
+
+subplot(3,4,9)
+myplot(A(:,:,1)')
+subplot(3,4,10)
+myplot(A(:,:,2)')
+subplot(3,4,6)
+myplot(A(:,:,3)')
+subplot(3,4,7)
+myplot(A(:,:,4)')
+subplot(3,4,3)
+myplot(A(:,:,5)')
+subplot(3,4,4)
+myplot(A(:,:,6)')
+
+
+%Colorbar
+subplot(3,10,30)
+x=1;
+y=(0:63)'/63*(cmax-cmin)+cmin;
+pcol(x,y,y);
+set(gca,'XTickLabel',[]);
+
+function [] = myplot( Q )
+global cmin cmax
+pcol( Q );
+caxis([cmin cmax])
---
+++ utils/matlab/tiles.m
@@ -0,0 +1,56 @@
+function [a] = tile(b,n)
+% a=tile(b,n);
+%
+% Extract single tile from cubed array
+%
+% b can have dimensions (M*6,M,Nr) or (M,M,Nr,6)
+%
+% n can be vector of integers between 1 and 6
+
+
+if min(n)<1 | max(n)>6
+ disp(sprintf('n=',n));
+ error('tile: second argument n is out of range');
+end
+
+if size(b,ndims(b))==6
+ switch ndims(b)
+  case 3,
+   a=b(:,:,n);
+  case 4,
+   a=b(:,:,:,n);
+  otherwise
+   error('tile: it seems that b has too many dimensions');
+ end
+elseif size(b,2)==6
+ m=size(b,1);
+ k=1;
+ for N=n;
+ switch ndims(b)
+  case 3,
+   a(:,:,k)=squeeze(b(:,N,:));
+  case 4,
+   a(:,:,:,k)=squeeze(b(:,N,:,:));
+  otherwise
+   error('tile: it seems that b has too many dimensions');
+ end
+ k=k+1;
+ end
+elseif size(b,1)==size(b,2)*6
+ m=size(b,2);
+ k=1;
+ for N=n;
+ switch ndims(b)
+  case 2,
+   a(:,:,k)=b((N-1)*m+1:N*m,:);
+  case 3,
+   a(:,:,:,k)=b((N-1)*m+1:N*m,:,:);
+  otherwise
+   error('tile: it seems that b has too many dimensions');
+ end
+ k=k+1;
+ end
+else
+ disp(sprintf('Size(b) = %i %i %i %i %i %i',size(b)));
+ error('tile: Size of first argument is not consistent with cubed array');
+end
