827e7b2e4df314518cc2234adb328dfe90963265
Jean-Michel Campin <jmc@ocean.mit.edu>
2001-09-27 15:39:55 +0000

    update of aim-LatLon experiment following the recent changes in pkg/aim

--- verification/aim.5l_LatLon/code/SIZE.h
+++ verification/aim.5l_LatLon/code/SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/aim.5l_LatLon/code/SIZE.h,v 1.2 2001/05/29 14:01:48 adcroft Exp $
+C $Header: /u/gcmpack/MITgcm/verification/aim.5l_LatLon/code/SIZE.h,v 1.3 2001/09/27 15:36:29 jmc Exp $
 C $Name:  $
 C
 C     /==========================================================\
@@ -38,8 +38,8 @@ C     Nr  - No. points in Z for full process domain.
       PARAMETER (
      &           sNx = 128,
      &           sNy =  64,
-     &           OLx =   5,
-     &           OLy =   5,
+     &           OLx =   3,
+     &           OLy =   3,
      &           nSx =   1,
      &           nSy =   1,
      &           nPx =   1,
