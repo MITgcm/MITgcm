83ffdb2b8809598adf2571ab7a725389d5ee2c3e
Patrick Heimbach <heimbach@mit.edu>
2001-09-28 04:18:58 +0000

    Replaced addynvars.h adffields.h by adcommon.h which contains all relevant adjoint common blocks needed for adjoint support routines.

--- pkg/autodiff/adffields.h
+++
@@ -1,7 +0,0 @@
-      common /adffields/ adfu, adfv, adqnet, adempmr, adsst, adsss
-      _RL adfu(1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
-      _RL adfv(1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
-      _RL adqnet(1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
-      _RL adempmr(1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
-      _RL adsst(1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
-      _RL adsss(1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
