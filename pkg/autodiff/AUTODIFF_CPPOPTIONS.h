d1517810881862706178118713e69ca500ad5740
Patrick Heimbach <heimbach@mit.edu>
2001-09-28 04:19:27 +0000

    Started to add comments...

--- pkg/autodiff/AUTODIFF_CPPOPTIONS.h
+++ pkg/autodiff/AUTODIFF_CPPOPTIONS.h
@@ -1,5 +1,14 @@
 
-C 
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
+CBOP
+C     !ROUTINE: AUTODIFF_CPPOPTIONS.h 
+C     !INTERFACE:
+C     include "AUTODIFF_CPPOPTIONS.h "
+C     !DESCRIPTION:
+C     *==========================================================*
+C     | CPP_EEMACROS.h                                            
+C     *==========================================================*
+C     CPP flags controlling which code is included in the files
+C     that will be compiled.
+C     These macros have been shifted to the common header file
+C     ECCO_CPPOPTIONS.h
+CEOP
