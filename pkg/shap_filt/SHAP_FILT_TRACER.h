2fd487f9ae3d8cb82a5045d5c569157a507390f1
Jean-Michel Campin <jmc@ocean.mit.edu>
2002-03-04 01:37:50 +0000

    remove unused S/R and .h files

--- pkg/shap_filt/SHAP_FILT_TRACER.h
+++
@@ -1,9 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/pkg/shap_filt/Attic/SHAP_FILT_TRACER.h,v 1.2 2001/05/29 14:01:40 adcroft Exp $
-C $Name:  $
-C
-C     /==========================================================\
-C     | SHAP_FILT_TRACER.h                                       |
-C     | o Globals used by Fortran shapiro filter routine.        |
-C     \==========================================================/
-      COMMON / SHFT_COMMON_R8 / tmpFld
-      _RL tmpFld(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
