df9e3012b8d05036af22b9d75fdfe4e181c0a38d
Patrick Heimbach <heimbach@mit.edu>
2002-09-16 18:15:21 +0000

    Configuration for tangent linear (forward mode) gradient checks.

---
+++ verification/carbon/code_ftl/ECCO_CPPOPTIONS.h
@@ -0,0 +1,68 @@
+C 
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C
+C ********************************************************************
+C ***                         ECCO Package                         ***
+C ********************************************************************
+C
+#undef  ALLOW_ECCO_FORWARD_RUN
+#undef  ALLOW_ECCO_DIAGNOSTICS_RUN
+#undef  ALLOW_ECCO_OPTIMIZATION
+
+C       >>> Do a long protocol.
+#undef ECCO_VERBOSE
+
+C ********************************************************************
+C ***                  Adjoint Support Package                     ***
+C ********************************************************************
+
+C o Include/exclude code in order to be able to automatically
+C   differentiate the MITgcmUV by using the Tangent Linear and
+C   Adjoint Model Compiler (TAMC).
+cph#define INCLUDE_AUTODIFF_PACKAGE
+C
+#define ALLOW_AUTODIFF_TAMC
+C       >>> Checkpointing as handled by TAMC
+#define ALLOW_TAMC_CHECKPOINTING
+
+C ********************************************************************
+C ***                     Calender Package                         ***
+C ********************************************************************
+C
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C
+C o Include the calendar tool.
+#undef ALLOW_CALENDAR
+#undef ALLOW_CAL_NENDITER
+
+C ********************************************************************
+C ***                Cost function Package                         ***
+C ********************************************************************
+C 
+C       >>> Cost function contributions
+#define ALLOW_COST
+#undef ALLOW_COST_TEST
+#define ALLOW_COST_TRACER
+#undef ALLOW_COST_ATLANTIC_HEAT
+#undef ALLOW_COST_VECTOR
+
+C ********************************************************************
+C ***               Control vector Package                         ***
+C ********************************************************************
+C 
+#undef  ALLOW_NONDIMENSIONAL_CONTROL_IO
+C       >>> Initial values.
+#define ALLOW_THETA0_CONTROL
+#define ALLOW_SALT0_CONTROL
+#define ALLOW_TR10_CONTROL
+#define ALLOW_TAUU0_CONTROL
+#define ALLOW_TAUV0_CONTROL
+#define ALLOW_SFLUX0_CONTROL
+#define ALLOW_HFLUX0_CONTROL
+#undef ALLOW_SSS0_CONTROL
+#undef ALLOW_SST0_CONTROL
+#undef ALLOW_DIFFKR_CONTROL
+#undef ALLOW_KAPGM_CONTROL
+
