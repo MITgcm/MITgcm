ac68bcbc527cbf1abca2b9ed959ea9a7d4b933f6
Jean-Michel Campin <jmc@ocean.mit.edu>
2002-09-27 19:58:16 +0000

    replaced by AIM_SIZE.h & AIM_GRID.h

--- pkg/aim/Lev_def.h
+++
@@ -1,7 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/pkg/aim/Attic/Lev_def.h,v 1.3 2001/05/29 19:28:53 cnh Exp $
-C $Name:  $
-
-      COMMON/NB_LEVELS/NLEVxy, NLEVxyU, NLEVxyV
-      INTEGER NLEVxy(IX*IL,MAX_NO_THREADS)
-      INTEGER NLEVxyU(IX*IL,MAX_NO_THREADS)
-      INTEGER NLEVxyV(IX*IL,MAX_NO_THREADS)
