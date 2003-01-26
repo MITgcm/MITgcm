00b29feb629575a346ec73df8fc9831c3ea6eac3
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-01-26 21:06:11 +0000

    r* coordinate added in #ifdef NONLIN_FRSURF block.  (modification to pressure gradient not yet implemented)

--- model/inc/SURFACE.h
+++ model/inc/SURFACE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/SURFACE.h,v 1.8 2002/12/10 02:47:26 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/SURFACE.h,v 1.9 2003/01/26 21:03:00 jmc Exp $
 C $Name:  $
 C
 CBOP
@@ -45,7 +45,7 @@ C     hDivFlow :: Div. Barotropic Flow at current time [transport unit m3/s]
 #endif
 
 #ifdef NONLIN_FRSURF
-C--   COMMON /SURF_CHANGE/ transient varariable used for Non-Lin Free-Surf
+C--   COMMON /SURF_CHANGE/ transient variables used for Non-Lin Free-Surf
 C     hFac_surfC ::  New thickness factor of the surface level
 C                        center (Tracer point)
 C     hFac_surfW ::  idem, West  interface (U point)
@@ -58,5 +58,39 @@ C     PmEpR :: keep the fresh water input (=-EmPmR) of the previous time step
       _RS  hFac_surfW(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  hFac_surfS(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  PmEpR(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+
+C--   COMMON /RSTAR_CHANGE/ transient variables used with r* Coordinate
+C     rStarFacC :: = dr/dr* = ratio of r-thickness / r*-thickness = h^n / H
+C     rStarFacW :: same but for West  face
+C     rStarFacS :: same but for South face
+C     rStarExpC :: column expansion factor = h^n+1/h^n , Centered
+C     rStarExpW :: column expansion factor = h^n+1/h^n , Western  face
+C     rStarExpS :: column expansion factor = h^n+1/h^n , Southern face
+C     rStarDhCDt:: relative time derivative of h_Center = d.eta/dt / H 
+C     rStarDhWDt:: relative time derivative of h_West_face  (u.point)
+C     rStarDhSDt:: relative time derivative of h_South_face (v.point)
+      COMMON /RSTAR_CHANGE/
+     &     rStarFacC, rStarFacW, rStarFacS,
+     &     rStarExpC, rStarExpW, rStarExpS,
+     &     rStarDhCDt,rStarDhWDt,rStarDhSDt
+      _RL  rStarFacC (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarFacW (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarFacS (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarExpC (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarExpW (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarExpS (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarDhCDt(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarDhWDt(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  rStarDhSDt(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+
+C--   COMMON /RSTAR_FIXED/ fixed thickness ratio ( r* discretization )
+C     h0FacC :: initial (and fixed in time) hFacC factor
+C     h0FacW :: initial (and fixed in time) hFacW factor
+C     h0FacS :: initial (and fixed in time) hFacS factor
+      COMMON /RSTAR_FIXED/
+     & h0FacC, h0FacW, h0FacS
+      _RS h0FacC(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RS h0FacW(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RS h0FacS(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 
 #endif /* NONLIN_FRSURF */
