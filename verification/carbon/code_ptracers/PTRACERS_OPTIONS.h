c508fbd32635bd60beab184671792a028de780f8
Patrick Heimbach <heimbach@mit.edu>
2003-06-27 02:11:12 +0000

    verification for adjoint with ptracers o adj. grdchk fail so far o tlm. grdchk ok o checked tapelev/comlev recomp.: OK

---
+++ verification/carbon/code_ptracers/PTRACERS_OPTIONS.h
@@ -0,0 +1,28 @@
+C $Header: /u/gcmpack/MITgcm/verification/carbon/code_ptracers/Attic/PTRACERS_OPTIONS.h,v 1.1 2003/06/27 02:11:12 heimbach Exp $
+C $Name:  $
+
+C CPP options file for PTRACERS package
+C
+C Use this file for selecting options within the PTRACERS package
+C
+C PTRACERS is enabled with ALLOW_PTRACERS in CPP_OPTIONS.h or in
+C the Makefile with DEFINES=-DALLOW_PTRACERS
+
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_PTRACERS
+
+C NUMBER_OF_PTRACERS defines how many passive tracers are allocated/exist.
+C This CPP macro is *only* used in PTRACERS.h to set an integer parameter.
+C <Please> do not make use of it elsewhere.
+C
+C NUMBER_OF_PTRACERS can be defined in CPP_OPTIONS.h, or in the Makefile
+C with DEFINES=-DNUMBER_OF_PTRACERS=4
+C
+C If NUMBER_OF_PTRACERS is not specified elsewhere and ALLOW_PTRACERS
+C is set then NUMBER_OF_PTRACERS is set here (default 1)
+#ifndef NUMBER_OF_PTRACERS
+#define NUMBER_OF_PTRACERS 1
+#endif
+
+#endif /* ALLOW_PTRACERS */
