a8844d79c521ccd815d546792ba9fc811157c7fc
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-07-31 18:22:10 +0000

    read initial state from files.

--- pkg/land/LAND_PARAMS.h
+++ pkg/land/LAND_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/land/LAND_PARAMS.h,v 1.1 2003/06/12 17:54:22 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/land/LAND_PARAMS.h,v 1.2 2003/07/31 18:22:10 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_LAND
@@ -13,11 +13,18 @@ C     *==========================================================*
 C--   COMMON /LAND_PAR_L/: logical parameters
 C     land_calc_grT :: step forward ground Temperature
 C     land_calc_grW :: step forward soil moiture
-
       COMMON /LAND_PAR_L/
      &  land_calc_grT, land_calc_grW
       LOGICAL land_calc_grT
       LOGICAL land_calc_grW
+
+C--   COMMON /LAND_PAR_C/: Character valued parameters
+C     land_grT_iniFile :: File containing initial ground Temp.
+C     land_grW_iniFile :: File containing initial ground Water.
+      COMMON /LAND_PAR_C/ 
+     &    land_grT_iniFile, land_grW_iniFile
+      CHARACTER*(MAX_LEN_FNAM) land_grT_iniFile
+      CHARACTER*(MAX_LEN_FNAM) land_grW_iniFile
 
 C--   COMMON /LAND_PAR_R/: real-type parameters
 C     land_deltaT     :: land model time-step
