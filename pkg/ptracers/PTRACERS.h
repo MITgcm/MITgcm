3c04bbe39081d5c31f4c0bad635a7a9ee7c6c030
Dimitris Menemenlis <dmenemenlis@gmail.com>
2003-09-25 03:01:59 +0000

    o Mods and bug fixes to pkg/cal, pkg/exf, etc., needed for computation   of tracer Green's fucntions for ocean inversion project.

--- pkg/ptracers/PTRACERS.h
+++ pkg/ptracers/PTRACERS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ptracers/Attic/PTRACERS.h,v 1.3 2003/09/24 04:52:39 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ptracers/Attic/PTRACERS.h,v 1.4 2003/09/25 03:01:59 dimitri Exp $
 C $Name:  $
 
 #ifdef ALLOW_PTRACERS
@@ -14,6 +14,19 @@ C Contains passive tracer fields and parameters.
 C     Package flag
       logical PTRACERSisON
       COMMON /PTRACERS_PACKAGE/ PTRACERSisON
+
+C NUMBER_OF_PTRACERS defines how many passive tracers are allocated/exist.
+C This CPP macro is *only* used in PTRACERS.h to set an integer parameter.
+C <Please> do not make use of it elsewhere.
+C
+C NUMBER_OF_PTRACERS can be defined in CPP_OPTIONS.h, or in the Makefile
+C with DEFINES=-DNUMBER_OF_PTRACERS=4
+C
+C If NUMBER_OF_PTRACERS is not specified elsewhere and ALLOW_PTRACERS
+C is set then NUMBER_OF_PTRACERS is set here (default 1)
+#ifndef NUMBER_OF_PTRACERS
+#define NUMBER_OF_PTRACERS 1
+#endif
 
 C     Number of tracers
       INTEGER PTRACERS_num
--- pkg/ptracers/PTRACERS_OPTIONS.h
+++ pkg/ptracers/PTRACERS_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_OPTIONS.h,v 1.1 2002/03/04 19:01:29 adcroft Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_OPTIONS.h,v 1.2 2003/09/25 03:01:59 dimitri Exp $
 C $Name:  $
 
 C CPP options file for PTRACERS package
@@ -11,18 +11,5 @@ C the Makefile with DEFINES=-DALLOW_PTRACERS
 #include "CPP_OPTIONS.h"
 
 #ifdef ALLOW_PTRACERS
-
-C NUMBER_OF_PTRACERS defines how many passive tracers are allocated/exist.
-C This CPP macro is *only* used in PTRACERS.h to set an integer parameter.
-C <Please> do not make use of it elsewhere.
-C
-C NUMBER_OF_PTRACERS can be defined in CPP_OPTIONS.h, or in the Makefile
-C with DEFINES=-DNUMBER_OF_PTRACERS=4
-C
-C If NUMBER_OF_PTRACERS is not specified elsewhere and ALLOW_PTRACERS
-C is set then NUMBER_OF_PTRACERS is set here (default 1)
-#ifndef NUMBER_OF_PTRACERS
-#define NUMBER_OF_PTRACERS 2
-#endif
 
 #endif /* ALLOW_PTRACERS */
