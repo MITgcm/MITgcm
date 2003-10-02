5fc09f9af72a79227d277588431f49e8f4e6e024
Patrick Heimbach <heimbach@mit.edu>
2003-10-02 22:47:16 +0000

    Adding verif. for global_ocean adjoint using vector invariant momenum

---
+++ verification/global_ocean.90x40x15/code_ad_vecinv/GMREDI_OPTIONS.h
@@ -0,0 +1,36 @@
+C $Header: /u/gcmpack/MITgcm/verification/global_ocean.90x40x15/code_ad_vecinv/Attic/GMREDI_OPTIONS.h,v 1.1 2003/10/02 22:47:15 heimbach Exp $
+C $Name:  $
+
+C CPP options file for GM/Redi package
+C
+C Use this file for selecting options within the GM/Redi package
+C
+C GM/Redi is enabled with ALLOW_GMREDI in CPP_OPTIONS.h
+
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_GMREDI
+
+C Designed to simplify the Ajoint code:
+C  exclude the clipping/tapering part of the code that is not used
+#define GM_EXCLUDE_CLIPPING
+#define GM_EXCLUDE_AC02_TAP
+#undef  GM_EXCLUDE_TAPERING 
+ 
+C This allows to use Visbeck et al formulation to compute K_GM+Redi
+#undef GM_VISBECK_VARIABLE_K
+
+C This allows the leading diagonal (top two rows) to be non-unity
+C (a feature required when tapering adiabatically).
+#define  GM_NON_UNITY_DIAGONAL
+
+C Allows to use different values of K_GM and K_Redi ; also to
+C be used with the advective form (Bolus velocity) of GM
+#define  GM_EXTRA_DIAGONAL
+
+C Allows to use the advective form (Bolus velocity) of GM
+C  instead of the Skew-Flux form (=default)
+#define  GM_BOLUS_ADVEC
+
+
+#endif /* ALLOW_GMREDI */
