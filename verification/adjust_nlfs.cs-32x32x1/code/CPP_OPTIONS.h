379aef4a0ce44800e8f0b3fae9e327cfbf1cfbcb
Ed Hill <ed@eh3.com>
2003-10-09 23:48:17 +0000

    o added "#define NONLIN_FRSURF"

---
+++ verification/adjust_nlfs.cs-32x32x1/code/CPP_OPTIONS.h
@@ -0,0 +1,67 @@
+C $Header: /u/gcmpack/MITgcm/verification/adjust_nlfs.cs-32x32x1/code/Attic/CPP_OPTIONS.h,v 1.3 2003/10/09 23:48:17 edhill Exp $
+C $Name:  $
+
+#include "PACKAGES_CONFIG.h"
+
+C CPP flags controlling particular source code features
+
+C o Include/exclude code for C-D grid method of integrating the 
+C   coriolis terms
+#undef  INCLUDE_CD_CODE
+
+C o Include/exclude temperature forcing code
+C#define  INCLUDE_T_FORCING_CODE
+
+C o Shortwave heating as extra term in external_forcing.F
+C Note: this should be a run-time option and not necessarily dependent on KPP
+#ifdef ALLOW_KPP
+#define  SHORTWAVE_HEATING
+#endif
+
+C o Include/exclude momentum forcing code
+#define INCLUDE_MOMENTUM_FORCING_CODE
+
+C o Include/exclude momentum eqn metric terms code
+#define INCLUDE_MOMENTUM_METRIC_TERM_CODE
+
+C o Include/exclude phi_hyd calculation code
+#define INCLUDE_PHIHYD_CALCULATION_CODE
+
+C o Include/exclude call to S/R CONVECT
+#define INCLUDE_CONVECT_CALL
+
+C o Include/exclude call to S/R CALC_DIFFUSIVITY
+#define INCLUDE_CALC_DIFFUSIVITY_CALL
+
+C o Include/exclude nonHydrostatic code
+#undef ALLOW_NONHYDROSTATIC
+
+C o Include pressure loading code
+#define ATMOSPHERIC_LOADING
+
+C o Use "Exact Convervation" of fluid in Free-Surface formulation
+C   so that d/dt(eta) is exactly equal to - Div.Transport
+#define EXACT_CONSERV
+
+C o Allow the use of Non-Linear Free-Surface formulation
+C   this implies that surface thickness (hFactors) vary with time
+#define NONLIN_FRSURF
+
+C o Use "OLD" UV discretisation near boundaries (*not* recommended)
+C   Note - only works with  #undef NO_SLIP_LATERAL  in calc_mom_rhs.F
+C          because the old code did not have no-slip BCs
+#undef  OLD_ADV_BCS
+
+C o Use "OLD" UV geometry on sphere (definately *NOT* recommended)
+C   Note - only works with  #undef NO_SLIP_LATERAL  in calc_mom_rhs.F
+C          because the old code did not have no-slip BCs
+#undef  OLD_UV_GEOMETRY
+
+C o Execution environment support options
+#include "CPP_EEOPTIONS.h"
+
+C o Include/exclude code specific to the ECCO/SEALION version.
+#undef INCLUDE_ECCO_PACKAGE
+#ifdef INCLUDE_ECCO_PACKAGE
+#include "ECCO_CPPOPTIONS.h"
+#endif
