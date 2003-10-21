c82e58ec746aa96bb706c100cc289d670da623a9
Ed Hill <ed@eh3.com>
2003-10-21 15:26:50 +0000

    o various cleanups and new default files for the TAF/TAMC build targets

---
+++ pkg/autodiff/AUTODIFF_OPTIONS.h
@@ -0,0 +1,24 @@
+C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_OPTIONS.h,v 1.2 2003/10/21 15:26:50 edhill Exp $
+C $Name:  $
+
+CBOP
+C     !ROUTINE: AUTODIFF_CPPOPTIONS.h 
+C     !INTERFACE:
+C     include "AUTODIFF_CPPOPTIONS.h "
+C     !DESCRIPTION:
+C     *==========================================================*
+C     | CPP_EEMACROS.h                                            
+C     *==========================================================*
+C     CPP flags controlling which code is included in the files
+C     that will be compiled.
+C     These macros have been shifted to the common header file
+C     ECCO_CPPOPTIONS.h
+CEOP
+
+#ifndef AUTODIFF_OPTIONS_H
+#define AUTODIFF_OPTIONS_H
+
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#endif /* AUTODIFF_CPPOPTIONS_H */
