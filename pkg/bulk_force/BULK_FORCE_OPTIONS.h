c23b3a6f07be59a27aa4ba38839276a9f021c7a4
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-10-22 18:38:48 +0000

    add "#undef CONSERV_BULKF" to clarify what is really used.

--- pkg/bulk_force/BULK_FORCE_OPTIONS.h
+++ pkg/bulk_force/BULK_FORCE_OPTIONS.h
@@ -1,14 +1,16 @@
-C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.2 2003/10/09 04:19:19 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.3 2003/10/22 18:38:48 jmc Exp $
 C $Name:  $
 
 #ifndef BULK_FORCE_OPTIONS_H
 #define BULK_FORCE_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
-#ifdef ALLOW_BULK_FORCE
 
 #include "CPP_OPTIONS.h"
 
+#ifdef ALLOW_BULK_FORCE
 C CPP Macros go here
+
+#undef CONSERV_BULKF
 
 #endif /* ALLOW_BULK_FORCE */
 #endif /* BULK_FORCE_OPTIONS_H */
