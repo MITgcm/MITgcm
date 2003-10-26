40b98c0285800b69480d0b32d61eff1e94deea53
Patrick Heimbach <heimbach@mit.edu>
2003-10-26 01:58:09 +0000

    moving and updating adcode to code_ad etc.

---
+++ verification/hs94.1x64x5/code_ad/CPP_OPTIONS.h
@@ -0,0 +1,63 @@
+C
+C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_ad/CPP_OPTIONS.h,v 1.1 2003/10/26 01:58:09 heimbach Exp $
+C $Name:  $
+
+C CPP flags controlling which code in included in the files that
+C will be compiled.
+
+#ifndef CPP_OPTIONS_H
+#define CPP_OPTIONS_H
+
+#include "PACKAGES_CONFIG.h"
+#include "AD_CONFIG.h"
+
+C o Shortwave heating as extra term in external_forcing.F
+#ifdef ALLOW_KPP
+#define  SHORTWAVE_HEATING
+#endif
+
+C o Include/exclude code for C-D grid method of integrating the 
+C   coriolis terms
+#undef  INCLUDE_CD_CODE
+
+C o Include/exclude momentum eqn metric terms code
+#define INCLUDE_MOMENTUM_METRIC_TERM_CODE
+
+C o Include/exclude phi_hyd calculation code
+#define INCLUDE_PHIHYD_CALCULATION_CODE
+
+C o Include/exclude call to S/R CONVECT
+#define INCLUDE_CONVECT_CALL
+
+C o Include/exclude call to S/R CALC_DIFFUSIVITY
+#define INCLUDE_CALC_DIFFUSIVITY_CALL
+
+C o Allow nonHydrostatic code
+#undef  ALLOW_NONHYDROSTATIC
+
+C o Use "natural" boundary conditions for salinity
+C   instead of the "virtual salt flux"
+#undef  USE_NATURAL_BCS
+
+C o Use "Exact Convervation" of fluid in Free-Surface formulation
+C   so that d/dt(eta) is exactly equal to - Div.Transport
+#define EXACT_CONSERV
+ 
+C o Allow the use of Non-Linear Free-Surface formulation
+C   this implies that surface thickness (hFactors) vary with time
+#undef NONLIN_FRSURF
+
+C o Read/write of checkpoint files for restarting.
+#undef OLD_STYLE_WITH_MANY_FILES
+
+C o Execution environment support options
+#include "CPP_EEOPTIONS.h"
+
+C o Include/exclude code specific to the ECCO/SEALION version.
+#define ALLOW_GRADIENT_CHECK
+
+#if (defined (INCLUDE_ECCO_PACKAGE) || defined (ALLOW_ADJOINT_RUN))
+#include "ECCO_CPPOPTIONS.h"
+#endif
+
+#endif /* CPP_OPTIONS_H */
---
+++ verification/hs94.1x64x5/code_ad/ECCO_CPPOPTIONS.h
@@ -0,0 +1,54 @@
+C
+C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_ad/Attic/ECCO_CPPOPTIONS.h,v 1.1 2003/10/26 01:58:09 heimbach Exp $
+C $Name:  $
+
+C ********************************************************************
+C ***                  Adjoint Support Package                     ***
+C ********************************************************************
+
+C o Include/exclude code in order to be able to automatically
+C   differentiate the MITgcmUV by using the Tangent Linear and
+C   Adjoint Model Compiler (TAMC).
+C
+#define ALLOW_AUTODIFF_TAMC
+C       >>> Checkpointing as handled by TAMC
+#define ALLOW_TAMC_CHECKPOINTING
+
+C ********************************************************************
+C ***                     Calender Package                         ***
+C ********************************************************************
+C
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C
+C o Include the calendar tool.
+#undef ALLOW_CALENDAR
+#undef ALLOW_CAL_NENDITER
+
+C ********************************************************************
+C ***                Cost function Package                         ***
+C ********************************************************************
+C 
+C       >>> Cost function contributions
+#define ALLOW_COST
+#define  ALLOW_COST_TEST
+#undef ALLOW_COST_TRACER
+
+C ********************************************************************
+C ***               Control vector Package                         ***
+C ********************************************************************
+C 
+#undef  ALLOW_NONDIMENSIONAL_CONTROL_IO
+C       >>> Initial values.
+#define ALLOW_THETA0_CONTROL
+#define ALLOW_SALT0_CONTROL
+#define ALLOW_TR10_CONTROL
+#undef ALLOW_TAUU0_CONTROL
+#undef ALLOW_TAUV0_CONTROL
+#undef ALLOW_SFLUX0_CONTROL
+#undef ALLOW_HFLUX0_CONTROL
+#undef ALLOW_SSS0_CONTROL
+#undef ALLOW_SST0_CONTROL
+#define ALLOW_DIFFKR_CONTROL
+#undef ALLOW_KAPGM_CONTROL
+
