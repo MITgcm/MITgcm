277c27cada7e4cbbac19bd8843269ef61b10a2ad
Patrick Heimbach <heimbach@mit.edu>
2003-11-13 19:35:10 +0000

    added missing cost_averages_bar_directives.h (plus name change)

---
+++ pkg/ecco/cost_averages_bar_directives.h
@@ -0,0 +1,15 @@
+c
+c     store directives for cost_averagesfields
+c     xx_..._mean_dummy
+c
+c     created: heimbach@mit.edu 4-Mar-2003
+c
+CADJ STORE xx_psbar_mean_dummy = onetape
+CADJ STORE xx_tbar_mean_dummy  = onetape
+CADJ STORE xx_sbar_mean_dummy  = onetape
+CADJ STORE xx_ubar_mean_dummy  = onetape
+CADJ STORE xx_vbar_mean_dummy  = onetape
+CADJ STORE xx_taux_mean_dummy  = onetape
+CADJ STORE xx_tauy_mean_dummy  = onetape
+CADJ STORE xx_hflux_mean_dummy = onetape
+CADJ STORE xx_sflux_mean_dummy = onetape
