aab5e9fbf455669321e7df00cb56707f331c6c4f
Jean-Michel Campin <jmc@ocean.mit.edu>
2003-11-20 15:01:10 +0000

    the place where NUMBER_OF_PTRACERS is defined has disappeared (in the  branch_genmake2 merging). put it back here (as in verification/carbon/code_ad_ptracers).

--- pkg/ptracers/PTRACERS_OPTIONS.h
+++ pkg/ptracers/PTRACERS_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_OPTIONS.h,v 1.3 2003/10/09 04:19:20 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_OPTIONS.h,v 1.4 2003/11/20 15:01:10 jmc Exp $
 C $Name:  $
 
 C CPP options file for PTRACERS package
@@ -11,6 +11,17 @@ C Use this file for selecting options within the PTRACERS package
 #ifdef ALLOW_PTRACERS
 
 #include "CPP_OPTIONS.h"
+
+C NUMBER_OF_PTRACERS defines how many passive tracers are allocated/exist.
+C This CPP macro is *only* used in PTRACERS.h to set an integer parameter.
+C <Please> do not make use of it elsewhere.
+
+
+C If NUMBER_OF_PTRACERS is not specified elsewhere and ALLOW_PTRACERS
+C is set then NUMBER_OF_PTRACERS is set here (default 1)
+#ifndef NUMBER_OF_PTRACERS
+#define NUMBER_OF_PTRACERS 1
+#endif
 
 C CPP Macros go here
 
