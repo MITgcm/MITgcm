dbf981c74d3721cb31fbbb9a31ef616fe35ffb27
Alistair Adcroft <aadcroft@princeton.edu>
2003-12-12 16:04:31 +0000

    o replaced initialization of Theta with a random number generator   in ini_theta.F with initialization from data in a file   - this allows us to get exactly the same number with and without MPI o re-tiled the expt for testing with MPI

--- verification/hs94.128x64x5/code/SIZE.h
+++ verification/hs94.128x64x5/code/SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/hs94.128x64x5/code/SIZE.h,v 1.5 2003/02/11 14:18:43 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/verification/hs94.128x64x5/code/SIZE.h,v 1.6 2003/12/12 16:04:27 adcroft Exp $
 C $Name:  $
 C
 C     /==========================================================\
@@ -37,11 +37,11 @@ C     Nr  - No. points in Z for full process domain.
       INTEGER Nr
       PARAMETER (
      &           sNx = 128,
-     &           sNy =  64,
+     &           sNy =  32,
      &           OLx =   2,
      &           OLy =   2,
      &           nSx =   1,
-     &           nSy =   1,
+     &           nSy =   2,
      &           nPx =   1,
      &           nPy =   1,
      &           Nx  = sNx*nSx*nPx,
