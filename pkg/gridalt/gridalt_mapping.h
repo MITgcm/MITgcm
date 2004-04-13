49e092647d0167d5f97d96923d4c71f0c5cff46f
Andrea Molod <andrea.molod@nasa.gov>
2004-04-13 15:23:50 +0000

    Add terms for more general mapping (other than just vertical for fizhi)

--- pkg/gridalt/gridalt_mapping.h
+++ pkg/gridalt/gridalt_mapping.h
@@ -1,6 +1,8 @@
 c Alternate grid Mapping Common
 c ------------------------------
-      common /gridalt_mapping/ nlperdyn,dpphys0,dpphys
+      common /gridalt_mapping/ nlperdyn,dpphys0,dpphys,
+     .                                             dxfalt,dyfalt,drfalt
       integer nlperdyn(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
       _RL dpphys0(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dpphys(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
+      _RL dxfalt,dyfalt,drfalt
