9c05b3873e66e3860ee8356fe203d3eff53ccfda
Alistair Adcroft <aadcroft@princeton.edu>
2004-05-14 17:43:11 +0000

    Created pkg/mom_common/  o changed pkg_groups and pkg_depend  o moved duplicate files in mom_vecinv and mom_fluxform to mom_common/ This is in preparation for checking in Smagorinsky and Leith type variable viscosities.

---
+++ pkg/mom_common/MOM_COMMON_OPTIONS.h
@@ -0,0 +1,18 @@
+C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_COMMON_OPTIONS.h,v 1.1 2004/05/14 17:43:11 adcroft Exp $
+C $Name:  $
+
+C CPP options file for mom_common package
+C
+C Use this file for selecting CPP options within the mom_common package
+
+#ifndef MOM_COMMON_OPTIONS_H
+#define MOM_COMMON_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_MOM_COMMON
+
+#include "CPP_OPTIONS.h"
+
+C CPP macros go here
+
+#endif /* ALLOW_MOM_COMMON */
+#endif /* MOM_COMMON_OPTIONS_H */
