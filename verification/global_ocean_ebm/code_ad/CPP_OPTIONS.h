cc3e83f2f37e123ed93cd069dcb15590fb37bd87
Patrick Heimbach <heimbach@mit.edu>
2004-05-21 21:43:15 +0000

    Verification for EBM adjoint * TLM OK * ADM not sufficient accuracy yet

---
+++ verification/global_ocean_ebm/code_ad/CPP_OPTIONS.h
@@ -0,0 +1,41 @@
+C
+C $Header: /u/gcmpack/MITgcm/verification/global_ocean_ebm/code_ad/CPP_OPTIONS.h,v 1.1 2004/05/21 21:43:14 heimbach Exp $
+C $Name:  $
+
+C CPP flags controlling which code in included in the files that
+C will be compiled.
+
+#ifndef CPP_OPTIONS_H
+#define CPP_OPTIONS_H
+
+#include "PACKAGES_CONFIG.h"
+
+#ifdef ALLOW_KPP
+#define  SHORTWAVE_HEATING
+#endif
+
+C o Include/exclude phi_hyd calculation code
+#define INCLUDE_PHIHYD_CALCULATION_CODE
+
+C o Include/exclude call to S/R CONVECT
+#define INCLUDE_CONVECT_CALL
+
+C o Include/exclude call to S/R CALC_DIFFUSIVITY
+#define INCLUDE_CALC_DIFFUSIVITY_CALL
+
+C o Allow nonHydrostatic code
+#undef  ALLOW_NONHYDROSTATIC
+
+C o Exact volume conservation
+#undef EXACT_CONSERV
+
+C o Execution environment support options
+#include "CPP_EEOPTIONS.h"
+
+#ifdef ALLOW_AUTODIFF
+# include "ECCO_CPPOPTIONS.h"
+#endif
+
+#endif /* CPP_OPTIONS_H */
+
+
