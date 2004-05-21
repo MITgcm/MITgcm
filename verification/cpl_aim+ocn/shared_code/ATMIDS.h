dd2563fa11616af3f12912264f23c956298407b1
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-05-21 20:21:39 +0000

    new version of coupler for AIM+thSIce and ocean

--- verification/cpl_aim+ocn/shared_code/ATMIDS.h
+++ verification/cpl_aim+ocn/shared_code/ATMIDS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/cpl_aim+ocn/shared_code/ATMIDS.h,v 1.1 2003/12/15 21:07:25 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/verification/cpl_aim+ocn/shared_code/ATMIDS.h,v 1.2 2004/05/21 20:21:39 jmc Exp $
 C $Name:  $
 
 C     /==========================================================\
@@ -7,30 +7,46 @@ C     |          are used to identify this component and the     |
 C     |          fields it exchanges with other components.      |
 C     \==========================================================/
       CHARACTER*(8)  atmCompName 
-      PARAMETER( atmCompName          = 'UV-Atmos'        )
+      PARAMETER( atmCompName         = 'UV-Atmos'       )
       CHARACTER*(10) atmDepthName
-      PARAMETER( atmDepthName         = 'ATM Depths'      )
+      PARAMETER( atmDepthName        = 'ATM Depths'     )
+      CHARACTER*(10) atmMxlDName
+      PARAMETER( atmMxlDName         = 'ATM ocMxlD'     )
       CHARACTER*(7)  atmSSTName
-      PARAMETER( atmSSTName           = 'ATM SST'         )
-      CHARACTER*(8)  atmTauXName
-      PARAMETER( atmTauXName          = 'ATM tauX'        )
-      CHARACTER*(8)  atmTauYName
-      PARAMETER( atmTauYName          = 'ATM tauY'        )
+      PARAMETER( atmSSTName          = 'ATM SST'        )
+      CHARACTER*(7)  atmSSSName
+      PARAMETER( atmSSSName          = 'ATM SSS'        )
+      CHARACTER*(9)  atmSSVsqName
+      PARAMETER( atmSSVsqName        = 'ATM SSVsq'      )
+
+      CHARACTER*(8)  atmSLPrName
+      PARAMETER( atmSLPrName         = 'ATM SLPr'       )
       CHARACTER*(12) atmHeatFluxName
-      PARAMETER( atmHeatFluxName      = 'ATM HeatFlux'    )
+      PARAMETER( atmHeatFluxName     = 'ATM HeatFlux'   )
+      CHARACTER*(14) atmQshortWaveName
+      PARAMETER( atmQshortWaveName   = 'ATM Qshortwave' )
+c     CHARACTER*(13) atmQlongWaveName
+c     PARAMETER( atmQlongWaveName    = 'ATM Qlongwave'  )
+c     CHARACTER*(11) atmQlatentName
+c     PARAMETER( atmQlatentName      = 'ATM Qlatent'    )
+c     CHARACTER*(13) atmQsensibleName
+c     PARAMETER( atmQsensibleName    = 'ATM Qsensible'  )
+c     CHARACTER*(14) atmUvelgroundName
+c     PARAMETER( atmUvelgroundName   = 'ATM Uvelground' )
+c     CHARACTER*(14) atmVvelgroundName
+c     PARAMETER( atmVvelgroundName   = 'ATM Vvelground' )
+      CHARACTER*(8)  atmTauXName
+      PARAMETER( atmTauXName         = 'ATM tauX'       )
+      CHARACTER*(8)  atmTauYName
+      PARAMETER( atmTauYName         = 'ATM tauY'       )
       CHARACTER*(9) atmEvMPrName
-      PARAMETER( atmEvMPrName         = 'ATM Ev-Pr'       )
+      PARAMETER( atmEvMPrName        = 'ATM Ev-Pr'      )
       CHARACTER*(10) atmRunOffName
-      PARAMETER( atmRunOffName        = 'ATM RunOff'      )
-      CHARACTER*(13) atmQlongwaveName
-      PARAMETER( atmQlongwaveName     = 'ATM Qlongwave'   )
-      CHARACTER*(14) atmQshortwaveName
-      PARAMETER( atmQshortwaveName    = 'ATM Qshortwave'  )
-      CHARACTER*(11) atmQlatentName
-      PARAMETER( atmQlatentName       = 'ATM Qlatent'     )
-      CHARACTER*(13) atmQsensibleName
-      PARAMETER( atmQsensibleName     = 'ATM Qsensible'   )
-      CHARACTER*(14) atmUvelgroundName
-      PARAMETER( atmUvelgroundName    = 'ATM Uvelground'  )
-      CHARACTER*(14) atmVvelgroundName
-      PARAMETER( atmVvelgroundName    = 'ATM Vvelground'  )
+      PARAMETER( atmRunOffName       = 'ATM RunOff'     )
+      CHARACTER*(12) atmROEnFxName
+      PARAMETER( atmROEnFxName       = 'ATM RO.EnFlx'   )
+      CHARACTER*(12) atmSaltFxName
+      PARAMETER( atmSaltFxName       = 'ATM SaltFlux'   )
+      CHARACTER*(10) atmSeaIceName
+      PARAMETER( atmSeaIceName       = 'ATM SeaIce'     )
+
