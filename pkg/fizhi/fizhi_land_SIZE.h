f4a0368053b322df008989cb7b0bcc1228cd3d8c
Andrea Molod <andrea.molod@nasa.gov>
2004-06-07 18:11:38 +0000

    Accomodate new common block names

--- pkg/fizhi/fizhi_land_SIZE.h
+++ pkg/fizhi/fizhi_land_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/Attic/land_SIZE.h,v 1.2 2004/05/05 00:39:21 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.1 2004/06/07 18:11:37 molod Exp $
 C $Name:  $
 
 c Land Grid Horizontal Dimension (Number of Tiles)
