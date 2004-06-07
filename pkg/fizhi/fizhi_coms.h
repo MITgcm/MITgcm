4b80db2c3bfa06c6404b63a18f2a37a19ae9acb2
Andrea Molod <andrea.molod@nasa.gov>
2004-06-07 18:35:24 +0000

    bi bj syntax errors

--- pkg/fizhi/fizhi_coms.h
+++ pkg/fizhi/fizhi_coms.h
@@ -1,5 +1,3 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_coms.h,v 1.2 2004/05/05 00:39:21 edhill Exp $
-C $Name:  $
 
 C The physics state uses the dynamics dimensions in the horizontal
 C     and the land dimensions in the horizontal for turbulence variables
@@ -9,29 +7,29 @@ c ----------------------------------------------------------------------
       common /physics_state/ uphy,vphy,thphy,sphy,
      .   ctmt,xxmt,yymt,zetamt,xlmt,khmt,tke
 
-      _RL uphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL vphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL thphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL sphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL ctmt(nchp,nSx,Nsy), xxmt(nchp,nSx,Nsy), yymt(nchp,nSx,Nsy)
-      _RL zetamt(nchp,nSx,Nsy)
-      _RL xlmt(nchp,Nrphys,nSx,Nsy), khmt(nchp,Nrphys,nSx,Nsy) 
-      _RL tke(nchp,Nrphys,nSx,Nsy)
+      _RL uphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL vphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL thphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL sphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL ctmt(nchp,Nsx,Nsy), xxmt(nchp,Nsx,Nsy), yymt(nchp,Nsx,Nsy)
+      _RL zetamt(nchp,Nsx,Nsy)
+      _RL xlmt(nchp,Nrphys,Nsx,Nsy), khmt(nchp,Nrphys,Nsx,Nsy) 
+      _RL tke(nchp,Nrphys,Nsx,Nsy)
 
 c Physics Tendency Common - Changes in state variables due to physics
 c ----------------------------------------------------------------------
       common /physics_tendency/ duphy,dvphy,dthphy,dsphy
 
-      _RL duphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL dvphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL dthphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      _RL dsphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
+      _RL duphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL dvphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL dthphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
+      _RL dsphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,Nsx,Nsy)
 
 c Physics Exports Common - Physics variables on other grids for export
 c ----------------------------------------------------------------------
       common /physics_exports/ guphy,gvphy,gthphy,gsphy
 
-      _RL guphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
-      _RL gvphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
-      _RL gthphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
-      _RL gsphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
+      _RL guphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,Nsx,Nsy)
+      _RL gvphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,Nsx,Nsy)
+      _RL gthphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,Nsx,Nsy)
+      _RL gsphy(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,Nsx,Nsy)
--- pkg/fizhi/fizhi_land_coms.h
+++ pkg/fizhi/fizhi_land_coms.h
@@ -1,15 +1,19 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_coms.h,v 1.1 2004/06/07 16:57:39 molod Exp $
-C $Name:  $
-
 c Land State Common
 c -----------------
       common /land_state/ tcanopy, tdeep, ecanopy, 
      . swetshal, swetroot, swetdeep, snodep, capac
-      _RL tcanopy(nchp), tdeep(nchp), ecanopy(nchp), swetshal(nchp)
-      _RL swetroot(nchp), swetdeep(nchp), snodep(nchp), capac(nchp)
+      _RL tcanopy(nchp,Nsx,Nsy)
+      _RL tdeep(nchp,Nsx,Nsy)
+      _RL ecanopy(nchp,Nsx,Nsy)
+      _RL swetshal(nchp,Nsx,Nsy)
+      _RL swetroot(nchp,Nsx,Nsy)
+      _RL swetdeep(nchp,Nsx,Nsy)
+      _RL snodep(nchp,Nsx,Nsy)
+      _RL capac(nchp,Nsx,Nsy)
 
 c Land Mapping Common
 c -------------------
       common /land_mapping/ chlt, chlon, igrd
-      _RL chlt(nchp), chlon(nchp)
-      integer igrd(nchp) 
+      _RL chlt(nchp,Nsx,Nsy)
+      _RL chlon(nchp,Nsx,Nsy)
+      integer igrd(nchp,Nsx,Nsy)
