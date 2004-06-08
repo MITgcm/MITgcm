68d0aa1535e8f286414717eadbea346d99d0f644
Andrea Molod <andrea.molod@nasa.gov>
2004-06-08 16:12:53 +0000

    Assembling fizhi calls

--- pkg/fizhi/fizhi_land_SIZE.h
+++ pkg/fizhi/fizhi_land_SIZE.h
@@ -1,7 +1,7 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.1 2004/06/07 18:11:37 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.2 2004/06/08 16:12:53 molod Exp $
 C $Name:  $
 
 c Land Grid Horizontal Dimension (Number of Tiles)
 c ------------------------------------------------
       integer nchp
-      parameter (nchp = 5)
+      parameter (nchp = sNx*sNy*10)
