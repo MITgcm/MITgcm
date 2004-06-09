9bc7f6e71e2f1acbc6fcc452e53680f9d0976c6f
Andrea Molod <andrea.molod@nasa.gov>
2004-06-09 20:33:37 +0000

    More input stuff for fizhi

--- pkg/fizhi/fizhi_earth_coms.h
+++ pkg/fizhi/fizhi_earth_coms.h
@@ -1,13 +1,13 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_earth_coms.h,v 1.1 2004/06/07 16:57:39 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_earth_coms.h,v 1.2 2004/06/09 20:33:36 molod Exp $
 C $Name:  $
 
 c Solid-Earth State Variables
 c ---------------------------
       common /earth_state/ phis_var, lwmask, tilefrac, surftype 
-      _RL phis_var(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,Nsy)
-      _RL lwmask(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,Nsy)
-      _RL tilefrac(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
-      integer surftype(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,Nsy)
+      _RL phis_var(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL lwmask(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL tilefrac(1-OLx:sNx+OLx,1-OLy:sNy+OLy,maxtyp,nSx,nSy)
+      integer surftype(1-OLx:sNx+OLx,1-OLy:sNy+OLy,maxtyp,nSx,nSy)
 
 c Solid_Earth Couplings
 c ---------------------
@@ -19,8 +19,8 @@ c ---------------------
       _RL chfr(nchp)
       _RL alai(nchp)
       _RL agrn(nchp)
-      _RL albvisdr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,Nsy)
-      _RL albvisdf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,Nsy)
-      _RL albnirdr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,Nsy)
-      _RL albnirdf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,Nsy)
-      _RL emiss(1-OLx:sNx+OLx,1-OLy:sNy+OLy,10,nSx,Nsy)
+      _RL albvisdr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL albvisdf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL albnirdr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL albnirdf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL emiss(1-OLx:sNx+OLx,1-OLy:sNy+OLy,10,nSx,nSy)
--- pkg/fizhi/fizhi_land_SIZE.h
+++ pkg/fizhi/fizhi_land_SIZE.h
@@ -1,7 +1,8 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.2 2004/06/08 16:12:53 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.3 2004/06/09 20:33:37 molod Exp $
 C $Name:  $
 
 c Land Grid Horizontal Dimension (Number of Tiles)
 c ------------------------------------------------
-      integer nchp
-      parameter (nchp = sNx*sNy*10)
+      integer nchp, maxtyp
+      parameter (maxtyp = 10)
+      parameter (nchp = sNx*sNy*maxtyp)
