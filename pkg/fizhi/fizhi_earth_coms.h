bcb723376df09fe9c60a3d684404dc7300f6258e
Andrea Molod <andrea.molod@nasa.gov>
2004-06-16 16:02:03 +0000

    Straighten out what is shadowed and what is not

--- pkg/fizhi/fizhi_earth_coms.h
+++ pkg/fizhi/fizhi_earth_coms.h
@@ -1,12 +1,12 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_earth_coms.h,v 1.3 2004/06/15 21:18:18 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_earth_coms.h,v 1.4 2004/06/16 16:02:03 molod Exp $
 C $Name:  $
 
 c Solid-Earth State Variables
 c ---------------------------
       common /earth_state/ phis_var, tilefrac, surftype 
-      _RL phis_var(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL tilefrac(1-OLx:sNx+OLx,1-OLy:sNy+OLy,maxtyp,nSx,nSy)
-      integer surftype(1-OLx:sNx+OLx,1-OLy:sNy+OLy,maxtyp,nSx,nSy)
+      _RL phis_var(sNx,sNy,Nsx,Nsy)
+      _RL tilefrac(sNx,sNy,maxtyp,Nsx,Nsy)
+      integer surftype(sNx,sNy,maxtyp,Nsx,Nsy)
 
 c Solid_Earth Couplings
 c ---------------------
@@ -19,9 +19,9 @@ c ---------------------
       _RL chfr(nchp)
       _RL alai(nchp)
       _RL agrn(nchp)
-      _RL albvisdr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL albvisdf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL albnirdr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL albnirdf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL emiss(1-OLx:sNx+OLx,1-OLy:sNy+OLy,10,nSx,nSy)
-      _RL landtype(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL albvisdr(sNx,sNy,Nsx,Nsy)
+      _RL albvisdf(sNx,sNy,Nsx,Nsy)
+      _RL albnirdr(sNx,sNy,Nsx,Nsy)
+      _RL albnirdf(sNx,sNy,Nsx,Nsy)
+      _RL emiss(sNx,sNy,10,Nsx,Nsy)
+      _RL landtype(sNx,sNy,Nsx,Nsy)
