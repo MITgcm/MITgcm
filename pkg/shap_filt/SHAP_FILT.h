fb09a37055ce3ef3de818b5532d07787da060c70
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-06-26 01:15:30 +0000

    o new parameter "nShapS": allow to treat S differently from T (nShapT).

--- pkg/shap_filt/SHAP_FILT.h
+++ pkg/shap_filt/SHAP_FILT.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/shap_filt/SHAP_FILT.h,v 1.6 2003/08/03 03:12:36 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/shap_filt/SHAP_FILT.h,v 1.7 2004/06/26 01:15:30 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_SHAP_FILT
@@ -18,20 +18,22 @@ C     Shap_funct :: define which Shapiro Filter function is used
 C        = 1  (S1) : [1 - d_xx^n - d_yy^n]
 C        = 4  (S4) : [1 - d_xx^n][1- d_yy^n]
 C        = 2  (S2) : [1 - (d_xx+d_yy)^n]
-C     nShap_Tr,UV :: (total) power of the filter for Tracer, Velocity
+C     nShap_T,S,UV :: (total) power of the filter for T,S, Velocity
 C  available only with Shap_funct=2 : 
 C      combine filter in Physical space (power of nShap_Phys) 
 C        and pure numerical filter (power nShap - nShap_Phys) 
 
-      INTEGER  Shap_funct, nShapT, nShapUV, nShapTrPhys, nShapUVPhys
+      INTEGER  Shap_funct, nShapT, nShapS, nShapUV 
+      INTEGER  nShapTrPhys, nShapUVPhys
       COMMON /SHAP_FILT_PARM_I/ 
-     &         Shap_funct, nShapT, nShapTrPhys, nShapUV, nShapUVPhys
+     &         Shap_funct, nShapT, nShapS, nShapTrPhys, 
+     &                            nShapUV, nShapUVPhys
 
 C-    Shapiro Filter (real) parameters
 C     Shap_Trtau    :: Time scale for tracer filter
 C     Shap_TrLength :: Length scale for tracer filter
 C     Shap_uvtau    :: Time scale for momentum filter
-C     Shap_TrLength :: Length scale for momentum filter
+C     Shap_uvLength :: Length scale for momentum filter
 C     Shap_noSlip   :: No-slip parameter (=0 free sleep ; =1 No-slip)
 C     Shap_diagFreq :: Frequency^-1 for diagnostic output (s)
       _RL Shap_Trtau, Shap_TrLength
