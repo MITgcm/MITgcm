c37348ab198588fbbbaf4b60758076e0c4f44bba
Dimitris Menemenlis <dmenemenlis@gmail.com>
2004-06-30 15:27:57 +0000

    added some comments to pkg/mdsio/MDSIO_OPTIONS.h

--- pkg/mdsio/MDSIO_OPTIONS.h
+++ pkg/mdsio/MDSIO_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mdsio/MDSIO_OPTIONS.h,v 1.5 2004/04/07 00:22:03 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mdsio/MDSIO_OPTIONS.h,v 1.6 2004/06/30 15:27:57 dimitri Exp $
 C $Name:  $
 
 #ifndef MDSIO_OPTIONS_H
@@ -30,13 +30,17 @@ C   present in the input and in the output files.  When
 C   MISSING_TILE_IO is defined, the input and output
 C   files do not include the missing tiles.  Therefore
 C   the 2-D dimension of input and output files is:
-C   #if defined(ALLOW_EXCH2) && !defined(MISSING_TILE_IO)
-C    x_size = exch2_domain_nxt * sNx = exch2_domain_nxt*exch2_tnx
-C    y_size = exch2_domain_nyt * sNy = exch2_domain_nyt*exch2_tny
-C   #else
-C    x_size = Nx = sNx * nSx * nPx
-C    y_size = Ny = sNy * nSy * nPy
-C   #endif
+C    #if defined(ALLOW_EXCH2) && !defined(MISSING_TILE_IO)
+C     x_size = exch2_domain_nxt * sNx = exch2_domain_nxt*exch2_tnx
+C     y_size = exch2_domain_nyt * sNy = exch2_domain_nyt*exch2_tny
+C    #else
+C     x_size = Nx = sNx * nSx * nPx
+C     y_size = Ny = sNy * nSy * nPy
+C    #endif-
+C   Notes:
+C    1) Pickup files are not compatible between the two formats.
+C    2) Defining MISSING_TILE_IO with no missing tiles is
+C       equivalent to the old, tile-dependent I/O format.
 #undef MISSING_TILE_IO
 
 #endif /* ALLOW_MDSIO */
