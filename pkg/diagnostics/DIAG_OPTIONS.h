c90c060abdb4b6419d0c21271339722d24ae8ec1
Ed Hill <ed@eh3.com>
2004-07-06 03:55:53 +0000

    o prepare diagnostics for MNC/NetCDF output    - add Nrphys to MNC    - encode KDIAG within GDIAG    - add use_mdsio and use_mnc flags to data.diagnostics    - start converting diagnostics to Protex format    - mdsio-based output for hs94.cs-32x32x5 is *identical*

---
+++ pkg/diagnostics/DIAG_OPTIONS.h
@@ -0,0 +1,22 @@
+C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAG_OPTIONS.h,v 1.1 2004/07/06 03:55:53 edhill Exp $
+C $Name:  $
+
+#ifndef DIAG_OPTIONS_H
+#define DIAG_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_DIAGNOSTICS
+
+#include "CPP_OPTIONS.h"
+
+CEH3 package-specific options go here
+
+C #define DIAG_DEBUG
+
+
+#endif /* ALLOW_DIAGNOSTICS */
+#endif /* DIAG_OPTIONS_H */
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
