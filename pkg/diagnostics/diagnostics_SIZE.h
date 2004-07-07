47146cb74fdfb695bbf4ea2def691f612a94b0a3
Ed Hill <ed@eh3.com>
2004-07-07 03:10:15 +0000

    o more protex-ing  o numerous formatting cleanups

--- pkg/diagnostics/diagnostics_SIZE.h
+++ pkg/diagnostics/diagnostics_SIZE.h
@@ -1,8 +1,8 @@
-C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/Attic/diagnostics_SIZE.h,v 1.3 2004/07/05 04:44:51 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/Attic/diagnostics_SIZE.h,v 1.4 2004/07/07 03:10:14 edhill Exp $
 C $Name:  $
 
-c Diagnostics Array Dimension
-c ---------------------------
+C     Diagnostics Array Dimension
+C     ---------------------------
       integer numdiags
       parameter (numdiags = 10)
 
