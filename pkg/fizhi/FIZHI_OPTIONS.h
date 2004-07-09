c83e0242d5c2ceded3f3a62dd34943d94d41c847
Ed Hill <ed@eh3.com>
2004-07-09 02:41:52 +0000

    o remove the "Nphys"-related MNC grid type definitions from MNC      and place them in the fizhi package per the discussions      with AM and JMC  o the code compiles cleanly but has not been run  o the two files added to fizhi do not (yet) interact with any of the      other fizhi routines--no changes to existing fizhi code

---
+++ pkg/fizhi/FIZHI_OPTIONS.h
@@ -0,0 +1,22 @@
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/FIZHI_OPTIONS.h,v 1.1 2004/07/09 02:41:52 edhill Exp $
+C $Name:  $
+
+#ifndef FIZHI_OPTIONS_H
+#define FIZHI_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_FIZHI
+
+#include "CPP_OPTIONS.h"
+
+C     Package-specific options go here
+
+C     #define FIZHI_DEBUG
+
+
+#endif /* ALLOW_FIZHI */
+#endif /* FIZHI_OPTIONS_H */
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
