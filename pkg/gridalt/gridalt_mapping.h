82a22443e0999fe343ddd4de412d8b15a984c6b3
Andrea Molod <andrea.molod@nasa.gov>
2004-07-13 21:21:05 +0000

    Debugging fizhi

--- pkg/gridalt/gridalt_mapping.h
+++ pkg/gridalt/gridalt_mapping.h
@@ -1,13 +1,15 @@
-C $Header: /u/gcmpack/MITgcm/pkg/gridalt/gridalt_mapping.h,v 1.4 2004/06/15 17:19:55 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/gridalt/gridalt_mapping.h,v 1.5 2004/07/13 21:21:05 molod Exp $
 C $Name:  $
+
+#include "PACKAGES_CONFIG.h"
 
 c Alternate grid Mapping Common
 c ------------------------------
 #ifdef ALLOW_FIZHI
-      common /gridalt_mapping/ nlperdyn,dpphys0,dpphys,
-     .                                             dxfalt,dyfalt,drfalt
       integer nlperdyn(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,Nsy)
       _RL dpphys0(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dpphys(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nrphys,nSx,nSy)
       _RL dxfalt,dyfalt,drfalt
+      common /gridalt_mapping/ nlperdyn,dpphys0,dpphys,
+     .                                             dxfalt,dyfalt,drfalt
 #endif
