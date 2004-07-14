175684e43e7d0cfc2956fb9e5aab1880542e9b90
Andrea Molod <andrea.molod@nasa.gov>
2004-07-14 17:31:58 +0000

    Debugging

---
+++ pkg/fizhi/snwmid.h
@@ -0,0 +1,5 @@
+
+c Note: SNWMID and SNWALB parameters modified to obtain improved albedo and radswg (L.Takacs 2/17/00)
+c ---------------------------------------------------------------------------------------------------
+      DATA SNWMID /100.,100.,100.,10.,100.,10.,10.,10.,1.e-16,10./
+
