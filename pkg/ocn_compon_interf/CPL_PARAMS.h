d4b28e407b076d976c5299164214f49ce4a91bfb
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-07-26 20:13:47 +0000

    update coupling interface to work with new staggerTimeStep

--- pkg/ocn_compon_interf/CPL_PARAMS.h
+++ pkg/ocn_compon_interf/CPL_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ocn_compon_interf/CPL_PARAMS.h,v 1.2 2004/05/21 19:42:26 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ocn_compon_interf/CPL_PARAMS.h,v 1.3 2004/07/26 20:13:47 jmc Exp $
 C $Name:  $
 
 #ifdef COMPONENT_MODULE
@@ -16,14 +16,17 @@ C     useImportFW   :: True => use the Imported Fresh Water flux fr cpl
 C     useImportTau  :: True => use the Imported Wind-Stress from couler
 C     useImportSLP  :: True => use the Imported Sea-level Pressure as
 C                              atmospheric loading
+C     useImportSIce :: True => use the Imported Sea-Ice mass as ice-loading
       COMMON /CPL_OCN_PAR_L/
      &  cpl_earlyExpImpCall,
-     &  useImportHFlx, useImportFW, useImportTau, useImportSLP
+     &  useImportHFlx, useImportFW, useImportTau, 
+     &  useImportSLP, useImportSIce
       LOGICAL cpl_earlyExpImpCall
       LOGICAL useImportHFlx
       LOGICAL useImportFW
       LOGICAL useImportTau
       LOGICAL useImportSLP
+      LOGICAL useImportSIce
 
 C--   COMMON /CPL_OCN_PAR_C/: Character valued parameters
 c     CHARACTER*(MAX_LEN_FNAM) cpl_ocnFile
