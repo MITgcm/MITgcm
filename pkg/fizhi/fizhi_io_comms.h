881cd8848f4ef8296df1ad67e614f959457de98c
Andrea Molod <andrea.molod@nasa.gov>
2004-08-06 23:17:55 +0000

    Stuff for mnc to read and write vegetation tile space fields

--- pkg/fizhi/fizhi_io_comms.h
+++ pkg/fizhi/fizhi_io_comms.h
@@ -1,9 +1,11 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_io_comms.h,v 1.1 2004/08/06 21:12:14 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_io_comms.h,v 1.2 2004/08/06 23:17:55 molod Exp $
 C $Name:  $
 
 
 C     FIZHI I/O flags
 
+      logical fizhi_mdsio_read_pickup,fizhi_mdsio_write_pickup
+      logical fizhi_mnc_write_pickup,fizhi_mnc_read_pickup
       COMMON /FIZHI_IO_VARS/
      &     fizhi_mdsio_read_pickup, 
      &     fizhi_mdsio_write_pickup, 
