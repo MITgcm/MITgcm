4ef02e4efb66e21aac5f9a35691c6ab6d5d5f698
Ed Hill <ed@eh3.com>
2004-08-06 21:12:14 +0000

    o more mnc additions within fizhi

---
+++ pkg/fizhi/fizhi_io_comms.h
@@ -0,0 +1,17 @@
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_io_comms.h,v 1.1 2004/08/06 21:12:14 edhill Exp $
+C $Name:  $
+
+
+C     FIZHI I/O flags
+
+      COMMON /FIZHI_IO_VARS/
+     &     fizhi_mdsio_read_pickup, 
+     &     fizhi_mdsio_write_pickup, 
+     &     fizhi_mnc_write_pickup, 
+     &     fizhi_mnc_read_pickup
+
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
