bac0c7c499986109322889a824f97f856f0a4727
Andrea Molod <andrea.molod@nasa.gov>
2004-08-24 19:33:15 +0000

    Work-around for lack of veg input data

---
+++ verification/fizhi-gridalt-hs/code/diagnostics_SIZE.h
@@ -0,0 +1,12 @@
+C $Header: /u/gcmpack/MITgcm/verification/fizhi-gridalt-hs/code/Attic/diagnostics_SIZE.h,v 1.1 2004/08/24 19:33:15 molod Exp $
+C $Name:  $
+
+C     Diagnostics Array Dimension
+C     ---------------------------
+      integer numdiags
+      parameter (numdiags = 300)
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
