25634f7b89e6b574890f9d5fb1741f069dfa3853
Ed Hill <ed@eh3.com>
2004-09-01 03:44:48 +0000

    o initial check-in

---
+++ verification/dic_example/code/GCHEM_OPTIONS.h
@@ -0,0 +1,15 @@
+#include "CPP_OPTIONS.h"
+#ifdef ALLOW_PTRACERS
+
+C    !ROUTINE: GCHEM_OPTIONS.h
+C    !INTERFACE:
+
+C    !DESCRIPTION:
+c options for biogeochemistry package
+#define PTRACERS_SEPARATE_FORCING
+#define DIC_BIOTIC
+#undef  ALLOW_FE
+
+#undef ALLOW_DIC_COST
+
+#endif
---
+++ verification/dic_example/code/PTRACERS_OPTIONS.h
@@ -0,0 +1,19 @@
+C $Header: /u/gcmpack/MITgcm/verification/dic_example/code/Attic/PTRACERS_OPTIONS.h,v 1.1 2004/09/01 03:44:40 edhill Exp $
+C $Name:  $
+
+C CPP options file for PTRACERS package
+C
+C Use this file for selecting options within the PTRACERS package
+
+#ifndef PTRACERS_OPTIONS_H
+#define PTRACERS_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_PTRACERS
+
+
+#include "CPP_OPTIONS.h"
+
+C CPP Macros go here
+
+#endif /* ALLOW_PTRACERS */
+#endif /* PTRACERS_OPTIONS_H */
