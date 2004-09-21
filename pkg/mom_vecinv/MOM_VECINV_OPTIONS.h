fd0a408c499c175a8e62734c2e2caa8538c4b749
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-09-21 12:57:50 +0000

    use a more standard discretization for biharmonic viscosity ;  (original version still available with #define MOM_VI_ORIGINAL_VISCA4 )

--- pkg/mom_vecinv/MOM_VECINV_OPTIONS.h
+++ pkg/mom_vecinv/MOM_VECINV_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mom_vecinv/MOM_VECINV_OPTIONS.h,v 1.2 2003/10/09 04:19:20 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mom_vecinv/MOM_VECINV_OPTIONS.h,v 1.3 2004/09/21 12:57:50 jmc Exp $
 C $Name:  $
 
 C CPP options file for mom_vecinv package
@@ -11,6 +11,10 @@ C Use this file for selecting CPP options within the mom_vecinv package
 #ifdef ALLOW_MOM_VECINV
 
 #include "CPP_OPTIONS.h"
+
+C use the original discretization (not recommended) for biharmonic viscosity
+C   that was in mom_vi_hdissip.F, version 1.1.2.1
+#undef MOM_VI_ORIGINAL_VISCA4
 
 C CPP macros go here
 
