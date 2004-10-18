d9cc6e8cf43e7bd841e6935322aac7278183b913
Martin Losch <Martin.Losch@awi.de>
2004-10-18 15:15:06 +0000

    o add a verification experiment "vermix" that tests 5 different vertical   mixing schemes in a highly idealized 2D configuration: kpp (default),   pp81, my82, ggl90, and opps.   - foring: wind stress and surface heat flux   - initially stable stratification (linear EOS) with on instability at     the surface (no salinity)

---
+++ verification/vermix/input/gendata.m
@@ -0,0 +1,83 @@
+% This is a matlab script that generates the input data
+% variable x resolution
+
+prec='real*8';
+ieee='b';
+
+% Dimensions of grid
+%nx=80;
+nx=3;
+ny=1;
+nz=20;
+% Nominal depth of model (meters)
+H=1000.0;
+% Size of domain
+Lx=15e3;
+
+dx = Lx/nx;
+dz = H/nz;
+% Horizontal resolution (m)
+%Variable resolution
+
+dy = dx;
+
+% Flux
+Qo=200;
+
+% Stratification
+gravity=9.81;
+talpha=2.0e-4;
+N2=2.56e-4;
+Tz=N2/(gravity*talpha);
+Sz=0.05;
+
+sprintf('delR = %d * %7.6g,',nz,dz)
+
+z=-dz/2:-dz:-H;
+Tref=Tz*z/(H/20)+2;
+Sref=34.025-Sz*z/(H/20);
+[sprintf('Tref =') sprintf(' %8.6g,',Tref)]
+[sprintf('Sref =') sprintf(' %8.6g,',Sref)]
+
+Q=zeros([nx,ny]);
+Q(1) = 0;
+Q(2) = 200;
+Q(3) = 0;
+fid=fopen('Qnet.forcing','w',ieee); fwrite(fid,Q,prec); fclose(fid);
+
+s=zeros([nx,ny,nz]);
+for k=1:nz
+ s(:,:,k) = s(:,:,k) + Sref(k);
+end
+fid=fopen('S.init','w',ieee); fwrite(fid,s,prec); fclose(fid);
+
+t0=1*ones([nx,ny,nz]);
+t=t0;
+fid=fopen('T.init','w',ieee); fwrite(fid,t,prec); fclose(fid);
+
+t=t0;
+t(2,1,1) = .5;
+fid=fopen('Tinstable.init','w',ieee); fwrite(fid,t,prec); fclose(fid);
+
+t=t0;
+for k=1:nz
+ t(:,:,k) = t(:,:,k) + Tref(k);
+end
+fid=fopen('Tstable.init','w',ieee); fwrite(fid,t,prec); fclose(fid);
+
+t=t0;
+t(2,1,1) = -.5;
+for k=1:nz
+ t(:,:,k) = t(:,:,k) + Tref(k);
+end
+fid=fopen('Tstable2.init','w',ieee); fwrite(fid,t,prec); fclose(fid);
+
+taux = 0*ones([nx,ny]);
+taux(2:3,:) = 1;
+fid=fopen('taux.forcing','w',ieee); fwrite(fid,taux,prec); fclose(fid);
+
+tke = 1e-6*ones([nx,ny,nz]);
+tke(2,1,1) = 1e-3;
+fid=fopen('TKE.init','w',ieee); fwrite(fid,tke,prec); fclose(fid);
+
+
