3d994937d29120cf2cf0c99f2a2cd52107112c86
Martin Losch <Martin.Losch@awi.de>
2004-11-24 03:04:34 +0000

    o replace obsolete PTRACERS_SEPARATE_FORCING by GCHEM_SEPARATE_FORCING

--- pkg/gchem/GCHEM_OPTIONS.h
+++ pkg/gchem/GCHEM_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/gchem/GCHEM_OPTIONS.h,v 1.5 2004/11/19 21:28:47 mlosch Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/gchem/GCHEM_OPTIONS.h,v 1.6 2004/11/24 03:04:34 mlosch Exp $
 C $Name:  $
 
 #ifndef GCHEM_OPTIONS_H
@@ -16,7 +16,7 @@ C    !DESCRIPTION:
 c options for biogeochemistry package
 CEOP
 
-#define PTRACERS_SEPARATE_FORCING
+#define GCHEM_SEPARATE_FORCING
 #define DIC_BIOTIC
 #undef  ALLOW_CFC
 #undef  ALLOW_FE
