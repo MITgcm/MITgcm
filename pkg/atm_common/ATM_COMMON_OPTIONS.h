19ba53617ed6916e2d86309ef7a7db8d13274ba3
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-12-19 00:10:08 +0000

    create new pkg (atm_common) for code that atmospheric physics pkgs share  only contains 1 S/R for now (later: coupling interface for AIM & FIZHI)

---
+++ pkg/atm_common/ATM_COMMON_OPTIONS.h
@@ -0,0 +1,18 @@
+C $Header: /u/gcmpack/MITgcm/pkg/atm_common/ATM_COMMON_OPTIONS.h,v 1.1 2004/12/19 00:10:08 jmc Exp $
+C $Name:  $
+
+C CPP options file for atm_common package
+C
+C Use this file for selecting CPP options within the atm_common package
+
+#ifndef ATM_COMMON_OPTIONS_H
+#define ATM_COMMON_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_ATM_COMMON
+
+#include "CPP_OPTIONS.h"
+
+C CPP macros go here
+
+#endif /* ALLOW_ATM_COMMON */
+#endif /* ATM_COMMON_OPTIONS_H */
