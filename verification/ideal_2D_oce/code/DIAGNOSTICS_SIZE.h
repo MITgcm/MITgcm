45067907ea51b70b91609d80fd856da751d45905
Jean-Michel Campin <jmc@ocean.mit.edu>
2004-12-20 02:40:09 +0000

    add diagnostics pkg to test the output

---
+++ verification/ideal_2D_oce/code/DIAGNOSTICS_SIZE.h
@@ -0,0 +1,24 @@
+C $Header: /u/gcmpack/MITgcm/verification/ideal_2D_oce/code/DIAGNOSTICS_SIZE.h,v 1.1 2004/12/20 02:40:09 jmc Exp $
+C $Name:  $
+
+
+C     Diagnostics Array Dimension
+C     ---------------------------
+C     ndiagMax   :: maximum total number of available diagnostics
+C     numlists   :: maximum number of diagnostics list (in data.diagnostics)
+C     numperlist :: maximum number of active diagnostics per list (data.diagnostics)
+C     numLevels  :: maximum number of levels to write    (data.diagnostics)
+C     numdiags   :: maximum size of the storage array for active diagnostics
+      INTEGER    ndiagMax
+      INTEGER    numlists
+      INTEGER    numperlist
+      INTEGER    numLevels
+      INTEGER    numdiags
+      PARAMETER( ndiagMax = 500 )
+      PARAMETER( numlists = 10, numperlist = 50, numLevels=2*Nr )
+      PARAMETER( numdiags = 25*Nr )
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
