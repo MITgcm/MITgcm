66dcd2f02b8257de2a6aa8d51a57caf4d6b204b0
Patrick Heimbach <heimbach@mit.edu>
2005-02-07 16:12:19 +0000

    Remove PACKAGES_CONFIG.h

--- verification/global_with_exf/code/CPP_OPTIONS.h
+++ verification/global_with_exf/code/CPP_OPTIONS.h
@@ -1,18 +1,14 @@
-C $Header: /u/gcmpack/MITgcm/verification/global_with_exf/code/CPP_OPTIONS.h,v 1.9 2004/05/03 21:35:04 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/verification/global_with_exf/code/CPP_OPTIONS.h,v 1.10 2005/02/07 16:12:19 heimbach Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
 #define CPP_OPTIONS_H
 
-#include "PACKAGES_CONFIG.h"
-
 C CPP flags controlling particular source code features
 
 C o Shortwave heating as extra term in external_forcing.F
 C Note: this should be a run-time option and not necessarily dependent on KPP
-#ifdef ALLOW_KPP
-# define SHORTWAVE_HEATING
-#endif
+#define SHORTWAVE_HEATING
 
 C o Include/exclude phi_hyd calculation code
 #define INCLUDE_PHIHYD_CALCULATION_CODE
@@ -41,11 +37,11 @@ C o Execution environment support options
 #include "CPP_EEOPTIONS.h"
 
 C o Include/exclude code specific to the ECCO/SEALION version.
-#if (defined (ALLOW_AUTODIFF) || \
-     defined (ALLOW_ECCO) || \
-     defined (ALLOW_EXF))
+C#if (defined (ALLOW_AUTODIFF) || \
+C     defined (ALLOW_ECCO) || \
+C     defined (ALLOW_EXF))
 # include "ECCO_CPPOPTIONS.h"
-#endif
+C#endif
 
 #endif /* CPP_OPTIONS_H */
 
