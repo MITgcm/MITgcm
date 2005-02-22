5c73211f7b10eff79e4c185784e689174d26bea4
Patrick Heimbach <heimbach@mit.edu>
2005-02-22 17:36:56 +0000

    Need one more storing to avoid recomputation in DIVA.

--- pkg/ecco/cost_averages_bar_directives.h
+++ pkg/ecco/cost_averages_bar_directives.h
@@ -9,6 +9,7 @@ CADJ STORE xx_tbar_mean_dummy  = onetape
 CADJ STORE xx_sbar_mean_dummy  = onetape
 CADJ STORE xx_ubar_mean_dummy  = onetape
 CADJ STORE xx_vbar_mean_dummy  = onetape
+CADJ STORE xx_wbar_mean_dummy  = onetape
 CADJ STORE xx_taux_mean_dummy  = onetape
 CADJ STORE xx_tauy_mean_dummy  = onetape
 CADJ STORE xx_hflux_mean_dummy = onetape
