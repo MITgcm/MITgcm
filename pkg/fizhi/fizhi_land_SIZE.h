e23875be3bc54c79c21eefa9a83f5e70858a3f58
Andrea Molod <andrea.molod@nasa.gov>
2005-02-25 19:02:20 +0000

    Make sure nchp is big enough for real vegetation tiles

--- pkg/fizhi/fizhi_land_SIZE.h
+++ pkg/fizhi/fizhi_land_SIZE.h
@@ -1,9 +1,8 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.4 2004/07/30 18:59:32 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.5 2005/02/25 19:02:20 molod Exp $
 C $Name:  $
 
 c Land Grid Horizontal Dimension (Number of Tiles)
 c ------------------------------------------------
       integer nchp, maxtyp
       parameter (maxtyp = 10)
-c     parameter (nchp = sNx*sNy*maxtyp)
-      parameter (nchp = 2048)
+      parameter (nchp = sNx*sNy*maxtyp)
