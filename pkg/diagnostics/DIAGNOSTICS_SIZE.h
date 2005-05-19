ff6082cb0df4bde0de8eba841155cb8232f91fad
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-05-19 03:35:57 +0000

    add new dimensions for Global/Regional statistics diagnostics

--- pkg/diagnostics/DIAGNOSTICS_SIZE.h
+++ pkg/diagnostics/DIAGNOSTICS_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGNOSTICS_SIZE.h,v 1.2 2004/12/13 23:07:05 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGNOSTICS_SIZE.h,v 1.3 2005/05/19 03:35:57 jmc Exp $
 C $Name:  $
 
 
@@ -8,15 +8,22 @@ C     ndiagMax   :: maximum total number of available diagnostics
 C     numlists   :: maximum number of diagnostics list (in data.diagnostics)
 C     numperlist :: maximum number of active diagnostics per list (data.diagnostics)
 C     numLevels  :: maximum number of levels to write    (data.diagnostics)
-C     numdiags   :: maximum size of the storage array for active diagnostics
+C     numdiags   :: maximum size of the storage array for active 2D/3D diagnostics
+C     nRegions   :: maximum number of regions (statistics-diagnostics)
+C     nStats     :: maximum number of statistics (e.g.: aver,min,max ...)
+C     diagSt_size:: maximum size of the storage array for statistics-diagnostics
+C Note : may need to increase "numdiags" when using several 2D/3D diagnostics,
+C  and "diagSt_size" (statistics-diags) since values here are deliberately small.
       INTEGER    ndiagMax
-      INTEGER    numlists
-      INTEGER    numperlist
-      INTEGER    numLevels
+      INTEGER    numlists, numperlist, numLevels
       INTEGER    numdiags
+      INTEGER    nRegions, nStats
+      INTEGER    diagSt_size
       PARAMETER( ndiagMax = 500 )
       PARAMETER( numlists = 10, numperlist = 50, numLevels=2*Nr )
-      PARAMETER( numdiags = 10 )
+      PARAMETER( numdiags = 1*Nr )
+      PARAMETER( nRegions = 0 , nStats = 4 )
+      PARAMETER( diagSt_size = 10*Nr )
 
 
 CEH3 ;;; Local Variables: ***
