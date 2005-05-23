f13426fb9cd9ce1715cb6ea67582ed8cf92671fa
Andrea Molod <andrea.molod@nasa.gov>
2005-05-23 20:51:09 +0000

    New diagnostic - delta pressure on alternate (fizhi for now) grid

---
+++ pkg/gridalt/GRIDALT_OPTIONS.h
@@ -0,0 +1,22 @@
+C $Header: /u/gcmpack/MITgcm/pkg/gridalt/GRIDALT_OPTIONS.h,v 1.1 2005/05/23 20:51:09 molod Exp $
+C $Name:  $
+
+#ifndef GRIDALT_OPTIONS_H
+#define GRIDALT_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_GRIDALT
+
+#include "CPP_OPTIONS.h"
+
+C     Package-specific options go here
+
+C     #define GRIDALT_DEBUG
+
+
+#endif /* ALLOW_GRIDALT */
+#endif /* GRIDALT_OPTIONS_H */
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
