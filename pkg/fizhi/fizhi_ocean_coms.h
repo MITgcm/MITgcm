3f946231fb24667623a361d2f82b2958f83f7253
Andrea Molod <andrea.molod@nasa.gov>
2005-06-14 18:14:21 +0000

    Fix to do IO only when really needed - other little stuff too

--- pkg/fizhi/fizhi_ocean_coms.h
+++ pkg/fizhi/fizhi_ocean_coms.h
@@ -1,9 +1,10 @@
-C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_ocean_coms.h,v 1.2 2004/07/28 01:25:07 molod Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/fizhi/fizhi_ocean_coms.h,v 1.3 2005/06/14 18:14:21 molod Exp $
 C $Name:  $
 
 c Ocean Exports
 c -------------------
-      common /ocean_exports/ sst, sice
+      common /ocean_exports/ sst, sice, ksst, kice
       _RL sst(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nsx,Nsy)
       _RL sice(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nsx,Nsy)
+      integer ksst, kice
 
