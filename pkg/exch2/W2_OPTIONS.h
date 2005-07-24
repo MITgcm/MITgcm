8c27c9de99ae0a1c5d1f50862d615c0948c6e1c0
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-07-24 01:35:06 +0000

    filling of face-corner halo regions is now optional (ifdef W2_FILL_NULL_REGIONS)  and using a filling value (non necessary zero, for testing purpose). Default is #undef W2_FILL_NULL_REGIONS

--- pkg/exch2/W2_OPTIONS.h
+++ pkg/exch2/W2_OPTIONS.h
@@ -1,2 +1,19 @@
+C $Header: /u/gcmpack/MITgcm/pkg/exch2/W2_OPTIONS.h,v 1.2 2005/07/24 01:35:06 jmc Exp $
+C $Name:  $
+ 
+C CPP options file for EXCH2 package
+
+#ifndef W2_OPTIONS_H
+#define W2_OPTIONS_H
+
+C ... W2_USE_E2_SAFEMODE description ...
 #define W2_USE_E2_SAFEMODE
+
+C Debug mode option:
 #undef  W2_E2_DEBUG_ON
+
+C Fill null regions (face-corner halo regions) with e2FillValue_RX (=0)
+C notes: for testing (allow to check that results are not affected)
+#undef W2_FILL_NULL_REGIONS
+
+#endif /* W2_OPTIONS_H */
