9260a3d03867c91a672a6a753736edb6f83b2ee8
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-07-27 01:02:54 +0000

    updated (to read new grid files).

--- verification/advect_cs/input/gendata.m
+++ verification/advect_cs/input/gendata.m
@@ -1,18 +1,24 @@
 for tl=1:6,
-lon(:,tl,:)=rdda(sprintf('LONC.%3.3i.bin',tl),[32 32],1,'real*8','b')*pi/180;
-lat(:,tl,:)=rdda(sprintf('LATC.%3.3i.bin',tl),[32 32],1,'real*8','b')*pi/180;
+%lon(:,tl,:)=rdda(sprintf('LONC.%3.3i.bin',tl),[32 32],1,'real*8','b')*pi/180;
+%lat(:,tl,:)=rdda(sprintf('LATC.%3.3i.bin',tl),[32 32],1,'real*8','b')*pi/180;
+ xx=rdda(sprintf('grid_cs32.face%3.3i.bin',tl),[33 33],1,'real*8','b');
+ lon(:,tl,:)=xx(1:32,1:32)*pi/180;
+ xx=rdda(sprintf('grid_cs32.face%3.3i.bin',tl),[33 33],2,'real*8','b');
+ lat(:,tl,:)=xx(1:32,1:32)*pi/180;
 end
 X=cos(lat).*sin(lon);Y=-cos(lat).*cos(lon);Z=sin(lat);clear lat lon
 
-lon0=-90  *pi/180;
+%lon0=-90  *pi/180;
+lon0=+90  *pi/180;
 lat0=  0  *pi/180;
 xo=cos(lat0).*sin(lon0);yo=-cos(lat0).*cos(lon0);zo=sin(lat0);
 ro=0.3;
 R=sqrt( (X-xo).^2 + (Y-yo).^2 + (Z-zo).^2 );
-s=1+(1+cos( pi*min(R/ro,1+0*R) ))/2;
-wrda('T.init',s,1,'real*8','b');
+t=1+(1+cos( pi*min(R/ro,1+0*R) ))/2;
+wrda('T.init',t,1,'real*8','b');
 
-lon0=  0  *pi/180;
+%lon0=  0  *pi/180;
+lon0=180  *pi/180;
 lat0=35   *pi/180;
 xo=cos(lat0).*sin(lon0);yo=-cos(lat0).*cos(lon0);zo=sin(lat0);
 ro=0.3;
