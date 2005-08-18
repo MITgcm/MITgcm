4fb2f9a7f23ba09e97885fe0c0558f21c993ee55
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-08-18 18:26:15 +0000

    changes for 2.D or 4.D input arrays.

--- utils/cs_grid/rotate_csAg_EN.m
+++ utils/cs_grid/rotate_csAg_EN.m
@@ -6,14 +6,16 @@ function [uE,vN] = rotate_csAg_EN(u,v)
 %  uE,vN are the Eastward,Northward components of the rotated flow field
 % assume that the 1rst 2 dimensions of (u,v) are [6*nc nc] for a CS(nc x nc) grid
 %
-% $Header: /u/gcmpack/MITgcm/utils/cs_grid/Attic/rotate_csAg_EN.m,v 1.1 2005/06/28 02:34:54 jmc Exp $
+% $Header: /u/gcmpack/MITgcm/utils/cs_grid/Attic/rotate_csAg_EN.m,v 1.2 2005/08/18 18:26:15 jmc Exp $
 
 %Rac='/home/jmc/grid_cs32/' ;
 Rac='grid_files/';
 
 NN=size(u);
-[nnx nc nz]=size(u); 
-nPg=nnx*nc;
+if length(NN) < 3, nz=1;
+else nz=prod(NN(3:end));
+end
+nnx=NN(1); nc=NN(2); nPg=nnx*nc;
 if nnx ~= 6*nc,
  fprintf('Error in CS-dim: %i %i %i \n',NN);
  return
@@ -34,7 +36,7 @@ for k=1:nz;
  vN(:,k)=uvEN(:,2).*u(:,k)+uvEN(:,1).*v(:,k);
 end
 
-uE=reshape(uE,[nnx NN(2:end)]);
-vN=reshape(vN,[nnx NN(2:end)]);
+uE=reshape(uE,NN);
+vN=reshape(vN,NN);
 
 return
--- utils/cs_grid/rotate_csCg_EN.m
+++ utils/cs_grid/rotate_csCg_EN.m
@@ -10,14 +10,16 @@ function [uE,vN] = rotate_csCg_EN(u,v)
 % >> v=rdmds('vVeltave.0000513360');
 % >> [uE,vN]=rotate_csCg_EN(u,v);
 %
-% $Header: /u/gcmpack/MITgcm/utils/cs_grid/Attic/rotate_csCg_EN.m,v 1.1 2005/06/28 02:34:54 jmc Exp $
+% $Header: /u/gcmpack/MITgcm/utils/cs_grid/Attic/rotate_csCg_EN.m,v 1.2 2005/08/18 18:26:15 jmc Exp $
 
 %Rac='/home/jmc/grid_cs32/' ;
 Rac='grid_files/';
 
 NN=size(u);
-if length(NN) < 3, nz=1; [nnx nc]=size(u); else [nnx nc nz]=size(u); end
-nPg=nnx*nc;
+if length(NN) < 3, nz=1;
+else nz=prod(NN(3:end));
+end 
+nnx=NN(1); nc=NN(2); nPg=nnx*nc;
 
 if nnx ~= 6*nc,
  fprintf('Error in CS-dim: %i %i %i \n',NN);
@@ -30,6 +32,8 @@ v=reshape(v,[nnx nc nz]);
 
 %- do simple average to put u,v at the center (A-grid):
 [uu,vv] = split_UV_cub(u,v);
+uu=reshape(uu,[nc+1 nc nz 6]);
+vv=reshape(vv,[nc nc+1 nz 6]);
 
 u=(uu(1:nc,:,:,:)+uu(2:nc+1,:,:,:))/2;
 v=(vv(:,1:nc,:,:)+vv(:,2:nc+1,:,:))/2;
@@ -49,7 +53,7 @@ for k=1:nz;
  vN(:,k)=uvEN(:,2).*u(:,k)+uvEN(:,1).*v(:,k);
 end
 
-uE=reshape(uE,[nnx NN(2:end)]);
-vN=reshape(vN,[nnx NN(2:end)]);
+uE=reshape(uE,NN);
+vN=reshape(vN,NN);
 
 return
