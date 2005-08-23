5a7300f0474a1f85e30e4abcab62c101a54ce22f
Patrick Heimbach <heimbach@mit.edu>
2005-08-23 13:46:57 +0000

    Modify one CPP construct to comply with cb2m.csh script.

--- pkg/ctrl/ctrl_weights.h
+++ pkg/ctrl/ctrl_weights.h
@@ -5,29 +5,25 @@ c     Define dummy weights as a placeholder
       _RL wtheta   ( nr,nsx,nsy )
       _RL wsalt    ( nr,nsx,nsy )
 
-#if (defined (ALLOW_OBCSN_COST_CONTRIBUTION) || \
-     defined (ALLOW_OBCSN_CONTROL))
+#if (defined (ALLOW_OBCSN_COST_CONTRIBUTION) || defined (ALLOW_OBCSN_CONTROL))
       common /ctrl_cost_weights_obcsn/
      &                      wobcsn, wobcsnLev
       _RL wobcsn     (                      nr,nobcs)
       _RL wobcsnLev  (1-olx:snx+olx,nr,nsx,nsy,nobcs)
 #endif
-#if (defined (ALLOW_OBCSS_COST_CONTRIBUTION) || \
-     defined (ALLOW_OBCSS_CONTROL))
+#if (defined (ALLOW_OBCSS_COST_CONTRIBUTION) || defined (ALLOW_OBCSS_CONTROL))
       common /ctrl_cost_weights_obcss/
      &                      wobcss, wobcssLev
       _RL wobcss     (                      nr,nobcs)
       _RL wobcssLev  (1-olx:snx+olx,nr,nsx,nsy,nobcs)
 #endif
-#if (defined (ALLOW_OBCSW_COST_CONTRIBUTION) || \
-     defined (ALLOW_OBCSW_CONTROL))
+#if (defined (ALLOW_OBCSW_COST_CONTRIBUTION) || defined (ALLOW_OBCSW_CONTROL))
       common /ctrl_cost_weights_obcsw/
      &                      wobcsw, wobcswLev
       _RL wobcsw     (                      nr,nobcs)
       _RL wobcswLev  (1-oly:sny+oly,nr,nsx,nsy,nobcs)
 #endif
-#if (defined (ALLOW_OBCSE_COST_CONTRIBUTION) || \
-     defined (ALLOW_OBCSE_CONTROL))
+#if (defined (ALLOW_OBCSE_COST_CONTRIBUTION) || defined (ALLOW_OBCSE_CONTROL))
       common /ctrl_cost_weights_obcse/
      &                      wobcse, wobcseLev
       _RL wobcse     (                      nr,nobcs)
