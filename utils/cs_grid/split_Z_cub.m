a6539bdeaa7d5d84beca0fa863602cb63b6811c4
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-09-06 17:44:36 +0000

    fix for 3.D (or 4.D) input array ; deal with the 2 missing corners.

--- utils/cs_grid/split_Z_cub.m
+++ utils/cs_grid/split_Z_cub.m
@@ -1,89 +1,81 @@
 function [z6t] = split_Z_cub(z3d)
 % [z6t] = split_Z_cub(z3d)
 %---------------------------------------------
-% split 2d/3d arrays Z to 2d/3d x 6 faces
+% split 2d/3d arrays z3d to 2d/3d x 6 faces
 % and add 1 column + 1 row
-% output is z6t(ny+1,ny+1,[nr],6)
-% take the average value (over the 3 neighbours) for the 2 missing corners
+%  => output is z6t(nc+1,nc+1,[nr],6)
+% either input is z3d(nc*6*nc+2,*): includes the 2 missing corners
+%   or   input is z3d(nc*6,nc,*): => use the average value (from the 3
+%                                 neighbours) for the 2 missing corners
 %----------------------------------------------
-dim=length(size(z3d));
-if dim == 2, nr=1; [nx ny]=size(z3d) ; else [nx ny nr]=size(z3d) ; end 
-nyp=ny+1;
+% $Header: /u/gcmpack/MITgcm/utils/cs_grid/Attic/split_Z_cub.m,v 1.2 2005/09/06 17:44:36 jmc Exp $
+% $Name:  $
+
+dims=size(z3d); nDim=length(dims);
+%fprintf(' nDim= %i , dims:',nDim);fprintf(' %i',dims);fprintf('\n');
+
+nc=fix((dims(1)-2)/6); nc=fix(sqrt(abs(nc)));
+if dims(1) == nc*nc*6+2,
+ nr=dims(2); if nDim > 2, nr=prod(dims(2:end)); end
+ nx=6*nc ; nPg=nx*nc; nPts=nPg+2; dims=[nPts 1 dims(2:end)];
+ z3d=reshape(z3d,[nPts nr]);
+ zzC=z3d(nPg+1:nPg+2,:); z3d=z3d(1:nPg,:);
+elseif dims(1) == 6*dims(2),
+ if nDim == 2, nr=1; else nr=prod(dims(3:end)); end
+ nc=dims(2); nx=6*nc ; nPg=nx*nc; nPts=nPg+2;
+ zzC=zeros(2,nr);
+else
+ fprintf(' Error in split_Z_cub: bad input dimensions :');
+ fprintf(' %i',dims); fprintf('\n');
+ z6t=0; return 
+end
+
 %=================================================================
 
-if dim == 2,
+ z3d=reshape(z3d,[nc 6 nc nr]); z3d=permute(z3d,[1 3 4 2]);
+ ncp=nc+1; z6t=zeros(ncp,ncp,nr,6);
 
-%- split on to 6 tile with overlap in i+1 & j+1 :
- z6t=zeros(nyp,ny+1,6);
+%-- split on to 6 faces:
+ z6t([1:nc],[1:nc],:,:)=z3d;
 
- for n=1:6,
-   z6t([1:ny],[1:ny],n)=z3d([(n-1)*ny+1:n*ny],[1:ny]);
- end
-
-  z6t(nyp,[1:ny], 1)=z3d(1+ny,[1:ny]);
-  z6t(nyp,[2:nyp],2)=z3d([4*ny:-1:1+(4-1)*ny],1);
-  z6t(nyp,[1:ny], 3)=z3d(1+3*ny,[1:ny]);
-  z6t(nyp,[2:nyp],4)=z3d([6*ny:-1:1+(6-1)*ny],1);
-  z6t(nyp,[1:ny], 5)=z3d(1+5*ny,[1:ny]);
-  z6t(nyp,[2:nyp],6)=z3d([2*ny:-1:1+(2-1)*ny],1);
+%-- add overlap in i+1 & j+1 :
+ z6t(ncp,[1:nc], :,1)=z3d(1,[1:nc],:,2);
+ z6t(ncp,[2:ncp],:,2)=z3d([nc:-1:1],1,:,4);
+ z6t(ncp,[1:nc], :,3)=z3d(1,[1:nc],:,4);
+ z6t(ncp,[2:ncp],:,4)=z3d([nc:-1:1],1,:,6);
+ z6t(ncp,[1:nc], :,5)=z3d(1,[1:nc],:,6);
+ z6t(ncp,[2:ncp],:,6)=z3d([nc:-1:1],1,:,2);
  
-  z6t([2:nyp],nyp,1)=z3d(1+2*ny,[ny:-1:1]);
-  z6t([1:ny], nyp,2)=z3d([(3-1)*ny+1:3*ny],1);
-  z6t([2:nyp],nyp,3)=z3d(1+4*ny,[ny:-1:1]);
-  z6t([1:ny], nyp,4)=z3d([(5-1)*ny+1:5*ny],1);
-  z6t([2:nyp],nyp,5)=z3d(1+0*ny,[ny:-1:1]);
-  z6t([1:ny], nyp,6)=z3d([(1-1)*ny+1:1*ny],1);   
+ z6t([2:ncp],ncp,:,1)=z3d(1,[nc:-1:1],:,3);
+ z6t([1:nc], ncp,:,2)=z3d([1:nc],1,:,3);
+ z6t([2:ncp],ncp,:,3)=z3d(1,[nc:-1:1],:,5);
+ z6t([1:nc], ncp,:,4)=z3d([1:nc],1,:,5);
+ z6t([2:ncp],ncp,:,5)=z3d(1,[nc:-1:1],:,1);
+ z6t([1:nc], ncp,:,6)=z3d([1:nc],1,:,1);   
 
-%- missing corner :
- zz1=z3d(2*ny,1)+z3d(4*ny,1)+z3d(6*ny,1);
- zz1=zz1/3;
-  z6t(nyp,1,2)=zz1;
-  z6t(nyp,1,4)=zz1;
-  z6t(nyp,1,6)=zz1;
+%----------------------------------------------------
 
- zz2=z3d(1,ny)+z3d(1+2*ny,ny)+z3d(1+4*ny,ny);
- zz2=zz2/3;
-  z6t(1,nyp,1)=zz2;
-  z6t(1,nyp,3)=zz2;
-  z6t(1,nyp,5)=zz2;
- 
+%-- missing corners :
+if dims(1) ~= nPts,
+%- use the average value (from the 3 neighbours)
+ zzC(1,:)=z3d(1,nc,:,1)+z3d(1,nc,:,3)+z3d(1,nc,:,5);
+ zzC(2,:)=z3d(nc,1,:,2)+z3d(nc,1,:,4)+z3d(nc,1,:,6);
+ zzC=zzC/3;
+ fprintf('split_Z_cub: fills 2 missing corners with local mean value\n');
+end
+%- 1rst (nPg+1) = N.W corner of face 1
+ z6t(1,ncp,:,1)=zzC(1,:);
+ z6t(1,ncp,:,3)=zzC(1,:);
+ z6t(1,ncp,:,5)=zzC(1,:);
+%- 2nd  (nPg+2) = S.E corner of face 2
+ z6t(ncp,1,:,2)=zzC(2,:);
+ z6t(ncp,1,:,4)=zzC(2,:);
+ z6t(ncp,1,:,6)=zzC(2,:);
+
+if nDim > 2,
+ z6t=reshape(z6t,[ncp ncp dims(3:end) 6]);
 else
-
-%- split on to 6 tile with overlap in i+1 & j+1 :
- z6t=zeros(ny+1,ny+1,nr,6);
-
- for n=1:6,
-   z6t([1:ny],[1:ny],:,n)=z3d([(n-1)*ny+1:n*ny],[1:ny],:);
- end
-
-  z6t(nyp,[1:ny], :,1)=z3d(1+ny,[1:ny],:);
-  z6t(nyp,[2:nyp],:,2)=z3d([4*ny:-1:1+(4-1)*ny],1,:);
-  z6t(nyp,[1:ny], :,3)=z3d(1+3*ny,[1:ny],:);
-  z6t(nyp,[2:nyp],:,4)=z3d([6*ny:-1:1+(6-1)*ny],1,:);
-  z6t(nyp,[1:ny], :,5)=z3d(1+5*ny,[1:ny],:);
-  z6t(nyp,[2:nyp],:,6)=z3d([2*ny:-1:1+(2-1)*ny],1,:);
- 
-  z6t([2:nyp],nyp,:,1)=z3d(1+2*ny,[ny:-1:1],:);
-  z6t([1:ny], nyp,:,2)=z3d([(3-1)*ny+1:3*ny],1,:);
-  z6t([2:nyp],nyp,:,3)=z3d(1+4*ny,[ny:-1:1],:);
-  z6t([1:ny], nyp,:,4)=z3d([(5-1)*ny+1:5*ny],1,:);
-  z6t([2:nyp],nyp,:,5)=z3d(1+0*ny,[ny:-1:1],:);
-  z6t([1:ny], nyp,:,6)=z3d([(1-1)*ny+1:1*ny],1,:);   
-
-%- missing corner :
-zz1=zeros(1,nr); zz2=zeros(1,nr);
- zz1=z3d(2*ny,1,:)+z3d(4*ny,1,:)+z3d(6*ny,1,:);
- zz1=zz1/3;
-  z6t(nyp,1,:,2)=zz1;
-  z6t(nyp,1,:,4)=zz1;
-  z6t(nyp,1,:,6)=zz1;
-
- zz2=z3d(ny,1,:)+z3d(ny,1+2*ny,:)+z3d(ny,1+4*ny,:)+z3d(6*ny,1,:);
- zz2=zz2/3;
-  z6t(1,nyp,:,1)=zz2;
-  z6t(1,nyp,:,3)=zz2;
-  z6t(1,nyp,:,5)=zz2;
- 
+ z6t=squeeze(z6t);
 end
 
 return
