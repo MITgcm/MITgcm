06c8b4d9e7cd86d4d9b9f09bd865e159eb8a5485
Jean Utke <jutke@allstate.com>
2005-10-12 03:06:38 +0000

    attempt to decouple the make targets by introducing a new suffix to generated files.

--- verification/OpenAD/code_ad/cb2m2.bash
+++ verification/OpenAD/code_ad/cb2m2.bash
@@ -1,12 +1,19 @@
+echo -n "make copies for all files"
+for j in `ls *.F`
+do 
+  cp $j ${j%.F}_cb2m.F
+  echo -n "."
+done
+echo ""
 for i in `ls *_mod.h | sed 's/\(.*\)_mod\.h/\1/'`
 do 
   echo -n "replacing: $i " 
-  for j in `egrep -l "include *\"$i" *.F`
+  echo  "s/include *\(\"${i}.h\"\)/include \"${i}_mod.h\"/" > script.sed
+  for j in `egrep -l "include *\"$i" *_cb2m.F`
   do 
     echo -n "."
-    echo  "s/include *\(\"${i}.h\"\)/include \"${i}_mod.h\"/" > script.sed
-    cat $j | sed -f script.sed | sed 's/IMPLICIT NONE//' | sed 's/implicit none//' > $j.1
-    mv $j.1 $j
+    cat $j | sed -f script.sed | sed 's/IMPLICIT NONE//' | sed 's/implicit none//' > ${j}.1
+    mv ${j}.1 $j	
   done
   echo ""     
 done
