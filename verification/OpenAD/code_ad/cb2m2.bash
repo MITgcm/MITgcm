40443fd96395094fcec7be127fa871843cffd433
Jean Utke <jutke@allstate.com>
2005-11-03 16:30:27 +0000

    various changes to distinguih fixed/free format F90

--- verification/OpenAD/code_ad/cb2m2.bash
+++ verification/OpenAD/code_ad/cb2m2.bash
@@ -1,7 +1,7 @@
 echo -n "make copies for all files"
 for j in `ls *.F`
 do 
-  cp $j ${j%.F}_cb2m.F
+  cp $j ${j%.F}_cb2m.FF90
   echo -n "."
 done
 echo ""
@@ -9,7 +9,7 @@ for i in `ls *_mod.h | sed 's/\(.*\)_mod\.h/\1/'`
 do 
   echo -n "replacing: $i " 
   echo  "s/include *\(\"${i}.h\"\)/include \"${i}_mod.h\"/" > script.sed
-  for j in `egrep -l "include *\"$i" *_cb2m.F`
+  for j in `egrep -l "include *\"$i" *_cb2m.FF90`
   do 
     echo -n "."
     cat $j | sed -f script.sed | sed 's/IMPLICIT NONE//' | sed 's/implicit none//' > ${j}.1
