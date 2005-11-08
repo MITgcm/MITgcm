95ff07dc12bc6269c7e2efa67c714dff682e629d
Jean-Michel Campin <jmc@ocean.mit.edu>
2005-11-08 01:54:53 +0000

    remove cg3d_x (not used) from this common bloc

--- model/inc/SOLVE_FOR_PRESSURE3D.h
+++ model/inc/SOLVE_FOR_PRESSURE3D.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/SOLVE_FOR_PRESSURE3D.h,v 1.4 2003/10/31 20:35:32 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/SOLVE_FOR_PRESSURE3D.h,v 1.5 2005/11/08 01:54:53 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -14,7 +14,15 @@ C     \ev
 CEOP
 
 #ifdef ALLOW_NONHYDROSTATIC
-      COMMON / SFP3D_COMMON_R8 / cg3d_x, cg3d_b
-      _RL  cg3d_x(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nR,nSx,nSy)
+
+C--   COMMON /SFP3D_COMMON_R/ variables (RL) used by 3-D pressure solver
+C     cg3d_x  :: Solution vector of the 3-D solver equation A.x=B 
+C     cg3d_b  :: Right Hand side vector of the 3-D solver equation A.x=B
+
+      COMMON /SFP3D_COMMON_R/ 
+     &                        cg3d_b
+c    &                        cg3d_x, cg3d_b
+c     _RL  cg3d_x(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nR,nSx,nSy)
       _RL  cg3d_b(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nR,nSx,nSy)
+
 #endif /* ALLOW_NONHYDROSTATIC */
