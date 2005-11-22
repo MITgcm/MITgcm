32ec7e18d0e45bf93fdbd44ad0fb09ae371a6eb1
Daniel Enderton <daniel@asharpenergy.com>
2005-11-22 23:05:02 +0000

    Functions for calculating heat transport on a cube sphere grid from fluxes and directly;  Direct caculation method missing latent and potential energy terms.

---
+++ utils/matlab/cs_grid/calcHeatTransDirect.m
@@ -0,0 +1,445 @@
+function HT_Out = ...
+	calcHeatTransDirect(d,g,time,HeatCst,DiffKh,blkFile,nBas,nlfs,nout)
+      
+% Output:
+%   HT(latitude,basin,field)
+%       latitude:   Latitude index of heat transport
+%       basin:      Basin (1 = global)
+%       field:      Field (1 = Eulerian circulation HT       )
+%                         (2 = HT by mean circulation        )
+%                         (3 = Residual [3=1-2]              )
+%                         (4 = HT by zonal mean circulation  )
+%                         (5 = Residual [5=2-4]              )
+%                         (6 = HT by horizontal diffusion    )
+%                         (7 = Integrated surface heat flux  )
+%                         (8-14 = Implied heating from fields)
+%
+% Description:
+%   Calculation heat transport, and to degree possible, decomposition.
+%   Heat transport is given in Watts and the implied surface heating/flux
+%   in W/m^2.  The incoming data arrays are all assumed to be of the
+%   dimensions [6*nc,nc,nr].
+
+% Error checking
+if nlfs ~= 1, error('Not ready to handle non-nonlinear free surface!'); end
+if nBas ~= 0, error('Not ready to handle multiple basins'); end
+
+MakePlots = 0;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                     Prepare / reform  incoming data                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Determine time indecies.
+if isempty(time), time = d.T; i_time = 1:length(time);  
+else [dump,i_time] = ismember(time,d.T); end
+
+% i_time = 1;
+
+nc = size(g.HFacC,2);
+nr = size(g.HFacC,3);
+nt = length(time);
+
+ac  = reshape(g.rA                     ,[6*nc*nc, 1]);
+hw  = reshape(g.HFacW(1:6*nc,1:nc,1:nr),[6*nc*nc,nr]);
+hs  = reshape(g.HFacS(1:6*nc,1:nc,1:nr),[6*nc*nc,nr]);
+dxc = reshape(g.dxC(1:6*nc,1:nc)       ,[6*nc*nc, 1]);
+dyc = reshape(g.dyC(1:6*nc,1:nc)       ,[6*nc*nc, 1]);
+dxg = reshape(g.dxG(1:6*nc,1:nc)       ,[6*nc*nc, 1]);
+dyg = reshape(g.dyG(1:6*nc,1:nc)       ,[6*nc*nc, 1]);
+drf = reshape(g.drF,[1,length(g.drF)]);
+
+u  = reshape(d.uVeltave(1:6*nc,1:nc,:,i_time),[6*nc*nc,nr,nt]);
+v  = reshape(d.vVeltave(1:6*nc,1:nc,:,i_time),[6*nc*nc,nr,nt]);
+ut = reshape(d.UTtave(1:6*nc,1:nc,:,i_time)  ,[6*nc*nc,nr,nt]);
+vt = reshape(d.VTtave(1:6*nc,1:nc,:,i_time)  ,[6*nc*nc,nr,nt]);
+hu = reshape(d.hUtave(1:6*nc,1:nc,:,i_time)  ,[6*nc*nc,nr,nt]);
+hv = reshape(d.hVtave(1:6*nc,1:nc,:,i_time)  ,[6*nc*nc,nr,nt]);
+if nout > 6, tf = reshape(d.tFluxtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]); end
+t  = d.Ttave(1:6*nc,1:nc,:,i_time);
+
+% Load broken line information.  Compute (tracer point) cell area between
+% broken lines for each basin.  There are nbkl broken lines and nbkl+1
+% bands between broken lines.  The variable "bkl_Zon" gives the zone number
+% (nbkl+1 total) for a given index between 0 and nbkl, that is, nbkl+1
+% total zones.  Comments block is for eventual addition of multiple basin
+% calculations.
+load(blkFile);
+ydim=length(bkl_Ylat);
+AreaZon=zeros(ydim+1,1+nBas);
+for j = 1:ydim+1
+    izon = find(bkl_Zon == j-1);
+    AreaZon(j,1) = sum(ac(izon));
+%     for b = 1:nBas,
+%         tmp = ac.*mskBc(:,b);
+%         AreaZon(j,1+b) = sum(tmp(izon));
+%     end
+end
+
+% Latitute plotting information.  Average latitude of a broken line
+% (ylatHF) is calculated from a mean value of the y value of all the edges.
+% The latitude at the surface flux points is a mean of the broken line mean
+% values.
+YlatAv=sum(bkl_Ysg,1)./(1+bkl_Npts');
+ylatHT = [-90,YlatAv,90];
+ylatSF = ( ylatHT(2:ydim+2) + ylatHT(1:ydim+1) )./2;
+
+% The variable "bkl_Flg" is -1/1 if edge (on a given broken) has a u point
+% and -2/2 if it has a v point.  Positive/negative values contribute
+% positively/negatively to northward heat transport (this depends on the
+% oreientation of the cell).  A zero value indicates an end of edges that
+% contribute to a broken line.  The u and v information is parced into two
+% seperate fields, ufac and vfac (-2/2 are reduced to -1/1 for vfac).
+ufac = zeros([size(bkl_Flg),1+nBas]);
+vfac = zeros([size(bkl_Flg),1+nBas]);
+ufac(:,:,1) = rem(bkl_Flg,2);
+vfac(:,:,1) = fix(bkl_Flg/2);
+% for jl=1:ydim,
+%     ie=bkl_Npts(jl);
+%     for b=1:nBas,
+%         ufac(1:ie,jl,1+b)=mskBw(bkl_IJuv(1:ie,jl),b).*ufac(1:ie,jl,1);
+%         vfac(1:ie,jl,1+b)=mskBs(bkl_IJuv(1:ie,jl),b).*vfac(1:ie,jl,1);
+%     end
+% end
+ufacabs = abs(ufac);
+vfacabs = abs(vfac);
+
+% Prepare mask(s).
+% ??? I temporarily took out the code to configure the masks beyond this
+%     global one.  Does this need to account for a ridge if present?
+mskG=ones(ydim+1,1+nBas);
+
+% Area factors.  "ArW_Dif" and "ArS_Dif" the areas for the western and
+% southern edge of cells, respectively  The "_Dif" suffix indicates the  
+% areas used for the diffusivity because there is an extra "dxc" or "dyc"
+% from the gradient (here for computational efficiency reasons).  The  
+% division by "dxc" and "dyc" is associates with gradient of temperature.
+%
+% ??? Why is the land mask (hw and hs) only in the diffusive area term?
+ArW = dyg*reshape(drf,[1,length(drf)]);
+ArS = dxg*reshape(drf,[1,length(drf)]);
+ArW_Dif=hw.*((dyg./dxc)*reshape(drf,[1,length(drf)]));
+ArS_Dif=hs.*((dxg./dyc)*reshape(drf,[1,length(drf)]));
+
+% Compute the temperature and its gradient and the velocity points:
+%   tbi/tdi = temperature between/difference i points (at u points)
+%   tbj/tdj = temperature between/difference j points (at v points)
+% The cube is first split and the extra points are added to the files.
+% Then the means and differences are taken.  Note that the division of dxc
+% and dyc for the gradient is not applied until later for computational
+% efficiency purposes.  The arrays are then croped and reshaped to the
+% format for the other field variables, [6*nc*nc,nr].  Note that the
+% split_C_cub function adds a row/column of tracer points in front of the
+% first row/column of the tile matries.  This, when the differences and
+% gradients are computed and cropped, the off indecies are selected from
+% [2:nc+1] rather than [1:nc].  (This was a bit mystifying to me).
+t6bi=zeros(nc,nc+1,nr,nt,6); t6di=t6bi;
+t6bj=zeros(nc+1,nc,nr,nt,6); t6dj=t6bj;
+t6t=split_C_cub(t);
+t6bi([1:nc],:,:,:,:) = ( t6t([1:nc],:,:,:,:) + t6t([2:nc+1],:,:,:,:) )./2;
+t6bj(:,[1:nc],:,:,:) = ( t6t(:,[1:nc],:,:,:) + t6t(:,[2:nc+1],:,:,:) )./2;
+t6di([1:nc],:,:,:,:) = ( t6t([1:nc],:,:,:,:) - t6t([2:nc+1],:,:,:,:) );
+t6dj(:,[1:nc],:,:,:) = ( t6t(:,[1:nc],:,:,:) - t6t(:,[2:nc+1],:,:,:) );
+tbi = t6bi([1:nc],[2:nc+1],:,:,:);
+tbj = t6bj([2:nc+1],[1:nc],:,:,:);
+tdi = t6di([1:nc],[2:nc+1],:,:,:);
+tdj = t6dj([2:nc+1],[1:nc],:,:,:);
+tbi=reshape(permute(tbi,[1,5,2,3,4]),[6*nc*nc,nr,nt]); 
+tbj=reshape(permute(tbj,[1,5,2,3,4]),[6*nc*nc,nr,nt]); 
+tdi=reshape(permute(tdi,[1,5,2,3,4]),[6*nc*nc,nr,nt]);
+tdj=reshape(permute(tdj,[1,5,2,3,4]),[6*nc*nc,nr,nt]);
+
+% Prepare output arrays.  "nout" is the number of transport output fields.
+% It is currently hard-coded, but could eventually be an input parameters
+% to set which output fields are desired if some of then become
+% computationally expensive.
+%   HT    = Heat transport
+%   SF    = Implied surface flux
+%   divFx = Integrated surface heat flux
+%   IntV  = Integrated volume transport
+%   IntT  = Integrated temperature
+%
+% ??? utz0/vtz0 What do you use this for?
+HT     = zeros(ydim+2,1+nBas,nt,nout);
+SF     = zeros(ydim+1,1+nBas,nt,nout);
+divFx  = zeros(ydim+1,1+nBas,nt,nout);
+IntV   = zeros(ydim,nr,1+nBas,nt);
+IntT   = zeros(ydim,nr,1+nBas,nt);
+IntM   = zeros(ydim,nr,1+nBas,nt);
+utz0   = zeros(6*nc*nc,nt);
+vtz0   = zeros(6*nc*nc,nt);
+divHfx = zeros(6*nc,nc,nt);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                     Make heat transport calculations                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Preparation for calculation of zonal average temperature.  The
+% tempereature multiplied by the appropriate length scale ("tbi_temp",
+% "tbj_temp") is summed up ("IntT" in the next section) and
+% divided by the total length ("IntM", composed from summing "hw_temp",
+% "hs_temp").
+hw_temp = zeros(size(hw));
+hs_temp = zeros(size(hs));
+for k=1:nr,
+    hw_temp(:,k) = dyg.*hw(:,k);
+    hs_temp(:,k) = dxg.*hs(:,k);
+end
+
+for it = 1:length(time)
+    
+    % ??? Again, what do you use this for?
+    utz0 = ut(:,it)*drf;
+    vtz0 = vt(:,it)*drf;
+    
+    %  uz / vz  = Volume transport though cell faces (velocity times area).
+    %             Used for zonal mean volume transport (4).
+    % utz1/vtz1 = Eulerian sensible heat transport through cell faces (1).
+    % utz2/vtz2 = Sensible heat transport through cell faces by Eulerian
+    %             mean circulations (2).
+    % dtx1/dty1 = Temperatude gradient at cell face times the area (when
+    %             multiplied by the diffusion will be the horizontal
+    %             diffusion heat transport) (6).
+    uz = ArW.*hu(:,:,it);
+    vz = ArS.*hv(:,:,it);
+    utz1 = sum(ArW.*ut(:,:,it),2);
+    vtz1 = sum(ArS.*vt(:,:,it),2);
+    utz2 = sum(ArW.*hu(:,:,it).*tbi(:,:,it),2);
+    vtz2 = sum(ArS.*hv(:,:,it).*tbj(:,:,it),2);
+    dtx1 = sum(ArW_Dif.*tdi(:,:,it),2);
+    dty1 = sum(ArS_Dif.*tdj(:,:,it),2);
+    
+    % Preparation for calculation of zonal average temperature.  The
+    % tempereature multiplied by the appropriate length scale ("tbi_temp",
+    % "tbj_temp") is summed up ("IntT" in the next section) and
+    % divided by the total length ("IntM", composed from summing "hw_temp",
+    % "hs_temp").
+    tbi_temp = hw_temp.*tbi(:,:,it);
+    tbj_temp = hs_temp.*tbj(:,:,it);
+
+    % Block 1:
+    % With the vertical integral of heat transport calculated across cell
+    % edges, the zonal integral (along the broken line) is computed to
+    % determine the total northward heat transport.  The first for loop is over
+    % the individual broken lines (determining the northward HT at the
+    % representative latitude).  The second loop is over the basins.  The third
+    % loop is over the individual edges along a specific broken line.  Note
+    % that an individual cell can have two edges (u and v) that have a HT
+    % contributions.  Hence the variable containing indecies of cells with
+    % edges along the broken lines, "bkl_IJuv", has some repeats.  Note that
+    % the variable "bkl_Npts" is the number of edges along a given broken line.
+    % Note also that the latitude axis starts at 2 because heat transport at
+    % extremes (latitute =  -90/90) is zero by definition.  Recall that index
+    % (1) is total Eulerian transport, (2) is from the mean circulations, and
+    % (3) is from the horizontal diffusion.
+    %
+    % Block 2:
+    % Here the heat transport associated mean zonal average circulation is
+    % calculated.  The zonal average volume transport v (IntV) and t
+    % (IntT/IntM) are computed first and are multiplied together at the
+    % end.
+    for jl=1:ydim
+        ie=bkl_Npts(jl);
+        for b=1:1+nBas
+            for ii=1:ie
+                ij=bkl_IJuv(ii,jl);
+                HT(1+jl,b,it,1) = HT(1+jl,b,it,1) ...
+                                  + ufac(ii,jl,b)*utz1(ij) ...
+                                  + vfac(ii,jl,b)*vtz1(ij);
+                HT(1+jl,b,it,2) = HT(1+jl,b,it,2) ...
+                                  + ufac(ii,jl,b)*utz2(ij) ...
+                                  + vfac(ii,jl,b)*vtz2(ij);
+                if nout >= 6
+                    HT(1+jl,b,it,6) = HT(1+jl,b,it,6) ...
+                                      + ufac(ii,jl,b)*dtx1(ij) ...
+                                      + vfac(ii,jl,b)*dty1(ij);
+                end
+                for k=1:nr
+                    IntV(jl,k,b,it) = IntV(jl,k,b,it) ...
+                                      + ufac(ii,jl,b).*uz(ij,k) ...
+                                      + vfac(ii,jl,b).*vz(ij,k); 
+                    IntT(jl,k,b,it) = IntT(jl,k,b,it) ...
+                                      + ufacabs(ii,jl,b).*tbi_temp(ij,k) ...
+                                      + vfacabs(ii,jl,b).*tbj_temp(ij,k); 
+                    IntM(jl,k,b,it) = IntM(jl,k,b,it) ...
+                                      + ufacabs(ii,jl,b).*hw_temp(ij,k) ...
+                                      + vfacabs(ii,jl,b).*hs_temp(ij,k);
+                end
+            end
+        end
+    end
+
+    % Prepare HT output, including muliplication of HeatCst.
+    HT(2:ydim+1,:,it,4) = sum(    IntV(:,:,:,it) ...
+                               .* IntT(:,:,:,it) ...
+                               ./ IntM(:,:,:,it),2);
+    HT(:,:,it,3) = HT(:,:,it,1) - HT(:,:,it,2);
+    HT(:,:,it,5) = HT(:,:,it,2) - HT(:,:,it,4);
+    if nout >= 6, HT(:,:,it,6) = DiffKh.*HT(:,:,it,6); end
+    HT(:,:,it,:)=HeatCst*HT(:,:,it,:);
+    
+    % Integrated heat flux in zones calculated from tFlux.
+    if nout > 6
+        for j = 1:ydim+1
+            I = find(bkl_Zon == j-1);
+            var = ac.*tf(:,it);
+            divFx(j,1,it,nout) = sum(var(I));
+            % for b=1:nBas
+            %     tmp=var.*mskBc(:,b);
+            %     divFx(j,1+b,nout)=sum(tmp(I));
+            % end
+        end
+        SF(:,:,it,nout) = divFx(:,:,it,nout)./AreaZon;
+        HT([2:(ydim+2)],:,it,nout) = cumsum(divFx(:,:,it,nout));
+    end
+
+    % % Masking for different basin.
+    % mskZ = ones(ydim+2,1+nBas);
+    % mskZ([2:ydim+1],:) = mskG([1:ydim],:) + mskG([2:ydim+1],:);
+    % mskZ = reshape(min(mskZ,1),(ydim+2)*(1+nBas),1);
+    % I = find(mskZ == 0);
+    % mskZ = reshape(mskZ,(ydim+2),1+nBas);
+    % for n=1:min(6,nout)
+    %     var=HT(:,:,n); 
+    %     var=reshape(var,(ydim+2)*(1+nBas),1);  var(I) = NaN;
+    %     var=reshape(var,(ydim+2),1+nBas);  HT(:,:,n) = var;
+    % end
+
+    % Horizontal divergence of vertically integrated heat transport.  Recall
+    % that "utz1" and "vtz1" are the vertically integrated Eulerian sensible
+    % heat transport through edges.
+    uT = reshape(utz1,6*nc,nc);
+    vT = reshape(vtz1,6*nc,nc);
+    [u6t,v6t] = split_UV_cub(uT,vT);
+    temp = u6t([2:nc+1],:,:) - u6t([1:nc],:,:) + ...
+           v6t(:,[2:nc+1],:) - v6t(:,[1:nc],:);
+    divHfx(:,:,it) =    reshape(permute(temp,[1 3 2]),[6*nc,nc]) ...
+                     ./ reshape(ac,[6*nc,nc]);
+    % ??? What exactly are you measuring with this metric?  Is this not
+    %     supposed to give the implied surface heat flux?  The result looks
+    %     slightly off; both for the scale and for the corners.
+    % merccube(xg,yg,divHfx); colorbar;
+
+    % Implied surface heat flux from heat transports (implied heating).
+    mskG = reshape(mskG,(ydim+1)*(1+nBas),1);
+    I = find(mskG==0);
+    mskG = reshape(mskG,ydim+1,1+nBas);
+    var = zeros(ydim+1,1+nBas); 
+    for n=1:min(nout,6),
+        var =   HT([2:ydim+2],:,it,n) ...
+              - HT([1:ydim+1],:,it,n);
+        var = reshape(var,(ydim+1)*(1+nBas),1); var(I)=NaN; 
+        var = reshape(var,ydim+1,1+nBas);
+        SF([1:ydim+1],:,it,n) = var./AreaZon;
+    end
+    
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Assign outputs                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+HT = HT*1e-15;
+
+HT_Out.time = time;
+HT_Out.HT = HT;
+HT_Out.SF = SF;
+HT_Out.ylatHT = ylatHT;
+HT_Out.ylatSF = ylatSF;
+HT_Out.AreaZon = AreaZon;
+
+try HT_Out.HTbyEulerCirc                = HT(:,:,:,1); catch, end
+try HT_Out.HTbyMeanCirc                 = HT(:,:,:,2); catch, end
+try HT_Out.HTbyEulerCircMinusMeanCirc   = HT(:,:,:,3); catch, end
+try HT_Out.HTbyZonMeanCirc              = HT(:,:,:,4); catch, end
+try HT_Out.HTbyMeanCircMinusZonMeanCirc = HT(:,:,:,5); catch, end
+try HT_Out.HTbyHorizDiff                = HT(:,:,:,6); catch, end
+% HT_Out.HTbyIntegSurfHeatFlux = HT(:,:,7);
+
+try HT_Out.SFbyEulerCirc                = SF(:,:,:,1); catch, end
+try HT_Out.SFbyMeanCirc                 = SF(:,:,:,2); catch, end
+try HT_Out.SFbyEulerCircMinusMeanCirc   = SF(:,:,:,3); catch, end
+try HT_Out.SFbyZonMeanCirc              = SF(:,:,:,4); catch, end
+try HT_Out.SFbyMeanCircMinusZonMeanCirc = SF(:,:,:,5); catch, end
+try HT_Out.SFbyHorizDiff                = SF(:,:,:,6); catch, end
+% HT_Out.SFbyIntegSurfHeatFlux = SF(:,7);
+
+% Output other fields?  utz0,vtz0,divHfx
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%       Stuff that might need to be added back in later (for basins)      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Block for constructing mskG for different basins:
+% if nBas > 0,
+%     mskBc=rdda([Rac,'maskC_bas.bin'],[6*nc*nc 3],1,'real*4','b');
+%     mskBw=rdda([Rac,'maskW_bas.bin'],[6*nc*nc 3],1,'real*4','b');
+%     mskBs=rdda([Rac,'maskS_bas.bin'],[6*nc*nc 3],1,'real*4','b');
+%     if nBas==2,
+%         mskBc(:,2)=mskBc(:,2)+mskBc(:,3);
+%         mskBw(:,2)=mskBw(:,2)+mskBw(:,3);
+%         mskBs(:,2)=mskBs(:,2)+mskBs(:,3);
+%         mskBc=min(1,mskBc); mskBw=min(1,mskBw); mskBs=min(1,mskBs);
+%     end
+%     %- load: np_Sep, ij_Sep, tp_Sep:
+%     sep_lineF=[Rac,'sepBas_cs32_60'];
+%     load(sep_lineF);
+%     fprintf([' + bassin mask & Sep.line:',sep_lineF,' \n']);
+%     %- compute mask for each bassin: -----------
+%     kMsep=1;
+%     if kMsep,
+%         mskW=1+min(1,ceil(hw(:,1)));
+%         mskS=1+min(1,ceil(hs(:,1)));
+%         for b=1:nBas,
+%             bs=b; b1=1+bs; b2=2+rem(bs,nBas);
+%             if nBas == 2, bs=b+b-1; b1=2; b2=3 ; end
+%             for j=1:ydim+1,
+%                 for i=1:np_Sep(bs,j),
+%                     ij=ij_Sep(bs,j,i); typ=abs(tp_Sep(bs,j,i));
+%                     if typ == 1,
+%                         mskG(j,b1)=mskG(j,b1)*mskW(ij);
+%                         mskG(j,b2)=mskG(j,b2)*mskW(ij);
+%                     elseif typ == 2,
+%                         mskG(j,b1)=mskG(j,b1)*mskS(ij);
+%                         mskG(j,b2)=mskG(j,b2)*mskS(ij);
+%                     end
+%                 end
+%             end
+%         end
+%         mskG=2-min(2,mskG); 
+%     end
+% end
+
+% Makes sure that there are no zeros in area AreaZon:
+% - set AreaZon to 1 if = zero
+% AreaZon=reshape(AreaZon,(ydim+1)*(1+nBas),1);
+% [I]=find(AreaZon==0); AreaZon(I)=1; 
+% AreaZon=reshape(AreaZon,ydim+1,1+nBas);
+
+% if kgr >= 3 & nBas > 1
+%     figure(nf+2);clf;
+%     subplot(211); yax=ylat2;
+%     plot(yax,mskZ(:,1),'r-',yax,mskZ(:,nb),'b-'); 
+%     % hold on; plot(yax,mskZ(:,3),'k-'); hold off
+%     grid; axis([-90 90 -.2 1.2]);
+%     legend('Global',['bas=',int2str(nb)]);
+%     title('Mask for Tranp.Heat')
+%     subplot(212); yax=ylat1;
+%     plot(yax,mskG(:,1),'r-',yax,mskG(:,nb),'b-');
+%     % hold on; plot(yax,mskG(:,3),'k-'); hold off
+%     grid; axis([-90 90 -.2 1.2]);
+%     legend('Global',['bas=',int2str(nb)]);
+%     title('Mask for Implied Heat Forcing')
+% end
+
+% if kgr >= 2
+%     figure(nf+1);clf;
+%     psi=zeros(ydim,nr+1);
+%     for k=nr:-1:1, psi(:,k)=psi(:,k+1)-IntV(:,k);end
+%     subplot(211); imagesc(ylat,[1:nr+1],psi');colorbar
+%     subplot(212); plot(ylat,psi(:,1),'k-'); grid;
+% end
---
+++ utils/matlab/cs_grid/calcHeatTransFluxes.m
@@ -0,0 +1,202 @@
+function HT = calcHeatTransFluxes(od,ad,sd,og,ag,time,LhVap,aHeat,oHeat,blkFile);
+
+% HT = CalcHT_Fluxes(od,ad,sd,og,ag,time,LhVap,aHeat,oHeat,blkFile);
+% 
+% Calculate heat transport from fluxes. Using a heat content budget
+% equation that equates the change in heat content of a zonal band with the
+% fluxes in and out of the top and bottom, find the poleward heat transport
+% by integrating from the north pole (HT = 0) southward.  The adjustment to
+% the flux fields required for the heat transport at the south pole to be
+% 0 is also tabulated.  In the atmosphere this adjustment is associated
+% with the frictional damping not coded to increase the temperature.
+%
+% This function is built to be used with cubed-sphere output from the
+% Aim-ocean coupled model of the MITgcm.  The zonal bands are defined by a
+% set of broken lines, the information for which is contain in the broken
+% line file "blkFile".
+%
+% Input:
+%   The incoming data must be in a structured array format (which is the
+%   format that comes from rdmnc).  The variables must have the following
+%   fields:
+%       od  [Ocean tave data]  T,Ttave
+%       ad  [Atmos tave data]  T,Ttave
+%       sd  [Aim tave data  ]  T,TSRtave,OLRtave,SSRtave,SLRtave,
+%                                EVAPtave,SHFtave,EnFxPrtave
+%       og  [Ocean grid data]  drF,HFacC,rA
+%       ag  [Atmos grid data]  drF,HFacC
+%
+%   Further, "time" is the model time levels over which the heat transport
+%   calcualtions are to be made (minimum of 3), use [] for all.  "LhVap" is
+%   the latent heat of vaporization, "aHeat" is Cp/g for the atmosphere and
+%   "oHeat" is Cp*Rho for the ocean.
+%
+% Output:
+%   HT is a structured array with the oceanic, atmpspheric, and total heat
+%   transport at time(2:end-1) in PW, the calculated adjustments, heat
+%   content (and its change) in the bands, and appropriate axis
+%   information.
+%
+% Usage:
+% 
+%   >> og = rdmnc('/GridOcn/grid.*');
+%   >> ag = rdmnc('/GridAtm/grid.*');
+%   >> od = rdmnc('/DataOcn/tave.*');
+%   >> ad = rdmnc('/DataAtm/tave.*');
+%   >> sd = rdmnc('/DataAtm/aim_tave.*');
+%   
+%   >> LhVap = 2500;
+%   >> aHeat = 1004./9.81;  % aCp/g
+%   >> oHeat = 3994.*1035;  % oCp*oRho
+%   
+%   >> HT = CalcHT_Fluxes(od,ad,sd,og,ag,[],LhVap,aHeat,oHeat,blkFile)
+%
+%   >> plot(HT.ylatHT,HT.HTocn(:,1),'g',...
+%           HT.ylatHT,HT.HTatm(:,1),'b',...
+%           HT.ylatHT,HT.HTtot(:,1),'r'); grid on;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                     Prepare / reform  incoming data                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Determine time indecies.
+if isequal(od.T,ad.T) & isequal(od.T,sd.T)
+    if isempty(time), time = od.T; i_time = 1:length(time);  
+    else [dump,i_time] = ismember(time,od.T); end
+else
+    error('Time axes must match for ocean, atmospheric, and aim data!');
+end
+
+% Grid information.
+nt = length(time);
+nc = size(og.HFacC,2);
+onr = size(og.HFacC,3);
+anr = size(ag.HFacC,3);
+
+odrf = og.drF;
+adrf = ag.drF;
+ac  = reshape(og.rA ,[6*nc*nc,1]);
+ohc = reshape(og.HFacC(1:6*nc,1:nc,1:onr),[6*nc*nc,onr]);
+ahc = reshape(ag.HFacC(1:6*nc,1:nc,1:anr),[6*nc*nc,anr]);
+
+% Temperature fields.
+ot = reshape(od.Ttave(1:6*nc,1:nc,1:onr,i_time),[6*nc*nc,onr,nt]);
+at = reshape(ad.Ttave(1:6*nc,1:nc,1:anr,i_time),[6*nc*nc,anr,nt]);
+
+% Aim fields.
+toaSW  = reshape(   sd.TSRtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+toaLW  = reshape(   sd.OLRtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+srfSW  = reshape(   sd.SSRtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+srfLW  = reshape(   sd.SLRtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+srfEvp = reshape(  sd.EVAPtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+srfSH  = reshape(   sd.SHFtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+srfEFP = reshape(sd.EnFxPrtave(1:6*nc,1:nc,i_time),[6*nc*nc,nt]);
+
+% Load broken line information.  Determine latitude axis of a broken line
+% by the mean of the latitude of the edges along the broken line.  These
+% are the latitude coordinates for the poleward heat transport calculations
+% and the surface flux / heat contents values are the mean between two
+% broken lines.
+load(blkFile);
+ydim=length(bkl_Ylat);
+YlatAv=sum(bkl_Ysg,1)./(1+bkl_Npts');
+ylatHT = [-90,YlatAv,90];
+ylatSF = ( ylatHT(2:ydim+2) + ylatHT(1:ydim+1) )./2;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                     Make heat transport calculations                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Top of atmosphere and surface fluxes.
+% ??? Evaporation/precipitation correctly tabulated?  The energy leaving a
+%     band (between broken lines) is given by the evaporation (times the
+%     latent heat of vaporization), but is all the incoming energy given by
+%     the energy flux associated with precipitation?  What about runoff or
+%     land processes?
+Ft = toaSW-toaLW;
+Fs = srfSW-srfLW-LhVap.*srfEvp-srfSH+srfEFP;
+
+% Loop over bands and calculate (per band) (1) surface area, (2) top of
+% atmpsphere and surface fluxes, and (3) oceanic and atmospheric heat
+% content.  There are nbkl broken lines and nbkl+1 bands between broken
+% lines.  The variable "bkl_Zon" gives the band number (nbkl+1 total) for a
+% given index between 0 and nbkl, that is, nbkl+1 total zones.
+AreaZon = NaN.*zeros([ydim+1,1]);
+Ft_Zon = NaN.*zeros([ydim+1,nt]);
+Fs_Zon = NaN.*zeros([ydim+1,nt]);
+HA = NaN.*zeros([ydim+1,nt]);
+HO = NaN.*zeros([ydim+1,nt]);
+dHAdt = NaN.*zeros([ydim+1,nt-2]);
+dHOdt = NaN.*zeros([ydim+1,nt-2]);
+for izon = 1:ydim+1
+    ii = find(bkl_Zon == izon-1);
+    AreaZon(izon,1) = sum(ac(ii));
+    for it = 1:length(time)
+        % ??? HFacC for surface fluxes? How to deal with runoff / land?
+        Ft_Zon(izon,it) = sum(Ft(ii,it).*ac(ii));
+        Fs_Zon(izon,it) = sum(Fs(ii,it).*ac(ii));
+        HA(izon,it) = sum(aHeat.*((at(ii,:,it).*ahc(ii,:))*adrf).*ac(ii));
+        HO(izon,it) = sum(oHeat.*((ot(ii,:,it).*ohc(ii,:))*odrf).*ac(ii));
+    end
+end
+
+% Calculate change in heat content (this means that the heat transport is
+% calculated for time(2:end-1) only, hence subsetting fluxes as well).
+for it = 2:length(time)-1
+    dHAdt(:,it-1) = (HA(:,it+1)-HA(:,it-1))./(time(it+1)-time(it-1));
+    dHOdt(:,it-1) = (HO(:,it+1)-HO(:,it-1))./(time(it+1)-time(it-1));
+end
+Ft_Zon = Ft_Zon(:,2:end-1);
+Fs_Zon = Fs_Zon(:,2:end-1);
+
+% Compute offsets.
+% ??? However small, why is there an oceanic offset?
+ocnadj_Zon = Fs_Zon - dHOdt;
+atmadj_Zon = Ft_Zon - Fs_Zon - dHAdt;
+for it = 1:size(ocnadj_Zon,2)
+    ocnadj(it) = sum(ocnadj_Zon(:,it))./sum(AreaZon);
+    atmadj(it) = sum(atmadj_Zon(:,it))./sum(AreaZon);
+end
+
+% Using a heat content budget equation that equates the change in heat
+% content of a band with the fluxes in and out of the top and bottom, find
+% the poleward heat transport by integrating from the north pole (HT = 0)
+% southward.  The above computed adjustment is required for the heat
+% transport at the south pole to be 0.
+HTocn = zeros(size(Ft_Zon)+[1,0]);
+HTatm = zeros(size(Ft_Zon)+[1,0]);
+for ilat = ydim+2:-1:2
+    HTocn(ilat-1,:) =   HTocn(ilat,:)    ...
+                      - Fs_Zon(ilat-1,:) ...
+                      + dHOdt(ilat-1,:)  ...
+                      + ocnadj.*AreaZon(ilat-1);
+    HTatm(ilat-1,:) =   HTatm(ilat,:)    ...
+                      - Ft_Zon(ilat-1,:) ...
+                      + Fs_Zon(ilat-1,:) ...
+                      + dHAdt(ilat-1,:)  ...
+                      + atmadj.*AreaZon(ilat-1);
+end
+HTtot = HTocn + HTatm;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Assign outputs                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+HT.time = time(2:end-1);
+HT.ylatSF = ylatSF;
+HT.ylatHT = ylatHT;
+HT.AreaZon = AreaZon;
+HT.Fs = Fs_Zon;
+HT.Ft = Ft_Zon;
+HT.HTtot = HTtot*1e-15;
+HT.HTocn = HTocn*1e-15;
+HT.HTatm = HTatm*1e-15;
+HT.HO = HO(:,2:end-1);
+HT.HA = HA(:,2:end-1);
+HT.dHOdt = dHOdt;
+HT.dHAdt = dHAdt;
+HT.ocnadj = ocnadj;
+HT.atmadj = atmadj;
