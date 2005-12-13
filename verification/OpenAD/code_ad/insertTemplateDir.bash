d42ae0c70d34efd8ac841d6d3f864683934b59f5
Jean Utke <jutke@allstate.com>
2005-12-13 17:35:06 +0000

    take care of plain pass through for no checkpoints at a certain level

--- verification/OpenAD/code_ad/insertTemplateDir.bash
+++ verification/OpenAD/code_ad/insertTemplateDir.bash
@@ -9,7 +9,12 @@ do
   then 
     echo "c\$openad XXX Template ../code_ad/ad_template.checkpoint.f" >> temp.sed
   else
-    echo "c\$openad XXX Template ../code_ad/ad_template.split.f" >> temp.sed
+    if [ "$srName" = "the_first_level_plain" -o "$srName" = "the_second_level_plain" -o "$srName" = "the_third_level_plain" -o "$srName" = "the_fourth_level_plain" ] 
+    then 
+      echo "c\$openad XXX Template ../code_ad/ad_template.plain.f" >> temp.sed
+    else 
+      echo "c\$openad XXX Template ../code_ad/ad_template.split.f" >> temp.sed
+    fi
   fi
 done
 cat $1 | sed -f temp.sed > $2
