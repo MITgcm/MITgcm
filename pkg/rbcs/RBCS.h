1830fa7b80bf22b7119566dd801eff446868b19b
Stephanie Dutkiewicz <stephd@mit.edu>
2005-12-19 19:14:34 +0000

    o changes to rbcs to include T/S relaxing, and to have different   tau's and different relaxing masks for T/S/PTRACERS

--- pkg/rbcs/RBCS.h
+++ pkg/rbcs/RBCS.h
@@ -5,35 +5,41 @@ C    !ROUTINE: RBCS.h
 C    !INTERFACE:
 
 C    !DESCRIPTION:
-C Contains tracer parameters and input files for relaxed boundary
-C conditions
+C Contains tracer parameters and input files for 3-D relaxation
 C Some of these can be read in from data.rbcs
+
+c number of mask to read
+      INTEGER maskLEN
+      PARAMETER(maskLEN = 3 )
  
-      _RL RBC_mask(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL RBC_mask(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy,maskLEN)
       _RL RBCtemp(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL RBCsalt(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL  tauRelax
+      _RL tauRelaxT
+      _RL tauRelaxS
       _RL rbcsForcingPeriod
       _RL rbcsForcingCycle
       INTEGER rbcsIniter
       LOGICAL useRBCtemp
       LOGICAL useRBCsalt
       LOGICAL useRBCptracers
-      CHARACTER*(MAX_LEN_FNAM) relaxBoundaryFile
+      CHARACTER*(MAX_LEN_FNAM) relaxMaskFile(maskLEN)
       CHARACTER*(MAX_LEN_FNAM) relaxTFile
       CHARACTER*(MAX_LEN_FNAM) relaxSFile
 #ifdef ALLOW_PTRACERS
       LOGICAL useRBCptrnum(PTRACERS_num)
       _RL RBC_ptracers(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy,
      &              PTRACERS_num)
+      _RL tauRelaxPTR(PTRACERS_num)
       CHARACTER*(MAX_LEN_FNAM) relaxPtracerFile(PTRACERS_num)
 #endif
 
       
 
       COMMON /RBCS_PARM01/
-     &          tauRelax,
-     &          relaxBoundaryFile,
+     &          tauRelaxT,
+     &          tauRelaxS,
+     &          relaxMaskFile,
      &          relaxTFile,
      &          relaxSFile,
      &          rbcsIniter,
@@ -49,6 +55,7 @@ C Some of these can be read in from data.rbcs
 #ifdef ALLOW_PTRACERS
       COMMON /RBCS_PARM02/
      &          useRBCptrnum,
+     &          tauRelaxPTR,
      &          relaxPtracerFile,
      &          RBC_ptracers
 #endif
