4a8ea014e357a5e5fc3735ad92be5a815782dfb0
Constantinos Evangelinos <constantinos.evangelinos@gmail.com>
2005-12-22 00:36:51 +0000

    Introduce runtime floating point control into MITgcm using the GSL. main.F calls fgsl_ieee_env_setup (), a wrapper around gsl_ieee_env_setup () from the GNU Scientific Library which reads in the environment variable GSL_IEEE_MODE and sets the environment accordingly. For more information look at: http://www.gnu.org/software/gsl/manual/gsl-ref_39.html#SEC515 Please read this carefully to avoid running into trouble with exceptions due to denormals and underflows. gsl_ieee_env.c provides fgsl_ieee_env_setup () in multiple forms that should be recognized by any fortran compiler. Currently needs to have declared GSLINC and GSLLIB in the optfile for the include and library linking strings needed to use GSL. For example on most Linux boxen GSLINC='' GSLLIB='-lgsl -lgslcblas' as the required files exist in /usr/include and /usr/lib.

---
+++ eesupp/src/gsl_ieee_env.c
@@ -0,0 +1,29 @@
+/* 
+ * A cross-platform way of providing access to gsl_ieee_env_setup 
+ * from Fortran on any system MITgcm runs on currently.
+ */
+
+#ifdef USE_GSL_IEEE
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_ieee_utils.h>
+
+void fgsl_ieee_env_setup ()
+{
+  gsl_ieee_env_setup ();
+}
+
+void fgsl_ieee_env_setup_ ()
+{
+  gsl_ieee_env_setup ();
+}
+
+void fgsl_ieee_env_setup__ ()
+{
+  gsl_ieee_env_setup ();
+}
+
+void FGSL_IEEE_ENV_SETUP ()
+{
+  gsl_ieee_env_setup ();
+}
+#endif
