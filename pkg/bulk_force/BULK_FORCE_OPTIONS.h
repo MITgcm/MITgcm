88ac963ab897a7180d9d03c078fc1b27b971570b
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-01-22 15:50:37 +0000

    - add option and parameters to use AIM surface-flux formulae.

--- pkg/bulk_force/BULK_FORCE_OPTIONS.h
+++ pkg/bulk_force/BULK_FORCE_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.3 2003/10/22 18:38:48 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.4 2006/01/22 15:50:37 jmc Exp $
 C $Name:  $
 
 #ifndef BULK_FORCE_OPTIONS_H
@@ -11,6 +11,10 @@ C $Name:  $
 C CPP Macros go here
 
 #undef CONSERV_BULKF
+
+C allow to use of AIM surface flux formulation (S/R BULKF_FORMULA_AIM)
+C rather than the default (S/R BULKF_FORMULA_LANL)
+#undef ALLOW_FORMULA_AIM
 
 #endif /* ALLOW_BULK_FORCE */
 #endif /* BULK_FORCE_OPTIONS_H */
