02e1437ea23b356c408fa6d699bd262c7b951244
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-01-24 02:59:47 +0000

    change names ("set" replace "level" for the region-mask array) that  were confusing

--- pkg/diagnostics/DIAGSTATS_REGIONS.h
+++ pkg/diagnostics/DIAGSTATS_REGIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGSTATS_REGIONS.h,v 1.1 2006/01/23 22:24:28 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGSTATS_REGIONS.h,v 1.2 2006/01/24 02:59:47 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -18,7 +18,7 @@ CEOP
 
 C     DIAG_STATS_REGMASK common block:
 C     diagSt_regMask :: region mask
-C     diagSt_kRegMsk :: region-mask level that identifies the region
+C     diagSt_kRegMsk :: region-mask set-index that identifies the region
 C     diagSt_vRegMsk :: region identificator value in the region mask
 C     (point i,j belong to region "n" <=> regMask(i,j,kRegMsk(n)) = vRegMsk(n))
 
@@ -31,12 +31,12 @@ c     _RS       diagSt_regMask(1-OLx:sNx+Olx,1-Oly:sNy+Oly,1,nSx,nSy)
 #endif /* DIAGSTATS_REGION_MASK */
       _RS       diagSt_vRegMsk(0:nRegions)
       INTEGER   diagSt_kRegMsk(0:nRegions)
-      INTEGER   nLevRegMskFile, nLevRegMask
+      INTEGER   nSetRegMskFile, nSetRegMask
       CHARACTER*(MAX_LEN_FNAM) diagSt_regMaskFile
       COMMON / DIAG_STATS_REGMASK /
      &     diagSt_regMask,
      &     diagSt_vRegMsk,
-     &     diagSt_kRegMsk, nLevRegMask,
-     &     nLevRegMskFile, diagSt_regMaskFile
+     &     diagSt_kRegMsk, nSetRegMask,
+     &     nSetRegMskFile, diagSt_regMaskFile
 
 C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
