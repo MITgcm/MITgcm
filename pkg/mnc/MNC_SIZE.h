6f79426b6104ead9f85fe516ae36e01b9a6b17c2
Ed Hill <ed@eh3.com>
2006-03-10 22:40:08 +0000

    with this change, the largest mnc common block is now smaller than the   largest diagnostics common block ("nm -S --size-sort ./mitgcmuv") on   some of the verification tests that use both mnc and diagnostics

--- pkg/mnc/MNC_SIZE.h
+++ pkg/mnc/MNC_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mnc/MNC_SIZE.h,v 1.5 2006/03/10 22:01:53 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mnc/MNC_SIZE.h,v 1.6 2006/03/10 22:40:08 edhill Exp $
 C $Name:  $
 C
 
@@ -10,7 +10,7 @@ C
       parameter ( MNC_MAX_CHAR =     40 )
       parameter ( MNC_MAX_PATH =    500 )
       parameter ( MNC_MAX_INFO =    800 )
-      parameter ( MNC_CW_MAX_I =    100 )
+      parameter ( MNC_CW_MAX_I =     20 )
       parameter ( MNC_CW_CVDAT =  50000 )
 
 CEH3 ;;; Local Variables: ***
