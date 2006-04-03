c7701e7f17149564629da755cbdb50fd442dc7ac
Andrea Molod <andrea.molod@nasa.gov>
2006-04-03 21:17:29 +0000

    Continuing to remove experiment

--- verification/fizhi-cs-aqualev10/code/fizhi_SIZE.h
+++
@@ -1,7 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-aqualev10/code/Attic/fizhi_SIZE.h,v 1.2 2005/12/01 19:01:06 molod Exp $
-C $Name:  $
-
-c Physics Grid Vertical Dimension
-c -------------------------------
-      integer Nrphys
-      parameter (Nrphys = 21)
