abbe9a44240eb1519c3229caa084eb7a21dcd148
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-06-22 14:11:57 +0000

    define "ALLOW_FORMULA_AIM" to check that bulkf_formula_aim.F compile

--- pkg/bulk_force/BULK_FORCE_OPTIONS.h
+++ pkg/bulk_force/BULK_FORCE_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.4 2006/01/22 15:50:37 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.5 2006/06/22 14:11:57 jmc Exp $
 C $Name:  $
 
 #ifndef BULK_FORCE_OPTIONS_H
@@ -14,7 +14,7 @@ C CPP Macros go here
 
 C allow to use of AIM surface flux formulation (S/R BULKF_FORMULA_AIM)
 C rather than the default (S/R BULKF_FORMULA_LANL)
-#undef ALLOW_FORMULA_AIM
+#define ALLOW_FORMULA_AIM
 
 #endif /* ALLOW_BULK_FORCE */
 #endif /* BULK_FORCE_OPTIONS_H */
