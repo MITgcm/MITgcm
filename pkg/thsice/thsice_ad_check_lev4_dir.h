1ea691a5fd4296b23a14b462f465e922d48ac7c9
Patrick Heimbach <heimbach@mit.edu>
2006-07-11 23:54:19 +0000

    Add lev4 storing

---
+++ pkg/thsice/thsice_ad_check_lev4_dir.h
@@ -0,0 +1,14 @@
+CADJ STORE iceMask = tapelev4, key = ilev_4
+CADJ STORE iceHeight  = tapelev4, key = ilev_4
+CADJ STORE snowHeight = tapelev4, key = ilev_4
+CADJ STORE Tsrf    = tapelev4, key = ilev_4
+CADJ STORE Qice1   = tapelev4, key = ilev_4
+CADJ STORE Qice2   = tapelev4, key = ilev_4
+CADJ STORE snowAge = tapelev4, key = ilev_4
+CADJ STORE sHeating = tapelev4, key = ilev_4
+CADJ STORE flxCndBt = tapelev4, key = ilev_4
+CADJ STORE hOceMxL = tapelev4, key = ilev_4
+
+#ifdef ATMOSPHERIC_LOADING
+CADJ STORE siceload = tapelev4, key = ilev_4
+#endif
