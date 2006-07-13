c8c53c6f0cbf0452c1a975c91b530a39a51258fe
Patrick Heimbach <heimbach@mit.edu>
2006-07-13 19:35:46 +0000

    Copying over from contrib to prepare verif.

---
+++ verification/OpenAD/code_ad/CPP_OPTIONS.h
@@ -0,0 +1,44 @@
+C
+C $Header: /u/gcmpack/MITgcm/verification/OpenAD/code_ad/CPP_OPTIONS.h,v 1.4 2006/07/13 19:35:46 heimbach Exp $
+C $Name:  $
+
+C CPP flags controlling which code in included in the files that
+C will be compiled.
+
+#ifndef CPP_OPTIONS_H
+#define CPP_OPTIONS_H
+
+#include "PACKAGES_CONFIG.h"
+
+C o Run as shallow water model
+#define SINGLE_LAYER_MODE
+
+#ifdef ALLOW_KPP
+#define  SHORTWAVE_HEATING
+#endif
+
+C o Include/exclude phi_hyd calculation code
+#define INCLUDE_PHIHYD_CALCULATION_CODE
+
+C o Include/exclude call to S/R CONVECT
+#undef INCLUDE_CONVECT_CALL
+
+C o Include/exclude call to S/R CALC_DIFFUSIVITY
+#undef INCLUDE_CALC_DIFFUSIVITY_CALL
+
+C o Allow nonHydrostatic code
+#undef  ALLOW_NONHYDROSTATIC
+
+C o Exact volume conservation
+#undef EXACT_CONSERV
+
+C o Execution environment support options
+#include "CPP_EEOPTIONS.h"
+
+#ifdef ALLOW_AUTODIFF
+# include "ECCO_CPPOPTIONS.h"
+#endif
+
+#endif /* CPP_OPTIONS_H */
+
+
