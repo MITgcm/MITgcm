2e86c3634cda45c4b71eda3ae754d1e05ab76ed5
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-08-12 20:27:18 +0000

    files moved to verification/tutorial_global_oce_latlon/diags_matlab

--- verification/global_ocean.90x40x15/diags_matlab/mit_globalmean.m
+++
@@ -1,9 +0,0 @@
-function [mdt, mdtt, mdtstd] = mit_globalmean(dt,rac3d)
-% function [mdt, mdtt, mdtstd] = mit_globalmean(dt,rac3d)
-  
-  denom = nansum(nansum(rac3d));
-  mdt    = squeeze(nansum(nansum(dt.*rac3d))./denom);
-  mdtt   = squeeze(nansum(nansum(abs(dt).*rac3d))./denom);
-  mdtstd = sqrt(squeeze(nansum(nansum(dt.^2.*rac3d))./denom));
-
-  return
