ea4d09597a57825b270ff13294edb11f8897cbb0
Gael Forget <gforget@mit.edu>
2006-10-25 01:15:55 +0000

    improve pkg/profiles namelist structure

--- pkg/profiles/profiles.h
+++ pkg/profiles/profiles.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/profiles/profiles.h,v 1.4 2006/07/14 22:12:23 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/profiles/profiles.h,v 1.5 2006/10/25 01:15:54 gforget Exp $
 C $Name:  $
 
 C============================================================
@@ -8,6 +8,8 @@ C============================================================
       PARAMETER ( NOBSGLOB = 100000  )
       INTEGER NFILESPROFMAX
       PARAMETER ( NFILESPROFMAX=10 )
+      INTEGER NVARMAX
+      PARAMETER ( NVARMAX=6 )
       INTEGER NLEVELMAX
       PARAMETER ( NLEVELMAX=100 )
 
@@ -24,45 +26,49 @@ C===========================================================
       integer profNo(NFILESPROFMAX,nsx,nsy)
       integer profDepthNo(NFILESPROFMAX,nsx,nsy)
 
-      logical vec_quantities(NFILESPROFMAX,6,nsx,nsy)
+      logical vec_quantities(NFILESPROFMAX,NVARMAX,nsx,nsy)
       integer fidforward(NFILESPROFMAX,nsx,nsy), 
      & fidadjoint(NFILESPROFMAX,nsx,nsy), 
      & fidtangent(NFILESPROFMAX,nsx,nsy)
       integer fiddata(NFILESPROFMAX,nsx,nsy)
-      character*(8) prof_names(6)
-      character*(12) prof_namesmask(6)
-      character*(14) prof_namesweight(6)
+      character*(8) prof_names(NVARMAX)
+      character*(12) prof_namesmask(NVARMAX)
+      character*(14) prof_namesweight(NVARMAX)
 
-      _RL profiles_data_buff(NLEVELMAX,1000,6,nsx,nsy)
-      _RL profiles_weight_buff(NLEVELMAX,1000,6,nsx,nsy)
+      _RL profiles_data_buff(NLEVELMAX,1000,NVARMAX,nsx,nsy)
+      _RL profiles_weight_buff(NLEVELMAX,1000,NVARMAX,nsx,nsy)
       integer profiles_minind_buff(nsx,nsy)
       integer profiles_maxind_buff(nsx,nsy)
       integer profiles_curfile_buff(nsx,nsy)
 
       integer profilesfile_equi_type
       integer prof_num_var_tot(NFILESPROFMAX,nsx,nsy)
-      integer prof_num_var_cur(NFILESPROFMAX,6,nsx,nsy)
+      integer prof_num_var_cur(NFILESPROFMAX,NVARMAX,nsx,nsy)
 
 C===========================================================
 C Common Blocks
 C===========================================================
-      COMMON /profiles_common/ prof_time,prof_lon,prof_lat,
-     &prof_ind_glob,profNo,profDepthNo, prof_depth,
-     &prof_names,prof_namesmask,prof_namesweight,
-     &vec_quantities, fidforward, fidadjoint, fidtangent,fiddata,
-     & prof_mask1D_cur, profilesfile_equi_type,
-     & prof_num_var_tot,prof_num_var_cur
-      COMMON /profiles_buff/ profiles_data_buff, profiles_weight_buff,
-     & profiles_minind_buff,profiles_maxind_buff,profiles_curfile_buff
+
+      COMMON /profiles_r/ prof_time, prof_lon, prof_lat,
+     & prof_depth, prof_mask1D_cur 
+      COMMON /profiles_i/ prof_ind_glob, profNo, profDepthNo,
+     & fidforward, fidadjoint, fidtangent, fiddata,
+     & prof_num_var_tot, prof_num_var_cur, profilesfile_equi_type
+      COMMON /profiles_l/ vec_quantities
+      COMMON /profiles_c/ prof_names, prof_namesmask, prof_namesweight
+
+      COMMON /profiles_buff_r/ profiles_data_buff, profiles_weight_buff
+      COMMON /profiles_buff_i/
+     & profiles_minind_buff, profiles_maxind_buff, profiles_curfile_buff
 
      
       COMMON /profiles_cost_r/
      &                objf_profiles,
      &                num_profiles,
      &                mult_profiles
-      _RL  objf_profiles(50,10,nsx,nsy)
-      _RL  num_profiles(50,10,nsx,nsy)
-      _RL  mult_profiles(50,10)
+      _RL  objf_profiles(NFILESPROFMAX,NVARMAX,nsx,nsy)
+      _RL  num_profiles(NFILESPROFMAX,NVARMAX,nsx,nsy)
+      _RL  mult_profiles(NFILESPROFMAX,NVARMAX)
 
       COMMON /profiles_cost_c/
      &                     profilesfiles
@@ -70,4 +76,6 @@ C===========================================================
 
       COMMON /profiles_ctrl_dummy/
      &                profiles_dummy
-      _RL profiles_dummy(50,10,nsx,nsy)
+      _RL profiles_dummy(NFILESPROFMAX,NVARMAX,nsx,nsy)
+
+
