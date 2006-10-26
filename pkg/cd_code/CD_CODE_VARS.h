958248ce20ab6a855ce3b6f23e4d3e71848a0692
Jean-Michel Campin <jmc@ocean.mit.edu>
2006-10-26 00:24:41 +0000

    - call pkg/rw S/R (rather than directly MDSIO) - all threads call I/0 S/R (needed for singleCPUIO to work in multi-threaded)

--- pkg/cd_code/CD_CODE_VARS.h
+++ pkg/cd_code/CD_CODE_VARS.h
@@ -1,18 +1,18 @@
-C $Header: /u/gcmpack/MITgcm/pkg/cd_code/CD_CODE_VARS.h,v 1.2 2003/10/30 12:00:41 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/cd_code/CD_CODE_VARS.h,v 1.3 2006/10/26 00:24:40 jmc Exp $
 C $Name:  $
 
-C     uVelD  - D grid zonal velocity
-C     vVelD  - D grid meridional velocity
+C     uVelD  :: D grid zonal velocity
+C     vVelD  :: D grid meridional velocity
 
 #ifdef ALLOW_CD_CODE
-      COMMON /DYNVARS_CD/ 
+      COMMON /DYNVARS_CD/
      &                   uVelD, vVelD,
      &                   etaNm1,
      &                   uNM1,  vNM1
-      _RL  uVeld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL  vVeld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  uVelD (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  vVelD (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  etaNm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL  uNm1  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL  vNm1  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  uNM1  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  vNM1  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 #endif /*  ALLOW_CD_CODE  */
 
