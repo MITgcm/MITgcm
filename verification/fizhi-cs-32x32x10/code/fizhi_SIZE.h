75db304b28c806567c9aa11a844960f873d2e3d4
Andrea Molod <andrea.molod@nasa.gov>
2006-11-27 17:30:31 +0000

    New size for new grid

--- verification/fizhi-cs-32x32x10/code/fizhi_SIZE.h
+++ verification/fizhi-cs-32x32x10/code/fizhi_SIZE.h
@@ -1,7 +1,7 @@
-C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-32x32x10/code/Attic/fizhi_SIZE.h,v 1.1 2005/07/13 15:16:59 molod Exp $
+C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-32x32x10/code/Attic/fizhi_SIZE.h,v 1.2 2006/11/27 17:30:31 molod Exp $
 C $Name:  $
 
 c Physics Grid Vertical Dimension
 c -------------------------------
       integer Nrphys
-      parameter (Nrphys = 20)
+      parameter (Nrphys = 21)
