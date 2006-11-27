35bc0de71906cacdeb04616458f7fd9f56bb9178
Andrea Molod <andrea.molod@nasa.gov>
2006-11-27 17:21:58 +0000

    New vertical grid size

--- verification/fizhi-cs-aqualev20/code/fizhi_SIZE.h
+++ verification/fizhi-cs-aqualev20/code/fizhi_SIZE.h
@@ -1,7 +1,7 @@
-C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-aqualev20/code/fizhi_SIZE.h,v 1.1 2006/04/03 20:55:14 molod Exp $
+C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-aqualev20/code/fizhi_SIZE.h,v 1.2 2006/11/27 17:21:58 molod Exp $
 C $Name:  $
 
 c Physics Grid Vertical Dimension
 c -------------------------------
       integer Nrphys
-      parameter (Nrphys = 28)
+      parameter (Nrphys = 30)
