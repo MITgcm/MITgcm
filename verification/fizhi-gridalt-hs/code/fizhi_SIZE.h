709c7e055ead72fd9dcf56049f14d6da3802f326
Andrea Molod <andrea.molod@nasa.gov>
2006-11-27 17:11:37 +0000

    Add proper fizhi_SIZE file for this experiment

---
+++ verification/fizhi-gridalt-hs/code/fizhi_SIZE.h
@@ -0,0 +1,7 @@
+C $Header: /u/gcmpack/MITgcm/verification/fizhi-gridalt-hs/code/fizhi_SIZE.h,v 1.1 2006/11/27 17:11:37 molod Exp $
+C $Name:  $
+
+c Physics Grid Vertical Dimension
+c -------------------------------
+      integer Nrphys
+      parameter (Nrphys = 21)
