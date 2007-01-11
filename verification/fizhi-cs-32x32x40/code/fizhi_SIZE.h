b837625b2f008904ff5e62eebdc81237351af559
Andrea Molod <andrea.molod@nasa.gov>
2007-01-11 17:56:31 +0000

    New verification experiment - 40 level full fizhi. this will replace 10 level exp.

---
+++ verification/fizhi-cs-32x32x40/code/fizhi_SIZE.h
@@ -0,0 +1,7 @@
+C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-32x32x40/code/fizhi_SIZE.h,v 1.1 2007/01/11 17:56:22 molod Exp $
+C $Name:  $
+
+c Physics Grid Vertical Dimension
+c -------------------------------
+      integer Nrphys
+      parameter (Nrphys = 45)
