ce4f256fede927c13ac7b4b3e20eec2e5280ce38
Andrea Molod <andrea.molod@nasa.gov>
2007-01-19 19:57:24 +0000

    remove this experiment - has been replaced with the 40 level one

--- verification/fizhi-cs-32x32x10/code/fizhi_SIZE.h
+++
@@ -1,7 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-32x32x10/code/Attic/fizhi_SIZE.h,v 1.2 2006/11/27 17:30:31 molod Exp $
-C $Name:  $
-
-c Physics Grid Vertical Dimension
-c -------------------------------
-      integer Nrphys
-      parameter (Nrphys = 21)
