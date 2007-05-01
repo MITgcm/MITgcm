9b49463ca457c45da7806241090b9a69cc4af138
Stephanie Dutkiewicz <stephd@mit.edu>
2007-05-01 20:48:42 +0000

    o add parameters (gchem_int* and gchem_rl*) that can be read in from   data.gchem

--- pkg/gchem/GCHEM.h
+++ pkg/gchem/GCHEM.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/gchem/GCHEM.h,v 1.8 2005/08/18 18:23:02 stephd Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/gchem/GCHEM.h,v 1.9 2007/05/01 20:48:41 stephd Exp $
 C $Name:  $
 
 #ifdef ALLOW_GCHEM
@@ -23,6 +23,9 @@ C  IronFile   : : file name of aeolian iron flux that may be needed for
 C                 biogeochemical experiments
 C  SilicaFile : : file name of surface silica that may be needed for
 C                 biogeochemical experiments
+C  Filename*  : : various spare filenames 
+C  gchem_int* : : place holder to read in a integer number, set at run time
+C  gchem_rl*  : : place holder to read in a real number, set at run time
 
 C  
       INTEGER nsubtime
@@ -36,6 +39,17 @@ C
       CHARACTER*(MAX_LEN_FNAM) Filename3
       CHARACTER*(MAX_LEN_FNAM) Filename4
       CHARACTER*(MAX_LEN_FNAM) Filename5
+      INTEGER gchem_int1
+      INTEGER gchem_int2
+      INTEGER gchem_int3
+      INTEGER gchem_int4
+      INTEGER gchem_int5
+      _RL     gchem_rl1
+      _RL     gchem_rl2
+      _RL     gchem_rl3
+      _RL     gchem_rl4
+      _RL     gchem_rl5
+
 
       COMMON /GCHEM_PARAMS/
      &                   WindFile,
@@ -48,7 +62,11 @@ C
      &                   Filename3,
      &                   Filename4,
      &                   Filename5,
-     &                   nsubtime
+     &                   nsubtime,
+     &           gchem_int1, gchem_int2, gchem_int3,
+     &           gchem_int4, gchem_int5,
+     &           gchem_rl1, gchem_rl2, gchem_rl3,
+     &           gchem_rl4, gchem_rl5
 CEOP
 
 #endif /* ALLOW_GCHEM */
