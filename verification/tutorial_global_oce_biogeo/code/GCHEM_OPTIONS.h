4fd7966d9ea09a5db3d227bf3744db5cc0ef8b98
Stephanie Dutkiewicz <stephd@mit.edu>
2007-05-01 22:52:45 +0000

    o add compile time flag (PAR_FROMSW) so that PAR needed in dic code   is taken from Qsw (rather than insol algorithm or read from file).

--- verification/tutorial_global_oce_biogeo/code/GCHEM_OPTIONS.h
+++ verification/tutorial_global_oce_biogeo/code/GCHEM_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code/Attic/GCHEM_OPTIONS.h,v 1.2 2007/05/01 21:47:07 stephd Exp $
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code/Attic/GCHEM_OPTIONS.h,v 1.3 2007/05/01 22:52:45 stephd Exp $
 C $Name:  $
 
 #ifndef GCHEM_OPTIONS_H
@@ -24,6 +24,7 @@ c specifics for dic pkg
 #undef  ALLOW_FE
 #define ALLOW_O2
 #undef READ_PAR
+#undef PAR_FROMSW
 #undef MINFE
 c -------------------------------------
 
