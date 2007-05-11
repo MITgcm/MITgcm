4aa4b2baf32f55ed68114cb052e35f8d0e472569
Patrick Heimbach <heimbach@mit.edu>
2007-05-11 03:59:56 +0000

    Change to a more "standard" PROFILES_OPTIONS.h

--- pkg/profiles/PROFILES_OPTIONS.h
+++ pkg/profiles/PROFILES_OPTIONS.h
@@ -1,5 +1,20 @@
-#include "CPP_OPTIONS.h"
-C 
-C CPP flags controlling which code is included in the files that
-C will be compiled.
+C $Header: /u/gcmpack/MITgcm/pkg/profiles/PROFILES_OPTIONS.h,v 1.2 2007/05/11 03:59:56 heimbach Exp $
+C $Name:  $
+
+C CPP options file for PROFILES package
 C
+C Use this file for selecting options within the PROFILES package
+
+#ifndef PROFILES_OPTIONS_H
+#define PROFILES_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_PROFILES
+
+#include "CPP_OPTIONS.h"
+
+#endif /* ALLOW_PROFILES */
+#endif /* PROFILES_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
