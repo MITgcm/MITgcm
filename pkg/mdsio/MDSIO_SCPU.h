713e6ff2abb701479db3a4582848492863c0fadb
Dimitris Menemenlis <dmenemenlis@gmail.com>
2007-07-19 15:34:50 +0000

    o gather_2d/scatter_2d: removed halo regions This means that the overlap regions need to be initialized and/or exchanged "outside" of these two routines as has generally been the case since revision 1.6 of scatter_2d.F

--- pkg/mdsio/MDSIO_SCPU.h
+++ pkg/mdsio/MDSIO_SCPU.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mdsio/Attic/MDSIO_SCPU.h,v 1.1 2005/11/08 23:03:17 cnh Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mdsio/Attic/MDSIO_SCPU.h,v 1.2 2007/07/19 15:34:50 dimitri Exp $
 C $Name:  $
 C
 C Buffers for single CPU IO that allow it to work in multi-threaded code
@@ -7,4 +7,4 @@ C     sharedLocalBuf - Heap storage buffer to which master thread copies
 C                      data (during read) as part of a scatter/gather and 
 C                      from which all threads read data (during read).
       COMMON /MDSIO_SCPU_R/ sharedLocalBuf
-      _RL sharedLocalBuf(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL sharedLocalBuf(1:sNx,1:sNy,nSx,nSy)
