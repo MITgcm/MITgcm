e9b4febd91c8423eae77979c86da09f9855674c2
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-07-25 21:16:54 +0000

    generate .F files from template uv_bgrid & uv_cgrid (<- only used when option W2_USE_R1_ONLY in W2_OPTIONS.h is defined)

--- pkg/exch2/W2_OPTIONS.h
+++ pkg/exch2/W2_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/exch2/W2_OPTIONS.h,v 1.2 2005/07/24 01:35:06 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/exch2/W2_OPTIONS.h,v 1.3 2007/07/25 21:16:54 jmc Exp $
 C $Name:  $
  
 C CPP options file for EXCH2 package
@@ -11,6 +11,9 @@ C ... W2_USE_E2_SAFEMODE description ...
 
 C Debug mode option:
 #undef  W2_E2_DEBUG_ON
+
+C Use only exch2_R1_cube (and avoid calling exch2_R2_cube)
+#undef W2_USE_R1_ONLY
 
 C Fill null regions (face-corner halo regions) with e2FillValue_RX (=0)
 C notes: for testing (allow to check that results are not affected)
