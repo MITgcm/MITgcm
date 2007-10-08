305d315bb82642f836a26a2ee2b52cceb55691f7
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-10-08 23:45:51 +0000

    add missing cvs $Header:$ or $Name:$

--- pkg/admtlm/arpack_debug.h
+++ pkg/admtlm/arpack_debug.h
@@ -1,6 +1,9 @@
+C $Header: /u/gcmpack/MITgcm/pkg/admtlm/arpack_debug.h,v 1.2 2007/10/08 23:45:51 jmc Exp $
+C $Name:  $
+
 c
-c\SCCS Information: @(#) 
-c FILE: debug.h   SID: 2.3   DATE OF SID: 11/16/95   RELEASE: 2 
+c\SCCS Information: @(#)
+c FILE: debug.h   SID: 2.3   DATE OF SID: 11/16/95   RELEASE: 2
 c
 c     %---------------------------------%
 c     | See debug.doc for documentation |
@@ -9,7 +12,7 @@ c     %---------------------------------%
      &         msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,
      &         mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,
      &         mcaupd, mcaup2, mcaitr, mceigh, mcapps, mcgets, mceupd
-      common /debug/ 
+      common /debug/
      &         logfil, ndigit, mgetv0,
      &         msaupd, msaup2, msaitr, mseigt, msapps, msgets, mseupd,
      &         mnaupd, mnaup2, mnaitr, mneigh, mnapps, mngets, mneupd,
