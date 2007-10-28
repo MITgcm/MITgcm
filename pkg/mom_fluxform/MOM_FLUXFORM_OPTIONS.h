229b6d70e73c979ca155f4b227711c5312a06635
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-10-28 21:38:21 +0000

    Special version of momentum advection intended to conserve momentum next to a bathymetry step or a coastline edge.

--- pkg/mom_fluxform/MOM_FLUXFORM_OPTIONS.h
+++ pkg/mom_fluxform/MOM_FLUXFORM_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mom_fluxform/MOM_FLUXFORM_OPTIONS.h,v 1.2 2003/10/09 04:19:19 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mom_fluxform/MOM_FLUXFORM_OPTIONS.h,v 1.3 2007/10/28 21:38:21 jmc Exp $
 C $Name:  $
 
 C CPP options file for mom_fluxform package
@@ -13,6 +13,10 @@ C Use this file for selecting CPP options within the mom_fluxform package
 #include "CPP_OPTIONS.h"
 
 C CPP macros go here
+
+C A trick to conserve U,V momemtum next to a step (vertical plane)
+C  or a coastline edge (horizontal plane).
+#undef MOM_BOUNDARY_CONSERVE
 
 #endif /* ALLOW_MOM_FLUXFORM */
 #endif /* MOM_FLUXFORM_OPTIONS_H */
