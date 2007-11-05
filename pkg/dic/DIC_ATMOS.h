1ef1c61fb12cb40334be4bb9b44a784c8867e023
Stephanie Dutkiewicz <stephd@mit.edu>
2007-11-05 21:02:03 +0000

    o adding flexibility for determining atmospheric pCO2   (now defaults to 278d-6, can be read in from data.gchem, can be    read in from file for time-varing values, and can be linked    to interactive well mixed atmospheric box)

---
+++ pkg/dic/DIC_ATMOS.h
@@ -0,0 +1,15 @@
+       COMMON /INTERACT_ATMOS_NEEDS/
+     &              co2atmos,
+     &              total_atmos_carbon, total_ocean_carbon,
+     &              total_atmos_carbon_year,
+     &              total_ocean_carbon_year,
+     &              total_atmos_carbon_start,
+     &              total_ocean_carbon_start
+
+       _RL co2atmos(1000)
+       _RL total_atmos_carbon
+       _RL total_ocean_carbon
+       _RL total_atmos_carbon_year
+       _RL total_atmos_carbon_start
+       _RL total_ocean_carbon_year
+       _RL total_ocean_carbon_start
