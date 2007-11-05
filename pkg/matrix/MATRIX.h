8f6dc1243de134865a5b95ad12720b2648a0f354
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-11-05 18:58:00 +0000

    split PTRACERS.h in 2 header files: PTRACERS_FIELDS.h & PTRACERS_PARAMS  declare Namelist in matrix_readparms.F (was previously in MATRIX.h)

--- pkg/matrix/MATRIX.h
+++ pkg/matrix/MATRIX.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/matrix/MATRIX.h,v 1.1 2005/04/18 19:38:23 spk Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/matrix/MATRIX.h,v 1.2 2007/11/05 18:58:00 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_MATRIX
@@ -10,8 +10,6 @@ C $Name:  $
      &   expMatrixWriteCount, impMatrixWriteCount,
      &   expMatrixWriteTime, impMatrixWriteTime
       
-      NAMELIST /MATRIX_PARM01/ expMatrixWriteTime, impMatrixWriteTime
-
       _RL MATRIX(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy,
      &	         PTRACERS_num,2)
       _RL PTRACERS_initial(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy,
