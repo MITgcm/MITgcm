fa492de2af2601d127ac1d03e763908c16d87bda
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-11-05 17:04:16 +0000

    add short matlab scripts to convert (both ways)  ptracer number to ptracer io-label (2 character long).

---
+++ utils/matlab/ioLb2num.m
@@ -0,0 +1,47 @@
+function [iTr] = ioLb2num( ioLb );
+% [iTr] = ioLb2num( ioLb );
+% convert MITgcm ptracer-io label (2 characters) to ptracer number
+%  do conversion on individual 2c label
+%  or on character array ( N x 2c ) and then return number array (N x 1)
+
+% $Header: /u/gcmpack/MITgcm/utils/matlab/ioLb2num.m,v 1.1 2007/11/05 17:04:16 jmc Exp $
+% $Name:  $
+
+ if size(ioLb,2) == 1 & size(ioLb,2) == 2,
+  N=1; ioLb=ioLb';
+ else
+  N=size(ioLb,1);
+ end
+
+%- step 1 : convert to 2 ASCII code: j1,j2
+ jj=double(ioLb); j1=jj(:,1); j2=jj(:,2);
+%fprintf(['%4i ',c1,c2,' %3i %3i\n'],i,j1,j2);
+
+% native2unicode() ; unicode2native() :
+% 0-9 :: 48-57
+% A-Z :: 65-90
+% a-z :: 97-122
+ n0=double('0'); ne=10+n0;
+ n1=double('A');
+ n2=double('a');
+ nd=double('-');
+
+%- step 2 : convert to 2 digits k1,k2 (both in [0,61])
+ iTr=-ones(N,1);
+ k1=j1-n1+36;
+ [J]=find( j1 <  ne ); k1(J)=j1(J)-n0;
+ [J]=find( j1 >= n2 ); k1(J)=j1(J)-n2+10;
+ [J]=find( j1 <  n0 ); k1(J)=0;
+ k2=j2-n1+36;
+ [I]=find( j2 <  ne ); k2(I)=j2(I)-n0;
+ [I]=find( j2 >= n2 ); k2(I)=j2(I)-n2+10;
+ [I]=find( j2 <  n0 ); k2(I)=0;
+
+%- step 3 : build number in modified 62 base from 2 digits k1,k2
+% 620 = 100 + (10*2*26) ; 52 = 26+26 ; 62=10+26+26
+ iTr= 0 + 62*k1 + k2;
+ [I]=find( j1 < ne & j2 < ne ); iTr(I)= 0 + 10*k1(I) + k2(I);
+ [I]=find( j1 < ne & j2 > ne ); iTr(I)=90 + 52*k1(I) + k2(I);
+ [I]=find( j1 < n0 | j2 < n0 ); iTr(I)=0;
+
+return
---
+++ utils/matlab/num2ioLb.m
@@ -0,0 +1,60 @@
+function [ioLb] = num2ioLb( iTr );
+% [ioLb ] = num2ioLb( iTr);
+% convert ptracer number to MITgcm ptracer-io label (2 characters)
+%  do conversion on individual number (return a 2c label)
+%  or on number array (N x 1) and then return a character array ( N x 2c )
+
+% $Header: /u/gcmpack/MITgcm/utils/matlab/num2ioLb.m,v 1.1 2007/11/05 17:04:16 jmc Exp $
+% $Name:  $
+
+ N=prod(size(iTr)); iTr=reshape(iTr,[N 1]);
+
+%- step 1 : express number in modified 62/52/10 base, digits: k1,k2
+
+%- base 52 in [100,620-1]
+ k2=fix(iTr-100);
+ k1=fix(k2/52);
+ k2=rem(k2,52)+10;
+%- base 10 in [1,100-1]
+ [I]=find( iTr < 100 );
+ s0=fix(iTr-0);
+ tt=fix(s0/10);
+ k1(I)=tt(I);
+ tt=rem(s0,10);
+ k2(I)=tt(I);
+%- base 62 in [620,62^2-1]
+ [I]=find( iTr >= 620 );
+ tt=fix(s0/62);
+ k1(I)=tt(I);
+ tt=rem(s0,62);
+ k2(I)=tt(I);
+
+%- step 2 : convert digits k1,k2 to ASCII code: j1,j2
+% 0-9 :: 48-57
+% A-Z :: 65-90
+% a-z :: 97-122
+ n0=double('0');
+ n1=double('A');
+ n2=double('a');
+ nd=double('-');
+% -36+65 = +29 ; -10+97 = +87
+ j1=k1-10+n2;
+ [I]=find(k1 >= 36 ); j1(I)=k1(I)-36+n1;
+ [I]=find(k1 < 10 ) ; j1(I)=k1(I)+n0;
+ j2=k2-10+n2;
+ [I]=find(k2 >= 36 ); j2(I)=k2(I)-36+n1;
+ [I]=find(k2 < 10 ) ; j2(I)=k2(I)+n0;
+
+ [I]=find( iTr >= 62*62 ); j1(I)=nd; j2(I)=nd;
+
+%- step 3 : do the conversion to ASCII code
+
+% uses: char(); double();
+ ioLb=char([j1 j2]);
+
+% native2unicode() ; unicode2native() : <- much slower
+%c1=native2unicode(j1);
+%c2=native2unicode(j2);
+%ioLb=strcat(c1,c2);
+
+return
