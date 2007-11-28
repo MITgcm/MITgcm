762bde3a3e8f7edeadee43268e23e84d387a8e9e
Dimitris Menemenlis <dmenemenlis@gmail.com>
2007-11-28 17:55:36 +0000

    some cosmetic changes to pkg/salt_plume, including addition of #ifdef ALLOW_SALT_PLUME's around each file as per JM's suggestion http://forge.csail.mit.edu/pipermail/mitgcm-devel/2007-November/003112.html

--- pkg/salt_plume/SALT_PLUME.h
+++ pkg/salt_plume/SALT_PLUME.h
@@ -1,27 +1,27 @@
-C $Header: /u/gcmpack/MITgcm/pkg/salt_plume/SALT_PLUME.h,v 1.2 2007/11/28 09:26:16 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/salt_plume/SALT_PLUME.h,v 1.3 2007/11/28 17:55:36 dimitri Exp $
 C $Name:  $
 
-C--   SALT_PLUME parameters
+#ifdef ALLOW_SALT_PLUME
 
-C     find surface where the potential density (ref.lev=surface) is
+C--   SALT_PLUME parameters
+C     Find surface where the potential density (ref.lev=surface) is
 C     larger than surface density plus SaltPlumeCriterion.  Initially
 C     use the default value 0.4 kg/m^3 of Duffy et al, 1999.
       _RL SaltPlumeCriterion
       COMMON /SALT_PLUME_PARAMS_R/ SaltPlumeCriterion
 
 C--   SALT_PLUME 2-dim. fields
-
 C     SaltPlumeDepth :: depth of penetration of salt plumes
 C                       rejected during sea ice growth
-      COMMON /DYNVARS_SALT_PLUME/ SaltPlumeDepth
-      _RL SaltPlumeDepth (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-
-C saltPlumeFlux :: Net downward salt flux in psu.kg/m^2/s
-C              flux of Salt rejected back into the ocean per time unit (second).
-C              Note: a) only used when salty sea-ice forms or melts.
+C     saltPlumeFlux :: Net downward salt flux in psu.kg/m^2/s
+C              Note: a) only used when salty sea-ice forms.
 C                    b) units: when salinity (unit= psu) is expressed
 C              	        in g/kg, saltPlumeFlux unit becomes g/m^2/s.
 C              > 0 for increasing in SSS.
 C              Southwest C-grid tracer point
-      COMMON /FFIELDS_saltPlumeFlux/ saltPlumeFlux
+      _RL SaltPlumeDepth (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  saltPlumeFlux (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      COMMON /DYNVARS_SALT_PLUME/ SaltPlumeDepth
+      COMMON /FFIELDS_saltPlumeFlux/ saltPlumeFlux
+
+#endif /* ALLOW_SALT_PLUME */
--- pkg/salt_plume/SALT_PLUME_OPTIONS.h
+++ pkg/salt_plume/SALT_PLUME_OPTIONS.h
@@ -1,20 +1,17 @@
-C $Header: /u/gcmpack/MITgcm/pkg/salt_plume/SALT_PLUME_OPTIONS.h,v 1.1 2007/11/28 00:18:17 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/salt_plume/SALT_PLUME_OPTIONS.h,v 1.2 2007/11/28 17:55:36 dimitri Exp $
 C $Name:  $
 
 C CPP options file for salt_plume package
-C
 C Use this file for selecting options within the salt_plume package
 
 #ifndef SALT_PLUME_OPTIONS_H
 #define SALT_PLUME_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_SALT_PLUME
 
 #include "CPP_OPTIONS.h"
 
 C Place CPP define/undef flag here
 
+#endif /* ALLOW_SALT_PLUME */
 #endif /* SALT_PLUME_OPTIONS_H */
-
-CEH3 ;;; Local Variables: ***
-CEH3 ;;; mode:fortran ***
-CEH3 ;;; End: ***
