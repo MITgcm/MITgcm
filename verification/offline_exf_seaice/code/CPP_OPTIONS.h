cd98e3c0519811eb319a2b5ecf555706e15296cc
Jean-Michel Campin <jmc@ocean.mit.edu>
2007-11-28 14:32:05 +0000

    remove #undef ALLOW_SALT_PLUME

--- verification/offline_exf_seaice/code/CPP_OPTIONS.h
+++ verification/offline_exf_seaice/code/CPP_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/offline_exf_seaice/code/Attic/CPP_OPTIONS.h,v 1.2 2007/09/03 15:14:42 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/verification/offline_exf_seaice/code/Attic/CPP_OPTIONS.h,v 1.3 2007/11/28 14:32:05 jmc Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
@@ -9,9 +9,6 @@ C CPP flags controlling particular source code features
 C o Shortwave heating as extra term in external_forcing.F
 C Note: this should be a run-time option
 #define SHORTWAVE_HEATING
-
-C o Include/exclude Duffy et al. (GRL 1999) salt plume scheme
-#undef ALLOW_SALT_PLUME
 
 C o Include/exclude phi_hyd calculation code
 #define INCLUDE_PHIHYD_CALCULATION_CODE
