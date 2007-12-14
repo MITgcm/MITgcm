e6b0d1006c16dc93df03536f73e23ca24f762556
Dimitris Menemenlis <dmenemenlis@gmail.com>
2007-12-14 21:52:02 +0000

    - reformulated availHeat in seaice_growth.F to be independent of AREA

--- pkg/seaice/SEAICE_OPTIONS.h
+++ pkg/seaice/SEAICE_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/seaice/SEAICE_OPTIONS.h,v 1.28 2007/12/05 07:28:29 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/seaice/SEAICE_OPTIONS.h,v 1.29 2007/12/14 21:52:01 dimitri Exp $
 C $Name:  $
 
 C     /==========================================================\
@@ -16,6 +16,10 @@ C     \==========================================================/
 
 C--   Write "text-plots" of certain fields in STDOUT for debugging.
 #undef SEAICE_DEBUG
+
+C--   This flag is meant for options that make more sense than the
+C     original options but that break the verification experiments.
+#undef SEAICE_BREAKS_VERIFICATION
 
 C--   Allow sea-ice dynamic code.
 C     This option is provided to allow use of TAMC
