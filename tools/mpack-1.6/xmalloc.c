e768bd1221978b862575bce618f673876d357e8f
Jean-Michel Campin <jmc@ocean.mit.edu>
2008-02-26 17:05:00 +0000

    put again new version (1.6-4, see MITgcm_contrib/mpack_src) of mpack sources. among others:  - fix MD5 code on some 64 bit platforms.  - fix lot of compilation warnings.

--- tools/mpack-1.6/xmalloc.c
+++ tools/mpack-1.6/xmalloc.c
@@ -23,14 +23,14 @@
  * SOFTWARE.
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-/* extern char *malloc(), *realloc(); */
 
 char *xmalloc (int size)
 {
     char *ret;
 
-    if (ret = (char *)malloc((unsigned) size))
+    if ((ret = malloc((unsigned) size)))
       return ret;
 
     fprintf(stderr, "Memory exhausted\n");
@@ -43,7 +43,7 @@ char *xrealloc (char *ptr, int size)
     char *ret;
 
     /* xrealloc (NULL, size) behaves like xmalloc (size), as in ANSI C */
-    if (ret = !ptr ? ((char *)malloc ((unsigned) size)) : ((char *)realloc (ptr, (unsigned) size)))
+    if ((ret = !ptr ? malloc ((unsigned) size) : realloc (ptr, (unsigned) size)))
       return ret;
 
     fprintf(stderr, "Memory exhausted\n");
