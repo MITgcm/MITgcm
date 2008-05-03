1736eae30e93cf38f372e3a4998984c92d257441
Patrick Heimbach <heimbach@mit.edu>
2008-05-03 00:07:08 +0000

    Arctic is different from global (prbbl bc of obcs)

--- pkg/seaice/seaice_ad_check_lev1_dir.h
+++ pkg/seaice/seaice_ad_check_lev1_dir.h
@@ -1,16 +1,17 @@
-C $Header: /u/gcmpack/MITgcm/pkg/seaice/seaice_ad_check_lev1_dir.h,v 1.17 2008/05/01 23:53:33 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/seaice/seaice_ad_check_lev1_dir.h,v 1.18 2008/05/03 00:02:53 heimbach Exp $
 C $Name:  $
 
+#ifdef AUTODIFF_SOMETIMES_NEEDED
 # ifdef EXACT_CONSERV
-cphCADJ STORE pmepr         = comlev1, key = ikey_dynamics
+CADJ STORE pmepr         = comlev1, key = ikey_dynamics
 # endif
 cphCADJ STORE uvel          = comlev1, key = ikey_dynamics
 cphCADJ STORE vvel          = comlev1, key = ikey_dynamics
-cphCADJ STORE salt          = comlev1, key = ikey_dynamics
-cphCADJ STORE totphihyd     = comlev1, key = ikey_dynamics
+CADJ STORE salt          = comlev1, key = ikey_dynamics
+CADJ STORE totphihyd     = comlev1, key = ikey_dynamics
 cphCADJ STORE runoff        = comlev1, key = ikey_dynamics
 
-cphCADJ STORE area          = comlev1, key = ikey_dynamics
+CADJ STORE area          = comlev1, key = ikey_dynamics
 cphCADJ STORE heff          = comlev1, key = ikey_dynamics
 cphCADJ STORE heffm         = comlev1, key = ikey_dynamics
 cphCADJ STORE hsnow         = comlev1, key = ikey_dynamics
@@ -19,10 +20,14 @@ cphCADJ STORE tice          = comlev1, key = ikey_dynamics
 CADJ STORE tices         = comlev1, key = ikey_dynamics
 # endif
 # ifdef SEAICE_ALLOW_DYNAMICS
-cphCADJ STORE uice          = comlev1, key = ikey_dynamics
-cphCADJ STORE vice          = comlev1, key = ikey_dynamics
+CADJ STORE uice          = comlev1, key = ikey_dynamics
+CADJ STORE vice          = comlev1, key = ikey_dynamics
 cphCADJ STORE zeta          = comlev1, key = ikey_dynamics
 cphCADJ STORE eta           = comlev1, key = ikey_dynamics
+# endif
+#endif /* AUTODIFF_SOMETIMES_NEEDED */
+
+# ifdef SEAICE_ALLOW_DYNAMICS
 #  ifdef SEAICE_CGRID
 CADJ STORE etan          = comlev1, key = ikey_dynamics
 CADJ STORE dwatn         = comlev1, key = ikey_dynamics
