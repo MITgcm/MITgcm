811d3e9bd3f58f70ededb9e4496f4171563c1a4f
Jean-Michel Campin <jmc@ocean.mit.edu>
2008-05-09 21:44:31 +0000

    remove option: GAD_ALLOW_SOM_ADVECT

--- pkg/ptracers/PTRACERS_MOD.h
+++ pkg/ptracers/PTRACERS_MOD.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_MOD.h,v 1.1 2008/05/08 19:50:08 jahn Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_MOD.h,v 1.2 2008/05/09 21:44:31 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_PTRACERS
@@ -17,14 +17,12 @@ CEOP
       use ptracers_dyn_state_mod
       use ptracers_dyn_state_data_mod
 
-#ifdef GAD_ALLOW_SOM_ADVECT
 C     This macro allows the second-order-moment member of the ptracers
 C     internal state data structure to be used like an simple array.
-C     Names are chosen such that macro expansion does not increase 
+C     Names are chosen such that macro expansion does not increase
 C     line length
 #define _Ptracers_som(a,b,c,d,e,f,g) \
-	PtrISt(g)%som_P(a,b,c,d,e,f)
-#endif /* GAD_ALLOW_SOM_ADVECT */
+        PtrISt(g)%som_P(a,b,c,d,e,f)
 
 #endif /* PTRACERS_ALLOW_DYN_STATE */
 #endif /* ALLOW_PTRACERS */
