f60dafae7d8c25df320be8fbc5b02b13a66c0888
Patrick Heimbach <heimbach@mit.edu>
2008-06-19 17:49:29 +0000

    More stores for NLFS.

--- pkg/autodiff/checkpoint_lev1_directives.h
+++ pkg/autodiff/checkpoint_lev1_directives.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/autodiff/checkpoint_lev1_directives.h,v 1.33 2008/05/01 23:53:33 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/autodiff/checkpoint_lev1_directives.h,v 1.34 2008/06/19 17:49:29 heimbach Exp $
 C $Name:  $
 c
 c     store directives for checkpoint level 1
@@ -19,6 +19,7 @@ CADJ STORE detahdt            = comlev1, key = ikey_dynamics
 CADJ STORE gs,gsnm1,gt,gtnm1  = comlev1, key = ikey_dynamics
 CADJ STORE salt,theta         = comlev1, key = ikey_dynamics
 CADJ STORE uvel,vvel,wvel     = comlev1, key = ikey_dynamics
+CADJ STORE surfaceforcingtice = comlev1, key = ikey_dynamics
 c
 # ifndef DISABLE_RSTAR_CODE
 CADJ STORE h0facc,h0facs,h0facw 
