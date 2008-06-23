6d16ee584116eb36e60006e078ba9d5f63e78de8
Jean Utke <jutke@allstate.com>
2008-06-23 20:52:58 +0000

    undo previous change by restoring separate MNC_SIZE.h (according to Patrick this is needed by some experiments)

--- pkg/mnc/MNC_COMMON.h
+++ pkg/mnc/MNC_COMMON.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mnc/MNC_COMMON.h,v 1.2 2008/06/20 20:35:54 utke Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mnc/MNC_COMMON.h,v 1.3 2008/06/23 20:52:58 utke Exp $
 C $Name:  $
 C
 C     ==========================================
@@ -127,22 +127,7 @@ C     v                 :: Vtype index
 C     c                 :: CV index
 CEOP
 
-C     ======== MNC_SIZE.h ===========================================
-
-      integer MNC_MAX_ID, MNC_MAX_FID
-      integer MNC_MAX_CHAR, MNC_MAX_CATT
-      integer MNC_MAX_PATH, MNC_MAX_INFO
-      integer MNC_CW_MAX_I, MNC_CW_CVDAT
-      parameter ( MNC_MAX_ID   =   3000 )
-      parameter ( MNC_MAX_FID  =    200 )
-      parameter ( MNC_MAX_CHAR =     40 )
-      parameter ( MNC_MAX_CATT =    100 )
-      parameter ( MNC_MAX_PATH =    500 )
-      parameter ( MNC_MAX_INFO =    800 )
-      parameter ( MNC_CW_MAX_I =     20 )
-      parameter ( MNC_CW_CVDAT =  50000 )
-
-C     ======== MNC_SIZE.h ============================================
+#include "MNC_SIZE.h"
 
 C=====================================================================
       COMMON /MNC_VARS_C/
---
+++ pkg/mnc/MNC_SIZE.h
@@ -0,0 +1,20 @@
+C $Header: /u/gcmpack/MITgcm/pkg/mnc/MNC_SIZE.h,v 1.9 2008/06/23 20:52:58 utke Exp $
+C $Name:  $
+C
+
+      integer MNC_MAX_ID, MNC_MAX_FID
+      integer MNC_MAX_CHAR, MNC_MAX_CATT
+      integer MNC_MAX_PATH, MNC_MAX_INFO
+      integer MNC_CW_MAX_I, MNC_CW_CVDAT
+      parameter ( MNC_MAX_ID   =   3000 )
+      parameter ( MNC_MAX_FID  =    200 )
+      parameter ( MNC_MAX_CHAR =     40 )
+      parameter ( MNC_MAX_CATT =    100 )
+      parameter ( MNC_MAX_PATH =    500 )
+      parameter ( MNC_MAX_INFO =    800 )
+      parameter ( MNC_CW_MAX_I =     20 )
+      parameter ( MNC_CW_CVDAT =  50000 )
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
