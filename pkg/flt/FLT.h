521db8079882c562faf2f93508354b9267dd2675
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-01-08 19:40:41 +0000

    - fix for cyclic BC (usingCartesianGrid)

--- pkg/flt/FLT.h
+++ pkg/flt/FLT.h
@@ -1,63 +1,113 @@
-C $Header: /u/gcmpack/MITgcm/pkg/flt/FLT.h,v 1.1 2001/09/13 17:43:55 adcroft Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/flt/FLT.h,v 1.2 2009/01/08 19:40:41 jmc Exp $
 C $Name:  $
 
-c     ==================================================================
-c     HEADER flt
-c     ==================================================================
-c
-c     o This header file contains variables that are used by the
-c       flt package. 
-c
-c     ==================================================================
-c     HEADER flt
-c     ==================================================================
-c
-c
-c     max_npart       - total number of floats
-c                       (this is read from the input files)
-c
-      _RL   max_npart
-      common /flt_n/ max_npart
+C     ==================================================================
+C     HEADER flt
+C     ==================================================================
+C
+C     o This header file contains variables that are used by the
+C       flt package.
+C
+C     ==================================================================
+C     HEADER flt
+C     ==================================================================
 
+C== Size ==
 
-c     max_npart_tile  - maximum number of floats on a tile
-c                       normally npart_tile < max_npart, but could be the same
-c                       for safety
-c     max_npart_exch  - maximum number of floats on a tile that can be
-c                       exchanged at one time
-c                       normally max_npart_exch < max_npart_tile,
-c                       but could be the same for safety
-c
+C     max_npart_tile :: maximum number of floats on a tile
+C                       normally npart_tile < max_npart, but could be the same
+C                       for safety
+C     max_npart_exch :: maximum number of floats on a tile that can be
+C                       exchanged at one time
+C                       normally max_npart_exch < max_npart_tile,
+C                       but could be the same for safety
+C
       INTEGER max_npart_tile, max_npart_exch
-      parameter (max_npart_tile = 300)
-      parameter (max_npart_exch =  50)
+      PARAMETER (max_npart_tile = 300)
+      PARAMETER (max_npart_exch =  50)
 
-c     npart_tile   - actual number of floats per tile
-c
+C== Constant ==
+
+C     flt_surf    :: "surface" level of the floats.
+C                    Can be deeper than 1, e.g. IF no mixed layer model is used
+C     flt_nan     :: qualifier for a float that is not yet released or that died
+      _RL flt_surf
+      _RL flt_nan
+      PARAMETER (flt_surf = 1.)
+      PARAMETER (flt_nan = -999.)
+
+C     Full domain extension:
+C     flt_xLo     :: Full domain lower X boundary
+C     flt_xHi     :: Full domain upper X boundary
+C     flt_yLo     :: Full domain lower Y boundary
+C     flt_yHi     :: Full domain upper X boundary
+C
+      _RL    flt_xLo, flt_xHi, flt_yLo, flt_yHi
+      COMMON / FLT_CONST_R /
+     &       flt_xLo, flt_xHi, flt_yLo, flt_yHi
+
+C== Parameters ==
+
+C     flt_noise   :: range of noise added to the velocity component
+C                    (randomly). The noise can be added or subtracted,
+C                    the range is +/- flt_noise/2
+C    flt_int_traj :: period between storing model state at float position, in s
+C    flt_int_prof :: period between float vertical profiles, in s
+C
+C    note:  flt_int_prof is the time between getting profiles, not the
+C           the return  cycle of the float to the surface. The latter can
+C           be specified individually for every float. Because the mechanism
+C           for return ing to the surface is called in the profiling routine
+C           flt_int_prof has to be the minimum of all iup(max_npart).
+C           The subsampling of profiles can be done later in the analysis.
+C
+      _RL flt_noise
+      _RL flt_int_traj, flt_int_prof
+      COMMON / FLT_PARAM_R /
+     &       flt_noise,
+     &       flt_int_traj, flt_int_prof
+
+C     flt_file    :: name of the file containing the initial positions.
+C                    At initialization the program first looks for a
+C                    global file flt_file.data. If that is not found it
+C                    looks for tiled files flt_file.iG.jG.data.
+C
+      CHARACTER*(MAX_LEN_FNAM) flt_file
+      COMMON / FLT_PARAM_C / flt_file
+
+C== Float State ==
+
+C     npart_tile  :: actual number of floats per tile
+C
       INTEGER npart_tile(nSx,nSy)
-      common /flt_t/ npart_tile
+      COMMON / FLT_STATE_I / npart_tile
 
-c     npart        - float identifier
-c     xpart        - x position of float (in units of XC)
-c     ypart        - y position of float (in units of YC)
-c     kpart        - vertical level of float (might be decimal
-c                    number in case of 3D floats)
-c     tstart       - start date of integration of float, in s.
-c                    Note: If tstart=-1 floats are integrated right from the 
-c                          beginning
-c     tend         - end   date of integration of float, in s.
-c                    Note: If tend=-1 floats are integrated till the end of
-c                          integration
-c     kfloat       - target depth of float.
-c     iup          - flag if the float 
-c                    should profile   ( >  0 = return cycle, in s, to surface) 
-c                    remain at depth  ( =  0 )
-c                    is a 3D float    ( = -1 ).
-c                    should be advected WITHOUT additional noise ( = -2 ). 
-c                    This implies that the float is non-profiling
-c                    is a mooring     ( = -3 ), i.e. the float is not advected
-c     itop         - time of float the surface, in s
-c
+C     max_npart   :: total number of floats
+C                    (this is read from the input files)
+C
+      _RL   max_npart
+      COMMON / FLT_STATE_R / max_npart
+
+C     npart       :: float identifier
+C     tstart      :: start date of integration of float, in s.
+C                    Note: If tstart=-1 floats are integrated right from the
+C                          beginning
+C     tend        :: end date of integration of float, in s.
+C                    Note: If tend=-1 floats are integrated till the end of
+C                          integration
+C     xpart       :: x position of float (in units of XC)
+C     ypart       :: y position of float (in units of YC)
+C     kpart       :: vertical level of float (might be decimal
+C                    number in case of 3D floats)
+C     kfloat      :: target depth of float.
+C     iup         :: flag IF the float
+C                    should profile   ( >  0 = RETURN  cycle, in s, to surface)
+C                    remain at depth  ( =  0 )
+C                    is a 3D float    ( = -1 )
+C                    should be advected WITHOUT additional noise ( = -2 ).
+C                    This implies that the float is non-profiling
+C                    is a mooring     ( = -3 ), i.e. the float is not advected
+C     itop        :: time of float the surface, in s
       _RL     npart(max_npart_tile,nSx,nSy)
       _RL    tstart(max_npart_tile,nSx,nSy)
       _RL      tend(max_npart_tile,nSx,nSy)
@@ -68,42 +118,9 @@ c
       _RL       iup(max_npart_tile,nSx,nSy)
       _RL      itop(max_npart_tile,nSx,nSy)
 
-      common /flt_v/ npart,tstart,tend,xpart,ypart,kpart,kfloat,iup,itop
+      COMMON / FLT_STATE_VAR /
+     &       npart, tstart, tend,
+     &       xpart, ypart, kpart,
+     &       kfloat, iup, itop
 
-c     flt_int_traj - period between storing model state at float position, in s
-c     flt_int_prof - period between float vertical profiles, in s
-c
-c     note: flt_int_prof is the time between getting profiles, NOT the
-c           the return cycle of the float to the surface. The latter can
-c           be specified individually for every float. Because the mechanism
-c           for returning to the surface is called in the profiling routine
-c           flt_int_prof has to be the minimum of all iup(max_npart).
-c           The subsampling of profiles can be done later in the analysis.
-c
-      _RL flt_int_traj, flt_int_prof
-      common /flt_param_i/ flt_int_traj, flt_int_prof
-
-c      flt_surf - "surface" level of the floats.
-c                  Can be deeper than 1, e.g. if no mixed layer model is used
-      _RL flt_surf
-      parameter (flt_surf = 1.)
-
-c     flt_nan    - qualifier for a float that is not yet released or that died
-c
-      _RL flt_nan
-      parameter (flt_nan = -999.)
-
-c     flt_noise - range of noise added to the velocity component
-c                 (randomly). The noise can be added or subtracted,
-c                 the range is +/- flt_noise/2
-c
-      _RL flt_noise
-      common /flt_param_n/ flt_noise
-
-c     flt_file  - name of the file containing the initial/restart positions.
-c                 At initialization the program first looks for a global
-c                 file flt_file.data. If that is not found it looks
-c                 for  flt_file.iG.jG.data. That is also written as a restart
-c
-      CHARACTER*(MAX_LEN_FNAM) flt_file
-      common /flt_param_d/ flt_file
+C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
