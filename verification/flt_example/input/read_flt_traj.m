a0fa0e855189b38e885a906b11fba960b262d6b2
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-02-05 01:03:33 +0000

    adapt to new output file (with depth & i,j indices)

--- verification/flt_example/input/read_flt_traj.m
+++ verification/flt_example/input/read_flt_traj.m
@@ -8,10 +8,10 @@ function [flt,data,header] = read_flt_traj(fName)
 % >> plot( flts(3).time, flts(3).x/1e3 )
 % >> for k=1:126;plot(flts(k).x/1e3,flts(k).y/1e3);hold on;end;hold off
 
-% $Header: /u/gcmpack/MITgcm/verification/flt_example/input/read_flt_traj.m,v 1.3 2009/01/04 01:03:59 jmc Exp $
+% $Header: /u/gcmpack/MITgcm/verification/flt_example/input/read_flt_traj.m,v 1.4 2009/02/05 01:03:33 jmc Exp $
 % $Name:  $
 
-imax=10;		% record size
+imax=13;		% record size
 ieee='b';		% IEEE big-endian format
 bytesPerRec=imax*8;	% 8 bytes per real*8
 
@@ -41,19 +41,22 @@ for k=1:size(fls,1)
  data=[data ldata(:,2:end)];
 end
 
-flt=struct('numsteps',[],'time',[],'x',[],'y',[],'k',[]);
+flt=struct('numsteps',[],'time',[],'x',[],'y',[],'z',[]);
 
 % Sort it all out
 for k=1:max(max(data(1,:)));
  j=find( data(1,:)==k );
  [t,jj]=sort( data(2,j) ); j=j(jj);
  flt(k).time=data(2,j);
- flt(k).x=data(3,j);
- flt(k).y=data(4,j);
- flt(k).k=data(5,j);
- flt(k).u=data(6,j);
- flt(k).v=data(7,j);
- flt(k).t=data(8,j);
- flt(k).s=data(9,j);
- flt(k).p=data(10,j);
+ flt(k).x=data( 3,j);
+ flt(k).y=data( 4,j);
+ flt(k).z=data( 5,j);
+ flt(k).i=data( 6,j);
+ flt(k).j=data( 7,j);
+ flt(k).k=data( 8,j);
+ flt(k).p=data( 9,j);
+ flt(k).u=data(10,j);
+ flt(k).v=data(11,j);
+ flt(k).t=data(12,j);
+ flt(k).s=data(13,j);
 end
