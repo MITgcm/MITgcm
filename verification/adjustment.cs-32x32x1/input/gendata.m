dd7ff44a462e4efe72449a57edd8989a1090933f
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-04-18 20:21:17 +0000

    changed to oceanic set-up with rectangular continent centered on face 2  (will allow to test "blank-tiles")

--- verification/adjustment.cs-32x32x1/input/gendata.m
+++ verification/adjustment.cs-32x32x1/input/gendata.m
@@ -1,104 +1,69 @@
-clear all
-nx=32;ny=nx;ntx=6;
+% This is a matlab script that generates the input data
 
-% Generate initial surface pressure
-h=zeros(nx,ntx,ny);
-tileno=2;
+% $Header: /u/gcmpack/MITgcm/verification/adjustment.cs-32x32x1/input/gendata.m,v 1.3 2009/04/18 20:21:17 jmc Exp $
+% $Name:  $
+
+nx=32; ny=nx; nf=6;
+kwr=1;
+
+%- flat bottom bathy with rectangular island on face 2
+dp=ones(nx,nf,ny);
+Htot=1366;
+dp=-Htot*dp;
+
+fc=2;
+i1=1; i2=nx; j1=ny/4 ; j2=1+3*ny/4;
+dp(:,fc,j1:j2)=0;
+dp(nx,1,j1:j2)=0;
+dp(j1:j2,4,1)=0;
+
+if kwr > 0,
+ fid=fopen('bathy_f2.bin','w','b');
+ fwrite(fid,dp,'real*8');
+ fclose(fid);
+end
+
+%----------
+
+h=zeros(nx,nf,ny);
+
+x=0.25*((0.5:nx)/nx-0.5);
+[X,Y]=ndgrid(x,x);
+R=sqrt(X.^2+Y.^2);
+
+
+tileno=1;
+
 o=1;
 i=round(nx*1/2);
 j=round(ny*1/2);
 h(i:i+o,tileno,j:j+o)=1;
 
-fid=fopen('square.bin','w','b');
-fwrite(fid,h,'real*8');
-fclose(fid);
+h(:,1,:)=0.5+0.5*cos(   pi*min(R,0*R+0.04)/0.04 );
 
-% Read grid info and convert to model input files
+%- 10 m max SSH anomaly:
+h=h*10;
 
-% DXC
-fid=fopen('DXC.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DXC.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-% DYC
-q=q';
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DYC.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
+if kwr > 0,
+ fid=fopen('ssh_eq.bin','w','b');
+ fwrite(fid,h,'real*8');
+ fclose(fid);
+end
 
-% DXG
-fid=fopen('DXG.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DXG.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-% DYG
-q=q';
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DYG.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
+return
+%----------
+% plot to check : ----------
+rDir='/home/jmc/grid_cs32/';
+G=load_grid(rDir,10);
+nc=G.dims(2); %nr=G.dims(3); nPxy=G.dims(1)*G.dims(2); nPp2=nPxy+2;
+ccB=[0 0]; shift=-1; cbV=0; AxBx=[-180 180 -90 90]; kEnv=0;
 
-% DXF
-fid=fopen('DXF.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DXF.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-% DYF
-q=q';
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DYF.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
+figure(1);clf;
+var=reshape(dp,[nx*nf ny]);
+grph_CS(var,G.xC,G.yC,G.xG,G.yG,ccB(1),ccB(2),shift,cbV,AxBx,kEnv);
 
-% DXV
-fid=fopen('DXV.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DXV.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-% DYU
-q=q';
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('DYU.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
+figure(2);clf;
+var=reshape(h,[nx*nf ny]);
+grph_CS(var,G.xC,G.yC,G.xG,G.yG,ccB(1),ccB(2),shift,cbV,AxBx,kEnv);
 
-% RAW
-fid=fopen('RAW.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('RAW.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-% RAS
-q=q';
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('RAS.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-
-% RA
-fid=fopen('RA.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('RA.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-
-% RAZ
-fid=fopen('RAZ.tile','r','b');q=fread(fid,[nx nx],'real*8');fclose(fid);
-Q(1:nx,:)=q;Q(nx+(1:nx),:)=q;Q(2*nx+(1:nx),:)=q;Q(3*nx+(1:nx),:)=q;Q(4*nx+(1:nx),:)=q;Q(5*nx+(1:nx),:)=q;
-fid=fopen('RAZ.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-
-% LONC
-fid=fopen('LONC.tile','r','b');q=fread(fid,[nx nx*6],'real*8');fclose(fid);
-q=reshape(q,[nx nx 6]);
-Q(1:nx,:)=q(:,:,1);Q(nx+(1:nx),:)=q(:,:,2);Q(2*nx+(1:nx),:)=q(:,:,3);Q(3*nx+(1:nx),:)=q(:,:,4);Q(4*nx+(1:nx),:)=q(:,:,5);Q(5*nx+(1:nx),:)=q(:,:,6);
-fid=fopen('LONC.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-
-% LATC
-fid=fopen('LATC.tile','r','b');q=fread(fid,[nx nx*6],'real*8');fclose(fid);
-q=reshape(q,[nx nx 6]);
-Q(1:nx,:)=q(:,:,1);Q(nx+(1:nx),:)=q(:,:,2);Q(2*nx+(1:nx),:)=q(:,:,3);Q(3*nx+(1:nx),:)=q(:,:,4);Q(4*nx+(1:nx),:)=q(:,:,5);Q(5*nx+(1:nx),:)=q(:,:,6);
-fid=fopen('LATC.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-
-lat=reshape(Q,[nx ntx nx]);
-rad=30;
-h=100*0.5*(1-cos( max(lat-90+rad,0)/rad*pi ));
-fid=fopen('cos.bin','w','b');fwrite(fid,h,'real*8');fclose(fid);
-h(:,2,:)=h(:,3,:);h(:,3,:)=0;
-fid=fopen('cos_equator.bin','w','b');fwrite(fid,h,'real*8');fclose(fid);
-
-% LONG
-fid=fopen('LONG.tile','r','b');q=fread(fid,[nx+1 (nx+1)*6],'real*8');fclose(fid);
-q=reshape(q,[nx+1 nx+1 6]);
-q=q(1:nx,1:nx,:);
-Q(1:nx,:)=q(:,:,1);Q(nx+(1:nx),:)=q(:,:,2);Q(2*nx+(1:nx),:)=q(:,:,3);Q(3*nx+(1:nx),:)=q(:,:,4);Q(4*nx+(1:nx),:)=q(:,:,5);Q(5*nx+(1:nx),:)=q(:,:,6);
-fid=fopen('LONG.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
-
-% LATG
-fid=fopen('LATG.tile','r','b');q=fread(fid,[nx+1 (nx+1)*6],'real*8');fclose(fid);
-q=reshape(q,[nx+1 nx+1 6]);
-q=q(1:nx,1:nx,:);
-Q(1:nx,:)=q(:,:,1);Q(nx+(1:nx),:)=q(:,:,2);Q(2*nx+(1:nx),:)=q(:,:,3);Q(3*nx+(1:nx),:)=q(:,:,4);Q(4*nx+(1:nx),:)=q(:,:,5);Q(5*nx+(1:nx),:)=q(:,:,6);
-fid=fopen('LATG.bin','w','b');fwrite(fid,Q,'real*8');fclose(fid);
+%----------
