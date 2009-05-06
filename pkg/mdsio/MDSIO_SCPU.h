bbf8cfabb0cd76b880792219a16d9c32ecc73b49
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-05-06 02:41:01 +0000

    in preparation for topology-generator within pkg/exch2:  xy_buffer_r4,8 declared with only 1 dimension: xyBuffer_size      which is set to W2_ioBufferSize (#ifdef ALLOW_EXCH2 ...)

--- pkg/mdsio/MDSIO_SCPU.h
+++ pkg/mdsio/MDSIO_SCPU.h
@@ -1,23 +1,21 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mdsio/Attic/MDSIO_SCPU.h,v 1.3 2008/12/30 00:13:35 jahn Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mdsio/Attic/MDSIO_SCPU.h,v 1.4 2009/05/06 02:41:01 jmc Exp $
 C $Name:  $
 C
 C Buffers for single CPU IO that allow it to work in multi-threaded code
 C
-C     sharedLocalBuf - Heap storage buffer to which master thread copies 
-C                      data (during read) as part of a scatter/gather and 
-C                      from which all threads read data (during read).
-      INTEGER x_size,y_size
+C     sharedLocalBuf :: Heap storage buffer to which master thread copies
+C                       data (during read) as part of a scatter/gather and
+C                       from which all threads read data (during read).
+      INTEGER xyBuffer_size
 #if defined(ALLOW_EXCH2) && !defined(MISSING_TILE_IO)
-      PARAMETER ( x_size = exch2_domain_nxt * sNx )
-      PARAMETER ( y_size = exch2_domain_nyt * sNy )
+      PARAMETER ( xyBuffer_size = W2_ioBufferSize )
 #else
-      PARAMETER ( x_size = Nx )
-      PARAMETER ( y_size = Ny )
+      PARAMETER ( xyBuffer_size = Nx*Ny )
 #endif
       COMMON /MDSIO_SCPU_R/ sharedLocalBuf
       _RL sharedLocalBuf(1:sNx,1:sNy,nSx,nSy)
 
-      COMMON /MDSIO_BUFFERS/ xy_buffer_r4, xy_buffer_r8, globalBuf 
-      Real*4 xy_buffer_r4(x_size,y_size)
-      Real*8 xy_buffer_r8(x_size,y_size)
+      COMMON /MDSIO_BUFFERS/ globalBuf, xy_buffer_r8, xy_buffer_r4
       Real*8 globalBuf(Nx,Ny)
+      Real*8 xy_buffer_r8(xyBuffer_size)
+      Real*4 xy_buffer_r4(xyBuffer_size)
