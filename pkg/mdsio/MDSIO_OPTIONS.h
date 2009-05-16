a5360d8fd1774395710d77dfab03ebe1b412da6d
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-05-16 13:36:42 +0000

    - remove MISSING_TILE_IO (from MDSIO_OPTIONS.h), replaced by run-time   parameter "W2_useE2ioLayOut" (read from file "data.exch2").

--- pkg/mdsio/MDSIO_OPTIONS.h
+++ pkg/mdsio/MDSIO_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mdsio/MDSIO_OPTIONS.h,v 1.6 2004/06/30 15:27:57 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mdsio/MDSIO_OPTIONS.h,v 1.7 2009/05/16 13:36:42 jmc Exp $
 C $Name:  $
 
 #ifndef MDSIO_OPTIONS_H
@@ -24,24 +24,6 @@ C Defining SAFE_IO stops the model from overwriting its own files
 #undef  ALLOW_BROKEN_MDSIO_GL
 #define _OLD_STATUS 'old'
 #endif
-
-C o By default pkg/mdsio assumes that missing tiles are
-C   present in the input and in the output files.  When
-C   MISSING_TILE_IO is defined, the input and output
-C   files do not include the missing tiles.  Therefore
-C   the 2-D dimension of input and output files is:
-C    #if defined(ALLOW_EXCH2) && !defined(MISSING_TILE_IO)
-C     x_size = exch2_domain_nxt * sNx = exch2_domain_nxt*exch2_tnx
-C     y_size = exch2_domain_nyt * sNy = exch2_domain_nyt*exch2_tny
-C    #else
-C     x_size = Nx = sNx * nSx * nPx
-C     y_size = Ny = sNy * nSy * nPy
-C    #endif-
-C   Notes:
-C    1) Pickup files are not compatible between the two formats.
-C    2) Defining MISSING_TILE_IO with no missing tiles is
-C       equivalent to the old, tile-dependent I/O format.
-#undef MISSING_TILE_IO
 
 #endif /* ALLOW_MDSIO */
 #endif /* MDSIO_OPTIONS_H */
