54d78f5995145b886d1ba000e265aa16cffc87ca
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-05-30 21:26:31 +0000

    - take buffer copy from/to array out of S/R exch2_send/recv into new   S/R exch2_put/get ; Exch of local variable now works with MPI+MTH   (tested by removing commom block statement in SOLVE_FOR_PRESSURE.h). - simplify argument list of S/R exch2_get_uv_bounds & get_scal_bounds - implement EXCH_IGNORE_CORNERS in scalar exchange (rx1);

--- pkg/exch2/W2_EXCH2_BUFFER.h
+++ pkg/exch2/W2_EXCH2_BUFFER.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/exch2/W2_EXCH2_BUFFER.h,v 1.1 2009/05/12 19:44:59 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/exch2/W2_EXCH2_BUFFER.h,v 1.2 2009/05/30 21:26:31 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -32,6 +32,13 @@ C--   COMMON /W2_EXCH2_BUF_RX/ real type Buffer used by W2-EXCH2
       COMMON /W2_EXCH2_BUF_RS/ E2BUFR1_RS, E2BUFR2_RS
       COMMON /W2_EXCH2_BUF_R4/ E2BUFR1_R4, E2BUFR2_R4
       COMMON /W2_EXCH2_BUF_R8/ E2BUFR1_R8, E2BUFR2_R8
+
+C--   COMMON /W2_EXCH2_BUF_I/ integer type Buffer used by W2-EXCH2
+C     iBuf1Filled :: actual length of buffer-1 which has been filled in.
+C     iBuf2Filled :: actual length of buffer-2 which has been filled in.
+      INTEGER iBuf1Filled( W2_maxNeighbours, nSx)
+      INTEGER iBuf2Filled( W2_maxNeighbours, nSx)
+      COMMON /W2_EXCH2_BUF_I/ iBuf1Filled, iBuf2Filled
 
 C--   COMMON /W2_EXCH2_COMMFLAG/ EXCH2 character Flag for type of communication
       CHARACTER W2_myCommFlag( W2_maxNeighbours, nSx )
