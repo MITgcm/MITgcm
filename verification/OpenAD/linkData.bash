0088ad0ab579f7397a7802f9ad6895c458d08ea9
Jean Utke <jutke@allstate.com>
2009-09-22 15:08:48 +0000

    argument

--- verification/OpenAD/linkData.bash
+++ verification/OpenAD/linkData.bash
@@ -1,9 +1,14 @@
 function Usage { 
-  echo "Usage: $0 <testName>"
+  echo "Usage: $0 [ <testName> ]"
   echo "       where <testName> is for instance  shallow_openad"
   echo "       executed in build_<testName>"
 }
-testName=${PWD##*/build_}
+if [ $# -eq 1 ]
+then 
+  testName=$1
+else 
+  testName=${PWD##*/build_}
+fi
 inputDir=../../../../MITgcm_contrib/heimbach/OpenAD/input_${testName}
 if [ ! -d ${inputDir} ] 
 then 
@@ -12,7 +17,8 @@ then
   exit -1
 fi
 ln -sf ${inputDir}/* .
+tDir=$PWD
 cd ../../global_ocean.90x40x15/input
 ./prepare_run > /dev/null
-cd ../../OpenAD/build_${testName}
+cd $tDir
 ln -sf ../../global_ocean.90x40x15/input/*.bin .
