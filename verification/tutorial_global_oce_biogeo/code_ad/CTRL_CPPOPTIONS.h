a180d1c5b43234a3c4af5d104301c27ed7944423
Patrick Heimbach <heimbach@mit.edu>
2009-10-16 16:20:05 +0000

    Add online dic sensitivity verification

---
+++ verification/tutorial_global_oce_biogeo/code_ad/CTRL_CPPOPTIONS.h
@@ -0,0 +1,8 @@
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code_ad/Attic/CTRL_CPPOPTIONS.h,v 1.1 2009/10/16 16:20:04 heimbach Exp $
+C $Name:  $
+
+#include "CPP_OPTIONS.h"
+C
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C
---
+++ verification/tutorial_global_oce_biogeo/code_ad/DIC_OPTIONS.h
@@ -0,0 +1,29 @@
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code_ad/Attic/DIC_OPTIONS.h,v 1.1 2009/10/16 16:20:04 heimbach Exp $
+C $Name:  $
+
+#ifndef DIC_OPTIONS_H
+#define DIC_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_DIC
+
+#include "CPP_OPTIONS.h"
+
+#define DIC_BIOTIC
+#define  ALLOW_FE
+#define ALLOW_O2
+#undef READ_PAR
+#undef MINFE
+c these all need to be defined for coupling to
+c atmospheric model
+#undef USE_QSW
+#undef USE_ATMOSCO2
+#undef USE_PLOAD
+
+#define ALLOW_OLD_VIRTUALFLUX
+
+#endif /* ALLOW_DIC */
+#endif /* DIC_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
