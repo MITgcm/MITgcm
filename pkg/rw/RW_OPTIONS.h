43b0ee4163fd2eb4b0d62c04efd5da9d36d49661
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-11-18 00:33:58 +0000

    for debugging: add S/R to write out smaller overlap array

--- pkg/rw/RW_OPTIONS.h
+++ pkg/rw/RW_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/rw/RW_OPTIONS.h,v 1.2 2007/10/22 13:20:07 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/rw/RW_OPTIONS.h,v 1.3 2009/11/18 00:33:58 jmc Exp $
 C $Name:  $
 
 C CPP options file for RW package
@@ -15,6 +15,10 @@ C Use this file for selecting options within the RW package
 C use READ_MFLDS S/R in "safe" mode (set/check/unset for each file to read)
 C involves more thread synchronization (could slow down multi-threaded run)
 #define RW_SAFE_MFLDS
+
+C disable writing of small-overlap size array (to reduce memory size 
+C   since those S/R do a local copy to 3-D full-size overlap array)
+#undef RW_DISABLE_SMALL_OVERLAP
 
 #endif /* ALLOW_RW */
 #endif /* RW_OPTIONS_H */
