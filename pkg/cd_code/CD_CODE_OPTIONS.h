da4112fbea7de84f49b20d872a6df8cb3b0281e4
Jean-Michel Campin <jmc@ocean.mit.edu>
2009-11-19 22:30:38 +0000

    Change defaut D-grid time-stepping (similar to C-grid). Add 2 options (in CD_CODE_OPTIONS.h), to reproduce old results.

--- pkg/cd_code/CD_CODE_OPTIONS.h
+++ pkg/cd_code/CD_CODE_OPTIONS.h
@@ -1,9 +1,9 @@
-C $Header: /u/gcmpack/MITgcm/pkg/cd_code/CD_CODE_OPTIONS.h,v 1.1 2003/10/28 22:57:59 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/cd_code/CD_CODE_OPTIONS.h,v 1.2 2009/11/19 22:29:36 jmc Exp $
 C $Name:  $
 
 C CPP options file for CD_CODE package
 C
-C Use this file for selecting CPP options within the mom_fluxform package
+C Use this file for selecting CPP options within the cd_code package
 
 #ifndef CD_CODE_OPTIONS_H
 #define CD_CODE_OPTIONS_H
@@ -11,6 +11,14 @@ C Use this file for selecting CPP options within the mom_fluxform package
 #ifdef ALLOW_CD_CODE
 
 #include "CPP_OPTIONS.h"
+
+C Change defaut to get the same time-stepping for D-grid momentum and
+C  C-grid momentum. D-grid velocity used to be stepped forward in time
+C  with Adams-Bashforth only on surface pressure term. Tests show that
+C  using AB on D-grid coriolis term improves stability (as expected from
+C  CD-scheme paper). The following 2 options allow to reproduce old results.
+#undef CD_CODE_NO_AB_MOMENTUM
+#undef CD_CODE_NO_AB_CORIOLIS
 
 C CPP macros go here
 
