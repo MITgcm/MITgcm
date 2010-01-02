6bf801d687f7dba1d4d0f5e3596b989aa9dda12d
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-01-02 22:46:29 +0000

    time-ave: use simpler (no level index) cumulative-time counter

--- pkg/bulk_force/BULKF_TAVE.h
+++ pkg/bulk_force/BULKF_TAVE.h
@@ -1,42 +1,45 @@
-C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULKF_TAVE.h,v 1.1 2006/05/09 18:59:48 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULKF_TAVE.h,v 1.2 2010/01/02 22:46:29 jmc Exp $
 C $Name:  $
 
+CBOP
+C     !ROUTINE: BULKF_TAVE.h
+C     !INTERFACE:
+C     include "BULKF_TAVE.h"
+C     !DESCRIPTION:
+C     \bv
 C     *==========================================================*
 C     | BULKF_TAVE.h
 C     | o Header for Bulk formula time-average output
 C     *==========================================================*
+C     \ev
+CEOP
 
 #ifdef ALLOW_BULK_FORCE
-
 #ifdef ALLOW_TIMEAVE
 
-C     Keep track of time
-      _RL BULKF_TimeAve(Nr, nSx,nSy)
-      COMMON /BULKF_TAVE/ BULKF_TimeAve
-
-C     Storage arrays for time-averages
-      _RL BULK_Qnet_AVE    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_EmPmR_AVE   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_fu_AVE  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_fv_AVE      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_latent_AVE  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_sensible_AVE(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_evap_AVE     (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_flwup_AVE    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_flwupnet_AVE (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_solar_AVE    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL BULK_ssq_AVE      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+C--   COMMON /BULKF_TAVE_VARS/  Time average Bulk-Formula variables
+      _RL BULK_timeAve     (nSx,nSy)
+      _RL BULK_Qnet_Ave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_EmPmR_Ave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_fu_Ave      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_fv_Ave      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_latent_Ave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_sensible_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_evap_Ave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_flwup_Ave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_flwupnet_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_solar_Ave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL BULK_ssq_Ave     (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
 
-      COMMON /BULKF_TAVE_ARRAYS/
-     &                       BULK_Qnet_AVE, BULK_EmPmR_AVE, 
-     &                       BULK_fu_AVE, BULK_fv_AVE,
-     &                       BULK_sensible_AVE, BULK_latent_AVE,
-     &                       BULK_evap_AVE, BULK_flwup_AVE,
-     &                       BULK_solar_AVE, BULK_ssq_AVE,
-     &                       BULK_flwupnet_AVE
+      COMMON /BULKF_TAVE_VARS/
+     &         BULK_timeAve, BULK_Qnet_Ave, BULK_EmPmR_Ave,
+     &                       BULK_fu_Ave, BULK_fv_Ave,
+     &                       BULK_sensible_Ave, BULK_latent_Ave,
+     &                       BULK_evap_Ave, BULK_flwup_Ave,
+     &                       BULK_solar_Ave, BULK_ssq_Ave,
+     &                       BULK_flwupnet_Ave
 
 
 #endif /* ALLOW_TIMEAVE */
-
 #endif /* ALLOW_BULK_FORCE */
