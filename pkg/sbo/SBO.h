14d0ff948729a30a4bb63c72abb2fec3267431cd
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-01-03 20:03:36 +0000

    - fix ocean bottom pressure 2-D time-ave output (was neither initialised nor filled). - add starting iteration in suffix of global-average output file   (to avoid over-writing the file after a restart) - use standard MDSIO pkg S/R instead of SBO_WRITEVECTOR (this fix byte-swap Pb).

--- pkg/sbo/SBO.h
+++ pkg/sbo/SBO.h
@@ -1,31 +1,31 @@
-C $Header: /u/gcmpack/MITgcm/pkg/sbo/SBO.h,v 1.7 2007/10/01 15:22:54 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/sbo/SBO.h,v 1.8 2010/01/03 20:03:36 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_SBO
 
-C     /==========================================================\
-C     | SBO.h                                                    |
-C     | o Basic header for SBO                                   |
-C     \==========================================================/
+C     *==========================================================*
+C     | SBO.h
+C     | o Basic header for SBO
+C     *==========================================================*
 
-C     xoamc        - x-comp oam due to currents        (kg-m**2/s)
-C     yoamc        - y-comp oam due to currents        (kg-m**2/s)
-C     zoamc        - z-comp oam due to currents        (kg-m**2/s)
-C     xoamp        - x-comp oam due to pressure        (kg-m**2/s)
-C     yoamp        - y-comp oam due to pressure        (kg-m**2/s)
-C     zoamp        - z-comp oam due to pressure        (kg-m**2/s)
-C     mass         - mass of oceans                           (kg)
-C     xcom         - x-comp of center-of-mass of oceans        (m)
-C     ycom         - y-comp of center-of-mass of oceans        (m)
-C     zcom         - z-comp of center-of-mass of oceans        (m)
-C     obp          - ocean-bottom pressure               (Pascals)
+C     xoamc       :: x-comp oam due to currents        (kg-m**2/s)
+C     yoamc       :: y-comp oam due to currents        (kg-m**2/s)
+C     zoamc       :: z-comp oam due to currents        (kg-m**2/s)
+C     xoamp       :: x-comp oam due to pressure        (kg-m**2/s)
+C     yoamp       :: y-comp oam due to pressure        (kg-m**2/s)
+C     zoamp       :: z-comp oam due to pressure        (kg-m**2/s)
+C     mass        :: mass of oceans                           (kg)
+C     xcom        :: x-comp of center-of-mass of oceans        (m)
+C     ycom        :: y-comp of center-of-mass of oceans        (m)
+C     zcom        :: z-comp of center-of-mass of oceans        (m)
+C     obp         :: ocean-bottom pressure               (Pascals)
 cph(
-C     area         - surface wet area                       (m**2)
-C     sboempmrwet  - net E-P-R over the ocean            (kg/m2/s)
-C     sboqnetwet   - net heat flux over the ocean         (W/m**2)
+C     area        :: surface wet area                       (m**2)
+C     sboempmrwet :: net E-P-R over the ocean            (kg/m2/s)
+C     sboqnetwet  :: net heat flux over the ocean         (W/m**2)
 cph)
 C
-      _RL xoamc, yoamc, zoamc, xoamp, yoamp, zoamp          
+      _RL xoamc, yoamc, zoamc, xoamp, yoamp, zoamp
       _RL mass, xcom, ycom, zcom
       _RL sbobp, sboarea, sboempmrwet, sboqnetwet
       _RL obp (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
@@ -33,7 +33,7 @@ C
      &             mass, xcom, ycom, zcom, obp, sbobp,
      &             sboarea, sboempmrwet, sboqnetwet
 
-C     sbo_taveFreq - SBO time-averaging frequency              (s)
+C     sbo_taveFreq :: SBO time-averaging frequency              (s)
 C
       _RL sbo_taveFreq
       COMMON /sbo_r/ sbo_taveFreq
@@ -41,25 +41,18 @@ C
 #ifdef ALLOW_TIMEAVE
 
 C----------------------------------------------------------------
-C     sbo_drctrecTave - next record to dump for SBO averaging files
+C     sbo_timeAve - time of temporal integration (s) for each thread
 C----------------------------------------------------------------
 
-      INTEGER sbo_drctrecTave
-      COMMON /SBO_RECORDNUM/ sbo_drctrecTave
-
-C----------------------------------------------------------------
-C     sbo_TimeAve - time of temporal integration (s) for each thread
-C----------------------------------------------------------------
-
-      _RL sbo_TimeAve(Nr,nSx,nSy)
-      COMMON /SBO_TAVE/ sbo_TimeAve
+      _RL sbo_timeAve(nSx,nSy)
+      COMMON /SBO_TAVE_COUNT/ sbo_timeAve
 
 C----------------------------------------------------------------
 C     OBPtave      - time-averaged ocean-bottom pressure (Pascals)
 C----------------------------------------------------------------
 
       _RL OBPtave (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      COMMON /SBO_TAVE_DIAGS/ OBPtave
+      COMMON /SBO_TAVE_FIELDS/ OBPtave
 
 #endif /* ALLOW_TIMEAVE */
 
