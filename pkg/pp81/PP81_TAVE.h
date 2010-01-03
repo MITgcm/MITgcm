d62f2fa599ea0b6dd1d847482846f3fe02acf10f
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-01-03 19:19:59 +0000

    - use simpler (no level index) cumulative-time counter: PP_timeAve(bi,bj) - cumulate fields only if doing time-ave output (PPtaveFreq > 0) - changed to only full time-step increment (no first & last    "half time-step"): this fix weird 1rst average after restart. - remove MULTIPLE_RECORD_PP81_STATE_FILES part (not fixed since a "stop"     was added on 2009/06/17)

--- pkg/pp81/PP81_TAVE.h
+++ pkg/pp81/PP81_TAVE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/pp81/PP81_TAVE.h,v 1.1 2009/06/17 14:24:06 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/pp81/PP81_TAVE.h,v 1.2 2010/01/03 19:19:59 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_PP81
@@ -8,34 +8,21 @@ C     | PP81_TAVE.h
 C     | o Header for PP81 time-average output
 C     *==========================================================*
 
-C----------------------------------------------------------------
-C     pp_drctrec     - next record to dump for PP files
-C----------------------------------------------------------------
-      INTEGER pp_drctrec
-      COMMON /PP81_RECORDNUM1/ pp_drctrec
-
 #ifdef ALLOW_TIMEAVE
 C----------------------------------------------------------------
-C     pp_drctrecTave - next record to dump for PP averaging files
+C     PP_timeAve - time of temporal integration (s) for each thread
 C----------------------------------------------------------------
 
-      INTEGER pp_drctrecTave
-      COMMON /PP81_RECORDNUM2/ pp_drctrecTave
-
-C----------------------------------------------------------------
-C     pp_TimeAve - time of temporal integration (s) for each thread
-C----------------------------------------------------------------
-
-      _RL pp_TimeAve(Nr,nSx,nSy)
-      COMMON /PP81_TAVE/ pp_TimeAve
+      _RL PP_timeAve(nSx,nSy)
+      COMMON /PP81_TAVE_COUNT/ PP_timeAve
 
 C----------------------------------------------------------------
 C     PP*tave    - Time-averaging PP variables
 C----------------------------------------------------------------
 
-      _RL PPviscArtave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL PPdiffKrtave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      COMMON /PP81_TAVE_DIAGS/
+      _RL PPviscArtave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL PPdiffKrtave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      COMMON /PP81_TAVE_FIELDS/
      &        PPviscArtave, PPdiffKrtave
 
 #endif /* ALLOW_TIMEAVE */
