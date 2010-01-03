19bd6d29d3480daa21365a7ece1392731a6945ef
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-01-03 18:55:06 +0000

    - use simpler (no level index) cumulative-time counter: GGL90_timeave(bi,bj) - cumulate fields only if doing time-ave output (GGL90_taveFreq > 0) - changed to only full time-step increment (no first & last "half time-step")    this fix weird 1rst average after restart.

--- pkg/ggl90/GGL90_TAVE.h
+++ pkg/ggl90/GGL90_TAVE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ggl90/GGL90_TAVE.h,v 1.1 2009/06/17 14:15:34 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ggl90/GGL90_TAVE.h,v 1.2 2010/01/03 18:55:06 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_GGL90
@@ -8,26 +8,14 @@ C     | GGL90_TAVE.h
 C     | o Header for GGL90 time-average output
 C     *==========================================================*
 
-C----------------------------------------------------------------
-C     my_drctrec     - next record to dump for GGL90 files
-C----------------------------------------------------------------
-      INTEGER ggl90_drctrec
-      COMMON /GGL90_RECORDNUM1/ ggl90_drctrec
-
 #ifdef ALLOW_TIMEAVE
-C----------------------------------------------------------------
-C     ggl90_drctrecTave - next record to dump for GGL90 averaging files
-C----------------------------------------------------------------
-
-      INTEGER ggl90_drctrecTave
-      COMMON /GGL90_RECORDNUM2/ ggl90_drctrecTave
 
 C----------------------------------------------------------------
-C     ggl90_TimeAve - time of temporal integration (s) for each thread
+C     GGL90_timeAve - time of temporal integration (s) for each thread
 C----------------------------------------------------------------
 
-      _RL ggl90_TimeAve(Nr,nSx,nSy)
-      COMMON /GGL90_TAVE/ ggl90_TimeAve
+      _RL GGL90_timeAve(nSx,nSy)
+      COMMON /GGL90_TAVE_COUNT/ GGL90_timeAve
 
 C----------------------------------------------------------------
 C     GGL90*tave    - Time-averaging GGL90 variables
@@ -36,7 +24,7 @@ C----------------------------------------------------------------
       _RL GGL90TKEtave     (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL GGL90viscArtave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL GGL90diffKrtave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      COMMON /GGL90_TAVE_DIAGS/
+      COMMON /GGL90_TAVE_FIELDS/
      &        GGL90TKEtave, GGL90viscArtave, GGL90diffKrtave
 
 #endif /* ALLOW_TIMEAVE */
