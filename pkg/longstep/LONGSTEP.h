fda3710353dc5fb7ba660239d673b8c53ffea739
Oliver Jahn <jahn@mit.edu>
2010-01-12 23:56:04 +0000

    longstep: fix tracer conservation with rstar/nonlinear free surface

--- pkg/longstep/LONGSTEP.h
+++ pkg/longstep/LONGSTEP.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/longstep/LONGSTEP.h,v 1.3 2009/09/09 22:48:02 jahn Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/longstep/LONGSTEP.h,v 1.4 2010/01/12 23:55:48 jahn Exp $
 C $Name:  $
 
 #ifdef ALLOW_LONGSTEP
@@ -27,6 +27,7 @@ C     LS_wVel         :: longstep average of vertical velocity
 C     LS_theta        :: longstep average of potential temperature
 C     LS_salt         :: longstep average of salinity
 C     LS_IVDConvCount :: longstep average of IVD convection counter
+C     LS_fwFlux       :: longstep average of either PmEpR or EmPmR (note sign!)
 C
       _RL LS_uVel (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL LS_vVel (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
@@ -34,18 +35,22 @@ C
       _RL LS_theta(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL LS_salt (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL LS_IVDConvCount(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL LS_fwFlux(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       INTEGER LS_uVelCount(nSx,nSy)
       INTEGER LS_vVelCount(nSx,nSy)
       INTEGER LS_wVelCount(nSx,nSy)
       INTEGER LS_thetaCount(nSx,nSy)
       INTEGER LS_saltCount(nSx,nSy)
       INTEGER LS_IVDConvCountCount(nSx,nSy)
+      INTEGER LS_fwFluxCount(nSx,nSy)
       COMMON /LONGSTEP_DYNVARS_R/
      &       LS_uVel, LS_vVel, LS_wVel,
-     &       LS_theta, LS_salt, LS_IVDConvCount
+     &       LS_theta, LS_salt, LS_IVDConvCount,
+     &       LS_fwFlux
       COMMON /LONGSTEP_DYNVARS_I/
      &       LS_uVelCount, LS_vVelCount, LS_wVelCount,
-     &       LS_thetaCount, LS_saltCount, LS_IVDConvCountCount
+     &       LS_thetaCount, LS_saltCount, LS_IVDConvCountCount,
+     &       LS_fwFluxCount
 
 #ifdef ALLOW_GMREDI
 C     Bottom row of tensor corresponds to W points
--- pkg/longstep/LONGSTEP_PARAMS.h
+++ pkg/longstep/LONGSTEP_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/longstep/LONGSTEP_PARAMS.h,v 1.1 2009/06/26 23:10:09 jahn Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/longstep/LONGSTEP_PARAMS.h,v 1.2 2010/01/12 23:55:48 jahn Exp $
 C $Name:  $
 
 #ifdef ALLOW_LONGSTEP
@@ -14,16 +14,20 @@ C Contains parameters for long tracer time step.
 CEOP
 
 C     COMMON /LONGSTEP_PARAMS/  LONGSTEP parameters:
-C     LS_nIter    :: number of dynamics time steps between ptracer steps
-C     LS_afterTS         :: use T,S at end of time step (after thermodynamics)
-C     LS_staggerTimeStep :: use U,V,W,T,S at end of time step
-C     (default: U,V,W,T,S at beginning of time step, i.e. like offline)
+C     LS_nIter        :: number of dynamics time steps between ptracer steps
+C     LS_whenToSample :: when to sample dynamical fields for the longstep average
+C                        0 - at beginning of timestep (reproduces offline results)
+C                        1 - after first THERMODYNAMICS but before DYNAMICS
+C                            (use use old U,V,W for advection, but new T,S for GCHEM if
+C                            staggerTimeStep=.FALSE.; reproduces online with
+C                            staggerTimeStep=.FALSE. for LS_nIter=1)
+C                        2 - after DYNAMICS and second THERMODYNAMICS
+C                            (use new U,V,W and T,S; reproduces online with
+C                            staggerTimeStep=.TRUE. for LS_nIter=1)
 
-      INTEGER LS_nIter
-      LOGICAL LS_staggerTimeStep
-      LOGICAL LS_afterTS
-      COMMON /LONGSTEP_PARAMS/ LS_nIter, LS_staggerTimeStep,
-     &                         LS_afterTS
+      INTEGER LS_nIter, LS_whenToSample
+      LOGICAL LS_usePmEpR
+      COMMON /LONGSTEP_PARAMS/ LS_nIter, LS_whenToSample, LS_usePmEpR
 
 #endif /* ALLOW_LONGSTEP */
 
