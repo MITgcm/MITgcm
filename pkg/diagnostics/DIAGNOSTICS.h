dd249f8e4f083aa603880db55fb2afaa696ba4a8
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-01-15 00:24:37 +0000

    add internal flag to check if adding diag to the list from the right place.

--- pkg/diagnostics/DIAGNOSTICS.h
+++ pkg/diagnostics/DIAGNOSTICS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGNOSTICS.h,v 1.15 2008/05/22 08:36:05 mlosch Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGNOSTICS.h,v 1.16 2010/01/15 00:24:37 jmc Exp $
 C $Name:  $
 
 C ======================================================================
@@ -6,10 +6,10 @@ C  Common blocks for diagnostics package.
 C  - DIAG_DEFINE contains the definition of all available diagnostics
 C        ndiagt :: total number of available diagnostics
 C         kdiag :: number of levels associated with the diagnostic
-C         cdiag :: list of available diagnostic names
-C         tdiag :: description of field in diagnostic
-C         gdiag :: parser field with characteristics of the diagnostics
 C         hdiag :: mate number (in available diag. list) of the diagnostic
+C         cdiag :: list of available diagnostic names
+C         gdiag :: parser field with characteristics of the diagnostics
+C         tdiag :: description of field in diagnostic
 C         udiag :: physical units of the diagnostic field
 C  - DIAG_STORE  contains the large array to store diagnostic fields
 C         qdiag :: storage array for 2D/3D diagnostic fields
@@ -26,10 +26,10 @@ C ======================================================================
 C--   DIAG_DEFINE common block:
 C       ndiagt :: total number of available diagnostics
 C       kdiag  :: number of levels associated with the diagnostic
-C       cdiag  :: list of available diagnostic names
-C       tdiag  :: description of field in diagnostic
-C       gdiag  :: parser field with characteristics of the diagnostics
 C       hdiag  :: mate number (in available diag. list) of the diagnostic
+C       cdiag  :: list of available diagnostic names
+C       gdiag  :: parser field with characteristics of the diagnostics
+C       tdiag  :: description of field in diagnostic
 C       udiag  :: physical units of the diagnostic field
 
       INTEGER        ndiagt
@@ -40,9 +40,10 @@ C       udiag  :: physical units of the diagnostic field
       CHARACTER*16   gdiag(ndiagMax)
       CHARACTER*16   udiag(ndiagMax)
 
-      COMMON / DIAG_DEFINE /
-     &  ndiagt, kdiag, hdiag,
-     &  cdiag, tdiag, gdiag, udiag
+      COMMON / DIAG_DEFINE_I /
+     &  ndiagt, kdiag, hdiag
+      COMMON / DIAG_DEFINE_C /
+     &  cdiag, gdiag, tdiag, udiag
 
 C--   DIAG_STORE common block:
 C       qdiag  :: storage array for 2D/3D diagnostic fields
@@ -55,34 +56,38 @@ C       pdiag  :: index of current averaging interval within the averaging-cycle
       INTEGER  ndiag(numDiags,nSx,nSy)
       INTEGER  pdiag(numlists,nSx,nSy)
 
-      COMMON / DIAG_STORE / qdiag, qSdiag, ndiag, pdiag
+      COMMON / DIAG_STORE_R / qdiag, qSdiag
+      COMMON / DIAG_STORE_I / ndiag, pdiag
 
-	
 C--   DIAG_SELECT common block:
-C     freq        :: frequency (in s) to write output stream # n
-C     phase       :: phase     (in s) to write output stream # n
+C     freq(n)     :: frequency (in s) to write output stream # n
+C     phase(n)    :: phase     (in s) to write output stream # n
 C     averageFreq :: frequency (in s) for periodic averaging interval
 C     averagePhase:: phase     (in s) for periodic averaging interval
 C     averageCycle:: number of averaging intervals in 1 cycle
 C     misvalFlt(n):: missing value for floats   to use in output stream #n
 C     misvalInt(n):: missing value for integers to use in output stream #n
+C     levs(:,n)   :: list of selected levels to write for output stream # n
+C     nlevels(n)  :: number of levels to write for output stream # n
 C     nfields(n)  :: number of active diagnostics for output stream # n
 C     nActive(n)  :: number of active diagnostics (including counters)
 C                    for output stream # n
 C     nlists      :: effective number of output streams
-C     idiag       :: slot number in large diagnostic array
-C     mdiag       :: slot number in large diagnostic array for the mate
-C     jdiag       :: short-list (active diag.) to long-list (available diag.) pointer
+C     idiag(:,n)  :: list of diag slot number in long-list of available diag.
+C     mdiag(:,n)  :: list of mate slot number in long-list of available diag.
+C     jdiag(:,n)  :: short-list (active diag.) to long-list (available diag.) pointer
 C     flds(:,n)   :: list of field names in output stream # n
 C     fnames(n)   :: output file name for output stream # n
 C     fflags(n)   :: character string with per-file flags
+C    settingDiags :: internal flag: enable adding/changing available diagnostics list
+C     dumpAtLast  :: always write time-ave (freq>0) diagnostics at the end of the run
 
       _RL freq(numlists), phase(numlists)
-      _RL levs (numLevels,numlists)
       _RL averageFreq(numlists), averagePhase(numlists)
       _RL misvalFlt(numlists)
-      INTEGER misvalInt(numlists)
+      _RL levs (numLevels,numlists)
       INTEGER averageCycle(numlists)
+      INTEGER misvalInt(numlists)
       INTEGER nlevels(numlists)
       INTEGER nfields(numlists)
       INTEGER nActive(numlists)
@@ -90,37 +95,41 @@ C     fflags(n)   :: character string with per-file flags
       INTEGER idiag(numperlist,numlists)
       INTEGER mdiag(numperlist,numlists)
       INTEGER jdiag(numperlist,numlists)
-      CHARACTER*8 flds (numperlist,numlists)
+      CHARACTER*8  flds  (numperlist,numlists)
       CHARACTER*80 fnames(numlists)
-      CHARACTER*8 fflags(numlists)
+      CHARACTER*8  fflags(numlists)
+      LOGICAL settingDiags
       LOGICAL dumpAtLast, diag_mdsio,  diag_mnc
       LOGICAL diag_pickup_read,        diag_pickup_write
       LOGICAL diag_pickup_read_mdsio,  diag_pickup_write_mdsio
       LOGICAL diag_pickup_read_mnc,    diag_pickup_write_mnc
 
-      COMMON / DIAG_SELECT /
-     &     freq, phase, levs,
-     &     averageFreq, averagePhase, averageCycle,
-     &     misvalFlt,  misvalInt,
-     &     nlevels, nfields, nActive, nlists,
-     &     idiag, mdiag, jdiag,
-     &     dumpAtLast, diag_mdsio, diag_mnc,
+      COMMON / DIAG_SELECT_R /
+     &     freq, phase, averageFreq, averagePhase,
+     &     misvalFlt, levs
+      COMMON / DIAG_SELECT_I /
+     &     averageCycle, misvalInt,
+     &     nlevels, nfields, nActive,
+     &     nlists, idiag, mdiag, jdiag
+      COMMON / DIAG_SELECT_C /
+     &     flds, fnames, fflags
+      COMMON / DIAG_SELECT_L /
+     &     settingDiags, dumpAtLast, diag_mdsio, diag_mnc,
      &     diag_pickup_read,        diag_pickup_write,
      &     diag_pickup_read_mdsio,  diag_pickup_write_mdsio,
-     &     diag_pickup_read_mnc,    diag_pickup_write_mnc,
-     &     flds, fnames, fflags
+     &     diag_pickup_read_mnc,    diag_pickup_write_mnc
 
 C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
 
 C--   DIAG_STATIS common block:
-C     iSdiag :: slot number in large diagnostic array
-C     mSdiag :: slot number in large diagnostic array for the mate
-C     jSdiag :: short-list (active diag.) to long-list (available diag.)
-C               pointer
-C     diagSt_region(j,n) :: flag to perform (=1) or not (=0) regional-statistics
-C                           over region # j for output stream # n
 C     diagSt_freq(n)   :: frequency (in s) to write output stream # n
 C     diagSt_phase(n)  :: phase     (in s) to write output stream # n
+C     iSdiag(:,n)      :: list of diag slot number in long-list of available diag.
+C     mSdiag(:,n)      :: list of mate slot number in long-list of available diag.
+C     jSdiag(:,n)      :: short-list (active diag.) to long-list (available
+C                         diag.) pointer
+C     diagSt_region(j,n) :: flag to perform (=1) or not (=0) regional-statistics
+C                           over region # j for output stream # n
 C     diagSt_nbFlds(n) :: number of active diagnostics for output stream # n
 C     diagSt_nbActv(n) :: number of active diagnostics (including counters)
 C                         for output stream # n
@@ -130,24 +139,28 @@ C     diagSt_Flds(:,n) :: list of field names in output stream # n
 C     diagSt_Fname(n)  :: output file name for output stream # n
 
       _RL       diagSt_freq(numlists), diagSt_phase(numlists)
-      CHARACTER*8  diagSt_Flds(numperlist,numlists)
-      CHARACTER*80 diagSt_Fname(numlists)
       INTEGER   iSdiag(numperlist,numlists)
-      INTEGER   jSdiag(numperlist,numlists)
       INTEGER   mSdiag(numperlist,numlists)
+      INTEGER   jSdiag(numperlist,numlists)
       INTEGER   diagSt_region(0:nRegions,numlists)
       INTEGER   diagSt_nbFlds(numlists)
       INTEGER   diagSt_nbActv(numlists)
       INTEGER   diagSt_nbLists
       INTEGER   diagSt_ioUnit(numlists)
+      CHARACTER*8  diagSt_Flds(numperlist,numlists)
+      CHARACTER*80 diagSt_Fname(numlists)
       LOGICAL   diagSt_ascii, diagSt_mnc
-      COMMON / DIAG_STATIS /
-     &     diagSt_freq, diagSt_phase,
-     &     iSdiag, jSdiag, mSdiag, diagSt_region,
+
+      COMMON / DIAG_STATIS_R /
+     &     diagSt_freq, diagSt_phase
+      COMMON / DIAG_STATIS_I /
+     &     iSdiag, mSdiag, jSdiag, diagSt_region,
      &     diagSt_nbFlds, diagSt_nbActv, diagSt_nbLists,
-     &     diagSt_ioUnit,
-     &     diagSt_Ascii, diagSt_mnc,
+     &     diagSt_ioUnit
+      COMMON / DIAG_STATIS_C /
      &     diagSt_Flds, diagSt_Fname
+      COMMON / DIAG_STATIS_L /
+     &     diagSt_Ascii, diagSt_mnc
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
