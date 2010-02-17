18ef5132e1f4dd3d734ae1aa0a6c63d5ba8efb97
Gael Forget <gforget@mit.edu>
2010-02-17 23:39:37 +0000

    ALLOW_3D_VISCAH/ALLOW_3D_VISCA4 bug fix

--- pkg/mom_common/MOM_VISC.h
+++ pkg/mom_common/MOM_VISC.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_VISC.h,v 1.1 2009/09/17 16:57:13 dfer Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_VISC.h,v 1.2 2010/02/17 23:39:37 gforget Exp $
 C $Name:  $
 
 C- Common file for length scales 
@@ -16,5 +16,23 @@ C- Common file for length scales
       _RL L4rdt_D(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL L4rdt_Z(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL recip_dt
+
+#ifdef ALLOW_3D_VISCAH
+C     viscAhDfld, viscAhZfld :: full 3D specification of Laplacian Viscosity
+C               coeff. for mixing of momentum horizontally ( units of r^2/s )
+      COMMON /DYNVARS_3D_VISCAH/
+     &                  viscAhDfld, viscAhZfld
+      _RL  viscAhDfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  viscAhZfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+#endif
+
+#ifdef ALLOW_3D_VISCA4
+C     viscA4Dfld, viscA4Zfld :: full 3D specification of Bi-harmonic Viscosity
+C               coeff. for mixing of momentum horizontally ( units of r^2/s )
+      COMMON /DYNVARS_3D_VISCA4/
+     &                  viscA4Dfld, viscA4Zfld
+      _RL  viscA4Dfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  viscA4Zfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+#endif
 
 #endif /* ALLOW_MOM_COMMON */
