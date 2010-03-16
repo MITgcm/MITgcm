ff02675122b35628e3d0866acd164ddcaf402ecc
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-03-16 00:08:27 +0000

    avoid unbalanced quote (single or double) in commented line

--- model/inc/CG3D.h
+++ model/inc/CG3D.h
@@ -1,21 +1,21 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/CG3D.h,v 1.9 2005/12/20 20:08:34 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/CG3D.h,v 1.10 2010/03/16 00:04:00 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_NONHYDROSTATIC
 CBOP
 C     !ROUTINE: CG3D.h
-C     !INTERFACE: 
+C     !INTERFACE:
 C     include "CG3D.h"
 C     !DESCRIPTION: \bv
 C     *==========================================================*
-C     | CG3D.h                                                    
-C     | o Three-dimensional conjugate gradient solver header.     
+C     | CG3D.h
+C     | o Three-dimensional conjugate gradient solver header.
 C     *==========================================================*
-C     | The common blocks set up here are used in the elliptic    
-C     | equation inversion. They are also used as the interface   
-C     | to the rest of the model. To set the source term for the  
-C     | solver set the appropriate array below. To read the       
-C     | solution read from the appropriate array below.           
+C     | The common blocks set up here are used in the elliptic
+C     | equation inversion. They are also used as the interface
+C     | to the rest of the model. To set the source term for the
+C     | solver set the appropriate array below. To read the
+C     | solution read from the appropriate array below.
 C     *==========================================================*
 C     \ev
 CEOP
@@ -43,8 +43,8 @@ C     cg3dNorm - A matrix normalisation factor.
 
 C--   COMMON /CG3D_WK_R/  Work array common block
 C     cg3d_q - Intermediate matrix-vector product term
-C     cg3d_r -   "
-C     cg3d_s -   "
+C     cg3d_r -   idem
+C     cg3d_s -   idem
       COMMON /CG3D_WK_R/
      & cg3d_q, cg3d_r, cg3d_s
       _RL  cg3d_q(1-1  :sNx+1  ,1-1  :sNy+1  ,Nr,nSx,nSy)
