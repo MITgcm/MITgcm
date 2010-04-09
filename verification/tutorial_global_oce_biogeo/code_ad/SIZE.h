75feccb469efcae03b7390718254079cae32feda
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-04-09 15:41:36 +0000

    - for MPI test, change from 4 proc to only 2 proc (needed for daily testing) - change comments to get zero diff with code dir version.

--- verification/tutorial_global_oce_biogeo/code_ad/SIZE.h
+++ verification/tutorial_global_oce_biogeo/code_ad/SIZE.h
@@ -1,29 +1,37 @@
-C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code_ad/SIZE.h,v 1.2 2010/04/08 15:43:16 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code_ad/SIZE.h,v 1.3 2010/04/09 15:41:36 jmc Exp $
 C $Name:  $
+
 C
-C     /==========================================================\
-C     | SIZE.h Declare size of underlying computational grid.    |
-C     |==========================================================|
-C     | The design here support a three-dimensional model grid   |
-C     | with indices I,J and K. The three-dimensional domain     |
-C     | is comprised of nPx*nSx blocks of size sNx along one axis|
-C     | nPy*nSy blocks of size sNy along another axis and one    |
-C     | block of size Nz along the final axis.                   |
-C     | Blocks have overlap regions of size OLx and OLy along the|
-C     | dimensions that are subdivided.                          |
-C     \==========================================================/
+CBOP
+C    !ROUTINE: SIZE.h
+C    !INTERFACE:
+C    include SIZE.h
+C    !DESCRIPTION: \bv
+C     *==========================================================*
+C     | SIZE.h Declare size of underlying computational grid.     
+C     *==========================================================*
+C     | The design here support a three-dimensional model grid    
+C     | with indices I,J and K. The three-dimensional domain      
+C     | is comprised of nPx*nSx blocks of size sNx along one axis 
+C     | nPy*nSy blocks of size sNy along another axis and one     
+C     | block of size Nz along the final axis.                    
+C     | Blocks have overlap regions of size OLx and OLy along the 
+C     | dimensions that are subdivided.                           
+C     *==========================================================*
+C     \ev
+CEOP
 C     Voodoo numbers controlling data layout.
-C     sNx - No. X points in sub-grid.
-C     sNy - No. Y points in sub-grid.
-C     OLx - Overlap extent in X.
-C     OLy - Overlat extent in Y.
-C     nSx - No. sub-grids in X.
-C     nSy - No. sub-grids in Y.
-C     nPx - No. of processes to use in X.
-C     nPy - No. of processes to use in Y.
-C     Nx  - No. points in X for the total domain.
-C     Ny  - No. points in Y for the total domain.
-C     Nr  - No. points in Z for full process domain.
+C     sNx :: No. X points in sub-grid.
+C     sNy :: No. Y points in sub-grid.
+C     OLx :: Overlap extent in X.
+C     OLy :: Overlat extent in Y.
+C     nSx :: No. sub-grids in X.
+C     nSy :: No. sub-grids in Y.
+C     nPx :: No. of processes to use in X.
+C     nPy :: No. of processes to use in Y.
+C     Nx  :: No. points in X for the total domain.
+C     Ny  :: No. points in Y for the total domain.
+C     Nr  :: No. points in Z for full process domain.
       INTEGER sNx
       INTEGER sNy
       INTEGER OLx
@@ -56,5 +64,6 @@ C                routine buufers.
       PARAMETER ( MAX_OLX = OLx,
      &            MAX_OLY = OLy )
 
-      integer     nobcs
-      parameter ( nobcs = 4 )
+c for ctrl 
+       integer     nobcs
+       parameter ( nobcs = 4 )
