175a18b00a32a3ce0feacd9c70eb5fcd14944cbb
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-04-11 20:59:27 +0000

    change DIC atmos-CO2 box (method 3, dic_int1=3) initialisation & restart: - use binary pickup file for restart ; - initialise atmos-CO2 box from dic_pCO2 parameter. - fix multi-threaded.

--- pkg/dic/DIC_ATMOS.h
+++ pkg/dic/DIC_ATMOS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/dic/DIC_ATMOS.h,v 1.3 2008/04/24 21:35:53 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/dic/DIC_ATMOS.h,v 1.4 2010/04/11 20:59:27 jmc Exp $
 C $Name:  $
 
        COMMON /INTERACT_ATMOS_NEEDS/
@@ -8,8 +8,7 @@ C $Name:  $
      &              total_ocean_carbon_year,
      &              total_atmos_carbon_start,
      &              total_ocean_carbon_start,
-     &              total_atmos_carbon_ini,
-     &              atpco2_ini
+     &              atpco2
 
        _RL co2atmos(1000)
        _RL total_atmos_carbon
@@ -18,5 +17,4 @@ C $Name:  $
        _RL total_atmos_carbon_start
        _RL total_ocean_carbon_year
        _RL total_ocean_carbon_start
-       _RL total_atmos_carbon_ini
-       _RL atpco2_ini
+       _RL atpco2
