27073e4a807ce25154c565535284fa2610086beb
Dimitris Menemenlis <dmenemenlis@gmail.com>
2010-04-26 22:12:29 +0000

    cleaning up ALLOW_SUBGLACIAL_RUNOFF code

--- pkg/icefront/ICEFRONT_OPTIONS.h
+++ pkg/icefront/ICEFRONT_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/icefront/ICEFRONT_OPTIONS.h,v 1.3 2010/04/22 18:24:44 yunx Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/icefront/ICEFRONT_OPTIONS.h,v 1.4 2010/04/26 22:12:29 dimitri Exp $
 C $Name:  $
 
 C     /==========================================================\
@@ -17,8 +17,11 @@ C     \==========================================================/
 
 #include "CPP_OPTIONS.h"
 
+C--   Allow code for specifying sub-glacial runoff.
+C     Adds capability for time-evolving specification
+C     of addmass array based on a 2D field, which is added
+C     at bottommost wet level at each horizontal location.
 #undef ALLOW_SUBGLACIAL_RUNOFF
 
 #endif /* ALLOW_ICEFRONT */
 #endif /* ICEFRONT_OPTIONS_H */
-
