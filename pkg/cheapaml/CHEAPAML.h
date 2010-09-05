3379260551dadc73fb21cc25f8cfb95b24202014
Jean-Michel Campin <jmc@ocean.mit.edu>
2010-09-05 03:56:18 +0000

    -split common bloc per type -comment out unused variables

--- pkg/cheapaml/CHEAPAML.h
+++ pkg/cheapaml/CHEAPAML.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/cheapaml/CHEAPAML.h,v 1.2 2010/08/24 14:07:52 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/cheapaml/CHEAPAML.h,v 1.3 2010/09/05 03:56:18 jmc Exp $
 C $Name:  $
 
 c #ifdef ALLOW_CHEAPAML
@@ -15,48 +15,63 @@ C     qair :: atmosphere specific humidity
 C     uwind :: zonal wind
 C     vwind :: meridional wind
 C     solar :: short wave insolation
-C
-      COMMON /CHEAPAML_PARMS/
-     &       Tr,qr,uwind,vwind,solar,
-     &       ustress, vstress,Tair,gTairm,
-c     &       ustress, vstress,
-     &       cheapaml_h,
-c     &       cheapaml_kdiff, cheapaml_taurelax1,
-c     &       cheapaml_taurelax2, cheapaml_xf,
-c     &       cheapaml_dtc,rhoa,cpair,stefan,
-     &       cheapaml_kdiff, 
-     &       cheapaml_taurelax2, 
-     &       rhoa,cpair,stefan,
-     &       dsolms,dsolmn,
-     &       xphaseinit,
-     &       cdrag_1,cdrag_2,cdrag_3,cheapaml_ntim,
-     &       AirTempFile, AirQFile,SolarFile, UWindFile,
-     &       VWindFile, TrFile, QrFile, useFreshwaterFlux
-     
+
+      COMMON /CHEAPAML_VARS/
+     &       Tr, qr,
+     &       Tair, gTairm,
+c    &       qair,
+     &       uwind, vwind, solar,
+     &       ustress, vstress
+
       _RL    Tr  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL    qr  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL    Tair  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL    gTairm  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL    qair  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL    Solar (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
+c     _RL    qair  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL    uwind (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
       _RL    vwind (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
+      _RL    Solar (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
       _RL    ustress (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
-      _RL    vstress (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy) 
+      _RL    vstress (1-olx:snx+olx,1-oly:sny+oly,nsx,nsy)
+
+      COMMON /CHEAPAML_PARMS_R/
+     &       cheapaml_h,
+     &       cheapaml_kdiff,
+c    &       cheapaml_taurelax1,
+     &       cheapaml_taurelax2,
+c    &       cheapaml_xf,
+c    &       cheapaml_dtc,
+     &       rhoa,cpair,stefan,
+     &       dsolms,dsolmn,
+     &       xphaseinit,
+     &       cdrag_1,cdrag_2,cdrag_3
       _RL    cheapaml_h
-      _RL    cheapaml_kdiff, cheapaml_taurelax1
-      _RL    cheapaml_taurelax2, cheapaml_xf
-      _RL    cheapaml_dtc,rhoa,cpair,stefan
+      _RL    cheapaml_kdiff
+c     _RL    cheapaml_taurelax1
+      _RL    cheapaml_taurelax2
+c     _RL    cheapaml_xf
+c     _RL    cheapaml_dtc
+      _RL    rhoa, cpair, stefan
       _RL    cdrag_1, cdrag_2, cdrag_3
       _RL     dsolms,dsolmn
       _RL     xphaseinit
+
+      COMMON /CHEAPAML_PARMS_I/
+     &       cheapaml_ntim
       INTEGER cheapaml_ntim
+
+      COMMON /CHEAPAML_PARMS_L/
+     &       useFreshwaterFlux
+      LOGICAL useFreshwaterFlux
+
+      COMMON /CHEAPAML_PARMS_C/
+     &       AirTempFile, AirQFile,SolarFile, UWindFile,
+     &       VWindFile, TrFile, QrFile
       CHARACTER*(MAX_LEN_FNAM) AirTempFile
       CHARACTER*(MAX_LEN_FNAM) AirQFile
       CHARACTER*(MAX_LEN_FNAM) SolarFile
       CHARACTER*(MAX_LEN_FNAM) UWindFile
       CHARACTER*(MAX_LEN_FNAM) VWindFile
-      CHARACTER*(MAX_LEN_FNAM) TRFile 
-      CHARACTER*(MAX_LEN_FNAM) QrFile 
-      LOGICAL useFreshwaterFlux
+      CHARACTER*(MAX_LEN_FNAM) TRFile
+      CHARACTER*(MAX_LEN_FNAM) QrFile
 c #endif /* ALLOW_CHEAPAML */
