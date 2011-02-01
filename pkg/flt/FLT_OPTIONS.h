7c4c60e70ffa6ac4bdd351458ee8865b94b2fe96
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-02-01 17:08:30 +0000

    add CPP options to prevent floats to re-enter the opposite side of a periodic domain

--- pkg/flt/FLT_OPTIONS.h
+++ pkg/flt/FLT_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/flt/FLT_OPTIONS.h,v 1.2 2010/12/22 21:20:49 jahn Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/flt/FLT_OPTIONS.h,v 1.3 2011/02/01 17:08:30 jmc Exp $
 C $Name:  $
 
 C CPP options file for FLT package
@@ -10,23 +10,23 @@ C CPP options file for FLT package
 
 #include "CPP_OPTIONS.h"
 
-c Include/Exclude part that allows 3-dimensional advection of floats
-c 
+C Include/Exclude part that allows 3-dimensional advection of floats
 #define ALLOW_3D_FLT
 
-c Use the alternative method of adding random noise to float advection
-c 
+C Use the alternative method of adding random noise to float advection
 #define USE_FLT_ALT_NOISE
 
-c Add noise also to the vertical velocity of 3D floats
-c  
+C Add noise also to the vertical velocity of 3D floats
 #ifdef ALLOW_3D_FLT
 #define ALLOW_FLT_3D_NOISE
 #endif
 
-c define this to revert to old second-order Runge-Kutta integration
-c
+C Define this to revert to old second-order Runge-Kutta integration
 #undef  FLT_SECOND_ORDER_RUNGE_KUTTA
+
+C Prevent floats to re-enter the opposite side of a periodic domain (stop instead)
+#undef FLT_WITHOUT_X_PERIODICITY
+#undef FLT_WITHOUT_Y_PERIODICITY
 
 #endif /* ALLOW_FLT */
 #endif /* FLT_OPTIONS_H */
