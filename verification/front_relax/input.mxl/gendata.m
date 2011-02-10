5eacf8adecaa0871e4ad1cbb126df4453dc879e6
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-02-10 23:32:59 +0000

    Second bathymetry file (used with input.bvp) with sloping bottom.

--- verification/front_relax/input.mxl/gendata.m
+++ verification/front_relax/input.mxl/gendata.m
@@ -1,4 +1,7 @@
 
+% $Header: /u/gcmpack/MITgcm/verification/front_relax/input.mxl/gendata.m,v 1.2 2011/02/10 23:32:59 jmc Exp $
+% $Name:  $
+
 dzL=20;
 hup=200; kL=round(hup/dzL);
 N=15;
@@ -15,8 +18,8 @@ fprintf(' %2.0f.,',dd);fprintf('\n');
 %- uniform dz=20 m down to 200m, then increasing exponentially
 dz=[ones(1,kL)*dzL dd]; nz=size(dz,2);
 zF=[0 -cumsum(dz)]; zC=(zF(1:nz)+zF(2:nz+1))/2;
-dx=1.e+5; nx=32; 
-xF=[0:nx]-nx/2; xF=xF*dx; xC=(xF(1:nx)+xF(2:nx+1))/2;
+dy=1.e+5; ny=32;
+yF=[0:ny]-ny/2; yF=yF*dy; yC=(yF(1:ny)+yF(2:ny+1))/2;
 
 %- set initial temperature:
 N2=4.e-6;
@@ -32,12 +35,12 @@ dTdy=-slope*dTdz;
 fprintf(' N= %9.3e ; g= %5.3f ; alphaT= %9.3e ; dTdz= %e\n', ...
          sqrt(N2),gravity,alphaT,dTdz);
 
-Ly=24.*dx; yc=0; Ho=2205; 
-T=zeros(nx,nz); 
+Ly=24.*dy; yM=0; Ho=2205;
+T=zeros(ny,nz);
 km=6; kmx=6;
 for k=km:nz,
  T(:,k)=10 +dTdz*zC(k) ...
-           +dTdy*Ly*(tanh((xC-yc-200.e3)/200.e3)-tanh((xC-yc+200.e3)/200.e3))/2 ...
+           +dTdy*Ly*(tanh((yC-yM-200.e3)/200.e3)-tanh((yC-yM+200.e3)/200.e3))/2 ...
            *exp(-(3*zC(k)/Ho)^2);
 end
 %Create a ML
@@ -51,23 +54,23 @@ end
 
 %-- Thermal wind
 alphaT=2.e-4;
-dbdx=zeros(nx+1,nz);
-dbdx(2:nx,:)=gravity*alphaT*(T([2:nx],:)-T([1:nx-1],:))/dx;
-vg=zeros(nx+1,nz+1);
+dbdy=zeros(ny+1,nz);
+dbdy(2:ny,:)=gravity*alphaT*(T([2:ny],:)-T([1:ny-1],:))/dy;
+ug=zeros(ny+1,nz+1);
 for k=nz:-1:1;
- vg(:,k) = vg(:,k+1) - dbdx(:,k)*dz(k)/fo ;
+ ug(:,k) = ug(:,k+1) - dbdy(:,k)*dz(k)/fo ;
 end
-vCg=vg([1:nx],[1:nz])+vg([2:nx+1],[2:nz+1])  ...
-   +vg([2:nx+1],[1:nz])+vg([1:nx],[2:nz+1]);
-vCg=vCg/4;
-%size(vCg)
+uCg=ug([1:ny],[1:nz])+ug([2:ny+1],[2:nz+1])  ...
+   +ug([2:ny+1],[1:nz])+ug([1:ny],[2:nz+1]);
+uCg=uCg/4;
+%size(uCg)
 
 % Salt (passive tracer)
-S=zeros(nx,nz);
+S=zeros(ny,nz);
 for k=1:nz,
-%S(:,k)=ones(nx,1)+zC(k)/Ho;  % Linear with z
+%S(:,k)=ones(ny,1)+zC(k)/Ho;  % Linear with z
 %S(:,k)=exp(2*zC(k)/Ho);      % Exponential with z
- S(:,k)=exp(-(2*xC/Ly).^2);   % Exponential with y
+ S(:,k)=exp(-(2*yC/Ly).^2);   % Exponential with y
 end
 S=10+2*S;
 
@@ -76,9 +79,10 @@ S=10+2*S;
 figure(1);clf;
 subplot(211);
 var=squeeze(T); var(find(var==0))=NaN;
-%pcolor(yc,zc,var'); colorbar;
-[cs,h]=contour(xC,zC,var',[6:.5:13 13.3]);clabel(cs);
+%pcolor(yC,zC,var'); colorbar;
+[cs,h]=contour(yC,zC,var',[6:.5:13 13.3]);clabel(cs);
 grid
+title('t\_ini');
 
 subplot(223);
 var=T(1,:);
@@ -87,10 +91,10 @@ grid
 axis([9.4 10 -300 0]);
 subplot(224);
 plot(var,zC,'b-');
-var=T(nx/2,:);
+var=T(ny/2,:);
 hold on;
 plot(var,zC,'r-');
-i1=nx/2-3; i2=1+nx-i1;
+i1=ny/2-3; i2=1+ny-i1;
 var=T(i1,:);
 plot(var,zC,'g-');
 var=T(i2,:);
@@ -100,27 +104,40 @@ grid;
 
 figure(2);clf;
 subplot(211);
-var=squeeze(vCg);
+var=squeeze(uCg);
 %pcolor(yc,zc,var'); colorbar;
 cl=[-10:10]/100;
-[cs,h]=contour(xC(1:nx),zC,var',cl);clabel(cs);
+[cs,h]=contour(yC(1:ny),zC,var',cl);clabel(cs);
 grid
-subplot(313);
-plot(xC,S(:,1),'k-'); axis([xF(1) xF(end) 10 12.1]);
-%var=squeeze(S);
-%cl=[-10:10]/10;
-%[cs,h]=contour(xC(1:nx),zC,var',cl);clabel(cs);
+title('u\_ini');
+
+subplot(223);
+plot(yC,S(:,1),'k-'); axis([yF(1) yF(end) 10 12.1]);
+%var=squeeze(S); cl=[-10:10]/10;
+%[cs,h]=contour(yC(1:ny),zC,var',cl);clabel(cs);
 grid
+title('s\_ini');
 
-return
+hSlp=[1:ny]*8/(ny-2); hSlp=hSlp-mean(hSlp);
+hSlp=zF(end)+300*(1+tanh(hSlp)); hSlp(1)=0; hSlp(ny)=0;
+subplot(224);
+plot(yC,hSlp,'b-');
+AA=axis;
+axis([yF(1) yF(end) AA(3:4)]);
+grid
+title('topo\_slp');
 
-H=zF(end)*ones(nx,1);
-H(1)=0; H(nx)=0;
+%return
+
+H=zF(end)*ones(ny,1);
+H(1)=0; H(ny)=0;
 fid=fopen('topog.bin','w','b'); fwrite(fid,H,'real*8'); fclose(fid);
+
+fid=fopen('topo_slp.bin','w','b'); fwrite(fid,hSlp,'real*8'); fclose(fid);
 
 fid=fopen('t_ini.bin','w','b'); fwrite(fid,T,'real*8'); fclose(fid);
 
-fid=fopen('u_ini.bin','w','b'); fwrite(fid,vCg,'real*8'); fclose(fid);
+fid=fopen('u_ini.bin','w','b'); fwrite(fid,uCg,'real*8'); fclose(fid);
 
 fid=fopen('s_ini.bin','w','b'); fwrite(fid,S,'real*8'); fclose(fid);
 
