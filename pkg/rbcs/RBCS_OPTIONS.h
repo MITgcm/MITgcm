4724a084e6a0034063407cc7b35035dbfea9398b
Gael Forget <gforget@mit.edu>
2011-05-21 00:44:53 +0000

    - bug fix and add CPP key in RBCS_OPTIONS.h

--- pkg/rbcs/RBCS_OPTIONS.h
+++ pkg/rbcs/RBCS_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/rbcs/RBCS_OPTIONS.h,v 1.2 2011/05/14 19:52:12 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/rbcs/RBCS_OPTIONS.h,v 1.3 2011/05/21 00:44:53 gforget Exp $
 C $Name:  $
 
 C CPP options file for pkg RBCS
@@ -16,6 +16,10 @@ C Place CPP define/undef flag here
 
 C o disable relaxation conditions on momemtum
 #undef DISABLE_RBCS_MOM
+
+C use RBCS only in early spin-up phase, reducing
+C the relaxation term strength to 0 over 3 years
+#undef ALLOW_RBCS_SPIN
 
 #endif /* ALLOW_RBCS */
 #endif /* RBCS_OPTIONS_H */
