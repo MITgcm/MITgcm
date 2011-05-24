af9a0eb5430718ed64d770204b315e166d9c527c
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-05-24 02:50:05 +0000

    identical to the default version in pkg/ctrl or pkg/cost

--- verification/tutorial_dic_adjoffline/code_ad/CTRL_CPPOPTIONS.h
+++
@@ -1,8 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/verification/tutorial_dic_adjoffline/code_ad/Attic/CTRL_CPPOPTIONS.h,v 1.1 2009/10/16 16:49:47 heimbach Exp $
-C $Name:  $
-
-#include "CPP_OPTIONS.h"
-C
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
--- verification/tutorial_tracer_adjsens/code_ad/CTRL_CPPOPTIONS.h
+++
@@ -1,8 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/verification/tutorial_tracer_adjsens/code_ad/Attic/CTRL_CPPOPTIONS.h,v 1.2 2007/10/09 02:36:42 jmc Exp $
-C $Name:  $
-
-#include "CPP_OPTIONS.h"
-C
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
