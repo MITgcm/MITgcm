dc819abbf17cf78cde43684fcdaf20ff640528bd
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-05-24 17:09:19 +0000

    like standard {PKG}_OPTIONS.h file (but with AD_CONFIG.h)

--- pkg/ctrl/CTRL_CPPOPTIONS.h
+++ pkg/ctrl/CTRL_CPPOPTIONS.h
@@ -1,8 +1,29 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ctrl/Attic/CTRL_CPPOPTIONS.h,v 1.2 2007/10/09 00:00:00 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ctrl/Attic/CTRL_CPPOPTIONS.h,v 1.3 2011/05/24 17:08:10 jmc Exp $
 C $Name:  $
 
+CBOP
+C !ROUTINE: CTRL_CPPOPTIONS.h
+C !INTERFACE:
+C #include "CTRL_CPPOPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for Control (ctrl) package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
+CEOP
+
+#ifndef CTRL_OPTIONS_H
+#define CTRL_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "AD_CONFIG.h"
+#ifdef ALLOW_CTRL
+
 #include "CPP_OPTIONS.h"
-C
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
+
+C-- Package-specific options go here
+C   Note: most of these options have been shifted to the common header
+C         file ECCO_CPPOPTIONS.h
+
+#endif /* ALLOW_CTRL */
+#endif /* CTRL_OPTIONS_H */
--- pkg/grdchk/GRDCHK_OPTIONS.h
+++ pkg/grdchk/GRDCHK_OPTIONS.h
@@ -1,23 +1,23 @@
-C $Header: /u/gcmpack/MITgcm/pkg/cost/Attic/COST_CPPOPTIONS.h,v 1.3 2011/05/24 17:08:09 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/grdchk/GRDCHK_OPTIONS.h,v 1.1 2011/05/24 17:09:19 jmc Exp $
 C $Name:  $
 
 CBOP
-C !ROUTINE: COST_CPPOPTIONS.h
+C !ROUTINE: GRDCHK_OPTIONS.h
 C !INTERFACE:
-C #include "COST_CPPOPTIONS.h"
+C #include "GRDCHK_OPTIONS.h"
 
 C !DESCRIPTION:
 C *==================================================================*
-C | CPP options file for Cost-Function (cost) package:
+C | CPP options file for Gradient-Check (grdchk) package:
 C | Control which optional features to compile in this package code.
 C *==================================================================*
 CEOP
 
-#ifndef COST_OPTIONS_H
-#define COST_OPTIONS_H
+#ifndef GRDCHK_OPTIONS_H
+#define GRDCHK_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
 #include "AD_CONFIG.h"
-#ifdef ALLOW_COST
+#ifdef ALLOW_GRDCHK
 
 #include "CPP_OPTIONS.h"
 
@@ -25,5 +25,5 @@ C-- Package-specific options go here
 C   Note: most of these options have been shifted to the common header
 C         file ECCO_CPPOPTIONS.h
 
-#endif /* ALLOW_COST */
-#endif /* COST_OPTIONS_H */
+#endif /* ALLOW_GRDCHK */
+#endif /* GRDCHK_OPTIONS_H */
