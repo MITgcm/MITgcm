cf7f1ba516985f09d51eebdc2af48b1acfecf33e
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-05-24 17:06:28 +0000

    closer to standard {PKG}_OPTIONS.h file (but with AD_CONFIG.h); +fix some comments.

--- pkg/autodiff/AUTODIFF_OPTIONS.h
+++ pkg/autodiff/AUTODIFF_OPTIONS.h
@@ -1,25 +1,29 @@
-C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_OPTIONS.h,v 1.3 2004/03/04 04:00:12 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_OPTIONS.h,v 1.4 2011/05/24 17:06:28 jmc Exp $
 C $Name:  $
 
 CBOP
-C     !ROUTINE: AUTODIFF_CPPOPTIONS.h 
-C     !INTERFACE:
-C     include "AUTODIFF_CPPOPTIONS.h "
-C     !DESCRIPTION:
-C     *==========================================================*
-C     | CPP_EEMACROS.h                                            
-C     *==========================================================*
-C     CPP flags controlling which code is included in the files
-C     that will be compiled.
-C     These macros have been shifted to the common header file
-C     ECCO_CPPOPTIONS.h
+C !ROUTINE: AUTODIFF_OPTIONS.h
+C !INTERFACE:
+C #include "AUTODIFF_OPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for AUTODIFF package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
 CEOP
 
 #ifndef AUTODIFF_OPTIONS_H
 #define AUTODIFF_OPTIONS_H
-
 #include "PACKAGES_CONFIG.h"
 #include "AD_CONFIG.h"
+#ifdef ALLOW_AUTODIFF
+
 #include "CPP_OPTIONS.h"
 
-#endif /* AUTODIFF_CPPOPTIONS_H */
+C-- Package-specific options go here
+C   Note: most of these options have been shifted to the common header
+C         file ECCO_CPPOPTIONS.h
+
+#endif /* ALLOW_AUTODIFF */
+#endif /* AUTODIFF_OPTIONS_H */
