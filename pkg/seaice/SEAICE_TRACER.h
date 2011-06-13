3721cfe5e450c6db10e72199fe91af5b3dca5621
Gael Forget <gforget@mit.edu>
2011-06-13 23:21:18 +0000

    - added diagnostics for seaice genereic tracers (SItr*). - added SItrUnit and SItrNameLong run time param (for SItr* diags). - in diag names, replaced 'PrTh' abbrev. of 'preceeding thermo' with 'PT'.

--- pkg/seaice/SEAICE_TRACER.h
+++ pkg/seaice/SEAICE_TRACER.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/seaice/SEAICE_TRACER.h,v 1.3 2011/06/09 19:37:01 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/seaice/SEAICE_TRACER.h,v 1.4 2011/06/13 23:21:18 gforget Exp $
 C $Name:  $
 
 CBOP
@@ -46,11 +46,16 @@ CEOP
 #if (defined SEAICE_AGE)||(defined ALLOW_SITRACER)
 C     IceAgeTrFile    - File containing initial sea ice age
 C     SItrName        - tracer name ('salinity', 'age', 'one', etc.)
+C     SItrNameLong    - tracer long name
+C     SItrUnit        - tracer unit ('psu','s','kg/kg', etc.)
 C     SItrMate        - variable to which the tracer is associated ('HEFF','AREA',etc.)
       CHARACTER*(MAX_LEN_FNAM) IceAgeTrFile(SEAICE_num)
       CHARACTER*(MAX_LEN_FNAM) SItrName(SItrMaxNum)
-      CHARACTER*(MAX_LEN_FNAM) SItrMate(SItrMaxNum)
-      COMMON /SEAICE_AGE_C/ IceAgeTrFile, SItrName, SItrMate
+      CHARACTER*(MAX_LEN_FNAM) SItrNameLong(SItrMaxNum)
+      CHARACTER*(MAX_LEN_FNAM) SItrUnit(SItrMaxNum)
+      CHARACTER*(4) SItrMate(SItrMaxNum)
+      COMMON /SEAICE_AGE_C/ IceAgeTrFile, SItrName, SItrMate,
+     &  SItrNameLong, SItrUnit
 #endif
 
 CEH3 ;;; Local Variables: ***
