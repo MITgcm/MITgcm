d40ec425887f4ad1e159f17828dae609b5a7f1b5
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-07-14 22:27:22 +0000

    same as code_ad version

--- verification/OpenAD/code_oad/GMREDI_OPTIONS.h
+++ verification/OpenAD/code_oad/GMREDI_OPTIONS.h
@@ -1,14 +1,13 @@
-C $Header: /u/gcmpack/MITgcm/verification/OpenAD/code_oad/GMREDI_OPTIONS.h,v 1.1 2009/01/29 21:46:49 utke Exp $
+C $Header: /u/gcmpack/MITgcm/verification/OpenAD/code_oad/GMREDI_OPTIONS.h,v 1.2 2011/07/14 22:27:22 jmc Exp $
 C $Name:  $
 
 C CPP options file for GM/Redi package
 C
 C Use this file for selecting options within the GM/Redi package
-C
+
 #ifndef GMREDI_OPTIONS_H
 #define GMREDI_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
-
 #ifdef ALLOW_GMREDI
 
 #include "CPP_OPTIONS.h"
@@ -17,8 +16,10 @@ C Designed to simplify the Ajoint code:
 C  exclude the clipping/tapering part of the code that is not used
 #define GM_EXCLUDE_CLIPPING
 #define GM_EXCLUDE_AC02_TAP
-#undef GM_EXCLUDE_TAPERING 
- 
+#define GM_EXCLUDE_FM07_TAP
+#undef GM_EXCLUDE_TAPERING
+#define GM_EXCLUDE_SUBMESO
+
 C This allows to use Visbeck et al formulation to compute K_GM+Redi
 #undef GM_VISBECK_VARIABLE_K
 
@@ -34,6 +35,8 @@ C Allows to use the advective form (Bolus velocity) of GM
 C  instead of the Skew-Flux form (=default)
 #define  GM_BOLUS_ADVEC
 
+C Allows to use the Boundary-Value-Problem method to evaluate GM Bolus transport
+#undef GM_BOLUS_BVP
 
 #endif /* ALLOW_GMREDI */
 #endif /* GMREDI_OPTIONS_H */
