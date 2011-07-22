36965c0b7f007b7a4991a756141dce9d6e295d84
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-07-22 19:47:14 +0000

    add specific parameter for velocity-potential solver (diag_cg2d),  (default = main code CG2D params)

--- pkg/diagnostics/DIAGNOSTICS_CALC.h
+++ pkg/diagnostics/DIAGNOSTICS_CALC.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGNOSTICS_CALC.h,v 1.1 2011/07/06 01:43:49 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/diagnostics/DIAGNOSTICS_CALC.h,v 1.2 2011/07/22 19:47:14 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -17,15 +17,25 @@ C     \ev
 CEOP
 
 C     DIAGNOSTICS_CALC common block:
+C-- used in Velocity-Potential calculation:
+C     diagCG_maxIters  :: max number of iterations in diag_cg2d solver
+C     diagCG_prtResFrq :: frequency for printing residual in CG iterations
+C     diagCG_resTarget :: residual target for diag_cg2d solver (no units)
 C-- used in Stream-Function calculation:
 C     iPsi0,jPsi0 :: indices of grid-point location where Psi == 0
 C     xPsi0,yPsi0 :: grid-point coordinate where Psi == 0
 
+      INTEGER diagCG_maxIters
+      INTEGER diagCG_prtResFrq
       INTEGER iPsi0(nSx,nSy)
       INTEGER jPsi0(nSx,nSy)
+      _RL     diagCG_resTarget
       _RS     xPsi0, yPsi0
       COMMON / DIAGNOSTICS_CALC_I /
+     &     diagCG_maxIters, diagCG_prtResFrq,
      &     iPsi0, jPsi0
+      COMMON / DIAGNOSTICS_CALC_RL /
+     &     diagCG_resTarget
       COMMON / DIAGNOSTICS_CALC_RS /
      &     xPsi0, yPsi0
 
