f84df7761e0170c9d655ceae8731c55bb5e74ded
Oliver Jahn <jahn@mit.edu>
2011-08-10 22:33:32 +0000

    make name of global variable a little more unique

--- eesupp/src/sigreg.c
+++ eesupp/src/sigreg.c
@@ -1,5 +1,5 @@
 /*
- * $Header: /u/gcmpack/MITgcm/eesupp/src/sigreg.c,v 1.6 2007/07/31 22:48:34 ce107 Exp $
+ * $Header: /u/gcmpack/MITgcm/eesupp/src/sigreg.c,v 1.7 2011/08/10 22:33:32 jahn Exp $
  * $Name:  $
 
 //BOP
@@ -26,12 +26,12 @@
 #include <errno.h>
 #include <ucontext.h>
 
-int * ip;
+int * sigreg_ip;
 
 static void killhandler(
     unsigned int sn, siginfo_t  si, struct ucontext *sc )
 {
-    *ip = *ip + 1;
+    *sigreg_ip = *sigreg_ip + 1;
     return;
 }
 #endif
@@ -41,7 +41,7 @@ void FC_NAMEMANGLE(sigreg) (int * aip)
 {
 #ifdef HAVE_SIGREG
     struct sigaction s;
-    ip = aip;
+    sigreg_ip = aip;
     s.sa_flags = SA_SIGINFO;
     s.sa_sigaction = (void *)killhandler;
     if(sigaction (SIGTERM,&s,(struct sigaction *)NULL)) {
