5d0c92356bb586918755992f398ce52d8c9c5371
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-08-26 19:47:00 +0000

    - fix for multi-tiles (add bi,bj indices to exp/impMatrixCounter); - fix for multi-threaded (remove DATA statement); - use standard DIFFERENT_MULTIPLE function; - add MATRIX_OPTIONS.h

--- pkg/matrix/MATRIX.h
+++ pkg/matrix/MATRIX.h
@@ -1,13 +1,15 @@
-C $Header: /u/gcmpack/MITgcm/pkg/matrix/MATRIX.h,v 1.3 2010/08/24 15:06:25 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/matrix/MATRIX.h,v 1.4 2011/08/26 19:46:59 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_MATRIX
 
-      INTEGER expMatrixCounter, impMatrixCounter, expMatrixWriteCount,
-     &   impMatrixWriteCount
+      INTEGER expMatrixCounter(nSx,nSy), impMatrixCounter(nSx,nSy)
+      INTEGER expMatrixWriteCount, impMatrixWriteCount
       _RL expMatrixWriteTime, impMatrixWriteTime
-      COMMON /MATRIX_PARAMS/ expMatrixCounter, impMatrixCounter,
-     &   expMatrixWriteCount, impMatrixWriteCount,
+      COMMON /MATRIX_PARAMS_I/
+     &   expMatrixCounter, impMatrixCounter,
+     &   expMatrixWriteCount, impMatrixWriteCount
+      COMMON /MATRIX_PARAMS_R/
      &   expMatrixWriteTime, impMatrixWriteTime
 
       _RL MATRIX(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy,
---
+++ pkg/matrix/MATRIX_OPTIONS.h
@@ -0,0 +1,14 @@
+C $Header: /u/gcmpack/MITgcm/pkg/matrix/MATRIX_OPTIONS.h,v 1.1 2011/08/26 19:47:00 jmc Exp $
+C $Name:  $
+
+#ifndef MATRIX_OPTIONS_H
+#define MATRIX_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_MATRIX
+
+C- Matrix pkg Macros and Options go here
+
+#endif /* ALLOW_MATRIX */
+#endif /* MATRIX_OPTIONS_H */
