86542ec61e21376afee4fbee2fe85f0445e07cd4
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-10-27 20:48:55 +0000

    switch off SEAICE_ALLOW_FREEDRIFT (getting too many recomputations  after today modifications in seaice_dynsolver.F & seaice_lsr.F)

--- verification/lab_sea/code_ad/SEAICE_OPTIONS.h
+++ verification/lab_sea/code_ad/SEAICE_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/lab_sea/code_ad/SEAICE_OPTIONS.h,v 1.11 2011/04/28 02:09:00 ifenty Exp $
+C $Header: /u/gcmpack/MITgcm/verification/lab_sea/code_ad/SEAICE_OPTIONS.h,v 1.12 2011/10/27 20:48:55 jmc Exp $
 C $Name:  $
 
 C     *==========================================================*
@@ -111,7 +111,7 @@ C--   not recommended
 #undef SEAICE_ALLOW_CLIPVELS
 
 C     enable free drift code
-#define SEAICE_ALLOW_FREEDRIFT
+#undef SEAICE_ALLOW_FREEDRIFT
 
 #endif /* SEAICE_OPTIONS_H */
 
