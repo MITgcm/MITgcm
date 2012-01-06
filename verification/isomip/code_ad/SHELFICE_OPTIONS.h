b14f3e9bf7b4e82a563b3fbb6abcbadd010b1bee
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-01-06 17:06:01 +0000

    track old version ${PKG}_OPTIONS.h files which try to come back.

--- verification/isomip/code_ad/SHELFICE_OPTIONS.h
+++ verification/isomip/code_ad/SHELFICE_OPTIONS.h
@@ -1,21 +1,21 @@
-C $Header: /u/gcmpack/MITgcm/verification/isomip/code_ad/SHELFICE_OPTIONS.h,v 1.1 2012/01/06 13:01:09 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/verification/isomip/code_ad/SHELFICE_OPTIONS.h,v 1.2 2012/01/06 17:06:01 jmc Exp $
 C $Name:  $
 
-C     /==========================================================\
-C     | SHELFICE_OPTIONS.h                                       |
-C     | o CPP options file for SHELFICE package.                 |
-C     |==========================================================|
-C     | Use this file for selecting options within the SHELFICE  |
-C     | package.                                                 |
-C     \==========================================================/
+C     *==========================================================*
+C     | SHELFICE_OPTIONS.h
+C     | o CPP options file for SHELFICE package.
+C     *==========================================================*
+C     | Use this file for selecting options within the SHELFICE
+C     | package.
+C     *==========================================================*
 
 #ifndef SHELFICE_OPTIONS_H
 #define SHELFICE_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
 
 #ifdef ALLOW_SHELFICE
-
-#include "CPP_OPTIONS.h"
+C     Package-specific Options & Macros go here
 
 C     allow code for simple ISOMIP thermodynamics
 #define ALLOW_ISOMIP_TD
@@ -26,4 +26,3 @@ C     following Holland and Jenkins, JPO, 1999
 
 #endif /* ALLOW_SHELFICE */
 #endif /* SHELFICE_OPTIONS_H */
-
