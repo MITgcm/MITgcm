3c804129590480459d16bedb779eec2ddf9d97e5
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-01-13 21:08:41 +0000

    - remove YNEG, RIVER, TMIX from common block; - remove also areaNm1 & hEffNm1 when SEAICE_GROWTH_LEGACY is undef

--- pkg/seaice/SEAICE.h
+++ pkg/seaice/SEAICE.h
@@ -1,39 +1,35 @@
-C $Header: /u/gcmpack/MITgcm/pkg/seaice/SEAICE.h,v 1.59 2011/12/28 19:15:53 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/seaice/SEAICE.h,v 1.60 2012/01/13 21:08:41 jmc Exp $
 C $Name:  $
 
 CBOP
 C !ROUTINE: SEAICE.h
 
 C !DESCRIPTION: \bv
-C     /==========================================================\
-C     | SEAICE.h                                                 |
-C     | o Basic header for sea ice model.                        |
-C     |   Contains most sea ice field declarations.              |
-C     \==========================================================/
+C     *==========================================================*
+C     | SEAICE.h
+C     | o Basic header for sea ice model.
+C     |   Contains most sea ice field declarations.
+C     *==========================================================*
 C
-C     UICE  - zonal ice velocity in m/s at South-West B-grid
-C             (or C-grid #ifdef SEAICE_CGRID) U point
-C             >0 from West to East
-C     UICEC - average of UICE(1) between last two time steps
-C     VICE  - meridional ice velocity in m/s at South-West B-grid
-C             (or C-grid #ifdef SEAICE_CGRID) V point
-C             >0 from South to North
-C             note: the South-West B-grid U and V points are on
+C     UICE  :: zonal ice velocity in m/s at South-West B-grid
+C              (or C-grid #ifdef SEAICE_CGRID) U point
+C              >0 from West to East
+C     VICE  :: meridional ice velocity in m/s at South-West B-grid
+C              (or C-grid #ifdef SEAICE_CGRID) V point
+C              >0 from South to North
+C              note: the South-West B-grid U and V points are on
 C                the lower, left-hand corner of each grid cell
-C     VICEC - average of VICE(1) between last two time steps
-C     AREA  - fractional ice-covered area in m^2/m^2
-C             at center of grid, i.e., tracer point
-C             0 is no cover, 1 is 100% cover
-C     HEFF  - effective ice thickness in m
-C             at center of grid, i.e., tracer point
-C             note: for non-zero AREA, actual ice
-C                thickness is HEFF / AREA
-C     HSNOW - effective snow thickness in m
-C             at center of grid, i.e., tracer point
-C             note: for non-zero AREA, actual snow
-C                thickness is HSNOW / AREA
-C     HSALT - effective sea ice salinity in g/m^2
-C             at center of grid, i.e., tracer point
+C     AREA  :: fractional ice-covered area in m^2/m^2
+C              at center of grid, i.e., tracer point
+C              0 is no cover, 1 is 100% cover
+C     HEFF  :: effective ice thickness in m
+C              at center of grid, i.e., tracer point
+C              note: for non-zero AREA, actual ice thickness is HEFF / AREA
+C     HSNOW :: effective snow thickness in m
+C              at center of grid, i.e., tracer point
+C              note: for non-zero AREA, actual snow thickness is HSNOW / AREA
+C     HSALT :: effective sea ice salinity in g/m^2
+C              at center of grid, i.e., tracer point
 C \ev
 CEOP
 
@@ -74,15 +70,18 @@ C--   Dynamical variables
       _RL UICE       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL VICE       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
-      COMMON/SEAICE_DYNVARS_2/AREANM1,HEFFNM1,UICENM1,VICENM1
-      _RL HEFFNM1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL AREANM1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL UICENM1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL VICENM1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+#ifdef SEAICE_GROWTH_LEGACY
+      COMMON/SEAICE_DYNVARS_2/ areaNm1, hEffNm1
+      _RL areaNm1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL hEffNm1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+#endif /* SEAICE_GROWTH_LEGACY */
 
+C     uIceC :: average of UICE between last two time steps
+C     vIceC :: average of VICE between last two time steps
       COMMON/SEAICE_DYNVARS_3/
      &     ETA,ZETA,PRESS, e11, e22, e12,
-     &     DRAGS,DRAGA,FORCEX,FORCEY,UICEC,VICEC
+     &     DRAGS,DRAGA,FORCEX,FORCEY,
+     &     uIceC, vIceC, uIceNm1, vIceNm1
       _RL ETA        (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ZETA       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 C     ice strength/pressure term
@@ -96,8 +95,10 @@ C
       _RL DRAGA      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL FORCEX     (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL FORCEY     (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL UICEC      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL VICEC      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL uIceC      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL vIceC      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL uIceNm1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL vIceNm1    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
 #ifndef SEAICE_CGRID
       COMMON/SEAICE_DYNVARS_BGRID/ AMASS, DAIRN
@@ -125,11 +126,6 @@ C
       _RL HSALT      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 #endif
 
-      COMMON/OFL/YNEG
-      COMMON/RIV/RIVER
-      _RL YNEG       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL RIVER      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-
 C     saltWtrIce contains m of salty ice melted (<0) or created (>0)
 C     frWtrIce contains m of freshwater ice melted (<0) or created (>0)
 C              that is, ice due to precipitation or snow
@@ -148,6 +144,10 @@ C     frWtrAtm contains freshwater flux from the atmosphere
       COMMON/MULTICATEGORY/TICES
       _RL TICES      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,MULTDIM,nSx,nSy)
 #endif
+
+C     TICE  :: Seaice/snow surface temperature
+      COMMON/SEAICE_TEMPERATURE/ TICE
+      _RL TICE       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
 #if (defined (SEAICE_CGRID) && defined (SEAICE_ALLOW_FREEDRIFT))
       COMMON /SEAICE_FD_FIELDS/
@@ -178,10 +178,6 @@ C     stressDivergenceX/Y - divergence of stress tensor
       _RL stressDivergenceY(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 #endif /* SEAICE_CGRID */
 
-      COMMON/MIX/TMIX,TICE
-      _RL TMIX       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL TICE       (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-
       COMMON/WIND_STRESS_ICE/TAUX,TAUY
 C     TAUX   - zonal      wind stress over ice at U point
 C     TAUY   - meridional wind stress over ice at V point
@@ -211,13 +207,11 @@ C--   KGEO    Level used as a proxy for geostrophic velocity.
      &       uHeffExportCell, vHeffExportCell
 #endif
 
-cif(
-#ifdef SHORTWAVE_HEATING
-      _RL SWFRACB
+C     SWFracB :: fraction of surface Short-Wave radiation reaching
+C                the bottom of ocean surface level
+      _RL SWFracB
       COMMON /SEAICE_SW_R/
-     &       SWFRACB
-#endif
-cif)
+     &       SWFracB
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
