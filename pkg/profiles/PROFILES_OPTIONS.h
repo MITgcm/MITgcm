9ee4af2fbf54a3538430e01dcb3d5405e922418c
Gael Forget <gforget@mit.edu>
2012-02-28 00:20:49 +0000

    - add AD_CONFIG.h include, in case we test for ALLOW_AJDOINT_RUN.

--- pkg/profiles/PROFILES_OPTIONS.h
+++ pkg/profiles/PROFILES_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/profiles/PROFILES_OPTIONS.h,v 1.3 2011/12/24 01:09:39 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/profiles/PROFILES_OPTIONS.h,v 1.4 2012/02/28 00:20:49 gforget Exp $
 C $Name:  $
 
 C CPP options file for PROFILES package
@@ -6,6 +6,7 @@ C Use this file for selecting options within the PROFILES package
 
 #ifndef PROFILES_OPTIONS_H
 #define PROFILES_OPTIONS_H
+#include "AD_CONFIG.h"
 #include "PACKAGES_CONFIG.h"
 #include "CPP_OPTIONS.h"
 
