95dd4a8dd5391081033122bd50258e5b04e2927a
Gael Forget <gforget@mit.edu>
2012-03-03 03:34:43 +0000

    - SEAICE_OPTIONS.h : define MCPHEE_OCEAN_ICE_HEAT_FLUX and GRADIENT_MIXED_LAYER_TURBULENCE_FACTOR   that only serve the purpose of stopping in seaice_check to aknowledge the change of defaults.

--- verification/1D_ocean_ice_column/code/SEAICE_OPTIONS.h
+++ verification/1D_ocean_ice_column/code/SEAICE_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/1D_ocean_ice_column/code/SEAICE_OPTIONS.h,v 1.12 2012/02/13 23:26:58 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/verification/1D_ocean_ice_column/code/SEAICE_OPTIONS.h,v 1.13 2012/03/03 03:34:43 gforget Exp $
 C $Name:  $
 
 C     *==========================================================*
@@ -56,6 +56,11 @@ C--   The freezing point of water is a simple linear dependence to salinity.
 C     The flag below is now obsolete, but kept to ease the transition
 C     from the old constant freezing point default (see seaice_check.F).
 #define SEAICE_VARIABLE_FREEZING_POINT
+
+C--   The flags below are obsolete, but kept to ease the transition
+C     from the old default to the new one (see seaice_check.F).
+#define MCPHEE_OCEAN_ICE_HEAT_FLUX
+#define GRADIENT_MIXED_LAYER_TURBULENCE_FACTOR
 
 C--   By default cdm wind stress under sea-ice is set to the
 C     same value as it would be if there was no sea-ice.
