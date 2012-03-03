610e3cd50d563c4dfafa1da8d3da1c624a8b4002
Dimitris Menemenlis <dmenemenlis@gmail.com>
2012-03-03 16:04:38 +0000

    changing pkg/frazil forcing terms from K/s to W/m^2

--- pkg/frazil/FRAZIL.h
+++ pkg/frazil/FRAZIL.h
@@ -1,11 +1,11 @@
-C $Header: /u/gcmpack/MITgcm/pkg/frazil/FRAZIL.h,v 1.1 2012/03/02 01:45:22 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/frazil/FRAZIL.h,v 1.2 2012/03/03 16:04:38 dimitri Exp $
 C $Name:  $
 
 #ifdef ALLOW_FRAZIL
 
-C     frazil_TendT           - temperature tendency (Kelvin/s)
-      _RL frazil_TendT(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      COMMON /FRAZIL_TENDENCY/ frazil_TendT
+C     FrazilForcingT : frazil temperature forcing, > 0 increases theta [W/m^2]
+      _RL FrazilForcingT(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      COMMON /FRAZIL_FORCING/ FrazilForcingT
 
 #endif /* ALLOW_FRAZIL */
 
