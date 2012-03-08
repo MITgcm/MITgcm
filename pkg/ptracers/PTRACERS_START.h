9b39915e342fb9096d2d25bc36a785fff9065bff
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-03-08 17:05:44 +0000

    -rename PTRACERS_RESTART.h to PTRACERS_START.h -add run-time flag to switch on/off tracer time-stepping

--- pkg/ptracers/PTRACERS_START.h
+++ pkg/ptracers/PTRACERS_START.h
@@ -1,20 +1,21 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ptracers/Attic/PTRACERS_RESTART.h,v 1.1 2007/12/17 22:03:15 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_START.h,v 1.1 2012/03/08 17:05:44 jmc Exp $
 C $Name:  $
 
 CBOP
-C     !ROUTINE: PTRACERS_RESTART.h
+C     !ROUTINE: PTRACERS_START.h
 C     !INTERFACE:
-C     include "PTRACERS_RESTART.h"
+C     include "PTRACERS_START.h"
 C
 C     !DESCRIPTION:
 C     \bv
 C     *==========================================================*
-C     | PTRACERS_RESTART.h
+C     | PTRACERS_START.h
 C     | o Holds passive-tracer internal parameters related
-C     |   to restart process
+C     |   to start and restart process
 C     *==========================================================*
 C     | Passive-tracer internal parameters/variables related to
-C     |  writing or reading pickup for a restart.
+C     | a) which tracer is stepped forward
+C     | b) writing or reading pickup for a restart.
 C     | Note:
 C     |  external parameters (read from parameter file "data.ptracers")
 C     |  stay in PTRACERS_PARAMS.h and should not appear here ;
@@ -24,10 +25,16 @@ C     *==========================================================*
 C     \ev
 CEOP
 
-C--   COMMON / PTRACERS_RESTART_I / Integer valued parameters used for restart
+C--   COMMON / PTRACERS_START_I / Integer valued parameters used for (re)start
 C     PTRACERS_StartAB  :: number of gPtr previous time levels that are
 C                      available to start (or restart) Adams-Bashforth
-      COMMON / PTRACERS_RESTART_I /
+      COMMON / PTRACERS_START_I /
      &  PTRACERS_StartAB
       INTEGER PTRACERS_StartAB(PTRACERS_num)
+
+C--   COMMON / PTRACERS_START_L / Logical valued parameters used for (re)start
+C     PTRACERS_StepFwd  :: switch on/off this tracer time-stepping
+      COMMON / PTRACERS_START_L /
+     &  PTRACERS_StepFwd
+      LOGICAL PTRACERS_StepFwd(PTRACERS_num)
 
