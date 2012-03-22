ac632d43e34c25930a11d39c0b927fa4b67ce1f1
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-03-22 14:25:21 +0000

    reflect latest changes in standard version from pkg/fizhi

--- verification/fizhi-cs-aqualev20/code/FIZHI_OPTIONS.h
+++ verification/fizhi-cs-aqualev20/code/FIZHI_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-aqualev20/code/FIZHI_OPTIONS.h,v 1.3 2011/12/24 01:17:51 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-aqualev20/code/FIZHI_OPTIONS.h,v 1.4 2012/03/22 14:23:30 jmc Exp $
 C $Name:  $
 
 #ifndef FIZHI_OPTIONS_H
@@ -9,12 +9,10 @@ C $Name:  $
 #ifdef ALLOW_FIZHI
 C     Package-specific options go here
 
-C     #define FIZHI_DEBUG
-
 C     use fixed day in the year:
 #define FIZHI_USE_FIXED_DAY
 
-C     try new version of S/R GETPWHERE
+C     use new version of S/R GETPWHERE
 #define TRY_NEW_GETPWHERE
 
 #endif /* ALLOW_FIZHI */
