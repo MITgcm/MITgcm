4e6e19459ddc67b68eea2de6bcb4febe0e3a36c5
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-05-11 23:36:56 +0000

    by default, also compile CG2D_SR version (define ALLOW_SRCG)

--- model/inc/CPP_OPTIONS.h
+++ model/inc/CPP_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/CPP_OPTIONS.h,v 1.47 2012/03/16 21:04:28 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/CPP_OPTIONS.h,v 1.48 2012/05/11 23:36:56 jmc Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
@@ -28,8 +28,8 @@ C o Include/exclude Implicit vertical advection code
 C o Include/exclude AdamsBashforth-3rd-Order code
 #undef ALLOW_ADAMSBASHFORTH_3
 
-C o Include/exclude code for single reduction cg-solver
-#undef ALLOW_SRCG
+C o Include/exclude code for single reduction Conjugate-Gradient solver
+#define ALLOW_SRCG
 
 C o Include/exclude nonHydrostatic code
 #undef ALLOW_NONHYDROSTATIC
