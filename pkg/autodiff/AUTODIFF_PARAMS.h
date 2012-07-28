59af019e97239ee4400bf443a44e7aa7be2eff45
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-07-28 19:55:08 +0000

    - add new run-time params (dumpAdVarExch & mon_AdVarExch) to select how  to   apply ADEXCH before monitor or snap-shot of ad-variables:    =0 : no ADEXCH ; =1 : do ADEXCH ; =2 : apply ADEXCH on a local copy.

--- pkg/autodiff/AUTODIFF_PARAMS.h
+++ pkg/autodiff/AUTODIFF_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_PARAMS.h,v 1.2 2012/07/05 19:32:54 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_PARAMS.h,v 1.3 2012/07/28 19:55:08 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -35,6 +35,14 @@ C     turnFreeDriftInAdMode :: switch SEAICE to Free-Drift in adjoint mode (def=
      &       turnFreeDriftInAdMode
 
 C--   COMMON /AUTODIFF_PARM_I/ Integer valued parameters used by the pkg.
+C     dumpAdVarExch :: control ad-variables exchange before dumping output
+C     mon_AdVarExch :: control ad-variables exchange before monitor output
+C      - for both   :: =0 : no exch ; =1 : apply adexch ;
+C      *AdVarExch - :: =2 : do adexch on a local copy.
+      INTEGER dumpAdVarExch
+      INTEGER mon_AdVarExch
+      COMMON /AUTODIFF_PARM_I/
+     &       dumpAdVarExch, mon_AdVarExch
 
 C--   COMMON /AUTODIFF_PARM_R/ "Real" valued parameters used by the pkg.
 
