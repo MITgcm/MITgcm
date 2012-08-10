d73774e1e6434847e06213310d5cb2d48a354388
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-08-10 17:37:20 +0000

    - clean-up and split content of old pkg/ecco/ECCO_CPPOPTIONS.h file into   separated pkg option-file (done according to old doc, not necessary accurate)

--- pkg/autodiff/AUTODIFF_OPTIONS.h
+++ pkg/autodiff/AUTODIFF_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_OPTIONS.h,v 1.6 2012/07/06 23:12:45 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_OPTIONS.h,v 1.7 2012/08/10 17:37:20 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -8,7 +8,7 @@ C #include "AUTODIFF_OPTIONS.h"
 
 C !DESCRIPTION:
 C *==================================================================*
-C | CPP options file for AUTODIFF package:
+C | CPP options file for AutoDiff (autodiff) package:
 C | Control which optional features to compile in this package code.
 C *==================================================================*
 CEOP
@@ -19,9 +19,40 @@ CEOP
 #include "CPP_OPTIONS.h"
 
 #ifdef ALLOW_AUTODIFF
-C-- Package-specific options go here
-C   Note: most of these options have been shifted to the common header
-C         file ECCO_CPPOPTIONS.h
+#ifdef ECCO_CPPOPTIONS_H
 
+C-- When multi-package option-file ECCO_CPPOPTIONS.h is used (directly included
+C    in CPP_OPTIONS.h), this option file is left empty since all options that
+C   are specific to this package are assumed to be set in ECCO_CPPOPTIONS.h
+
+#else /* ndef ECCO_CPPOPTIONS_H */
+C   ==================================================================
+C-- Package-specific Options & Macros go here
+
+C o Include/exclude code in order to be able to automatically
+C   differentiate the MITgcmUV by using the Tangent Linear and
+C   Adjoint Model Compiler (TAMC).
+#define ALLOW_AUTODIFF_TAMC
+
+C       >>> Checkpointing as handled by TAMC
+#define ALLOW_TAMC_CHECKPOINTING
+
+C       >>> Extract adjoint state
+#define ALLOW_AUTODIFF_MONITOR
+
+C       >>> DO 2-level checkpointing instead of 3-level
+c#undef AUTODIFF_2_LEVEL_CHECKPOINT
+
+C extend to 4-level checkpointing
+c#undef AUTODIFF_4_LEVEL_CHECKPOINT
+
+C o use divided adjoint to split adjoint computations
+#undef ALLOW_DIVIDED_ADJOINT
+
+C o Enable some temporary constructs for OpenAD
+#undef ALLOW_AUTODIFF_OPENAD
+
+C   ==================================================================
+#endif /* ndef ECCO_CPPOPTIONS_H */
 #endif /* ALLOW_AUTODIFF */
 #endif /* AUTODIFF_OPTIONS_H */
