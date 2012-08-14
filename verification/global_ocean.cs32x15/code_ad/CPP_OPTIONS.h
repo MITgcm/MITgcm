4dd75565359f4682510254d4496a0f837f482b53
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-08-14 03:09:00 +0000

    updated to reduce diff with standard version (from model/inc): - remove #include "PACKAGES_CONFIG.h" (adopted for all fwd built since Oct 2003) - avoid including ECCO_CPPOPTIONS.h if PACKAGES_CONFIG.h has not be included   (consistent with ECCO_CPPOPTIONS.h beeing a multi-PKG option file); - set ALLOW_3D_DIFFKR independently of ALLOW_DIFFKR_CONTROL which should   now be defined (in ECCO_CPPOPTIONS.h) only if ALLOW_3D_DIFFKR is defined.

--- verification/global_ocean.cs32x15/code_ad/CPP_OPTIONS.h
+++ verification/global_ocean.cs32x15/code_ad/CPP_OPTIONS.h
@@ -1,10 +1,20 @@
-C $Header: /u/gcmpack/MITgcm/verification/global_ocean.cs32x15/code_ad/CPP_OPTIONS.h,v 1.5 2012/08/07 21:24:55 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/verification/global_ocean.cs32x15/code_ad/CPP_OPTIONS.h,v 1.6 2012/08/14 03:08:59 jmc Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
 #define CPP_OPTIONS_H
 
-#include "PACKAGES_CONFIG.h"
+CBOP
+C !ROUTINE: CPP_OPTIONS.h
+C !INTERFACE:
+C #include "CPP_OPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | main CPP options file for the model:
+C | Control which optional features to compile in model/src code.
+C *==================================================================*
+CEOP
 
 C CPP flags controlling particular source code features
 
@@ -21,6 +31,9 @@ C o Include/exclude call to S/R CONVECT
 C o Include/exclude call to S/R CALC_DIFFUSIVITY
 #define INCLUDE_CALC_DIFFUSIVITY_CALL
 
+C o Allow full 3D specification of vertical diffusivity
+#define ALLOW_3D_DIFFKR
+
 C o Allow latitudinally varying BryanLewis79 vertical diffusivity
 #undef ALLOW_BL79_LAT_VARY
 
@@ -32,6 +45,10 @@ C o Include/exclude AdamsBashforth-3rd-Order code
 
 C o Include/exclude nonHydrostatic code
 #undef ALLOW_NONHYDROSTATIC
+
+C o Allow mass source or sink of Fluid in the interior
+C   (3-D generalisation of oceanic real-fresh water flux)
+#undef ALLOW_ADDFLUID
 
 C o Include pressure loading code
 #define ATMOSPHERIC_LOADING
@@ -47,6 +64,9 @@ C o Include/exclude balancing surface forcing fluxes code
 C o Include/exclude balancing surface forcing relaxation code
 #define ALLOW_BALANCE_RELAX
 
+C o Include/exclude GM-like eddy stress in momentum code
+#undef ALLOW_EDDYPSI
+
 C o Use "Exact Convervation" of fluid in Free-Surface formulation
 C   so that d/dt(eta) is exactly equal to - Div.Transport
 #define EXACT_CONSERV
@@ -54,8 +74,17 @@ C   so that d/dt(eta) is exactly equal to - Div.Transport
 C o Allow the use of Non-Linear Free-Surface formulation
 C   this implies that surface thickness (hFactors) vary with time
 #define NONLIN_FRSURF
-#undef DISABLE_RSTAR_CODE
+#undef  DISABLE_RSTAR_CODE
 #define DISABLE_SIGMA_CODE
+
+C o Include/exclude code for single reduction Conjugate-Gradient solver
+#undef ALLOW_SRCG
+
+C o Choices for implicit solver routines solve_*diagonal.F
+C   The following has low memory footprint, but not suitable for AD
+#undef SOLVE_DIAGONAL_LOWMEMORY
+C   The following one suitable for AD but does not vectorize
+#undef SOLVE_DIAGONAL_KINNER
 
 C o ALLOW isotropic scaling of harmonic and bi-harmonic terms when
 C   using an locally isotropic spherical grid with (dlambda) x (dphi*cos(phi))
@@ -80,11 +109,6 @@ C   The definition of the flag is commented to avoid interference with
 C   such other header files.
 C#define COSINEMETH_III
 
-C o Use "OLD" UV discretisation near boundaries (*not* recommended)
-C   Note - only works with  #undef NO_SLIP_LATERAL  in calc_mom_rhs.F
-C          because the old code did not have no-slip BCs
-#undef  OLD_ADV_BCS
-
 C o Use LONG.bin, LATG.bin, etc., initialization for ini_curviliear_grid.F
 C   Default is to use "new" grid files (OLD_GRID_IO undef) but OLD_GRID_IO
 C   is still useful with, e.g., single-domain curvilinear configurations.
@@ -93,19 +117,14 @@ C   is still useful with, e.g., single-domain curvilinear configurations.
 C o Execution environment support options
 #include "CPP_EEOPTIONS.h"
 
-#ifdef ALLOW_AUTODIFF
+C o Include/exclude single header file containing multiple packages options
+C   (AUTODIFF, COST, CTRL, ECCO, EXF ...) instead of the standard way where
+C   each of the above pkg get its own options from its specific option file.
+C   Although this method, inherited from ECCO setup, has been traditionally
+C   used for all adjoint built, work is in progress to allow to use the
+C   standard method also for adjoint built.
+#ifdef PACKAGES_CONFIG_H
 # include "ECCO_CPPOPTIONS.h"
 #endif
 
-C o Allow full 3D specification of vertical diffusivity
-#ifdef ALLOW_DIFFKR_CONTROL
-C - Need to be defined if using DIFFKR_CONTROL
-C   (alternatively, could have put this in ECCO_CPPOPTIONS)
-#define ALLOW_3D_DIFFKR
-#else
-C - otherwise, can be turned on or off hereafter:
-#undef  ALLOW_3D_DIFFKR
-#endif /* ALLOW_DIFFKR_CONTROL */
-
 #endif /* CPP_OPTIONS_H */
-
