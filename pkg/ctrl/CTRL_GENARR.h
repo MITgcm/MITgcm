e9013668606df9ef76e7a40c056427a7d800458d
Patrick Heimbach <heimbach@mit.edu>
2012-09-11 23:46:15 +0000

    Enable packing/unpacking of generic control variables. Compiles, but not yet tested.

--- pkg/ctrl/CTRL_GENARR.h
+++ pkg/ctrl/CTRL_GENARR.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ctrl/CTRL_GENARR.h,v 1.1 2012/07/31 16:05:56 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ctrl/CTRL_GENARR.h,v 1.2 2012/09/11 23:46:15 heimbach Exp $
 C $Name:  $
 
 c     ==================================================================
@@ -11,6 +11,9 @@ c     ==================================================================
       character*(MAX_LEN_FNAM)
      &     xx_genarr2d_file(maxCtrlArr2D)
      &   , xx_genarr3d_file(maxCtrlArr3D)
+     
+      character*( 80) fname_genarr2d(3,maxCtrlArr2D)
+      character*( 80) fname_genarr3d(3,maxCtrlArr3D)
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
