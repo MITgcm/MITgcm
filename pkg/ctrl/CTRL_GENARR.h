33df9605c0bf97063c89d44bcf1e09e84e915885
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-09-12 22:20:06 +0000

    Use the same local file-name variable (defined locally, instead of in  CTRL_GENARR.h) for different variables. For now, only implemented for  GENARR2/3D_CONTROL (provided as an example for future extension).

--- pkg/ctrl/CTRL_GENARR.h
+++ pkg/ctrl/CTRL_GENARR.h
@@ -1,23 +1,17 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ctrl/CTRL_GENARR.h,v 1.2 2012/09/11 23:46:15 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ctrl/CTRL_GENARR.h,v 1.3 2012/09/12 22:20:06 jmc Exp $
 C $Name:  $
 
-c     ==================================================================
-c     CTRL_GENARR.h
-c     ==================================================================
+C     ==================================================================
+C     CTRL_GENARR.h
+C     ==================================================================
 
-      common /controlfiles_carr/
-     &     xx_genarr2d_file
-     &   , xx_genarr3d_file
-      character*(MAX_LEN_FNAM)
-     &     xx_genarr2d_file(maxCtrlArr2D)
-     &   , xx_genarr3d_file(maxCtrlArr3D)
-     
-      character*( 80) fname_genarr2d(3,maxCtrlArr2D)
-      character*( 80) fname_genarr3d(3,maxCtrlArr3D)
+      COMMON /CONTROLFILES_CARR/
+     &     xx_genarr2d_file,
+     &     xx_genarr3d_file
+
+      CHARACTER*(MAX_LEN_FNAM) xx_genarr2d_file(maxCtrlArr2D)
+      CHARACTER*(MAX_LEN_FNAM) xx_genarr3d_file(maxCtrlArr3D)
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
 CEH3 ;;; End: ***
-
-
-
