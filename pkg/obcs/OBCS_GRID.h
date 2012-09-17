c816a8028495fde053df6372bcdd58894582ce00
Jean-Michel Campin <jmc@ocean.mit.edu>
2012-09-17 21:47:33 +0000

    - change settings of OB index in case OB coincides with tile edge (shared by   2 tiles), so that common OB location is registered by both tiles (instead of   only 1). However, for Southern and Western OB, index value = 0 is not   recognized as valid OB index (=default null value) and this needs to be fixed. - new parameter "OB_indexNone" for null index value (for now, still zero)   to replace hard-coded null index value of zero.

--- pkg/obcs/OBCS_GRID.h
+++ pkg/obcs/OBCS_GRID.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/obcs/OBCS_GRID.h,v 1.3 2012/01/20 01:26:15 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/obcs/OBCS_GRID.h,v 1.4 2012/09/17 21:47:33 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_OBCS
@@ -22,6 +22,7 @@ CEOP
 C tileHasOB[N,S,E,W] :: this tile has OB at Northern/Southern/Eastern/Western edge
 C   OB_Jn, OB_Js     :: indices of Northern & Southern OB location (cell center pt)
 C   OB_Ie, OB_Iw     :: indices of  Eastern & Western  OB location (cell center pt)
+C   OB_indexNone     :: null value for OB index (if row or column without OB)
 C OBCS_indexStatus   :: track status of OB indices setting
 C OBCS_insideMask    :: Inside OB region mask (zero beyond OB).
 
@@ -34,11 +35,12 @@ C OBCS_insideMask    :: Inside OB region mask (zero beyond OB).
 
       COMMON /OBCS_GRID_INDICES/
      &  OB_Jn, OB_Js, OB_Ie, OB_Iw,
-     &  OBCS_indexStatus
+     &  OB_indexNone, OBCS_indexStatus
       INTEGER OB_Jn(1-OLx:sNx+OLx,nSx,nSy)
       INTEGER OB_Js(1-OLx:sNx+OLx,nSx,nSy)
       INTEGER OB_Ie(1-OLy:sNy+OLy,nSx,nSy)
       INTEGER OB_Iw(1-OLy:sNy+OLy,nSx,nSy)
+      INTEGER OB_indexNone
       INTEGER OBCS_indexStatus
 
       COMMON /OBCS_MASK_RS/
