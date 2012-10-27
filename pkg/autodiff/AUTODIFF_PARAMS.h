e6026aeb57636dd67753b7fe0ac06afe4c500dcb
Patrick Heimbach <heimbach@mit.edu>
2012-10-27 00:38:21 +0000

    Add inAdMode switch for SEAICEuseDYNAMICS (extend logic in ADAUTODIFF_INADMODE_SET)

--- pkg/autodiff/AUTODIFF_PARAMS.h
+++ pkg/autodiff/AUTODIFF_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_PARAMS.h,v 1.5 2012/09/04 14:31:29 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/autodiff/AUTODIFF_PARAMS.h,v 1.6 2012/10/27 00:38:21 heimbach Exp $
 C $Name:  $
 
 CBOP
@@ -28,6 +28,7 @@ C     turnFreeDriftInAdMode :: switch SEAICE to Free-Drift in adjoint mode (def=
       LOGICAL useGGL90inAdMode,    useGGL90inFwdMode
       LOGICAL useSALT_PLUMEinAdMode, useSALT_PLUMEInFwdMode
       LOGICAL turnFreeDriftInAdMode
+      LOGICAL SEAICEuseDYNAMICSinAdMode, SEAICEuseDYNAMICSinFwdMode
 
 C-    Logical for ad dump format (if true then write all records 
 C       to one file per variable; else write one file per record)
@@ -40,7 +41,8 @@ C       to one file per variable; else write one file per record)
      &       useSEAICEinAdMode, useSEAICEinFwdMode,
      &       useGGL90inAdMode,    useGGL90inFwdMode,
      &       useSALT_PLUMEinAdMode, useSALT_PLUMEInFwdMode,
-     &       turnFreeDriftInAdMode, dumpAdByRec
+     &       turnFreeDriftInAdMode, dumpAdByRec,
+     &       SEAICEuseDYNAMICSinAdMode, SEAICEuseDYNAMICSinFwdMode
 
 C--   COMMON /AUTODIFF_PARM_I/ Integer valued parameters used by the pkg.
 C     dumpAdVarExch :: control ad-variables exchange before dumping output
