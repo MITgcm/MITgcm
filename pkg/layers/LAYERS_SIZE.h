7c344c6bbf24e842b88a304c8cb0a8c5de8dba6c
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-01-08 21:52:34 +0000

    add documentation for "layers_maxNum" (missing from previous modif)

--- pkg/layers/LAYERS_SIZE.h
+++ pkg/layers/LAYERS_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/layers/LAYERS_SIZE.h,v 1.2 2012/09/19 18:48:18 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/layers/LAYERS_SIZE.h,v 1.3 2013/01/08 21:52:34 jmc Exp $
 C $Name:  $
 
 C ======================================================================
@@ -9,10 +9,11 @@ C    of vertical layers for isopycnal averaging so that the proper array
 C    sizes can be declared in the LAYERS.h header file.
 C
 C  - Variables -
-C        NLayers      :: the number if isopycnal layers (must match data.layers)
-C        FineGridFact :: how many fine-grid cells per dF cell
-C        FineGridMax  :: the number of points in the finer vertical grid
-C                         used for interpolation
+C      NLayers        :: the number of isopycnal layers (must match data.layers)
+C      FineGridFact   :: how many fine-grid cells per dF cell
+C      FineGridMax    :: the number of points in the finer vertical grid
+C                        used for interpolation
+C      layers_maxNum  :: max number of tracer fields used for layer averaging
       INTEGER    Nlayers, FineGridFact, FineGridMax, layers_maxNum
       PARAMETER( Nlayers = 20 )
       PARAMETER( FineGridFact = 10 )
