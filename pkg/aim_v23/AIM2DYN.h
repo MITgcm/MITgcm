07f2439e50de1b3bbf0f491f6484cf174daa68ad
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-01-21 21:55:32 +0000

    store near surface wind-speed in specific array (aim_surfWind) for export  to coupler.

--- pkg/aim_v23/AIM2DYN.h
+++ pkg/aim_v23/AIM2DYN.h
@@ -1,25 +1,28 @@
-C $Header: /u/gcmpack/MITgcm/pkg/aim_v23/AIM2DYN.h,v 1.2 2005/06/23 20:02:50 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/aim_v23/AIM2DYN.h,v 1.3 2013/01/21 21:55:32 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_AIM
+C     *==========================================================*
+C     | AIM2DYN.h
+C     | o AIM output fields in dynamics conforming arrays
+C     *==========================================================*
 
-C     /==========================================================\
-C     | AIM2DYN.h                                                |
-C     | o AIM output fields in dynamics conforming arrays        |
-C     \==========================================================/
-
-C     aim_drag - Holds AIM surface drag term : C_drag*Rho*|V| ( kg.m-2.s-1 )
+C--   COMMON /AIM_DDT/
+C     aim_drag :: Holds AIM surface drag term : C_drag*Rho*|V| ( kg.m-2.s-1 )
 C                location : AIM "A" grid = center of the dynamics "C" grid
-C     aim_dTdt - Net tendency for potential temperature ( oK/s    )
-C     aim_dSdt - Net tendency for water vapor           ( g/kg/s )
+C     aim_dTdt :: Net tendency for potential temperature ( K/s    )
+C     aim_dSdt :: Net tendency for water vapor           ( g/kg/s )
+C aim_surfWind :: near surface atmospheric wind speed    ( m/s )
 C
       COMMON /AIM_DDT/
-     &                   aim_drag, 
+     &                   aim_drag,
      &                   aim_dTdt,
-     &                   aim_dSdt
+     &                   aim_dSdt,
+     &                   aim_surfWind
       _RL  aim_drag  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  aim_dTdt  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  aim_dSdt  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  aim_surfWind (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
 #endif /* ALLOW_AIM */
 
