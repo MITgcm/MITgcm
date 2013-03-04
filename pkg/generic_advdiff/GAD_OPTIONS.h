d077f4a7023faea230432dc180400e4970aff0ff
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-03-04 18:20:46 +0000

    implement compressible flow method for multi-dim advection (similar to  gad_som_advect.F); add new option "GAD_MULTIDIM_COMPRESSIBLE" (since  TAF generates many recomputations) to use it; no yet coded with implicit  vertical advection.

--- pkg/generic_advdiff/GAD_OPTIONS.h
+++ pkg/generic_advdiff/GAD_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/generic_advdiff/GAD_OPTIONS.h,v 1.15 2011/12/24 01:04:47 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/generic_advdiff/GAD_OPTIONS.h,v 1.16 2013/03/04 18:20:46 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -40,6 +40,11 @@ C As of checkpoint41, the inclusion of multi-dimensional advection
 C introduces excessive recomputation/storage for the adjoint.
 C We can disable it here using CPP because run-time flags are insufficient.
 #undef DISABLE_MULTIDIM_ADVECTION
+
+C Use compressible flow method for multi-dim advection instead of old, less
+C accurate jmc method. Note: option has no effect on SOM advection which
+C always use compressible flow method.
+#undef GAD_MULTIDIM_COMPRESSIBLE
 
 C This enable the use of 2nd-Order Moment advection scheme (Prather, 1986) for
 C Temperature and Salinity ; due to large memory space (10 times more / tracer)
