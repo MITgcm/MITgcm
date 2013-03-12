bbb0f3777d47239aee96dc8c2717cd07c134f169
Dimitris Menemenlis <dmenemenlis@gmail.com>
2013-03-12 16:32:37 +0000

    a few more tweaks to color scale range, plus added label >>> but note that pcolor will create discontinuities at edges >>> use mycrossmap.m instead (to be checked in soon)

--- utils/matlab/cs_grid/read_cs/crossmap.m
+++ utils/matlab/cs_grid/read_cs/crossmap.m
@@ -1,4 +1,4 @@
-function crossmap(TS,cx)
+function crossmap(TS,cx,label)
 
 % Function crossmap(fld,cx)
 % plot a cube sphere field as a cross
@@ -6,10 +6,14 @@ function crossmap(TS,cx)
 % INPUTS
 % fld   input cube sphere field (n,6,n) where n is the face dimension
 % cx    two element vector [cmin cmax] sets scaling for caxis
+% label colorbar label
 
 if nargin<2
 cx=[min(TS(:)) max(TS(:))];
-cx=[-1 1]*min(abs(cx));
+end
+
+if nargin<3
+label='';
 end
 
 a1=squeeze(TS(:,1,:));
@@ -23,19 +27,16 @@ tmp=cat(1,a2,rot90(a4,-1),rot90(a5,-1),a1);
 
 subplot('position',[.05 .35 .9 .3])
 h1=pcolor(tmp');shading flat
-set(gca,'xtick',[],'ytick',[])
+set(gca,'visible','off')
 pos=get(gca,'position');
-box off
 
 ax3=axes('position',[pos(1)+pos(3)/4*2 pos(2)+pos(4) pos(3)/4 pos(4)]);
 h31=pcolor(rot90(a3',2));shading flat
-set(gca,'xtick',[],'ytick',[])
-box off
+set(gca,'visible','off')
 
 ax6=axes('position',[pos(1)+pos(3)/4*2 pos(2)-pos(4) pos(3)/4 pos(4)]);
 h61=pcolor(rot90(a6'));shading flat
-set(gca,'xtick',[],'ytick',[])
-box off
+set(gca,'visible','off')
 
 axx=findobj(gcf,'type','axes');
 set(axx,'clim',cx)
@@ -47,3 +48,4 @@ set(axx,'clim',cx)
 %new for R2009
 ax=colorbar('location','southoutside');
 set(ax,'position',[pos(1)+pos(3)/4/2/2 pos(2)+pos(4)*1.5 pos(3)/4*1.5 pos(4)/5]);
+set(get(ax,'xlabel'),'string',label)
