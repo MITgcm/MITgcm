8d496754956e6bdffef44f0055e4eebbc4324708
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-05-02 20:01:21 +0000

    remove snow-precip output & time-ave

--- pkg/thsice/THSICE_TAVE.h
+++ pkg/thsice/THSICE_TAVE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/thsice/THSICE_TAVE.h,v 1.4 2009/12/29 23:07:43 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/thsice/THSICE_TAVE.h,v 1.5 2013/05/02 20:01:21 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -25,15 +25,15 @@ C     ice_snowH_Ave  :: cumulated Snow thickness [m]
 C     ice_Tsrf_Ave   :: cumulated surface Temperature [oC]
 C     ice_Tice1_Ave  :: cumulated 1srt level Temp. [oC]
 C     ice_Tice2_Ave  :: cumulated 2nd  level Temp. [oC]
-C     ice_snowPr_Ave :: cumulated snow precipitation (+=down) [kg/m2/s]
+cC    ice_snowPr_Ave :: cumulated snow precipitation (+=down) [kg/m2/s]
 C     ice_flx2oc_Ave :: cumulated heat flux out of the ocean (+=up) [W/m2]
 C     ice_frw2oc_Ave :: cumulated fresh-water flux out off the ocean (E-P) [m/s]
 C     ice_salFx_Ave  :: cumulated salt flux out of the ocean (+=up) [psu.kg/m2]
 C     ice_flxAtm_Ave :: cumulated net heat flux from Atmosphere (+=down) [W/m2]
 C     ice_frwAtm_Ave :: cumulated fresh-water flux from Atmos. (+=up) [kg/m2/s]
 C     ice_albedo_Ave :: cumulated sea-ice albedo [0-1]
-C     ICE_tMxL_Ave   :: cumulated ocean mixed-layer Temp. [oC]
-C     ICE_sMxL_Ave   :: cumulated ocean mixed-layer salinity [psu]
+C     ice_tMxL_Ave   :: cumulated ocean mixed-layer Temp. [oC]
+C     ice_sMxL_Ave   :: cumulated ocean mixed-layer salinity [psu]
 
       _RL ice_timeAve(nSx,nSy)
       _RL ice_fract_Ave (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
@@ -42,23 +42,23 @@ C     ICE_sMxL_Ave   :: cumulated ocean mixed-layer salinity [psu]
       _RL ice_Tsrf_Ave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_Tice1_Ave (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_Tice2_Ave (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL ice_snowPr_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+c     _RL ice_snowPr_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_flx2oc_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_frw2oc_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_salFx_Ave (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_flxAtm_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_frwAtm_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL ice_albedo_Ave(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL ICE_tMxL_Ave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL ICE_sMxL_Ave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL ice_tMxL_Ave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL ice_sMxL_Ave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
       COMMON /THSICE_TAVE_ARRAYS/ ice_timeAve,
      &                 ice_fract_Ave, ice_iceH_Ave,
      &                 ice_snowH_Ave, ice_Tsrf_Ave,
-     &                 ice_Tice1_Ave, ice_Tice2_Ave, ice_snowPr_Ave,
+     &                 ice_Tice1_Ave, ice_Tice2_Ave,
      &                 ice_flx2oc_Ave,ice_frw2oc_Ave,ice_salFx_Ave,
      &                 ice_flxAtm_Ave,ice_frwAtm_Ave,ice_albedo_Ave,
-     &                 ICE_tMxL_Ave, ICE_sMxL_Ave
+     &                 ice_tMxL_Ave, ice_sMxL_Ave
 
 #endif /* ALLOW_TIMEAVE */
 #endif /* ALLOW_THSICE */
