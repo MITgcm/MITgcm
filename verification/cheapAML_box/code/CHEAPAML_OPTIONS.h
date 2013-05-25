4582417724f6588637575d80510d1fff5f5a51ad
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-05-25 18:10:37 +0000

    reproduce old results by defining option "INCONSISTENT_WIND_LOCATION"

---
+++ verification/cheapAML_box/code/CHEAPAML_OPTIONS.h
@@ -0,0 +1,24 @@
+C $Header: /u/gcmpack/MITgcm/verification/cheapAML_box/code/CHEAPAML_OPTIONS.h,v 1.1 2013/05/25 18:10:37 jmc Exp $
+C $Name:  $
+
+C Options file for CheapAML package
+C Use this file for selecting options within the CheapAML package
+
+#ifndef CHEAPAML_OPTIONS_H
+#define CHEAPAML_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_CHEAPAML
+C Place CPP define/undef flag here
+
+C to reproduce old results, with inconsitent wind location,
+C  grid-cell center and grid-cell edges (C-grid).
+#define INCONSISTENT_WIND_LOCATION
+
+#endif /* ALLOW_CHEAPAML */
+#endif /* CHEAPAML_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
