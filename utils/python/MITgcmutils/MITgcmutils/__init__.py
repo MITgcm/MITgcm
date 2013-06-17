770dd54535a73def07f0cc543a4d1d89446d98e6
Martin Losch <Martin.Losch@awi.de>
2013-06-17 08:01:21 +0000

    load new subpackage, access functions in subpackage, e.g., like this: >> import MITgcmutils as mit >> x=mit.rdmds('XG'); y=mit.rdmds('YG'); e=mit.rdmds('Eta') >> mit.cs.pcol(x,y,e)

--- utils/python/MITgcmutils/MITgcmutils/__init__.py
+++ utils/python/MITgcmutils/MITgcmutils/__init__.py
@@ -3,7 +3,8 @@ from .mds import rdmds, wrmds
 from .ptracers import iolabel,iolabel2num
 from .diagnostics import readstats
 from .mnc import rdmnc, mnc_files
+import cs
 
 __all__ = ['NaN', 'Inf', 'rdmds', 'wrmds', 'iolabel', 'iolabel2num',
-           'readstats', 'rdmnc', 'mnc_files']
+           'readstats', 'rdmnc', 'mnc_files', 'cs']
 
