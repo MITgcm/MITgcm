e9a88bebd834927229b26a5d7eae4afa3e6a80d2
Patrick Heimbach <heimbach@mit.edu>
2013-06-21 17:36:32 +0000

    New OpenAD verification exp

---
+++ verification/hs94.1x64x5/code_oad/CTRL_SIZE.h
@@ -0,0 +1,25 @@
+C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_oad/CTRL_SIZE.h,v 1.1 2013/06/21 17:36:31 heimbach Exp $
+C $Name:  $
+
+c     ==================================================================
+c     CTRL_SIZE.h
+c     ==================================================================
+
+C     Generic control variable array dimension
+C     ----------------------------------------
+C
+C     maxCtrlArr2D :: number of 2-dim. generic control variables
+C     maxCtrlArr3D :: number of 3-dim. generic control variables
+
+      integer     maxCtrlArr2D
+      parameter ( maxCtrlArr2D = 3 )
+
+      integer     maxCtrlArr3D
+      parameter ( maxCtrlArr3D = 3 )
+
+      integer     maxCtrlTim2D
+      parameter ( maxCtrlTim2D = 1 )
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
