0d5d00bd7f19c9cffcd1d28b15d05b370a975bda
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-06-28 21:25:25 +0000

    allows to add some damping on uVel,vVel (strength only function of k)

--- pkg/atm_phys/ATM_PHYS_PARAMS.h
+++ pkg/atm_phys/ATM_PHYS_PARAMS.h
@@ -1,52 +1,39 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_phys/ATM_PHYS_PARAMS.h,v 1.1 2013/05/08 22:14:14 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_phys/ATM_PHYS_PARAMS.h,v 1.2 2013/06/28 21:25:25 jmc Exp $
 C $Name:  $
 
 #ifdef ALLOW_ATM_PHYS
 
-C     Package flag
-c     LOGICAL myPa_MNC
-c     LOGICAL myPa_MDSIO
-c     COMMON /MYPA_PACKAGE/
-c    &                     myPa_MNC, myPa_MDSIO
-
-C     MYPA parameters
-c     LOGICAL myPa_StaV_Cgrid
-c     LOGICAL myPa_Tend_Cgrid
+C--   ATM_PHYS parameters
+C     atmPhys_addTendT :: apply ATM_PHYS tendency to temperature
+C     atmPhys_addTendS :: apply ATM_PHYS tendency to Specific Humid
+C     atmPhys_addTendU :: apply ATM_PHYS tendency to U-component wind
+C     atmPhys_addTendV :: apply ATM_PHYS tendency to V-component wind
+C     atmPhys_stepSST  :: step forward SST
       LOGICAL atmPhys_addTendT
       LOGICAL atmPhys_addTendS
       LOGICAL atmPhys_addTendU
       LOGICAL atmPhys_addTendV
-
-C-    additional parameters:
       LOGICAL atmPhys_stepSST
-c     LOGICAL myPa_doSwitch2
-c     INTEGER myPa_index1
-c     INTEGER myPa_index2
-c     _RL myPa_param1
-c     _RL myPa_param2
-c     CHARACTER*(MAX_LEN_FNAM) myPa_string1
-c     CHARACTER*(MAX_LEN_FNAM) myPa_string2
+
+C-    horizontal momentum damping (e.g., stratospheric drag)
+C     atmPhys_tauDampUV :: damping time-scale (s)
+C     atmPhys_dampUVfac :: damping coefficient for each level
+      _RL     atmPhys_tauDampUV
+      _RL     atmPhys_dampUVfac(Nr)
 
 C-    file names for initial conditions:
-c     CHARACTER*(MAX_LEN_FNAM) myPa_Scal1File
-c     CHARACTER*(MAX_LEN_FNAM) myPa_Scal2File
-c     CHARACTER*(MAX_LEN_FNAM) myPa_VelUFile
-c     CHARACTER*(MAX_LEN_FNAM) myPa_VelVFile
-
+C     atmPhys_SSTFile  :: name of initial SST [in K] file
+C     atmPhys_QflxFile :: name of Q-flux file
       CHARACTER*(MAX_LEN_FNAM) atmPhys_SSTFile
       CHARACTER*(MAX_LEN_FNAM) atmPhys_QflxFile
 
-      COMMON /MYPA_PARAMS_L/
-c    &       myPa_StaV_Cgrid, myPa_Tend_Cgrid, myPa_doSwitch2,
+      COMMON /ATM_PHYS_PARAMS_L/
      &       atmPhys_addTendT, atmPhys_addTendS,
      &       atmPhys_addTendU, atmPhys_addTendV,
      &       atmPhys_stepSST
-c     COMMON /MYPA_PARAMS_I/ myPa_index1, myPa_index2
-c     COMMON /MYPA_PARAMS_R/ myPa_param1, myPa_param2
-      COMMON /MYPA_PARAMS_C/ 
-c    &       myPa_string1, myPa_string2,
-c    &       myPa_Scal1File, myPa_Scal2File,
-c    &       myPa_VelUFile,  myPa_VelVFile,
+      COMMON /ATM_PHYS_PARAMS_R/
+     &       atmPhys_tauDampUV, atmPhys_dampUVfac
+      COMMON /ATM_PHYS_PARAMS_C/
      &       atmPhys_SSTFile, atmPhys_QflxFile
 
 C-- from driver-atmosphere module:
