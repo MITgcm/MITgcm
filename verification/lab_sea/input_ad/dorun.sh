5fb27a71465fd2d339dbc162b98beda840062076
Patrick Heimbach <heimbach@mit.edu>
2013-07-18 20:09:20 +0000

    Rudimentary script for DIVA

---
+++ verification/lab_sea/input_ad/dorun.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+\rm costf* divided.ctrl snapshot*
+
+mitgcmuv_ad > output_adm.txt.1
+mitgcmuv_ad > output_adm.txt.2
+mitgcmuv_ad > output_adm.txt.3
+mitgcmuv_ad > output_adm.txt.4
+mitgcmuv_ad > output_adm.txt
+
