b1a3936b07f23b446378febda108488adf27fa5d
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-07-22 15:06:40 +0000

    try to avoid the need for file "oadtempflile"; should works as long as this script is not moved (or linked) elsewhere,  outside of OADTOOLS (i.e., tools/OAD_support).

--- tools/OAD_support/insertTemplateDir.bash
+++ tools/OAD_support/insertTemplateDir.bash
@@ -1,30 +1,30 @@
-#!/bin/bash
-OADTOOLS=`awk '{print $1 }' < oadtempflile`
+#! /usr/bin/env bash
+OADTOOLS=`dirname $0`
+#OADTOOLS=`awk '{print $1 }' < oadtempflile`
 echo "#generated file" > temp.sed
 for i in `grep '^ *SUBROUTINE ' $1 | awk '{print $2}'`
-do 
+do
   # extract the name
   srName=${i%%\(*}
   echo "/^ *SUBROUTINE $srName(/i\\" >> temp.sed
-  case "$srName" in 
+  case "$srName" in
     "OpenAD_main_do_loop" )
       echo "c\$openad XXX Template $OADTOOLS/ad_template.revolve.f" >> temp.sed
       ;;
-    "OpenAD_cg2d" ) 
+    "OpenAD_cg2d" )
       echo "c\$openad XXX Template $OADTOOLS/ad_template.sa_cg2d.f" >> temp.sed
       ;;
     "OpenAD_exch1_rl" |          \
     "OpenAD_exch1_rs" |          \
-    "OpenAD_global_max_r8" |       \
-    "OpenAD_global_sum_r8" )  
+    "OpenAD_global_max_r8" |     \
+    "OpenAD_global_sum_r8" )
       tmpName=${srName#OpenAD_}
       echo "c\$openad XXX Template $OADTOOLS/ad_template.${tmpName}.f" >> temp.sed
       ;;
-    *) 
+    *)
       echo "test $OADTOOLS"
       echo "c\$openad XXX Template $OADTOOLS/ad_template.split.f" >> temp.sed
       ;;
   esac
 done
 cat $1 | sed -f temp.sed > $2
-  
