845dcd8102c4a3a175ae1f244509ff2a8f014a57
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-07-28 21:02:33 +0000

    - move horiz-viscosity 3-D arrays viscAh_W & viscA4_W (used in calc_gw.F)   from model/inc/NH_VARS.h to pkg/mom_common/MOM_VISC.h   + initialise them in pkg/mom_common/mom_init_fixed.F - store in common block (in MOM_VISC.h): useHarmonicVisc, useBiharmonicVisc   & useVariableVisc, (previously local flag harmonic, biharmonic   & useVariableViscosity, were set for each k in mom_common/mom_calc_visc.F)

--- pkg/mom_common/MOM_VISC.h
+++ pkg/mom_common/MOM_VISC.h
@@ -1,26 +1,32 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_VISC.h,v 1.3 2011/03/31 20:33:01 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_VISC.h,v 1.4 2013/07/28 21:02:33 jmc Exp $
 C $Name:  $
 
 C- Common file for length scales
 
 #ifdef ALLOW_MOM_COMMON
 
-      COMMON /MOM_VISC_L/ L2_D, L2_Z,
-     &                    L3_D, L3_Z,
-     &                    L4rdt_D, L4rdt_Z,
-     &                    recip_dt
+C--   COMMON /MOM_VISC_PAR_L/ logical-type parameters for Momemtum viscosity
+C     useHarmonicVisc   :: harmonic   horizontal viscosity is used
+C     useBiharmonicVisc :: biharmonic horizontal viscosity is used
+C     useVariableVisc   :: variable (in space or time) viscosity is used
+      COMMON /MOM_VISC_PAR_L/
+     &        useHarmonicVisc, useBiharmonicVisc, useVariableVisc
+      LOGICAL useHarmonicVisc, useBiharmonicVisc, useVariableVisc
+
+      COMMON /MOM_VISC_LENGTH/ L2_D, L2_Z,
+     &                         L3_D, L3_Z,
+     &                         L4rdt_D, L4rdt_Z
       _RL L2_D(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL L2_Z(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL L3_D(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL L3_Z(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL L4rdt_D(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL L4rdt_Z(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL recip_dt
 
 #ifdef ALLOW_3D_VISCAH
 C     viscAhDfld, viscAhZfld :: full 3D specification of Laplacian Viscosity
 C               coeff. for mixing of momentum horizontally ( units of m^2/s )
-      COMMON /DYNVARS_3D_VISCAH/
+      COMMON /MOM_VISC_3D_VISCAH/
      &                  viscAhDfld, viscAhZfld
       _RL  viscAhDfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  viscAhZfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
@@ -29,10 +35,19 @@ C               coeff. for mixing of momentum horizontally ( units of m^2/s )
 #ifdef ALLOW_3D_VISCA4
 C     viscA4Dfld, viscA4Zfld :: full 3D specification of Bi-harmonic Viscosity
 C               coeff. for mixing of momentum horizontally ( units of m^4/s )
-      COMMON /DYNVARS_3D_VISCA4/
+      COMMON /MOM_VISC_3D_VISCA4/
      &                  viscA4Dfld, viscA4Zfld
       _RL  viscA4Dfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  viscA4Zfld (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 #endif
+
+#ifdef ALLOW_NONHYDROSTATIC
+C     viscAh_W  :: Horizontal  harmonic  viscosity for vertical momentum
+C     viscA4_W  :: Horizontal biharmonic viscosity for vertical momentum
+      COMMON /MOM_VISC_NH/
+     &                  viscAh_W, viscA4_W
+      _RL  viscAh_W(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL  viscA4_W(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+#endif /* ALLOW_NONHYDROSTATIC */
 
 #endif /* ALLOW_MOM_COMMON */
