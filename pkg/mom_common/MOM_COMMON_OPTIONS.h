1405aae35496a3f9fd58608c7bfc7d5b5bf70866
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-07-30 19:05:54 +0000

    add Option to enable isotropic 3-D Smagorinsky (Smag3D)

--- pkg/mom_common/MOM_COMMON_OPTIONS.h
+++ pkg/mom_common/MOM_COMMON_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_COMMON_OPTIONS.h,v 1.3 2011/12/24 01:09:39 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/mom_common/MOM_COMMON_OPTIONS.h,v 1.4 2013/07/30 19:05:54 jmc Exp $
 C $Name:  $
 
 C CPP options file for mom_common package
@@ -11,6 +11,9 @@ C Use this file for selecting CPP options within the mom_common package
 
 #ifdef ALLOW_MOM_COMMON
 C     Package-specific options go here
+
+C allow isotropic 3-D Smagorinsky viscosity
+#undef ALLOW_SMAG_3D
 
 C allow full 3D specification of horizontal Laplacian Viscosity
 #undef ALLOW_3D_VISCAH
