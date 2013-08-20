01e0754d66d46a25579f2efa7f713c8faf429933
Oliver Jahn <jahn@mit.edu>
2013-08-20 17:27:03 +0000

    link templates to build directory for preprocessing

--- tools/OAD_support/insertTemplateDir.bash
+++ tools/OAD_support/insertTemplateDir.bash
@@ -9,21 +9,21 @@ do
   echo "/^ *SUBROUTINE $srName(/i\\" >> temp.sed
   case "$srName" in
     "OpenAD_main_do_loop" )
-      echo "c\$openad XXX Template $OADTOOLS/ad_template.revolve.f" >> temp.sed
+      echo "c\$openad XXX Template ad_template.revolve.f" >> temp.sed
       ;;
     "OpenAD_cg2d" )
-      echo "c\$openad XXX Template $OADTOOLS/ad_template.sa_cg2d.f" >> temp.sed
+      echo "c\$openad XXX Template ad_template.sa_cg2d.f" >> temp.sed
       ;;
     "OpenAD_exch1_rl" |          \
     "OpenAD_exch1_rs" |          \
     "OpenAD_global_max_r8" |     \
     "OpenAD_global_sum_r8" )
       tmpName=${srName#OpenAD_}
-      echo "c\$openad XXX Template $OADTOOLS/ad_template.${tmpName}.f" >> temp.sed
+      echo "c\$openad XXX Template ad_template.${tmpName}.f" >> temp.sed
       ;;
     *)
       echo "test $OADTOOLS"
-      echo "c\$openad XXX Template $OADTOOLS/ad_template.split.f" >> temp.sed
+      echo "c\$openad XXX Template ad_template.split.f" >> temp.sed
       ;;
   esac
 done
