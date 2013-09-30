4fc2fc8031d1bf0b5075bbc8653779875c0f140c
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-09-30 18:19:42 +0000

    add 1rst version of isotropic 3-D Smagorinsky code (from Louis-Philippe),   for now all in this exp. code dir.

---
+++ verification/tutorial_deep_convection/code/MOM_COMMON_OPTIONS.h
@@ -0,0 +1,25 @@
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_deep_convection/code/MOM_COMMON_OPTIONS.h,v 1.1 2013/09/30 18:19:41 jmc Exp $
+C $Name:  $
+
+C CPP options file for mom_common package
+C Use this file for selecting CPP options within the mom_common package
+
+#ifndef MOM_COMMON_OPTIONS_H
+#define MOM_COMMON_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_MOM_COMMON
+C     Package-specific options go here
+
+C allow isotropic 3-D Smagorinsky viscosity
+#define ALLOW_SMAG_3D
+
+C allow full 3D specification of horizontal Laplacian Viscosity
+#undef ALLOW_3D_VISCAH
+
+C allow full 3D specification of horizontal Biharmonic Viscosity
+#undef ALLOW_3D_VISCA4
+
+#endif /* ALLOW_MOM_COMMON */
+#endif /* MOM_COMMON_OPTIONS_H */
