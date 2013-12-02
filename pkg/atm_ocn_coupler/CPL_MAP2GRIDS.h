8276725b84da8d6c5168e8e7e3187cb852557fe4
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-12-02 22:03:09 +0000

    - move some input parameters from CPL_MAP2GRIDS.h to CPL_PARAMS.h   (no need to include CPL_MAP2GRIDS.h in cpl_read_params.F anymore) - report set-up to clog file (I/O unit now passed as input argument) - check we don't go over RunOff map size (=ROsize) and skip reading   grid-cell area if no RunOff map to set.

--- pkg/atm_ocn_coupler/CPL_MAP2GRIDS.h
+++ pkg/atm_ocn_coupler/CPL_MAP2GRIDS.h
@@ -1,8 +1,8 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/CPL_MAP2GRIDS.h,v 1.3 2006/06/15 23:29:17 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/CPL_MAP2GRIDS.h,v 1.4 2013/12/02 22:03:09 jmc Exp $
 C $Name:  $
 
 C     *==========================================================*
-C     | CPL_MAP2GRIDS.h 
+C     | CPL_MAP2GRIDS.h
 C     |   Declare arrays used for mapping coupling fields from
 C     |     one grid (atmos., ocean) to the other grid
 C     *==========================================================*
@@ -11,16 +11,13 @@ C     *==========================================================*
       PARAMETER (ROsize=Nx_atm*Ny_atm)
 
 C--   COMMON / RUNOFF_MAP/: to map runoff from atmos. grid to ocean grid
-C     nROmap :: Nunber of connected grid points.
+C     nROmap  :: Nunber of connected grid points.
 C     ijROatm :: index of land grid point that runoff to the ocean
 C     ijROocn :: index of ocean grid point where the runoff ends
 C     arROmap :: fraction of the land runoff ijROatm that go to ijROocn
-C     runoffmapFile :: Input file for setting runoffmap
       COMMON / RUNOFF_MAP_I / nROmap, ijROocn, ijROatm
       INTEGER nROmap
       INTEGER ijROocn(ROsize), ijROatm(ROsize)
       COMMON / RUNOFF_MAP_R / arROmap
       _RL arROmap(ROsize)
-      COMMON / RUNOFF_MAP_C / runoffmapFile
-      CHARACTER*80 runoffmapFile
 
--- pkg/atm_ocn_coupler/CPL_PARAMS.h
+++ pkg/atm_ocn_coupler/CPL_PARAMS.h
@@ -1,13 +1,26 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/CPL_PARAMS.h,v 1.3 2006/06/15 23:29:17 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/CPL_PARAMS.h,v 1.4 2013/12/02 22:03:08 jmc Exp $
 C $Name:  $
 
 C     *==========================================================*
 C     | CPL_PARAMS.h
 C     |  - parameter for the Coupler, holds in common block
 C     *==========================================================*
-      
-C--   COMMON /CPL_PAR_I/: Integer parameters
+
+C--   COMMON /CPL_PAR_I/ Integer parameters
+C     runOffMapSize    :: Nunber of connected grid points in RunOff-Map
 C     nCouplingSteps   :: Number of coupling steps
+C     nSteps_atm       :: Number of coupling steps done by ATM
+C     nSteps_ocn       :: Number of coupling steps done by OCN
       COMMON /CPL_PAR_I/
-     &     nCouplingSteps
+     &     runOffMapSize,
+     &     nCouplingSteps,
+     &     nSteps_atm, nSteps_ocn
+      INTEGER runOffMapSize
       INTEGER nCouplingSteps
+      INTEGER nSteps_atm, nSteps_ocn
+
+C--   COMMON /CPL_PAR_C/ Character parameters
+C     runOffMapFile    :: Input file for setting runoffmap
+      COMMON /CPL_PAR_C/ runOffMapFile
+      CHARACTER*80 runOffMapFile
+
