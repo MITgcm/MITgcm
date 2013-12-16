3440679da03a9f4ae7196a68a44ee288994a42b2
Jean-Michel Campin <jmc@ocean.mit.edu>
2013-12-16 21:17:18 +0000

    add formula for MINMOD limiter

--- pkg/generic_advdiff/GAD_FLUX_LIMITER.h
+++ pkg/generic_advdiff/GAD_FLUX_LIMITER.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/generic_advdiff/GAD_FLUX_LIMITER.h,v 1.5 2001/09/19 20:45:09 adcroft Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/generic_advdiff/GAD_FLUX_LIMITER.h,v 1.6 2013/12/16 21:17:18 jmc Exp $
 C $Name:  $
 
 CBOP
@@ -20,6 +20,10 @@ C Lax-Wendroff is recovered with:
 C \begin{equation*}
 C Limiter(Cr)=1
 C \end{equation*}
+C The Min-Mod limiter is obtained with:
+C \begin{equation*}
+C Limiter(Cr)=max(0,min(1,Cr))
+C \end{equation*}
 C The current limiter of choice is the "Superbee" limiter:
 C \begin{equation*}
 C Limiter(Cr)=max(0,max(min(1,2*Cr),min(2,Cr)))
@@ -31,9 +35,11 @@ C Statement function to describe flux limiter
       _RL Limiter
 C Upwind        Limiter(Cr)=0.
 C Lax-Wendroff  Limiter(Cr)=1.
+C Min-Mod       Limiter(Cr)=max(0.,min(1.,Cr))
 C Suberbee      Limiter(Cr)=max(0.,max(min(1.,2*Cr),min(2.,Cr)))
 
 c     Limiter(Cr)=0.
 c     Limiter(Cr)=1.
+c     Limiter(Cr)=max(0.D0,min(1.D0,Cr))
       Limiter(Cr)=max(0.D0,max(min(1.D0,2.D0*Cr),
-     &                         min(2.D0,Cr))) 
+     &                         min(2.D0,Cr)))
