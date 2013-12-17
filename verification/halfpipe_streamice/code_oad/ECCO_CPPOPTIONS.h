243831f5e5c60ae9f7a147e0a62006b4e7e5077d
Patrick Heimbach <heimbach@mit.edu>
2013-12-17 19:08:34 +0000

    Committing starting setup using OpenAD (incomplete).

---
+++ verification/halfpipe_streamice/code_oad/ECCO_CPPOPTIONS.h
@@ -0,0 +1,52 @@
+C $Header: /u/gcmpack/MITgcm/verification/halfpipe_streamice/code_oad/ECCO_CPPOPTIONS.h,v 1.1 2013/12/17 19:08:33 heimbach Exp $
+C $Name:  $
+
+#ifndef ECCO_CPPOPTIONS_H
+#define ECCO_CPPOPTIONS_H
+
+C
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+
+C ********************************************************************
+C ***                  Adjoint Support Package                     ***
+C ********************************************************************
+
+C o Include/exclude code in order to be able to automatically
+C   differentiate the MITgcmUV by using the Tangent Linear and
+C   Adjoint Model Compiler (TAMC).
+C
+#define ALLOW_AUTODIFF_TAMC
+C       >>> Checkpointing as handled by TAMC
+#define ALLOW_TAMC_CHECKPOINTING
+C
+#define ALLOW_AUTODIFF_OPENAD
+C       >>> Extract adjoint state
+#undef ALLOW_AUTODIFF_MONITOR
+C
+C       >>> DO 2-level checkpointing instead of 3-level
+#undef AUTODIFF_2_LEVEL_CHECKPOINT
+C
+C o use divided adjoint to split adjoint computations
+#undef ALLOW_DIVIDED_ADJOINT
+#undef ALLOW_DIVIDED_ADJOINT_MPI
+
+C ********************************************************************
+C ***                Cost function Package                         ***
+C ********************************************************************
+C
+C       >>> Cost function contributions
+#define ALLOW_COST_TEST
+
+C ********************************************************************
+C ***               Control vector Package                         ***
+C ********************************************************************
+C
+#undef EXCLUDE_CTRL_PACK
+#undef  ALLOW_NONDIMENSIONAL_CONTROL_IO
+C
+C       >>> Initial values.
+#define ALLOW_GENARR2D_CONTROL
+
+#endif /* ECCO_CPPOPTIONS_H */
+
---
+++ verification/halfpipe_streamice/code_oad/STREAMICE_OPTIONS.h
@@ -0,0 +1,33 @@
+C $Header: /u/gcmpack/MITgcm/verification/halfpipe_streamice/code_oad/STREAMICE_OPTIONS.h,v 1.1 2013/12/17 19:08:33 heimbach Exp $
+C $Name:  $
+
+C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
+
+C CPP options file for MYPACKAGE
+C
+C Use this file for selecting options within package "streamice"
+
+#ifndef STREAMICE_OPTIONS_H
+#define STREAMICE_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_STREAMICE
+
+#include "CPP_OPTIONS.h"
+
+C Place CPP define/undef flag here
+
+#define STREAMICE_CONSTRUCT_MATRIX
+#define STREAMICE_HYBRID_STRESS
+#define USE_ALT_RLOW
+#undef STREAMICE_GEOM_FILE_SETUP
+#define STREAMICE_SMOOTH_FLOATATION
+#undef ALLOW_PETSC
+#undef ALLOW_STREAMICE_2DTRACER
+#undef STREAMICE_TRACER_AB
+
+#endif /* ALLOW_MYPACKAGE */
+#endif /* MYPACKAGE_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
