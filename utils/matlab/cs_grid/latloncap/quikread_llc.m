c025ca051f6004d5f4165bc36a55b9bf7f0a583e
Dimitris Menemenlis <dmenemenlis@gmail.com>
2014-02-28 03:37:04 +0000

    Starting to add capability to automatically extract latitude/longitude regions.

--- utils/matlab/cs_grid/latloncap/quikread_llc.m
+++ utils/matlab/cs_grid/latloncap/quikread_llc.m
@@ -1,31 +1,59 @@
-function fld=quikread_llc(fnam,nx,kx,prec,tiles);
+function fld=quikread_llc(fnam,nx,kx,prec,pname,minlat,maxlat,minlon,maxlon);
 
-% Function quikread_llc(fnam,nx,kx,prec,tiles);
-% read lat-lon-cap field
+% Function quikread_llc(fnam,nx,kx,prec,pname,minlat,maxlat,minlon,maxlon);
+% Read lat-lon-cap field
+% If there is less than 5 input arguments: read the complete field.
+% If there is more than 5 input arguments: read a region.
 %
 % INPUTS
-% fnam  input path and file name
-% nx    tile dimension (default 270)
-% kx    vertical indices to read, e.g., 1:50 (default 1)
-% prec  numeric precision (see fread; default 'real*4')
-% tiles llc tiles to read (default 1:13)
+% fnam   input path and file name
+% nx     tile dimension (default 270)
+% kx     vertical indices to read, e.g., 1:50 (default 1)
+% prec   numeric precision (see fread; default 'real*4')
+% pname  directory path name that contains XC.data and YC.data
+% minlat minimum latitude of region to extract
+% maxlat maximum latitude of region to extract
+% minlon minimum longitude of region to extract
+% maxlon maximum longitude of region to extract
 %
 % OUTPUTS
-% fld  output array of dimension nx*nx*length(tiles)*length(kx)
-%
-% EXAMPLES
-% fld=quikread_llc('Depth.data',270); quikplot_llc(fld)
-% fld=quikread_llc('Depth.data',270,1,'real*4',4:7); quikpcolor(fld')
-% fld=quikread_llc('Depth.data',270,1,'real*4',8:10); quikpcolor(fld')
+% fld  output array
 %
 % SEE ALSO
 % read_llc_fkij quilplot_llc quikpcolor
+%
+% EXAMPLES
+%
+% % read and plot complete field
+% fld=quikread_llc('Depth.data',270);
+% quikplot_llc(fld)
+%
+% % read and plot the region 120W to 40W and 80S and 60N
+% fld=quikread_llc('Depth.data',270,1,'real*4','',-80,60,-120,-40);
+% quikpcolor(fld')
 
-if nargin < 5, tiles=1:13; end
+if nargin < 9, maxlon=180; end
+if nargin < 8, minlon=-180; end
+if nargin < 7, maxlat=90; end
+if nargin < 6, minlat=-90; end
+if nargin < 5, pname=''; end
 if nargin < 4, prec='real*4'; end
 if nargin < 3, kx=1; end
 if nargin < 2, nx=270; end
 if nargin < 1, error('please specify input file name'); end
+
+if minlon < -180
+    error('minlon<-180 not yet implemented: please email menemenlis@jpl.nasa.gov')
+end
+if maxlon > 180
+    error('maxlon>180 not yet implemented: please email menemenlis@jpl.nasa.gov')
+end
+if minlat >= maxlat
+    error('maxlat must be greater than minlat')
+end
+if minlon >= maxlon
+    error('maxlon must be greater than minlon')
+end
 
 switch prec
  case {'int8','integer*1'}
@@ -38,53 +66,54 @@ switch prec
   preclength=8;
 end
 
-if preclength<=4
-    % if input is single precision,
-    % output single precision to save space
-    fld=zeros(nx,nx*length(tiles),length(kx),'single');
-else
-    fld=zeros(length(ix),length(jx),length(kx));
-end
-
-
-if length(tiles)==13
+if nargin < 6
+    if preclength <= 4
+        % if input is single precision,
+        % output single precision to save space
+        fld=zeros(nx,nx*13,length(kx),'single');
+    else
+        fld=zeros(nx,nx*13,length(kx));
+    end
     fid=fopen(fnam,'r','ieee-be');
-    if preclength<=4
+    if preclength <= 4
         % if input is single precision,
         % read single precision to save space
         prec=[prec '=>' prec];
     end    
     for k=1:length(kx)
-        skip=(kx(k)-1)*nx*nx*13;
-        if(fseek(fid,skip*preclength,'bof')<0), error('past end of file'); end
+        if kx(k) > 1
+            skip=(kx(k)-1)*nx*nx*13;
+            if(fseek(fid,skip*preclength,'bof')<0)
+                error('past end of file');
+            end
+        end
         fld(:,:,k)=reshape(fread(fid,nx*nx*13,prec),nx,nx*13);
-    end    
+    end
     fid=fclose(fid);
 else
-    ix=1:nx;
-    for k=1:length(kx)
-        for tl=1:length(tiles)
-            switch tiles(tl)
-              case {1,2,3}
-                face=1;
-              case {4,5,6}
-                face=2;
-              case 7
-                face=3;
-              case {8,9,10}
-                face=4;
-              case {11,12,13}
-                face=5;
+    fc=[];
+    for f=1:5
+        yc=read_llc_fkij([pname 'YC.data'],nx,f);
+        xc=read_llc_fkij([pname 'XC.data'],nx,f);
+        [i j]=find(yc>=minlat&yc<=maxlat&xc>=minlon&xc<=maxlon);
+        if ~isempty(i)
+            if isempty(fc)
+                fc=f;
+                ix=min(i):max(i);
+                jx=min(j):max(j);
+            else
+                error(['multiface output not yet implemented: please email menemenlis@jpl.nasa.gov'])
             end
-            switch tiles(tl)
-              case {1,4,7,8,11}
-                jx=1:nx;
-              case {2,5,9,12}
-                jx=(nx+1):(2*nx);
-              case {3,6,10,13}
-                jx=(2*nx+1):(3*nx);
-            end
-            fld(:,(nx*(tl-1)+1):(nx*tl),k)=read_llc_fkij(fnam,nx,face,kx(k),ix,jx,prec);
         end
+    end
+    if preclength <= 4
+        % if input is single precision,
+        % output single precision to save space
+        fld=zeros(length(ix),length(jx),length(kx),'single');
+    else
+        fld=zeros(length(ix),length(jx),length(kx));
+    end
+    for k=1:length(kx)
+        fld(:,:,k)=read_llc_fkij(fnam,nx,fc,kx(k),ix,jx,prec);
     end
 end
