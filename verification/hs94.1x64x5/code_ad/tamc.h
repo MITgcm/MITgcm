b8cd7fb7ee2a36051d38555b669ac00aed2dc2eb
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-04-05 21:45:54 +0000

    closer to standard version in pkg/autodiff

--- verification/hs94.1x64x5/code_ad/tamc.h
+++ verification/hs94.1x64x5/code_ad/tamc.h
@@ -1,47 +1,45 @@
-C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_ad/tamc.h,v 1.3 2008/05/09 18:42:27 dfer Exp $
+C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_ad/tamc.h,v 1.4 2014/04/05 21:45:54 jmc Exp $
 C $Name:  $
 
-#include "PACKAGES_CONFIG.h"
+C     ================================================================
+C     HEADER TAMC
+C     ================================================================
+C
+C     o Header for the use of the Tangent Linear and Adjoint Model
+C       Compiler (TAMC).
+C
+C     started: Christian Eckert eckert@mit.edu  04-Feb-1999
+C     changed: Patrick Heimbach heimbach@mit.edu 06-Jun-2000
+C              - New parameter nlevchk_0 for dimensionalising
+C                common blocks in the undef ALLOW_TAMC_CHECKPOINTING case
+C              - nhreads_chkpt was declared at the wrong place
+C              - new keys, separate for different packages
 
-c     ================================================================
-c     HEADER TAMC
-c     ================================================================
-c
-c     o Header for the use of the Tangent Linear and Adjoint Model
-c       Compiler (TAMC).
-c
-c     started: Christian Eckert eckert@mit.edu  04-Feb-1999
-c     changed: Patrick Heimbach heimbach@mit.edu 06-Jun-2000
-c              - New parameter nlevchk_0 for dimensionalising
-c                common blocks in the undef ALLOW_TAMC_CHECKPOINTING case
-c              - nhreads_chkpt was declared at the wrong place
-c              - new keys, separate for different packages
+C     ================================================================
+C     HEADER TAMC
+C     ================================================================
+#ifdef ALLOW_AUTODIFF_TAMC
 
-c     ================================================================
-c     HEADER TAMC
-c     ================================================================
-
-
-c     TAMC checkpointing parameters:
-c     ==============================
-c
-c     The checkpointing parameters have to be consistent with other model
-c     parameters and variables. This has to be checked before the model is
-c     run.
-c
-c     nyears_chkpt   - Number of calendar years affected by the assimilation
-c                      experiment; nyears_chkpt has to be at least equal to
-c                      the result of cal_IntYears(mythid).
-c     nmonths_chkpt  - Number of months per year; nmonth_chkpt has to be at
-c                      least equal to nmonthyear.
-c     ndays_chkpt    - Number of days per month; nday_chkpt has to be at least
-c                      equal to nmaxdaymonth.
-c     nsteps_chkpt   - Number of steps per day; nsteps_chkpt has to be at
-c                      least equal to cal_nStepDay(mythid)
-c     ncheck_chkpt   - Number of innermost checkpoints.
-c
-c     ngeom_chkpt    - Geometry factor.
-c     nthreads_chkpt - Number of threads to be used; nth_chkpt .eq. nTx*nTy
+C     TAMC checkpointing parameters:
+C     ==============================
+C
+C     The checkpointing parameters have to be consistent with other model
+C     parameters and variables. This has to be checked before the model is
+C     run.
+C
+C     nyears_chkpt   :: Number of calendar years affected by the assimilation
+C                       experiment; nyears_chkpt has to be at least equal to
+C                       the result of cal_IntYears(mythid).
+C     nmonths_chkpt  :: Number of months per year; nmonth_chkpt has to be at
+C                       least equal to nmonthyear.
+C     ndays_chkpt    :: Number of days per month; nday_chkpt has to be at least
+C                       equal to nmaxdaymonth.
+C     nsteps_chkpt   :: Number of steps per day; nsteps_chkpt has to be at
+C                       least equal to cal_nStepDay(mythid)
+C     ncheck_chkpt   :: Number of innermost checkpoints.
+C
+C     ngeom_chkpt    :: Geometry factor.
+C     nthreads_chkpt :: Number of threads to be used; nth_chkpt .eq. nTx*nTy
 
       integer nyears_chkpt
       integer nmonths_chkpt
@@ -66,7 +64,7 @@ c     nthreads_chkpt - Number of threads to be used; nth_chkpt .eq. nTx*nTy
       integer    nchklev_3
       parameter( nchklev_3      =  60 )
 
-c--   Note always check for the correct sizes of the common blocks!
+C--   Note always check for the correct sizes of the common blocks!
 
 #else /* ALLOW_TAMC_CHECKPOINTING undefined */
 
@@ -113,11 +111,18 @@ c     and writing data.
 
       INTEGER act0, act1, act2, act3, act4
       INTEGER max0, max1, max2, max3
-      INTEGER iikey, kkey, passkey, igadkey, 
+      INTEGER iikey, kkey, passkey, igadkey,
      &        itdkey, idynkey, igmkey, ikppkey, iptrkey
 
-c     ================================================================
-c     END OF HEADER TAMC
-c     ================================================================
+#ifdef ALLOW_CG2D_NSA
+C     Parameter that is needed for the tape complev_cg2d_iter
+C     cannot be smaller than the allowed number of iterations in cg2d
+C     (numItersMax >= cg2dMaxIters in data-file)
+      INTEGER numItersMax
+      PARAMETER ( numItersMax = 100 )
+#endif
 
-
+#endif /* ALLOW_AUTODIFF_TAMC */
+C     ================================================================
+C     END OF HEADER TAMC
+C     ================================================================
