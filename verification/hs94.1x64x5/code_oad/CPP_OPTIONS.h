bc17e304d3021dd2268bdef1b29ada0063cb0915
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-04-05 23:16:34 +0000

    do not use ECCO_CPPOPTIONS.h anymore for this OpenAD Adjoint experiment.

--- verification/hs94.1x64x5/code_oad/CPP_OPTIONS.h
+++ verification/hs94.1x64x5/code_oad/CPP_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_oad/CPP_OPTIONS.h,v 1.2 2013/11/18 23:51:25 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_oad/CPP_OPTIONS.h,v 1.3 2014/04/05 23:16:34 jmc Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
@@ -124,8 +124,8 @@ C   each of the above pkg get its own options from its specific option file.
 C   Although this method, inherited from ECCO setup, has been traditionally
 C   used for all adjoint built, work is in progress to allow to use the
 C   standard method also for adjoint built.
-#ifdef PACKAGES_CONFIG_H
-# include "ECCO_CPPOPTIONS.h"
-#endif
+c#ifdef PACKAGES_CONFIG_H
+c# include "ECCO_CPPOPTIONS.h"
+c#endif
 
 #endif /* CPP_OPTIONS_H */
