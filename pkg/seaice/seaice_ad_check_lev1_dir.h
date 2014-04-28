ab025b71837db767a1464d2a9279e8189ea30a61
Martin Losch <Martin.Losch@awi.de>
2014-04-28 17:12:25 +0000

    align a few expression and remove blank space at the beginning of the line that slipped in revision 1.19 and went unnoticed (only possible if no-one ever uses AUTODIFF_SOMETIMES_NEEDED, should be renamed into AUTODIFF_HARDLY_EVER_NEED or so ...)

--- pkg/seaice/seaice_ad_check_lev1_dir.h
+++ pkg/seaice/seaice_ad_check_lev1_dir.h
@@ -1,8 +1,8 @@
-C $Header: /u/gcmpack/MITgcm/pkg/seaice/seaice_ad_check_lev1_dir.h,v 1.32 2014/04/28 15:53:04 mlosch Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/seaice/seaice_ad_check_lev1_dir.h,v 1.33 2014/04/28 17:12:25 mlosch Exp $
 C $Name:  $
 
-CADJ STORE hsnow   = comlev1, key=ikey_dynamics, kind=isbyte
-CADJ STORE tices   = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE hsnow = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE tices = comlev1, key=ikey_dynamics, kind=isbyte
 
 CADJ STORE area  = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE heff  = comlev1, key=ikey_dynamics, kind=isbyte
@@ -13,7 +13,7 @@ CADJ STORE vice  = comlev1, key=ikey_dynamics, kind=isbyte
 # ifdef EXACT_CONSERV
 CADJ STORE pmepr      = comlev1, key = ikey_dynamics
 # endif
-  cphCADJ STORE uvel    = comlev1, key=ikey_dynamics, kind=isbyte
+cphCADJ STORE uvel    = comlev1, key=ikey_dynamics, kind=isbyte
 cphCADJ STORE vvel    = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE salt    = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE totphihyd  = comlev1, key=ikey_dynamics, kind=isbyte
@@ -21,8 +21,8 @@ cphCADJ STORE runoff     = comlev1, key=ikey_dynamics, kind=isbyte
 
 CADJ STORE area    = comlev1, key=ikey_dynamics, kind=isbyte
 cphCADJ STORE heff    = comlev1, key=ikey_dynamics, kind=isbyte
-cphCADJ STORE heffm      = comlev1, key=ikey_dynamics, kind=isbyte
-cphCADJ STORE hsnow      = comlev1, key=ikey_dynamics, kind=isbyte
+cphCADJ STORE heffm   = comlev1, key=ikey_dynamics, kind=isbyte
+cphCADJ STORE hsnow   = comlev1, key=ikey_dynamics, kind=isbyte
 # ifdef SEAICE_ALLOW_DYNAMICS
 CADJ STORE uice    = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE vice    = comlev1, key=ikey_dynamics, kind=isbyte
@@ -37,8 +37,8 @@ CADJ STORE stressdivergencey = comlev1, key=ikey_dynamics, kind=isbyte
 #endif
 # ifdef SEAICE_ALLOW_DYNAMICS
 #  ifdef SEAICE_CGRID
-CADJ STORE etan    = comlev1, key=ikey_dynamics, kind=isbyte
-CADJ STORE dwatn      = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE etan        = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE dwatn       = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE seaicemasku = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE seaicemaskv = comlev1, key=ikey_dynamics, kind=isbyte
 #  endif
@@ -57,7 +57,7 @@ CADJ STORE objf_ice    = comlev1, key=ikey_dynamics, kind=isbyte
 # ifdef ALLOW_SEAICE_COST_EXPORT
 CADJ STORE uHeffExportCell = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE vHeffExportCell = comlev1, key=ikey_dynamics, kind=isbyte
-CADJ STORE icevolMeanCell = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE icevolMeanCell  = comlev1, key=ikey_dynamics, kind=isbyte
 # endif
 
 #ifdef ALLOW_OBCS
