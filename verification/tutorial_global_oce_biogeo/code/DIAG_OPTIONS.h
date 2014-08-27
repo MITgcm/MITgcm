4a06e663aca4664c8de1d4d16ff765ee7a5636ab
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-08-27 18:19:03 +0000

    allow to use pkg/diagnostics (default numDiags=1*Nr was too small)  with regional Stats-Diags.

---
+++ verification/tutorial_global_oce_biogeo/code/DIAG_OPTIONS.h
@@ -0,0 +1,28 @@
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code/DIAG_OPTIONS.h,v 1.1 2014/08/27 18:19:03 jmc Exp $
+C $Name:  $
+
+#ifndef DIAG_OPTIONS_H
+#define DIAG_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_DIAGNOSTICS
+C     Package-specific Options & Macros go here
+
+C allow to define specific regions and the corresponding mask ;
+C  used to perform regional statistics over a limited area
+#define DIAGSTATS_REGION_MASK
+
+C allow to stop & restart at any time (i.e. not at a multiple of
+C  the diagnostics frequency) reading diagnostics storage arrays
+C  from pickup file.
+C Note: Use with cautious since it does not work for all restart
+C  cases (e.g., changing data.diagnostics).
+#undef  DIAGNOSTICS_HAS_PICKUP
+
+#endif /* ALLOW_DIAGNOSTICS */
+#endif /* DIAG_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
