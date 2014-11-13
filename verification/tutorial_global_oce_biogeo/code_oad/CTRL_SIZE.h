a26a94d0ab07fe1a2a3487736327229c84c39cb0
Gael Forget <gforget@mit.edu>
2014-11-13 23:50:07 +0000

    - add maxCtrlProc.

--- verification/tutorial_global_oce_biogeo/code_oad/CTRL_SIZE.h
+++ verification/tutorial_global_oce_biogeo/code_oad/CTRL_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code_oad/CTRL_SIZE.h,v 1.1 2013/04/23 16:29:35 jahn Exp $
+C $Header: /u/gcmpack/MITgcm/verification/tutorial_global_oce_biogeo/code_oad/CTRL_SIZE.h,v 1.2 2014/11/13 23:50:07 gforget Exp $
 C $Name:  $
 
 c     ==================================================================
@@ -8,8 +8,10 @@ c     ==================================================================
 C     Generic control variable array dimension
 C     ----------------------------------------
 C
-C     maxCtrlArr2D :: number of 2-dim. generic control variables
-C     maxCtrlArr3D :: number of 3-dim. generic control variables
+C     maxCtrlArr2D :: number of 2-d generic init. ctrl variables
+C     maxCtrlArr3D :: number of 3-d generic init. ctrl variables
+C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
+C     maxCtrlProc  :: number of pre-processing options per ctrl variable
 
       integer     maxCtrlArr2D
       parameter ( maxCtrlArr2D = 1 )
@@ -19,6 +21,9 @@ C     maxCtrlArr3D :: number of 3-dim. generic control variables
 
       integer     maxCtrlTim2D
       parameter ( maxCtrlTim2D = 1 )
+
+      integer     maxCtrlProc
+      parameter ( maxCtrlProc = 1 )
 
 CEH3 ;;; Local Variables: ***
 CEH3 ;;; mode:fortran ***
