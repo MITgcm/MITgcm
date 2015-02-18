48ef8ad28f670347304443a172679d5ec6cd5b25
Patrick Heimbach <heimbach@mit.edu>
2015-02-18 20:47:01 +0000

    Update verification.

--- verification/hs94.1x64x5/code_oad/OPENAD_OPTIONS.h
+++ verification/hs94.1x64x5/code_oad/OPENAD_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_oad/OPENAD_OPTIONS.h,v 1.1 2015/01/17 07:20:49 heimbach Exp $
+C $Header: /u/gcmpack/MITgcm/verification/hs94.1x64x5/code_oad/OPENAD_OPTIONS.h,v 1.2 2015/02/18 20:47:01 heimbach Exp $
 C $Name:  $
 
 CBOP
@@ -20,7 +20,9 @@ CEOP
 
 #ifdef ALLOW_OPENAD
 
-#define ALLOW_OPENAD_ACTIVE_FILE
+cph#define ALLOW_OPENAD_ACTIVE_READ_XYZ
+#define ALLOW_OPENAD_ACTIVE_READ_XY
+#define ALLOW_OPENAD_ACTIVE_WRITE
 
 #endif /* ALLOW_OPENAD */
 #endif /* OPENAD_OPTIONS_H */
