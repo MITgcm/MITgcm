029e5460cb551dc8d19bff393c458d6e459b222f
Patrick Heimbach <heimbach@mit.edu>
2015-02-18 12:41:22 +0000

    Change to 3D active read

---
+++ verification/OpenAD/code_oad/OPENAD_OPTIONS.h
@@ -0,0 +1,28 @@
+C $Header: /u/gcmpack/MITgcm/verification/OpenAD/code_oad/OPENAD_OPTIONS.h,v 1.1 2015/02/18 12:41:22 heimbach Exp $
+C $Name:  $
+
+CBOP
+C !ROUTINE: OPENAD_OPTIONS.h
+C !INTERFACE:
+C #include "OPENAD_OPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for OpenAD (openad) package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
+CEOP
+
+#ifndef OPENAD_OPTIONS_H
+#define OPENAD_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_OPENAD
+
+#define ALLOW_OPENAD_ACTIVE_READ_XYZ
+#undef ALLOW_OPENAD_ACTIVE_READ_XY
+#undef ALLOW_OPENAD_ACTIVE_WRITE
+
+#endif /* ALLOW_OPENAD */
+#endif /* OPENAD_OPTIONS_H */
