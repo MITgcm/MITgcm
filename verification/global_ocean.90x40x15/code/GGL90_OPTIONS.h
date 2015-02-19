ce268b818e80b9117373c6b327a4acd8a5c76de8
Martin Losch <Martin.Losch@awi.de>
2015-02-19 16:00:45 +0000

    compile ggl90 with IDEMIX

---
+++ verification/global_ocean.90x40x15/code/GGL90_OPTIONS.h
@@ -0,0 +1,34 @@
+C $Header: /u/gcmpack/MITgcm/verification/global_ocean.90x40x15/code/GGL90_OPTIONS.h,v 1.1 2015/02/19 16:00:45 mlosch Exp $
+C $Name:  $
+
+C     *=============================================================*
+C     | GGL90_OPTIONS.h
+C     | o CPP options file for GGL90 package.
+C     *=============================================================*
+C     | Use this file for selecting options within the GGL90
+C     | package.
+C     *=============================================================*
+
+#ifndef GGL90_OPTIONS_H
+#define GGL90_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_GGL90
+C     Package-specific Options & Macros go here
+
+C     Enable horizontal diffusion of TKE.
+#undef ALLOW_GGL90_HORIZDIFF
+
+C     Use horizontal averaging for viscosity and diffusivity as
+C     originally implemented in OPA.
+#undef ALLOW_GGL90_SMOOTH
+
+c-----------------------------------------------------------------------
+c       include IDEMIX model
+c-----------------------------------------------------------------------
+#define ALLOW_GGL90_IDEMIX
+
+
+#endif /* ALLOW_GGL90 */
+#endif /* GGL90_OPTIONS_H */
