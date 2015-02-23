57a618d609548ee39b34b0eb3e984685704f31f7
Martin Losch <Martin.Losch@awi.de>
2015-02-23 13:17:09 +0000

    fix comments to only refer to Olbers, D. and Eden, C. (2013) and IDEMIX 1

--- pkg/ggl90/GGL90.h
+++ pkg/ggl90/GGL90.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ggl90/GGL90.h,v 1.9 2015/02/22 21:01:23 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ggl90/GGL90.h,v 1.10 2015/02/23 13:17:09 mlosch Exp $
 C $Name:  $
 
 #ifdef ALLOW_GGL90
@@ -38,11 +38,9 @@ C     mxlMaxFlag      - Flag for limiting mixing-length method (default=0)
 C     mxlSurfFlag     - Flag to force mixing near ocean surface (default=.FALSE.)
 C
 C     useIDEMIX       - turn on internal wave mixing contribution modeled by
-C                       IDEMIX:
-C                     - Olbers, D. and Eden, C. (2013)
+C                       IDEMIX version 1:
+C                     - Olbers, D. and Eden, C. (2013), J. Phys. Oceano.
 C                       doi:10.1175/JPO-D-12-0207.1
-C                     - Eden, C. and Olbers, D. (2014)
-C                       doi:10.1175/JPO-D-13-0224.1
 C
 C     GGL90dumpFreq   - analogue of dumpFreq (= default)
 C     GGL90taveFreq   - analogue of taveFreq (= default)
