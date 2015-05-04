63c850351a734f4a9a2d2613b3f742b38ca2645d
Oliver Jahn <jahn@mit.edu>
2015-05-04 16:39:09 +0000

    do not always import cs and llc (so we do not depend on matplotlib)

--- utils/python/MITgcmutils/MITgcmutils/__init__.py
+++ utils/python/MITgcmutils/MITgcmutils/__init__.py
@@ -3,8 +3,6 @@ from .mds import rdmds, wrmds
 from .ptracers import iolabel,iolabel2num
 from .diagnostics import readstats
 from .mnc import rdmnc, mnc_files
-from . import cs
-from . import llc
 
 __all__ = ['NaN', 'Inf', 'rdmds', 'wrmds', 'iolabel', 'iolabel2num',
            'readstats', 'rdmnc', 'mnc_files', 'cs', 'llc']
