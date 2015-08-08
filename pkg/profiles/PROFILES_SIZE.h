38287224dd7228f1a7a7d0e19de4e3f39dd5e797
Gael Forget <gforget@mit.edu>
2015-08-08 18:36:19 +0000

    - PROFILES_SIZE.h: rm CPP switch in NUM_INTERP_POINTS setting. - profiles_init_fixed.F: determine iINTERP (integer up to   NUM_INTERP_POINTS) per file; improve summary in STDOUT. - profiles_readparms.F: if data.profiles does not contain   specifications of prof_names and prof_namesmod then   revert to prof_T/S, theta/salt as default names; check   consistency between prof_names and prof_namesmod settings.

--- pkg/profiles/PROFILES_SIZE.h
+++ pkg/profiles/PROFILES_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/profiles/PROFILES_SIZE.h,v 1.1 2015/08/06 15:48:20 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/profiles/PROFILES_SIZE.h,v 1.2 2015/08/08 18:36:19 gforget Exp $
 C $Name:  $
 
 C============================================================
@@ -18,9 +18,5 @@ C============================================================
       INTEGER NLEVELMAX
       PARAMETER ( NLEVELMAX=110 )
       INTEGER NUM_INTERP_POINTS
-#ifndef ALLOW_PROFILES_GENERICGRID
       PARAMETER (NUM_INTERP_POINTS = 4)
-#else
-      PARAMETER (NUM_INTERP_POINTS = 1)
-#endif
 
