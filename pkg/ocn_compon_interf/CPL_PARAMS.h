31ebb84b18a8f549bbdb263a95c3706fedb9a10c
Jean-Michel Campin <jmc@ocean.mit.edu>
2015-11-12 00:55:18 +0000

    Start a new way to set-up optionally-exchanged variables (previously, only  there for exchange_DIC): switch on/off from 1 place, at run-time, in coupler  data.cpl (new) instead of at compile time in ATMIDS.h & OCNIDS.h ; - move checking of comp. configs out of cpl_exch_configs.F into new S/R   ATM/OCN_CHECK_CPLCONFIG; - add error-counter to track errors (e.g., in "data.cpl") and to stop   cleanly in ATM/OCN_CHECK_CPLCONFIG; - add new S/R CPL_IMPORT_CPLPARMS to import coupling-exchange selectors   from coupler ; called from initialise_fixed.F, after PACKAGES_BOOT and   before PACKAGES_READPARMS;

--- pkg/ocn_compon_interf/CPL_PARAMS.h
+++ pkg/ocn_compon_interf/CPL_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ocn_compon_interf/CPL_PARAMS.h,v 1.6 2013/07/18 20:02:08 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ocn_compon_interf/CPL_PARAMS.h,v 1.7 2015/11/12 00:55:18 jmc Exp $
 C $Name:  $
 
 #ifdef COMPONENT_MODULE
@@ -10,6 +10,12 @@ C     |   this version is specific to 1 component (ocean)
 C     *==========================================================*
 
 C--   COMMON /CPL_OCN_PAR_L/: logical parameters
+C     ocn_cplSequential  :: use Sequential Coupling (instead of Synchronous)
+C     ocn_cplExch_RunOff :: exchange RunOff     fields with coupler
+C     ocn_cplExch1W_sIce :: 1-way exchange of seaice fields with coupler
+C     ocn_cplExch2W_sIce :: 2-way exchange of ThSIce fields with coupler
+C     ocn_cplExch_SaltPl :: exchange Salt-Plume fields with coupler
+C     ocn_cplExch_DIC    :: exchange DIC        fields with coupler
 C     useImportHFlx :: True => use the Imported HeatFlux from couler
 C     useImportFW   :: True => use the Imported Fresh Water flux fr cpl
 C     useImportTau  :: True => use the Imported Wind-Stress from couler
@@ -20,11 +26,22 @@ C     useImportCO2  :: True => use the Imported atmos. CO2 from coupler
 C     useImportWSpd :: True => use the Imported surface Wind speed fr cpl
 
       COMMON /CPL_OCN_PAR_L/
+     &  ocn_cplSequential,
+     &  ocn_cplExch_RunOff,
+     &  ocn_cplExch1W_sIce, ocn_cplExch2W_sIce, ocn_cplExch_SaltPl,
+     &  ocn_cplExch_DIC,
      &  useImportHFlx, useImportFW, useImportTau,
      &  useImportSLP, useImportSIce, useImportFIce,
      &  useImportCO2, useImportWSpd,
      &  cpl_snapshot_mdsio, cpl_snapshot_mnc,
      &  cpl_timeave_mdsio, cpl_timeave_mnc
+
+      LOGICAL ocn_cplSequential
+      LOGICAL ocn_cplExch_RunOff
+      LOGICAL ocn_cplExch1W_sIce
+      LOGICAL ocn_cplExch2W_sIce
+      LOGICAL ocn_cplExch_SaltPl
+      LOGICAL ocn_cplExch_DIC
       LOGICAL useImportHFlx
       LOGICAL useImportFW
       LOGICAL useImportTau
@@ -36,6 +53,15 @@ C     useImportWSpd :: True => use the Imported surface Wind speed fr cpl
       LOGICAL
      &     cpl_snapshot_mdsio, cpl_snapshot_mnc,
      &     cpl_timeave_mdsio, cpl_timeave_mnc
+
+C--   COMMON /CPL_OCN_PAR_I/: Integer valued parameters
+C     maxNumberPrint :: max number of printed Export/Import messages
+C     countPrtExp    :: counter for printed Export message
+C     countPrtImp    :: counter for printed Import message
+C     cplErrorCount  :: counter for errors in coupling config
+      COMMON /CPL_OCN_PAR_I/
+     &  cplErrorCount
+      INTEGER cplErrorCount
 
 C--   COMMON /CPL_OCN_PAR_C/: Character valued parameters
 c     CHARACTER*(MAX_LEN_FNAM) cpl_ocnFile
