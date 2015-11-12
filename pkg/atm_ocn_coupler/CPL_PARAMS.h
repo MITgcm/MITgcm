6502611145ea9d22c089618ffc4c349810c82f03
Jean-Michel Campin <jmc@ocean.mit.edu>
2015-11-12 00:49:04 +0000

    Start a new way to set-up optionally-exchanged variables (previously, only  there for exchange_DIC): switch on/off from 1 place, at run-time, in coupler  data.cpl (new) instead of at compile time in ATMIDS.h & OCNIDS.h : - change parameter file from "data" to "data.cpl" (in cpl_read_params.F),   and read namelist directly (without eessup S/R calls) instead of   in the very un-flexible old way, 1 param per line, in fixed order; - add error-counter to track errors (e.g., missing "data.cpl") and to stop   cleanly in CPL_CHECK_CPLCONFIG - add new coupling-exchange selectors (some not yet active) including   cpl_exchange_DIC (to set to 3 to replace atmCpl_exchange_DIC=T in ATMIDS.h                                        and ocnCpl_exchange_DIC=T in OCNIDS.h); - add 2 new S/R (CPL_SEND_ATM/OCN_CPLPARMS) to send coupler-params to each   component ; called directly from coupler.F, after comp. registrations;

--- pkg/atm_ocn_coupler/CPL_PARAMS.h
+++ pkg/atm_ocn_coupler/CPL_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/CPL_PARAMS.h,v 1.4 2013/12/02 22:03:08 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/CPL_PARAMS.h,v 1.5 2015/11/12 00:49:04 jmc Exp $
 C $Name:  $
 
 C     *==========================================================*
@@ -7,17 +7,41 @@ C     |  - parameter for the Coupler, holds in common block
 C     *==========================================================*
 
 C--   COMMON /CPL_PAR_I/ Integer parameters
+C     cpl_sequential   :: =0/1 : selects Synchronous/Sequential Coupling
+C-- cpl_exchange_[xxx] :: controls exchange of [xxx] fields with OCN & ATM comp:
+C                      ::  =0 : none ; =1,3 : exch [xxx] fields with OCN comp.
+C                      ::              =2,3 : exch [xxx] fileds with ATM comp.
+C     cpl_exchange_RunOff :: controls exchange of RunOff fields
+C     cpl_exchange1W_sIce :: controls 1-way exchange of seaice (step fwd in ATM)
+C     cpl_exchange2W_sIce :: controls 2-way exchange of ThSIce variables
+C     cpl_exchange_SaltPl :: controls exchange of Salt-Plume fields
+C     cpl_exchange_DIC    :: controls exchange of DIC variables
+C--
 C     runOffMapSize    :: Nunber of connected grid points in RunOff-Map
 C     nCouplingSteps   :: Number of coupling steps
 C     nSteps_atm       :: Number of coupling steps done by ATM
 C     nSteps_ocn       :: Number of coupling steps done by OCN
+C     cplErrorCount    :: counter for errors in coupling config
       COMMON /CPL_PAR_I/
+     &     cpl_sequential,
+     &     cpl_exchange_RunOff,
+     &     cpl_exchange1W_sIce, cpl_exchange2W_sIce,
+     &     cpl_exchange_SaltPl,
+     &     cpl_exchange_DIC,
      &     runOffMapSize,
      &     nCouplingSteps,
-     &     nSteps_atm, nSteps_ocn
+     &     nSteps_atm, nSteps_ocn,
+     &     cplErrorCount
+      INTEGER cpl_sequential
+      INTEGER cpl_exchange_RunOff
+      INTEGER cpl_exchange1W_sIce
+      INTEGER cpl_exchange2W_sIce
+      INTEGER cpl_exchange_SaltPl
+      INTEGER cpl_exchange_DIC
       INTEGER runOffMapSize
       INTEGER nCouplingSteps
       INTEGER nSteps_atm, nSteps_ocn
+      INTEGER cplErrorCount
 
 C--   COMMON /CPL_PAR_C/ Character parameters
 C     runOffMapFile    :: Input file for setting runoffmap
