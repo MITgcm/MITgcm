3fd4b811eee8f03e8286eaef367a94a520bd2d04
Jean-Michel Campin <jmc@ocean.mit.edu>
2015-11-12 01:01:58 +0000

    add ATM_CPL_OPTIONS.h in component pkg and include it in all pkg source files

---
+++ pkg/atm_compon_interf/ATM_CPL_OPTIONS.h
@@ -0,0 +1,13 @@
+C $Header: /u/gcmpack/MITgcm/pkg/atm_compon_interf/ATM_CPL_OPTIONS.h,v 1.1 2015/11/12 01:01:57 jmc Exp $
+C $Name:  $
+
+#ifndef ATM_CPL_OPTIONS_H
+#define ATM_CPL_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_ATM_COMPON_INTERF
+C     Package-specific Options & Macros go here
+
+#endif /* ALLOW_ATM_COMPON_INTERF */
+#endif /* ATM_CPL_OPTIONS_H */
