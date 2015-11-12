1be817fa630c78449d6d9c9f55847da728950fc6
Jean-Michel Campin <jmc@ocean.mit.edu>
2015-11-12 01:06:40 +0000

    Start a new way to set-up optionally-exchanged variables (previously, only  there for exchange_DIC): switch on/off from 1 place, at run-time, in coupler  data.cpl (new) instead of at compile time in ATMIDS.h & OCNIDS.h: - move checking of comp. configs out of cpl_exch_configs.F into new S/R   ATM/OCN_CHECK_CPLCONFIG; - add error-counter to track errors (e.g., in "data.cpl") and to stop   cleanly in ATM/OCN_CHECK_CPLCONFIG; - add new S/R CPL_IMPORT_CPLPARMS to import coupling-exchange selectors   from coupler ; called from initialise_fixed.F, after PACKAGES_BOOT and   before PACKAGES_READPARMS.

--- pkg/atm_compon_interf/CPL_PARAMS.h
+++ pkg/atm_compon_interf/CPL_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_compon_interf/CPL_PARAMS.h,v 1.6 2013/09/24 23:13:54 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_compon_interf/CPL_PARAMS.h,v 1.7 2015/11/12 01:06:40 jmc Exp $
 C $Name:  $
 
 #ifdef COMPONENT_MODULE
@@ -10,6 +10,12 @@ C     |   this version is specific to 1 component (atmos)
 C     *==========================================================*
 
 C--   COMMON /CPL_ATM_PAR_L/: logical parameters
+C     atm_cplSequential  :: use Sequential Coupling (instead of Synchronous)
+C     atm_cplExch_RunOff :: exchange RunOff     fields with coupler
+C     atm_cplExch1W_sIce :: 1-way exchange of seaice fields with coupler
+C     atm_cplExch2W_sIce :: 2-way exchange of ThSIce fields with coupler
+C     atm_cplExch_SaltPl :: exchange Salt-Plume fields with coupler
+C     atm_cplExch_DIC    :: exchange DIC        fields with coupler
 C     cpl_oldPickup :: restart from an old pickup (= until checkpoint 59h)
 C     useImportMxlD :: True => use Imported Mix.Layer Detph from coupler
 C     useImportSST  :: True => use the Imported SST from coupler
@@ -17,10 +23,20 @@ C     useImportSSS  :: True => use the Imported SSS from coupler
 C     useImportVsq  :: True => use the Imported Surf. velocity^2
 C     useImportFlxCO2 :: True => use the Imported air-sea CO2 fluxes from coupler
       COMMON /CPL_ATM_PAR_L/
+     &  atm_cplSequential,
+     &  atm_cplExch_RunOff,
+     &  atm_cplExch1W_sIce, atm_cplExch2W_sIce, atm_cplExch_SaltPl,
+     &  atm_cplExch_DIC,
      &  cpl_oldPickup,
      &  useImportMxlD, useImportSST, useImportSSS,
      &  useImportVsq, useImportFlxCO2
 
+      LOGICAL atm_cplSequential
+      LOGICAL atm_cplExch_RunOff
+      LOGICAL atm_cplExch1W_sIce
+      LOGICAL atm_cplExch2W_sIce
+      LOGICAL atm_cplExch_SaltPl
+      LOGICAL atm_cplExch_DIC
       LOGICAL cpl_oldPickup
       LOGICAL useImportMxlD, useImportSST, useImportSSS
       LOGICAL useImportVsq, useImportFlxCO2
@@ -30,13 +46,16 @@ C     cplSendFrq_iter :: send data to coupler every "cplSendFrq" iter
 C     maxNumberPrint  :: max number of printed Export/Import messages
 C     countPrtExp     :: counter for printed Export message
 C     countPrtImp     :: counter for printed Import message
+C     cplErrorCount   :: counter for errors in coupling config
       COMMON /CPL_ATM_PAR_I/
      &  cplSendFrq_iter,
-     &  maxNumberPrint, countPrtExp, countPrtImp
+     &  maxNumberPrint, countPrtExp, countPrtImp,
+     &  cplErrorCount
       INTEGER cplSendFrq_iter
       INTEGER maxNumberPrint
       INTEGER countPrtExp
       INTEGER countPrtImp
+      INTEGER cplErrorCount
 
 C--   COMMON /CPL_ATM_PAR_C/: Character valued parameters
 c     CHARACTER*(MAX_LEN_FNAM) cpl_atmFile
