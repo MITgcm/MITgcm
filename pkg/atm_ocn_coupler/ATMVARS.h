df704bd4eb6276a0d6c7d4fd18f2996c4a1860fd
Jean-Michel Campin <jmc@ocean.mit.edu>
2016-01-06 00:28:15 +0000

    - add option to export RunOff to OCN (to use for DIC river input) - add 2-way thSIce vars exchange to allow to use seaice dynamics in OCN

--- pkg/atm_ocn_coupler/ATMVARS.h
+++ pkg/atm_ocn_coupler/ATMVARS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/ATMVARS.h,v 1.7 2013/12/02 23:21:47 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/ATMVARS.h,v 1.8 2016/01/06 00:28:14 jmc Exp $
 C $Name:  $
 
 C     *==========================================================*
@@ -9,11 +9,16 @@ C     *==========================================================*
 
 C     landMask_atm   :: Atmosphere land mask (=1 : full land grid-point;
 C                       =0 : full ocean grid-point);
+
+C--   fields send to ATM:
 C     OcMxlD_atm     :: Ocean mixed-layer depths on atmos. grid (m)
 C     SST_atm        :: Sea surface temperature on atmos. grid ( oC).
 C     SSS_atm        :: Sea surface temperature on atmos. grid (psu).
 C     vSq_atm        :: Sea surface velocity square on atmos. grid (m2/s2)
+C-    optionally sent:
 C     fluxCO2_atm    :: flux of CO2 from ocn->Atm on atmos. grid (mol/m2/s)
+
+C--   fields received from ATM:
 C     atmSLPr_atm    :: Sea Level atmos. pressure on atmos. grid (Pa)
 C     HeatFlux_atm   :: net Heat flux on atmos. grid (W/m2, +=upward)
 C     qShortWave_atm :: net shortwave radiation on atmos. grid (W/m2, +=upward)
@@ -23,49 +28,66 @@ C     TauY_atm       :: Meridional momentum flux on atmos. grid ( N/m^2, same
 C                       sign as the wind ; positive wind == northward flow)
 C     EvMPr_atm      :: Fresh water flux (=Evap-Precip) on atmos. grid
 C                       ( kg/m2/s, positive into atmosphere).
+C-    optionally received:
 C     RunOff_atm     :: Fresh water flux (=RunOff) on atmos. grid
 C                       ( kg/m2/s, positive is leaving the land bucket)
 C     ROEnFx_atm     :: Energy carried by RunOff on atmos. grid
 C                       ( W/m2, +=leaving land bucket)
-C     SaltFlx_atm    :: salt flux from sea-ice compon. on atmos. grid
+C     SaltFlx_atm    :: salt flux from seaice compon. on atmos. grid
 C                       ( g/m2/s, +=upward=leaving the ocean)
-C     sIceMass_atm   :: sea-ice mass  on atmos. grid (kg/m2)
+C     sIceMass_atm   :: seaice mass  on atmos. grid (kg/m2)
+C     saltPlmFlx_atm :: salt-plume flux on atmos. grid, for salt_plume pkg
 C     aCO2_atm       :: atmos CO2 on atmos. grid (parts by volume)
 C     wSpeed_atm     :: surface windspeed on atmos. grid (m/s)
-C     sIceFrac_atm   :: sea-ice fraction  on atmos. grid
+
+C--   fields used for multiple purpose:
+C     sIceFrac_atm   :: seaice fraction on atmos. grid
+
+C--   fields sent to & received from ATM:
+C     sIceThick_atm  :: seaice thickness [m]                  on atmos. grid
+C     sIceSnowH_atm  :: snow thickness over seaice  [m]       on atmos. grid
+C     sIceQ1_atm     :: seaice enthalpy of ice layer 1 [J/kg] on atmos. grid
+C     sIceQ2_atm     :: seaice enthalpy of ice layer 2 [J/kg] on atmos. grid
 
       COMMON /ATMVARS_R/
      &       landMask_atm,
-     &       OcMxlD_atm, SST_atm, SSS_atm, vSq_atm, fluxCO2_atm,
+     &       OcMxlD_atm, SST_atm, SSS_atm, vSq_atm,
+     &       fluxCO2_atm,
      &       atmSLPr_atm, HeatFlux_atm, qShortWave_atm,
-c    &       qLatent_atm, qSensible_atm, qLongWave_atm,
-c    &       uVelGround_atm, vVelGround_atm,
-     &       TauX_atm, TauY_atm,
-     &       EvMPr_atm, RunOff_atm, ROEnFx_atm,
-     &       SaltFlx_atm, sIceMass_atm,
-     &       aCO2_atm, wSpeed_atm, sIceFrac_atm
+     &       TauX_atm, TauY_atm, EvMPr_atm,
+     &       RunOff_atm, ROEnFx_atm,
+     &       SaltFlx_atm, sIceMass_atm, saltPlmFlx_atm,
+     &       aCO2_atm, wSpeed_atm,
+     &       sIceFrac_atm,
+     &       sIceThick_atm, sIceSnowH_atm, sIceQ1_atm, sIceQ2_atm
 
       _RL landMask_atm   (Nx_atm,Ny_atm)
+
       _RL OcMxlD_atm     (Nx_atm,Ny_atm)
       _RL SST_atm        (Nx_atm,Ny_atm)
       _RL SSS_atm        (Nx_atm,Ny_atm)
       _RL vSq_atm        (Nx_atm,Ny_atm)
       _RL fluxCO2_atm    (Nx_atm,Ny_atm)
+
       _RL atmSLPr_atm    (Nx_atm,Ny_atm)
       _RL HeatFlux_atm   (Nx_atm,Ny_atm)
       _RL qShortWave_atm (Nx_atm,Ny_atm)
-c     _RL qLatent_atm    (Nx_atm,Ny_atm)
-c     _RL qSensible_atm  (Nx_atm,Ny_atm)
-c     _RL qLongWave_atm  (Nx_atm,Ny_atm)
-c     _RL uVelGround_atm (Nx_atm,Ny_atm)
-c     _RL vVelGround_atm (Nx_atm,Ny_atm)
       _RL TauX_atm       (Nx_atm,Ny_atm)
       _RL TauY_atm       (Nx_atm,Ny_atm)
       _RL EvMPr_atm      (Nx_atm,Ny_atm)
+
       _RL RunOff_atm     (Nx_atm,Ny_atm)
       _RL ROEnFx_atm     (Nx_atm,Ny_atm)
       _RL SaltFlx_atm    (Nx_atm,Ny_atm)
       _RL sIceMass_atm   (Nx_atm,Ny_atm)
+      _RL saltPlmFlx_atm (Nx_atm,Ny_atm)
       _RL aCO2_atm       (Nx_atm,Ny_atm)
       _RL wSpeed_atm     (Nx_atm,Ny_atm)
+
       _RL sIceFrac_atm   (Nx_atm,Ny_atm)
+      _RL sIceThick_atm  (Nx_atm,Ny_atm)
+      _RL sIceSnowH_atm  (Nx_atm,Ny_atm)
+      _RL sIceQ1_atm     (Nx_atm,Ny_atm)
+      _RL sIceQ2_atm     (Nx_atm,Ny_atm)
+
+C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
--- pkg/atm_ocn_coupler/OCNVARS.h
+++ pkg/atm_ocn_coupler/OCNVARS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/OCNVARS.h,v 1.7 2013/12/02 23:21:47 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_ocn_coupler/OCNVARS.h,v 1.8 2016/01/06 00:28:14 jmc Exp $
 C $Name:  $
 
 C     *==========================================================*
@@ -9,11 +9,16 @@ C     *==========================================================*
 
 C     landMask_ocn   :: Atmosphere land mask (=1 : full land grid-point;
 C                       =0 : full ocean grid-point);
+
+C--   fields received from OCN:
 C     OcMxlD_ocn     ::- Ocean mixed-layer depths on ocean grid (m)
 C     SST_ocn        :: Sea surface temperature on ocean grid ( oC).
 C     SSS_ocn        :: Sea surface temperature on ocean grid (psu).
 C     vSq_ocn        :: Sea surface velocity square on ocean grid (m2/s2)
+C-    optionally received:
 C     fluxCO2_ocn    :: flux of CO2 from ocn->Atm on ocean grid (mol/m2/s)
+
+C--   fields send to OCN:
 C     atmSLPr_ocn    :: Sea Level atmos. pressure on ocean grid (Pa)
 C     HeatFlux_ocn   :: net Heat flux on ocean grid (W/m2, +=upward)
 C     qShortWave_ocn :: net shortwave radiation on ocean grid (W/m2, +=upward)
@@ -23,44 +28,62 @@ C     TauY_ocn       :: Meridional momentum flux on ocean grid ( N/m^2, same
 C                       sign as the wind ; positive wind == northward flow)
 C     FWFlux_ocn     :: Fresh water flux on ocean grid
 C                       ( kg/m2/s, positive out of ocean).
-C     SaltFlx_ocn    :: salt flux from sea-ice compon. on ocean grid
+C-    optionally sent:
+C     SaltFlx_ocn    :: salt flux from seaice compon. on ocean grid
 C                       ( g/m2/s, +=upward=leaving the ocean)
-C     sIceMass_ocn   :: sea-ice mass  on ocean grid (kg/m2)
+C     sIceMass_ocn   :: seaice mass  on ocean grid (kg/m2)
+C     saltPlmFlx_ocn :: salt-plume flux for salt_plume pkg
+C     RunOff_ocn     :: Run-Off (kg/m2/s) used for DIC pkg calculations
 C     aCO2_ocn       :: atmos CO2 on ocean grid (parts by volume)
 C     wSpeed_ocn     :: surface windspeed on ocean grid (m/s)
-C     sIceFrac_ocn   :: sea-ice fraction  on ocean grid
+
+C--   fields used for multiple purpose:
+C     sIceFrac_ocn   :: seaice fraction  on ocean grid
+
+C--   fields sent to & received from OCN:
+C     sIceThick_ocn  :: seaice thickness [m]
+C     sIceSnowH_ocn  :: snow thickness over seaice  [m]
+C     sIceQ1_ocn     :: seaice enthalpy of ice layer 1 [J/kg]
+C     sIceQ2_ocn     :: seaice enthalpy of ice layer 2 [J/kg]
 
       COMMON /OCNVARS_R/
      &       landMask_ocn,
      &       OcMxlD_ocn, SST_ocn, SSS_ocn, vSq_ocn,
      &       fluxCO2_ocn,
      &       atmSLPr_ocn, HeatFlux_ocn, qShortWave_ocn,
-c    &       qLatent_ocn, qSensible_ocn, qLongWave_ocn,
-c    &       uVelGround_ocn, vVelGround_ocn,
-     &       TauX_ocn, TauY_ocn,
-     &       FWFlux_ocn,
-     &       SaltFlx_ocn, sIceMass_ocn,
-     &       aCO2_ocn, wSpeed_ocn, sIceFrac_ocn
+     &       TauX_ocn, TauY_ocn, FWFlux_ocn,
+     &       SaltFlx_ocn, sIceMass_ocn, saltPlmFlx_ocn,
+     &       RunOff_ocn, aCO2_ocn, wSpeed_ocn,
+     &       sIceFrac_ocn,
+     &       sIceThick_ocn, sIceSnowH_ocn, sIceQ1_ocn, sIceQ2_ocn
 
       _RL landMask_ocn   (Nx_ocn,Ny_ocn)
+
       _RL OcMxlD_ocn     (Nx_ocn,Ny_ocn)
       _RL SST_ocn        (Nx_ocn,Ny_ocn)
       _RL SSS_ocn        (Nx_ocn,Ny_ocn)
       _RL vSq_ocn        (Nx_ocn,Ny_ocn)
       _RL fluxCO2_ocn    (Nx_ocn,Ny_ocn)
+
       _RL atmSLPr_ocn    (Nx_ocn,Ny_ocn)
       _RL HeatFlux_ocn   (Nx_ocn,Ny_ocn)
       _RL qShortWave_ocn (Nx_ocn,Ny_ocn)
-c     _RL qLatent_ocn    (Nx_ocn,Ny_ocn)
-c     _RL qSensible_ocn  (Nx_ocn,Ny_ocn)
-c     _RL qLongWave_ocn  (Nx_ocn,Ny_ocn)
-c     _RL uVelGround_ocn (Nx_ocn,Ny_ocn)
-c     _RL vVelGround_ocn (Nx_ocn,Ny_ocn)
       _RL TauX_ocn       (Nx_ocn,Ny_ocn)
       _RL TauY_ocn       (Nx_ocn,Ny_ocn)
       _RL FWFlux_ocn     (Nx_ocn,Ny_ocn)
+
       _RL SaltFlx_ocn    (Nx_ocn,Ny_ocn)
       _RL sIceMass_ocn   (Nx_ocn,Ny_ocn)
+      _RL saltPlmFlx_ocn (Nx_ocn,Ny_ocn)
+
+      _RL RunOff_ocn     (Nx_ocn,Ny_ocn)
       _RL aCO2_ocn       (Nx_ocn,Ny_ocn)
       _RL wSpeed_ocn     (Nx_ocn,Ny_ocn)
+
       _RL sIceFrac_ocn   (Nx_ocn,Ny_ocn)
+      _RL sIceThick_ocn  (Nx_ocn,Ny_ocn)
+      _RL sIceSnowH_ocn  (Nx_ocn,Ny_ocn)
+      _RL sIceQ1_ocn     (Nx_ocn,Ny_ocn)
+      _RL sIceQ2_ocn     (Nx_ocn,Ny_ocn)
+
+C---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
