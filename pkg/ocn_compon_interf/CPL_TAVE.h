644a2c3fdf25f3dffe0cc821bda61d00199bdbda
Jean-Michel Campin <jmc@ocean.mit.edu>
2016-01-06 00:58:23 +0000

    add "#ifdef ALLOW_DIC" around DIC related time-ave output fields

--- pkg/ocn_compon_interf/CPL_TAVE.h
+++ pkg/ocn_compon_interf/CPL_TAVE.h
@@ -1,10 +1,10 @@
-C $Header: /u/gcmpack/MITgcm/pkg/ocn_compon_interf/CPL_TAVE.h,v 1.1 2010/01/03 19:26:54 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/ocn_compon_interf/CPL_TAVE.h,v 1.2 2016/01/06 00:58:23 jmc Exp $
 C $Name:  $
 
 CBOP
-C     !ROUTINE: LAND_TAVE.h
+C     !ROUTINE: CPL_TAVE.h
 C     !INTERFACE:
-C     include "LAND_TAVE.h"
+C     include "CPL_TAVE.h"
 C     !DESCRIPTION:
 C     \bv
 C     *==========================================================*
@@ -25,11 +25,6 @@ C     CPL_timeAve :: Cumulated time [s]
 C     SLPtave     :: Atmospheric Sea-Level pressure [Pa=N/m2]
 C     HFtave      :: Net surface heat-flux   [W/m2, +=upward]
 C     QSWtave     :: Net shortwave heat flux [W/m2, +=upward]
-C     QLTtave     :: Latent heat flux        [W/m2, +=upward]
-C     QSNtave     :: Sensible heat flux      [W/m2, +=upward]
-C     QLWtave     :: Net longwave heat flux  [W/m2, +=upward]
-C     UGtave      :: Wind speed @ ground, zonal  component [m/s]
-C     VGtave      :: Wind speed @ ground, merid. component [m/s]
 C     TXtave      :: Surface stress [Pa=N/m2], zonal compon.
 C     TYtave      :: Surface stress [Pa=N/m2], merid compon.
 C     FWtave      :: Net fresh water flux (=E-P-R) [kg/m2/s, +=upward]
@@ -46,23 +41,18 @@ C     fCO2tave    :: Flux of CO2 from ocean->atm [mol/m2/s]
       COMMON /CPL_TAVE_VARS/
      &                   CPL_timeAve,
      &                   SLPtave, HFtave, QSWtave,
-c    &                   QLTtave, QSNtave, QLWtave,
-c    &                   UGtave, VGtave,
      &                   TXtave, TYtave,
      &                   FWtave, SFxtave, SICtave,
-     &                   MXLtave, SSTtave, SSStave, vSqtave,
-     &                   aCO2tave, sWSpdtave,
+     &                   MXLtave, SSTtave, SSStave, vSqtave
+#ifdef ALLOW_DIC
+     &                 , aCO2tave, sWSpdtave,
      &                   iceftave, fCO2tave
+#endif /* ALLOW_DIC */
 
       _RL  CPL_timeAve(nSx,nSy)
       _RL  SLPtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  HFtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  QSWtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-c     _RL  QLTtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-c     _RL  QSNtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-c     _RL  QLWtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-c     _RL  UGtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-c     _RL  VGtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  TXtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  TYtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  FWtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
@@ -72,10 +62,12 @@ c     _RL  VGtave    (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  SSTtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  SSStave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  vSqtave   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+#ifdef ALLOW_DIC
       _RL  aCO2tave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  sWSpdtave (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  iceftave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL  fCO2tave  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+#endif /* ALLOW_DIC */
 
 #endif /* ALLOW_TIMEAVE */
 #endif /* COMPONENT_MODULE */
