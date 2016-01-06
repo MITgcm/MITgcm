fa4a0a5486a1bc73e8360e5f1b580608e43a555a
Jean-Michel Campin <jmc@ocean.mit.edu>
2016-01-06 00:45:23 +0000

    - hold in common block coupler optionally-exchanged variable switch   and set default "useImportXXX" accordingly; + check & stop if inconsistent. - add new param: useImportThSIce for 2-way thSIce vars exchange

--- pkg/atm_compon_interf/CPL_PARAMS.h
+++ pkg/atm_compon_interf/CPL_PARAMS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/atm_compon_interf/CPL_PARAMS.h,v 1.7 2015/11/12 01:06:40 jmc Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/atm_compon_interf/CPL_PARAMS.h,v 1.8 2016/01/06 00:45:23 jmc Exp $
 C $Name:  $
 
 #ifdef COMPONENT_MODULE
@@ -8,6 +8,24 @@ C     | o Header file for Coupling component interface
 C     *==========================================================*
 C     |   this version is specific to 1 component (atmos)
 C     *==========================================================*
+
+C--   COMMON /CPL_ATM_SWITCH/: from coupler, control switch
+C                              of optionnally exchanged fields;
+C     cpl_exchange_RunOff :: controls exchange of RunOff fields
+C     cpl_exchange1W_sIce :: controls 1-way exchange of seaice (step fwd in ATM)
+C     cpl_exchange2W_sIce :: controls 2-way exchange of ThSIce variables
+C     cpl_exchange_SaltPl :: controls exchange of Salt-Plume fields
+C     cpl_exchange_DIC    :: controls exchange of DIC variables
+      COMMON /CPL_ATM_SWITCH/
+     &     cpl_exchange_RunOff,
+     &     cpl_exchange1W_sIce, cpl_exchange2W_sIce,
+     &     cpl_exchange_SaltPl,
+     &     cpl_exchange_DIC
+      INTEGER cpl_exchange_RunOff
+      INTEGER cpl_exchange1W_sIce
+      INTEGER cpl_exchange2W_sIce
+      INTEGER cpl_exchange_SaltPl
+      INTEGER cpl_exchange_DIC
 
 C--   COMMON /CPL_ATM_PAR_L/: logical parameters
 C     atm_cplSequential  :: use Sequential Coupling (instead of Synchronous)
@@ -21,7 +39,8 @@ C     useImportMxlD :: True => use Imported Mix.Layer Detph from coupler
 C     useImportSST  :: True => use the Imported SST from coupler
 C     useImportSSS  :: True => use the Imported SSS from coupler
 C     useImportVsq  :: True => use the Imported Surf. velocity^2
-C     useImportFlxCO2 :: True => use the Imported air-sea CO2 fluxes from coupler
+C     useImportThSIce :: True => use the Imported thSIce state var from coupler
+C     useImportFlxCO2 :: True => use the Imported air-sea CO2 flux from coupler
       COMMON /CPL_ATM_PAR_L/
      &  atm_cplSequential,
      &  atm_cplExch_RunOff,
@@ -29,7 +48,7 @@ C     useImportFlxCO2 :: True => use the Imported air-sea CO2 fluxes from couple
      &  atm_cplExch_DIC,
      &  cpl_oldPickup,
      &  useImportMxlD, useImportSST, useImportSSS,
-     &  useImportVsq, useImportFlxCO2
+     &  useImportVsq, useImportThSIce, useImportFlxCO2
 
       LOGICAL atm_cplSequential
       LOGICAL atm_cplExch_RunOff
@@ -39,7 +58,7 @@ C     useImportFlxCO2 :: True => use the Imported air-sea CO2 fluxes from couple
       LOGICAL atm_cplExch_DIC
       LOGICAL cpl_oldPickup
       LOGICAL useImportMxlD, useImportSST, useImportSSS
-      LOGICAL useImportVsq, useImportFlxCO2
+      LOGICAL useImportVsq, useImportThSIce, useImportFlxCO2
 
 C--   COMMON /CPL_ATM_PAR_I/: Integer valued parameters
 C     cplSendFrq_iter :: send data to coupler every "cplSendFrq" iter
