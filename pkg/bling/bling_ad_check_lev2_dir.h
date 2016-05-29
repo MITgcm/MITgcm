a046fcbb0f3c084434018805b86a1a19e2da1894
Matthew Mazloff <mmazloff@ucsd.edu>
2016-05-29 01:50:29 +0000

    Fix recomps associated with bug fix in gchem_fields_load

--- pkg/bling/bling_ad_check_lev2_dir.h
+++ pkg/bling/bling_ad_check_lev2_dir.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/bling/bling_ad_check_lev2_dir.h,v 1.1 2016/05/19 20:29:26 mmazloff Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/bling/bling_ad_check_lev2_dir.h,v 1.2 2016/05/29 01:50:29 mmazloff Exp $
 C $Name:  $
 
 #ifdef ALLOW_BLING
@@ -13,6 +13,17 @@ CADJ STORE P_sm              = tapelev2, key = ilev_2
 CADJ STORE P_lg              = tapelev2, key = ilev_2 
 CADJ STORE P_diaz            = tapelev2, key = ilev_2
 CADJ STORE chl               = tapelev2, key = ilev_2
+
+C CMM(
+CADJ STORE atmosp0        = tapelev2, key = ilev_2
+CADJ STORE atmosp1        = tapelev2, key = ilev_2
+CADJ STORE feinput0       = tapelev2, key = ilev_2
+CADJ STORE feinput1       = tapelev2, key = ilev_2
+CADJ STORE ice0           = tapelev2, key = ilev_2
+CADJ STORE ice1           = tapelev2, key = ilev_2
+CADJ STORE silica0        = tapelev2, key = ilev_2
+CADJ STORE silica1        = tapelev2, key = ilev_2
+C CMM)
 
 #ifdef ALLOW_BLING_COST
 CADJ STORE totcost  = tapelev2, key = ilev_2
--- pkg/bling/bling_ad_check_lev3_dir.h
+++ pkg/bling/bling_ad_check_lev3_dir.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/pkg/bling/bling_ad_check_lev3_dir.h,v 1.1 2016/05/19 20:29:26 mmazloff Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/bling/bling_ad_check_lev3_dir.h,v 1.2 2016/05/29 01:50:29 mmazloff Exp $
 C $Name:  $
 
 #ifdef ALLOW_BLING
@@ -13,6 +13,17 @@ CADJ STORE P_sm              = tapelev3, key = ilev_3
 CADJ STORE P_lg              = tapelev3, key = ilev_3 
 CADJ STORE P_diaz            = tapelev3, key = ilev_3
 CADJ STORE chl               = tapelev3, key = ilev_3
+
+C CMM(
+CADJ STORE atmosp0        = tapelev3, key = ilev_3
+CADJ STORE atmosp1        = tapelev3, key = ilev_3
+CADJ STORE feinput0       = tapelev3, key = ilev_3
+CADJ STORE feinput1       = tapelev3, key = ilev_3
+CADJ STORE ice0           = tapelev3, key = ilev_3
+CADJ STORE ice1           = tapelev3, key = ilev_3
+CADJ STORE silica0        = tapelev3, key = ilev_3
+CADJ STORE silica1        = tapelev3, key = ilev_3
+C CMM)
 
 #ifdef ALLOW_BLING_COST
 CADJ STORE totcost  = tapelev3, key = ilev_3
