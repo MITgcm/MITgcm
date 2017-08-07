3b83d20fc028eeabd8e5c6cf17d2974dfad4450c
Jean-Michel Campin <jmc@ocean.mit.edu>
2017-08-07 21:15:41 +0000

    add opton to process vector field at A-grid location

--- utils/matlab/cs_grid/split_UV_cub.m
+++ utils/matlab/cs_grid/split_UV_cub.m
@@ -1,39 +1,41 @@
 function [u6t,v6t] = split_UV_cub(u3d,v3d,ksign,kad)
 % [u6t,v6t] = split_UV_cub(u3d,v3d,[ksign,kad])
 %---------------------------------------------
-% split 2d/3d arrays u,v to 3d x 6 faces, and:
-% (kad=1): add 1 column to U and one row to V
-%  => output is u6t(nc+1,nc,[nr],6) & v6t(nc,nc+1,[nr],6)
-% (kad=2): add 1 column + 2 rows to U and 2 columns + 1 row to V
-%  => output is u6t(nc+1,nc+2,[nr],6) & v6t(nc+2,nc+1,[nr],6)
+% Split 2d/3d vector field u,v (on C-grid if kad > 0, on A-grid if kad < 0)
+% into 3d x 6 faces and:
+%  kad=1: (C-grid) add 1 column to U and one row to V <== at the end !!!
+%   => output is u6t(nc+1,nc,[nr],6) & v6t(nc,nc+1,[nr],6)
+% kad=2: add 1 column + 2 rows to U and 2 columns + 1 row to V
+%   => output is u6t(nc+1,nc+2,[nr],6) & v6t(nc+2,nc+1,[nr],6)
+% kad=-1: assuming input u3d & v3d are on A-grid,
+%           add 1 column to U and one row to V <== at the begining !!!
+%   => output is u6t(nc+1,nc,[nr],6) & v6t(nc,nc+1,[nr],6)
 %----------------------------------------------
 % Written by jmc@ocean.mit.edu, 2005.
-% $Header: /u/gcmpack/MITgcm/utils/matlab/cs_grid/split_UV_cub.m,v 1.1 2005/09/15 16:22:24 jmc Exp $
+% $Header: /u/gcmpack/MITgcm/utils/matlab/cs_grid/split_UV_cub.m,v 1.2 2017/08/07 21:15:41 jmc Exp $
 % $Name:  $
 
 if nargin < 3 , ksign = 0; end
 if nargin < 4 , kad = 1; end
 % ksign = 0 ==> no sign ; ksign = 1 ==> change the sign where needed
 % ==> in fact, used only if kad=2
-plmn = 1 - 2*ksign ; 
+plmn = 1 - 2*ksign ;
 
 dims=size(u3d);
 if dims(1) ~= 6*dims(2),
   fprintf(' ERROR in split_UV_cub: 1rst array has the wrong shape !\n');
   fprintf(' dimensions:'); fprintf(' %i',dims); fprintf(' ; => STOP\n');
-  u6t=0; v6t=0; return; 
+  u6t=0; v6t=0; return;
 end
 dims=size(v3d); nDim=length(dims);
 %fprintf(' nDim= %i , dims:',nDim);fprintf(' %i',dims);fprintf('\n');
 if dims(1) ~= 6*dims(2),
   fprintf(' ERROR in split_UV_cub:  2nd array has the wrong shape !\n');
   fprintf(' dimensions:'); fprintf(' %i',dims); fprintf(' ; => STOP\n');
-  u6t=0; v6t=0; return; 
+  u6t=0; v6t=0; return;
 end
 nx=dims(1); nc=dims(2); ncp=nc+1; n2p=nc+2;
 if nDim == 2, nr=1; else nr=prod(dims(3:end)); end
-%u3d=reshape(u3d,[nx nc nr]);
-%v3d=reshape(v3d,[nx nc nr]);
 
 %=================================================================
 
@@ -44,7 +46,7 @@ if nDim == 2, nr=1; else nr=prod(dims(3:end)); end
 
 if kad == 0,
 %- split on to 6 faces with no overlap:
- dims(1)=nc; 
+ dims(1)=nc;
  u6t=u3d;
  v6t=v3d;
 
@@ -69,7 +71,7 @@ elseif kad == 1,
   v6t([1:nc],ncp,:,3)=u3d(1,[nc:-1:1],:,5);
   v6t([1:nc],ncp,:,4)=v3d([1:nc],1,:,5);
   v6t([1:nc],ncp,:,5)=u3d(1,[nc:-1:1],:,1);
-  v6t([1:nc],ncp,:,6)=v3d([1:nc],1,:,1);   
+  v6t([1:nc],ncp,:,6)=v3d([1:nc],1,:,1);
 
 elseif kad == 2,
 
@@ -94,7 +96,7 @@ elseif kad == 2,
   v6t([2:ncp],ncp,:,3)=u3d(1,[nc:-1:1],:,5);
   v6t([2:ncp],ncp,:,4)=v3d([1:nc],1,:,5);
   v6t([2:ncp],ncp,:,5)=u3d(1,[nc:-1:1],:,1);
-  v6t([2:ncp],ncp,:,6)=v3d([1:nc],1,:,1);   
+  v6t([2:ncp],ncp,:,6)=v3d([1:nc],1,:,1);
 
 %- add overlap in j=0,nc+1 for u and i=0,nc+1 for v :
   u6t([1:ncp], 1 ,:,1)=u6t([1:ncp],ncp,:,6);
@@ -124,6 +126,29 @@ elseif kad == 2,
   v6t(n2p,[1:ncp],:,4)=u6t([ncp:-1:1], 2 ,:,6)*plmn;
   v6t( 1 ,[1:ncp],:,6)=v6t(ncp,[1:ncp],:,5);
   v6t(n2p,[1:ncp],:,6)=u6t([ncp:-1:1], 2 ,:,2)*plmn;
+
+elseif kad == -1,
+
+ dims(1)=ncp;
+%- split on to 6 faces with overlap in i-1 for u and j-1 for v :
+ u6t=zeros(ncp,nc,nr,6);
+ v6t=zeros(nc,ncp,nr,6);
+ u6t([2:ncp],:,:,:)=u3d(:,:,:,:);
+ v6t(:,[2:ncp],:,:)=v3d(:,:,:,:);
+
+  u6t(1,[1:nc],:,1)=v6t([nc:-1:1],ncp,:,5);
+  u6t(1,[1:nc],:,3)=v6t([nc:-1:1],ncp,:,1);
+  u6t(1,[1:nc],:,5)=v6t([nc:-1:1],ncp,:,3);
+  u6t(1,[1:nc],:,2)=u6t(ncp,[1:nc],:,1);
+  u6t(1,[1:nc],:,4)=u6t(ncp,[1:nc],:,3);
+  u6t(1,[1:nc],:,6)=u6t(ncp,[1:nc],:,5);
+
+  v6t([1:nc],1,:,1)=v6t([1:nc],ncp,:,6);
+  v6t([1:nc],1,:,3)=v6t([1:nc],ncp,:,2);
+  v6t([1:nc],1,:,5)=v6t([1:nc],ncp,:,4);
+  v6t([1:nc],1,:,2)=u6t(ncp,[nc:-1:1],:,6);
+  v6t([1:nc],1,:,4)=u6t(ncp,[nc:-1:1],:,2);
+  v6t([1:nc],1,:,6)=u6t(ncp,[nc:-1:1],:,4);
 
 end
 
