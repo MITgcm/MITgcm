bd05e3d9ecef92ef981f2924d264dc7a0699a06b
Jean-Michel Campin <jmc@ocean.mit.edu>
2017-12-29 19:38:34 +0000

    - fix storage of gchemTendency (was not available when GCHEM_SEPARATE_FORCING   was defined and now when GCHEM_ADD2TR_TENDENCY is undef)

--- pkg/gchem/gchem_ad_check_lev3_dir.h
+++ pkg/gchem/gchem_ad_check_lev3_dir.h
@@ -1,6 +1,6 @@
-C $Header: /u/gcmpack/MITgcm/pkg/gchem/gchem_ad_check_lev3_dir.h,v 1.1 2008/04/24 21:29:01 gforget Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/gchem/gchem_ad_check_lev3_dir.h,v 1.2 2017/12/29 19:38:34 jmc Exp $
 C $Name:  $
 
-#ifdef ALLOW_GCHEM
+#ifdef GCHEM_ADD2TR_TENDENCY
 CADJ STORE gchemTendency   = tapelev3, key = ilev_3
-#endif /* ALLOW_GCHEM */
+#endif /* GCHEM_ADD2TR_TENDENCY */
