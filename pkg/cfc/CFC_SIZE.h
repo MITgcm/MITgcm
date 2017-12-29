08a25024dcd36558be20238aa60b0b1ae8fcf302
Jean-Michel Campin <jmc@ocean.mit.edu>
2017-12-29 18:32:38 +0000

    - new header file, used to set last dimension of gchemTendency array

---
+++ pkg/cfc/CFC_SIZE.h
@@ -0,0 +1,27 @@
+C $Header: /u/gcmpack/MITgcm/pkg/cfc/CFC_SIZE.h,v 1.1 2017/12/29 18:32:38 jmc Exp $
+C $Name:  $
+
+#ifndef CFC_SIZE_H
+#define CFC_SIZE_H
+
+CBOP
+C    !ROUTINE: CFC_SIZE.h
+C    !INTERFACE:
+C #include CFC_SIZE.h
+
+C    !DESCRIPTION:
+C Contains CFC tracer array size (number of tracers).
+
+C CFC_Tr_num   :: defines how many CFC tracer are allocated.
+      INTEGER CFC_Tr_num
+      PARAMETER( CFC_Tr_num = 2 )
+
+C CFC_pTr_i1   :: pTracer index of first CFC tracer
+      COMMON /CFC_INDEX/ CFC_pTr_i1
+      INTEGER CFC_pTr_i1
+CEOP
+#endif /* CFC_SIZE_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
