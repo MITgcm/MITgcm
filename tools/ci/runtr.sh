0bebabf132aa8bafd3612b5517dcc133b1193423
christophernhill <christophernhill@gmail.com>
2019-03-11 23:30:54 -0400

    more

--- tools/ci/runtr.sh
+++ tools/ci/runtr.sh
@@ -7,5 +7,5 @@
 # MITGCM_EXP   - MITgcm test to run
 # MITGCM_TROPT - Test report options
 #
-${MITGCM_DECMD} "cd /MITgcm/verification; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
-${MITGCM_DECMD} "cd /MITgcm/verification; python verification_parser.py -filename ${MITGCM_EXP}/testreport_out.txt -threshold ${MITGCM_PRECS}"
+eval ${MITGCM_DECMD} "cd /MITgcm/verification; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
+# ${MITGCM_DECMD} "cd /MITgcm/verification; python verification_parser.py -filename ${MITGCM_EXP}/testreport_out.txt -threshold ${MITGCM_PRECS}"
