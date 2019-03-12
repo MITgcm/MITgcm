0352daaddc36b21f3e9ebb6ee1cd7cf226399355
christophernhill <christophernhill@gmail.com>
2019-03-11 23:35:24 -0400

    more

--- tools/ci/runtr.sh
+++ tools/ci/runtr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+set -v
 #
 # Test report running script for Travis CI 
 # - called from .travis.yml script: section with environment variables 
@@ -7,5 +9,5 @@
 # MITGCM_EXP   - MITgcm test to run
 # MITGCM_TROPT - Test report options
 #
-eval ${MITGCM_DECMD} "cd /MITgcm/verification; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
+eval ${MITGCM_DECMD} "cd /MITgcm/verification; pwd ; ./testreport -t ${MITGCM_EXP} ${MITGCM_TROPT} | tee ${MITGCM_EXP}/testreport_out.txt"
 # ${MITGCM_DECMD} "cd /MITgcm/verification; python verification_parser.py -filename ${MITGCM_EXP}/testreport_out.txt -threshold ${MITGCM_PRECS}"
