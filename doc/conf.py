a528106617b55234854da1f2913221b6021d48dd
Oliver Jahn <jahn@mit.edu>
2019-10-11 13:53:14 -0400

    Avoid installing matplotlib for doc build

--- doc/conf.py
+++ doc/conf.py
@@ -39,6 +39,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinxcontrib.programoutput',
     'mitgcm']
 
+autodoc_mock_imports = ['matplotlib', 'mpl_toolkits']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
