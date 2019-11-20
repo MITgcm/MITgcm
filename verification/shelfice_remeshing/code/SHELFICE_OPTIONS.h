8bbfe4d4c618c4afb84adc3c6caa57c4fcc8bb42
Jean-Michel Campin <jmc@ocean.mit.edu>
2019-11-20 13:11:44 -0500

    update Option files
    - CPP option "SHI_USTAR_TOPDR" has been turned into a run-time param
    - use default DIAG_OPTIONS.h file (from pkg/diagnostics)

--- verification/shelfice_remeshing/code/SHELFICE_OPTIONS.h
+++ verification/shelfice_remeshing/code/SHELFICE_OPTIONS.h
@@ -15,18 +15,15 @@ C     *==========================================================*
 C     Package-specific Options & Macros go here
 
 C     allow code for simple ISOMIP thermodynamics
-!#define ALLOW_ISOMIP_TD
+#undef ALLOW_ISOMIP_TD
 
 C     allow friction velocity-dependent transfer coefficient
 C     following Holland and Jenkins, JPO, 1999
 #define SHI_ALLOW_GAMMAFRICT
-C     in uStar expression, use wet-point method to average velocity
-C     at grid-cell center
-!#define SHI_USTAR_WETPOINT
 
+C     allow (vertical) remeshing whenever ocean top thickness factor
+C     exceeds thresholds
 #define ALLOW_SHELFICE_REMESHING
-#define SHI_USTAR_TOPDR
-#undef ALLOW_SHELFICE_GROUNDED_ICE
 
 #endif /* ALLOW_SHELFICE */
 #endif /* SHELFICE_OPTIONS_H */
