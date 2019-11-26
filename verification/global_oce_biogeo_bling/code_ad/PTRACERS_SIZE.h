331566cbebe7c2ec93ebc0270c1fc23e87b4266a
Jean-Michel Campin <jmc@ocean.mit.edu>
2019-11-26 16:45:22 -0500

    New exeriment "global_oce_biogeo_bling" using pkg/bling
    This experiment was developped by Matt Mazloff and added to "verification_other"
    in May 2016. The full history of development is available in git repos:
      https://github.com/MITgcm/verification_other
    under same dir name "global_oce_biogeo_bling"

---
+++ verification/global_oce_biogeo_bling/code_ad/PTRACERS_SIZE.h
@@ -0,0 +1,29 @@
+#ifdef ALLOW_PTRACERS
+
+CBOP
+C    !ROUTINE: PTRACERS_SIZE.h
+C    !INTERFACE:
+C #include PTRACERS_SIZE.h
+
+C    !DESCRIPTION:
+C Contains passive tracer array size (number of tracers).
+
+C PTRACERS_num defines how many passive tracers are allocated/exist.
+C  and is set here (default 1)
+C
+C     Number of tracers
+      INTEGER PTRACERS_num
+      PARAMETER(PTRACERS_num = 8 )
+
+#ifdef ALLOW_AUTODIFF_TAMC
+CMM in tamc.h      INTEGER    iptrkey
+      INTEGER    maxpass
+      PARAMETER( maxpass     = PTRACERS_num + 2 )
+#endif
+
+CEOP
+#endif /* ALLOW_PTRACERS */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
