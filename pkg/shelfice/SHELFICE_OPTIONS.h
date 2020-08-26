fe4be5862afaf7171fa2faea07d20f81109479f2
Ou Wang <owang01@yahoo.com>
2020-08-26 09:41:54 -0700

    Push combined package of shelfice and icefront

--- pkg/shelfice/SHELFICE_OPTIONS.h
+++ pkg/shelfice/SHELFICE_OPTIONS.h
@@ -1,3 +1,6 @@
+C $Header: /u/gcmpack/MITgcm/pkg/shelfice/SHELFICE_OPTIONS.h,v 1.5 2015/12/17 01:52:05 jmc Exp $
+C $Name:  $
+
 C     *==========================================================*
 C     | SHELFICE_OPTIONS.h
 C     | o CPP options file for SHELFICE package.
@@ -14,18 +17,21 @@ C     *==========================================================*
 #ifdef ALLOW_SHELFICE
 C     Package-specific Options & Macros go here
 
+C     allow Ian Fenty's shelfice/icefront merged code
+#define shelfice_new_thermo
+
 C     allow code for simple ISOMIP thermodynamics
 #define ALLOW_ISOMIP_TD
 
 C     allow friction velocity-dependent transfer coefficient
 C     following Holland and Jenkins, JPO, 1999
 #define SHI_ALLOW_GAMMAFRICT
+C     in uStar expression, use wet-point method to average velocity
+C     at grid-cell center
+#undef SHI_USTAR_WETPOINT
 
-C     allow (vertical) remeshing whenever ocean top thickness factor
-C     exceeds thresholds
-#undef ALLOW_SHELFICE_REMESHING
-C     and allow to print message to STDOUT when this happens
-#define SHELFICE_REMESH_PRINT
+CC use 3d shiTransCoeffT and shiTransCoeffS
+C#define ALLOW_shiTransCoeff_3d
 
 #endif /* ALLOW_SHELFICE */
 #endif /* SHELFICE_OPTIONS_H */
