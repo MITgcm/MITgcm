1b91573f888de313ef03c606fd2a0c602f5e46d9
yqliu11 <yuliu@awi.de>
2020-11-10 10:34:10 +0100

    Another way to calculate sidedrag stress, consider 0, 1 or 2 influence from coast lateral drag.

--- pkg/seaice/SEAICE.h
+++ pkg/seaice/SEAICE.h
@@ -143,8 +143,8 @@ C     in analogy to the maximal compressive stress PRESS0
       _RL CbotC      (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 #endif /* SEAICE_ALLOW_BOTTOMDRAG */
 #ifdef SEAICE_ALLOW_SIDEDRAG 
-      COMMON/SEAICE_SIDEDRAG/SideDragx,SideDragy
-      _RL SideDragx  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      COMMON/SEAICE_SIDEDRAG/SideDragX,SideDragY
+      _RL SideDragX  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL SideDragy  (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 #endif /* SEAICE_ALLOW_SIDEDRAG */
 
