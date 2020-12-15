3898b4e37a2c58c19108b2cbf8eb94f41057785d
Martin Losch <martin.losch@awi.de>
2020-12-15 13:19:33 +0100

    replace ECCO_CPPOPTIONS.h with package specific header files

---
+++ verification/offline_exf_seaice/code_ad/CTRL_OPTIONS.h
@@ -0,0 +1,51 @@
+CBOP
+C !ROUTINE: CTRL_OPTIONS.h
+C !INTERFACE:
+C #include "CTRL_OPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for Control (ctrl) package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
+CEOP
+
+#ifndef CTRL_OPTIONS_H
+#define CTRL_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_CTRL
+#ifdef ECCO_CPPOPTIONS_H
+
+C-- When multi-package option-file ECCO_CPPOPTIONS.h is used (directly included
+C    in CPP_OPTIONS.h), this option file is left empty since all options that
+C   are specific to this package are assumed to be set in ECCO_CPPOPTIONS.h
+
+#else /* ndef ECCO_CPPOPTIONS_H */
+C   ==================================================================
+C-- Package-specific Options & Macros go here
+
+C allow use of legacy ecco/ctrl codes
+#define ECCO_CTRL_DEPRECATED
+
+#undef EXCLUDE_CTRL_PACK
+#define  CTRL_SET_PREC_32
+#define  ALLOW_NONDIMENSIONAL_CONTROL_IO
+
+C       >>> Initial values.
+#define ALLOW_THETA0_CONTROL
+
+C       >>> Atmospheric state.
+#define ALLOW_ATEMP_CONTROL
+#define ALLOW_SWDOWN_CONTROL
+
+C       >>> Generic Control.
+#undef ALLOW_GENARR2D_CONTROL
+#undef ALLOW_GENARR3D_CONTROL
+#undef ALLOW_GENTIM2D_CONTROL
+
+C   ==================================================================
+#endif /* ndef ECCO_CPPOPTIONS_H */
+#endif /* ALLOW_CTRL */
+#endif /* CTRL_OPTIONS_H */
