3590c3ddd7a3c282135693bcb796033836445d58
Martin Losch <martin.losch@awi.de>
2021-06-17 20:12:57 +0200

    remove unnecessary "#include PACKAGES_CONFIG.h" from all tamc.h

--- verification/OpenAD/code_ad_singlelayer/tamc.h
+++ verification/OpenAD/code_ad_singlelayer/tamc.h
@@ -1,5 +1,3 @@
-#include "PACKAGES_CONFIG.h"
-
 c     ================================================================
 c     HEADER TAMC
 c     ================================================================
@@ -119,11 +117,9 @@ cph      PARAMETER( maxpass     = PTRACERS_num + 2 )
 
       INTEGER act0, act1, act2, act3, act4
       INTEGER max0, max1, max2, max3
-      INTEGER iikey, kkey, passkey, igadkey, 
+      INTEGER iikey, kkey, passkey, igadkey,
      &        itdkey, idynkey, igmkey, iptrkey
 
 c     ================================================================
 c     END OF HEADER TAMC
 c     ================================================================
-
-
