844f2a301410e9a0ca1c97856cfc8ecf3e8cceba
Martin Losch <martin.losch@awi.de>
2022-03-22 21:33:41 +0100

    two out of three experiments, the third one fails with NaN, unclear
    - using the same recipe as for tutorial_tracer_adjsens with genarr3d
    and genarr2d for the forcing control variables
    - not clear why the kpp-run explodes, to be examined

--- verification/OpenAD/code_oad/CTRL_SIZE.h
+++ verification/OpenAD/code_oad/CTRL_SIZE.h
@@ -13,13 +13,13 @@ C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
 C     maxCtrlProc  :: number of pre-processing options per ctrl variable
 
       integer     maxCtrlArr2D
-      parameter ( maxCtrlArr2D = 1 )
+      parameter ( maxCtrlArr2D = 4 )
 
       integer     maxCtrlArr3D
       parameter ( maxCtrlArr3D = 2 )
 
       integer     maxCtrlTim2D
-      parameter ( maxCtrlTim2D = 4 )
+      parameter ( maxCtrlTim2D = 1 )
 
       integer     maxCtrlProc
       parameter ( maxCtrlProc = 1 )
