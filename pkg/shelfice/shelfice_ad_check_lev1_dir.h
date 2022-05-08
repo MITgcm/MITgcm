2de2cece2f9545b21570747f6b5a5b72e412cf42
Martin Losch <martin.losch@awi.de>
2022-05-08 19:05:10 +0200

    add two more store directives to complete recent merge

--- pkg/shelfice/shelfice_ad_check_lev1_dir.h
+++ pkg/shelfice/shelfice_ad_check_lev1_dir.h
@@ -5,4 +5,8 @@ CADJ STORE shelficeForcingT = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE shelficeForcingS = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE cMeanSHIforT     = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE cMeanSHIforS     = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE shelficeFreshWaterFlux
+CADJ &                      = comlev1, key=ikey_dynamics, kind=isbyte
+CADJ STORE shelficeLoadAnomaly
+CADJ &                      = comlev1, key=ikey_dynamics, kind=isbyte
 #endif /* ALLOW_SHELFICE */
