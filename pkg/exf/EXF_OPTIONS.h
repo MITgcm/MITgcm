4f5f52b32a3d67abb2ec298afca75b6320a5f2e8
mjlosch <Martin.Losch@awi.de>
2022-05-23 13:42:29 +0200

    fix small issues following Ou's suggestions, improve comment

--- pkg/exf/EXF_OPTIONS.h
+++ pkg/exf/EXF_OPTIONS.h
@@ -174,7 +174,7 @@ C Note: To use ALLOW_BULKFORMULAE or EXF_READ_EVAP, needs #define ALLOW_ATM_TEMP
 # define ALLOW_BULKFORMULAE
 C use Large and Yeager (2004) modification to Large and Pond bulk formulae
 # undef  ALLOW_BULK_LARGEYEAGER04
-C use Large and Yeager (2009), Climate Dynamics, 33, pages 341-364
+C use drag formulation of Large and Yeager (2009), Climate Dyn., 33, pp 341-364
 # undef  ALLOW_BULK_LARGEYEAGER09
 # undef  EXF_READ_EVAP
 # ifndef ALLOW_BULKFORMULAE
