5f7bb52e52ffa9093d6b168c58581f1205bffe12
Jean-Michel Campin <jmc@ocean.mit.edu>
2022-12-26 22:47:46 -0500

    Set ALLOW_GENCOST_CONTRIBUTION in the right header file
    - Move setting of ALLOW_GENCOST_CONTRIBUTION from COST_OPTIONS.h
      to ECCO_OPTIONS.h (since it is only used within pkg/ecco);
    - Also list (as #undef) CPP options that were found defined in a
      verification (or _other) experiment code dir.

--- pkg/cost/COST_OPTIONS.h
+++ pkg/cost/COST_OPTIONS.h
@@ -1,3 +1,8 @@
+#ifndef COST_OPTIONS_H
+#define COST_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
 CBOP
 C !ROUTINE: COST_OPTIONS.h
 C !INTERFACE:
@@ -9,11 +14,6 @@ C | CPP options file for Cost-Function (cost) package:
 C | Control which optional features to compile in this package code.
 C *==================================================================*
 CEOP
-
-#ifndef COST_OPTIONS_H
-#define COST_OPTIONS_H
-#include "PACKAGES_CONFIG.h"
-#include "CPP_OPTIONS.h"
 
 #ifdef ALLOW_COST
 #ifdef ECCO_CPPOPTIONS_H
@@ -45,31 +45,15 @@ C       >>> Cost function contributions
 #undef ALLOW_COST_ATLANTIC_HEAT
 
 #undef ALLOW_COST_TEST
+#undef ALLOW_COST_DEPTH
 #undef ALLOW_COST_TSQUARED
 #undef ALLOW_COST_TRACER
 
-C       >>> ALLOW_GENCOST_CONTRIBUTION: interactive way to add basic 2D cost
-C         function terms.
-C       > In data.ecco, this requires the specification of data file (name,
-C         frequency, etc.), bar file name for corresp. model average, standard
-C         error file name, etc.
-C       > In addition, adding such cost terms requires editing ecco_cost.h to
-C         increase NGENCOST, and editing cost_gencost_customize.F to implement
-C         the actual model average (i.e. the bar file content).
-#undef ALLOW_GENCOST_CONTRIBUTION
-C       >>> free form version of GENCOST: allows one to use otherwise defined
-C         elements (e.g. psbar and and topex data) while taking advantage of the
-C         cost function/namelist slots that can be made available using
-C         ALLOW_GENCOST_CONTRIBUTION. To this end ALLOW_GENCOST_CONTRIBUTION
-C         simply switches off tests that check whether all of the gencost
-C         elements (e.g. gencost_barfile and gencost_datafile) are specified
-C         in data.ecco.
-C       > While this option increases flexibility within the gencost framework,
-C         it implies more room for error, so it should be used cautiously, and
-C         with good knowledge of the rest of pkg/ecco.
-C       > It requires providing a specific cost function routine, and editing
-C         cost_gencost_all.F accordingly.
-#undef ALLOW_GENCOST_FREEFORM
+C   List these options here:
+#undef ALLOW_COST_TEMP
+#undef ALLOW_COST_HFLUXM
+#undef ALLOW_DIC_COST
+#undef ALLOW_THSICE_COST_TEST
 
 C   ==================================================================
 #endif /* ndef ECCO_CPPOPTIONS_H */
