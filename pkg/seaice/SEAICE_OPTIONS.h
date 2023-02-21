9522adb15146976ec00aed48b26c4fd8ffe60d88
mjlosch <Martin.Losch@awi.de>
2023-02-21 15:43:35 +0100

    add undefined and undocumented flags with some documentation

--- pkg/seaice/SEAICE_OPTIONS.h
+++ pkg/seaice/SEAICE_OPTIONS.h
@@ -60,6 +60,11 @@ C--   Tracers of ice and/or ice cover.
 #ifdef ALLOW_SITRACER
 C--   To try avoid 'spontaneous generation' of tracer maxima by advdiff.
 # define ALLOW_SITRACER_ADVCAP
+
+C--   Include code to diagnose sea ice tracer budgets in
+C     seaice_advdiff.F and seaice_tracer_phys.F. Diagnostics are
+C     computed the "call diagnostics_fill" statement is commented out.
+# undef ALLOW_SITRACER_DEBUG_DIAG
 #endif
 
 C--   Enable grease ice parameterization
@@ -91,42 +96,68 @@ C     the operational C-grid discretization.
 
 C--   Only for the C-grid version it is possible to
 #ifdef SEAICE_CGRID
+
 C     enable advection of sea ice momentum
 # undef SEAICE_ALLOW_MOM_ADVECTION
+
 C     enable JFNK code by defining the following flag
 # define SEAICE_ALLOW_JFNK
+
 C     enable Krylov code by defining the following flag
 # define SEAICE_ALLOW_KRYLOV
+
+C--   Use a different order when mapping 2D velocity arrays to 1D vector
+C     before passing it to FGMRES.
+# undef SEAICE_JFNK_MAP_REORDER
+
 C     to reproduce old verification results for JFNK
 # undef SEAICE_PRECOND_EXTRA_EXCHANGE
+
 C     enable LSR to use global (multi-tile) tri-diagonal solver
 # undef SEAICE_GLOBAL_3DIAG_SOLVER
+
 C     enable EVP code by defining the following flag
 # define SEAICE_ALLOW_EVP
 # ifdef SEAICE_ALLOW_EVP
 C--   When set use SEAICE_zetaMin and SEAICE_evpDampC to limit viscosities
 C     from below and above in seaice_evp: not necessary, and not recommended
 #  undef SEAICE_ALLOW_CLIPZETA
+
+C     Include code to avoid underflows in EVP-code (copied from CICE).
+C     Many compilers can handle this more efficiently with the help of a flag.
+#  undef SEAICE_EVP_ELIMINATE_UNDERFLOWS
+
+C     Include code to print residual of EVP iteration for debugging/diagnostics
+#  undef ALLOW_SEAICE_EVP_RESIDUAL
 # endif /* SEAICE_ALLOW_EVP */
+
 C     smooth regularization (without max-function) of delta for
 C     better differentiability
 # undef SEAICE_DELTA_SMOOTHREG
+
 C     regularize zeta to zmax with a smooth tanh-function instead
 C     of a min(zeta,zmax). This improves convergence of iterative
 C     solvers (Lemieux and Tremblay 2009, JGR). No effect on EVP
 # define SEAICE_ZETA_SMOOTHREG
+
+C--   Different yield curves within the VP rheology framework
 C     allow the truncated ellipse rheology (runtime flag SEAICEuseTEM)
 # undef SEAICE_ALLOW_TEM
+
 C     allow the use of the Mohr Coulomb rheology (runtime flag SEAICEuseMCS)
 C     as defined in (Ip 1991) /!\ This is known to give unstable results,
 C     use with caution
 # undef SEAICE_ALLOW_MCS
+
 C     allow the use of Mohr Coulomb with elliptical plastic potential
 C     (runtime flag SEAICEuseMCE)
 # undef SEAICE_ALLOW_MCE
+
 C     allow the teardrop and parabolic lens  rheology
 C     (runtime flag SEAICEuseTD and SEAICEusePL)
 # undef SEAICE_ALLOW_TEARDROP
+
+C--   LSR solver settings
 C     Use LSR vector code; not useful on non-vector machines, because it
 C     slows down convergence considerably, but the extra iterations are
 C     more than made up by the much faster code on vector machines. For
@@ -134,17 +165,24 @@ C     the only regularly test vector machine these flags a specified
 C     in the build options file SUPER-UX_SX-8_sxf90_awi, so that we comment
 C     them out here.
 # undef SEAICE_VECTORIZE_LSR
+
 C     Use zebra-method (alternate lines) for line-successive-relaxation
 C     This modification improves the convergence of the vector code
 C     dramatically, so that is may actually be useful in general, but
 C     that needs to be tested. Can be used without vectorization options.
 # undef SEAICE_LSR_ZEBRA
+
+C     Include code to print residual of nonlinear outer loop of LSR
+# undef SEAICE_ALLOW_CHECK_LSR_CONVERGENCE
+
 C     This flag is also required for an actual adjoint of seaice_lsr;
 C     increases memory requirements a lot.
 # undef SEAICE_LSR_ADJOINT_ITER
+
 C     Use parameterisation of grounding ice for a better representation
 C     of fastice in shallow seas
 # undef SEAICE_ALLOW_BOTTOMDRAG
+
 #else /* not SEAICE_CGRID, but old B-grid */
 C--   By default for B-grid dynamics solver wind stress under sea-ice is
 C     set to the same value as it would be if there was no sea-ice.
@@ -155,23 +193,54 @@ C--   By default for B-grid dynamics solver surface tilt is obtained
 C     indirectly via geostrophic velocities. Define following CPP
 C     in order to use ETAN instead.
 # define EXPLICIT_SSH_SLOPE
+
 C--   Defining this flag turns on FV-discretization of the B-grid LSOR solver.
 C     It is smoother and includes all metric terms, similar to C-grid solvers.
 C     It is here for completeness, but its usefulness is unclear.
 # undef SEAICE_LSRBNEW
 #endif /* SEAICE_CGRID */
 
+C--   Some regularisations
 C--   When set limit the Ice-Loading to mass of 1/5 of Surface ocean grid-box
 #undef SEAICE_CAP_ICELOAD
+
 C--   When set use SEAICE_clipVelocties = .true., to clip U/VICE at 40cm/s,
 C     not recommended
 #undef SEAICE_ALLOW_CLIPVELS
+
 C--   When set cap the sublimation latent heat flux in solve4temp according
 C     to the available amount of ice+snow. Otherwise this term is treated
 C     like all of the others -- residuals heat and fw stocks are passed to
 C     the ocean at the end of seaice_growth in a conservative manner.
 C     SEAICE_CAP_SUBLIM is not needed as of now, but kept just in case.
 #undef SEAICE_CAP_SUBLIM
+
+C--   AD flags
+C--   TAF related flag, currently only used in seaice_ad_check_lev[1-4]_dir.h;
+C     it is unclear if this is ever needed.
+#undef AUTODIFF_SOMETIMES_NEEDED
+
+C--   Reset fields to zero to stabilise AD code of dynamics solver
+C     (resulting in wrong gradients)
+#undef SEAICE_DYN_STABLE_ADJOINT
+
+C--   Another flag to simplify dependencies for TAF-generated AD-code
+C     the thermodynamic part, mostly by resetting variables to zero
+#undef SEAICE_MODIFY_GROWTH_ADJ
+
+C--   These flags are not strictly AD-related but may help obtaining
+C     simpler AD-code
+C--   Do not compile code that resets AREA (or AREAITD) to a mininum value
+C     of SEAICE_area_floor (=SIeps with default of 1e-5) if there is
+C     some finite sea ice thickness
+#undef DISABLE_AREA_FLOOR
+
+C--   Do not compile growth/thermodynamics code (avoiding this code can
+C     also be done by setting runtime parameter usePWthermodynamics=F)
+#undef DISABLE_SEAICE_GROWTH
+
+C--   Do not compile/use seaice-related obcs code when using obcs.
+#undef DISABLE_SEAICE_OBCS
 
 C--   Use the adjointable sea-ice thermodynamic model
 C     in seaice_growth_adx.F instead of seaice_growth.F
