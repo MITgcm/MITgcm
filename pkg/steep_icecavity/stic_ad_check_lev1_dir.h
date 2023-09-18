934d420050bd1cf56fed94dc0e590ecb7d835973
Ou Wang <owang01@yahoo.com>
2023-09-18 09:14:44 -0700

    add TAF store directives for pkg/steep_icecavity to remove recomputation warnings
    The store directives are only needed if pkg/ecco is turned on

--- pkg/steep_icecavity/stic_ad_check_lev1_dir.h
+++ pkg/steep_icecavity/stic_ad_check_lev1_dir.h
@@ -5,4 +5,8 @@ CADJ STORE shelficeForcingT = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE shelficeForcingS = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE cMeanSHIforT     = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE cMeanSHIforS     = comlev1, key=ikey_dynamics, kind=isbyte
+#ifdef ALLOW_ECCO
+CADJ STORE shelficeFreshWaterFlux=comlev1,key=ikey_dynamics,kind=isbyte
+CADJ STORE shelficeLoadAnomaly   =comlev1, key=ikey_dynamics,kind=isbyte
+#endif /* ALLOW_ECCO */
 #endif /* ALLOW_STEEP_ICECAVITY */
