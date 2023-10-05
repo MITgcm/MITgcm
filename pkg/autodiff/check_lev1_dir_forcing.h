789667450b1576dd2d78f6bb5f25f375cc1a1b2b
mjlosch <Martin.Losch@awi.de>
2023-10-05 16:46:58 +0200

    some cleanup
    - fix a cpp-flag name for store directive
    - move call to stic_init_depths to where it belongs (ini_masks_etc.F)
    - fix cpp-flag and include files in pkg/ecco

--- pkg/autodiff/check_lev1_dir_forcing.h
+++ pkg/autodiff/check_lev1_dir_forcing.h
@@ -80,7 +80,7 @@ CADJ STORE climvstr   = comlev1, key = ikey_dynamics, kind = isbyte
 #ifdef ALLOW_GEOTHERMAL_FLUX
 CADJ STORE geothermalFlux = comlev1, key=ikey_dynamics, kind=isbyte
 #endif
-#ifdef ALLOW_ADDFLUID
+#ifdef ALLOW_STEEP_ICECAVITY
 CADJ STORE addMass    = comlev1, key=ikey_dynamics, kind=isbyte
 #endif
 # ifdef ALLOW_BLING
