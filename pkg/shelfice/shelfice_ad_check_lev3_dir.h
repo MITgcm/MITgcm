005af54e3898ef3292785cd13d64816929dba19f
Jean-Michel Campin <jm-c@users.noreply.github.com>
2024-01-12 15:10:27 -0500

    More robust setting of kTopC (#800)
    * More robust setting of kTopC
    
    - if using remeshing, set kTopC fct of shelficeMass/shelficeMassInit to allow to
      consider thin ice-shelf (where Ro_surf is zero); this requires to load shelficeMass
      from pickup-file before calling SHELFICE_INIT_FIXED
    - without remeshing, keep original criteria (fct of Ro_surf) but not sensitive to machine
      truncation error (see issue #798).
    Also:
    - reset R_shelfIce and shelficeMassInit to zero where kTopC=0
    - allow to print (currently with debugLevel >= 3) list of location with
      potential mis-match between kTopC and shelficeMassInit or R_shelfIce.
    
    * Simplify + back to original shelfice_read_pickup.F
    
    - setting of "kTopC": consider only 2 cases
    - adjust some comments
    - revert changes in "shelfice_read_pickup.F" (not needed)
    
    * new param "SHI_update_kTopC"
    
    - just to allow to disable update of kTopC
    - default set according to ALLOW_SHELFICE_REMESHING ; + reset to F when SHELFICEMassStepping=F
    - stop if used without ALLOW_SHELFICE_REMESHING defined ;
    - add a weak warning if SHELFICEMassStepping but "shelficeMass" is not initialised from file
    - adjust warning into a stop if useSEAICE=T with SHI_update_kTopC
    - edits in SHELFICE.h (more logical ordering, parms then fields)
    
    * protect kTopC update code
    
    put kTopC update code within #ifdef ALLOW_SHELFICE_REMESHING and if SHI_update_kTopC
    
    * avoid un-used variables
    
    * Improve error message
    
    * document improving pkg/shelfice kTopC setting

--- pkg/shelfice/shelfice_ad_check_lev3_dir.h
+++ pkg/shelfice/shelfice_ad_check_lev3_dir.h
@@ -1,7 +1,9 @@
 #ifdef ALLOW_SHELFICE
+# ifdef ALLOW_SHELFICE_REMESHING
+CADJ STORE kTopC            = tapelvi3, key = ilev_3
+# endif
 CADJ STORE phi0surf         = tapelev3, key = ilev_3
 CADJ STORE shelficeMass     = tapelev3, key = ilev_3
-CADJ STORE kTopC            = tapelvi3, key = ilev_3
 CADJ STORE shelficeForcingT = tapelev3, key = ilev_3
 CADJ STORE shelficeForcingS = tapelev3, key = ilev_3
 CADJ STORE cMeanSHIforT     = tapelev3, key = ilev_3
