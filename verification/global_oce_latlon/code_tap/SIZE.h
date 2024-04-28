7f7b15000f81a357141d9aca2ce61382f3508107
Jean-Michel Campin <jmc@ocean.mit.edu>
2024-04-28 00:23:43 -0400

    smaller Overlap-size (reduce from 3 to 2)

--- verification/global_oce_latlon/code_tap/SIZE.h
+++ verification/global_oce_latlon/code_tap/SIZE.h
@@ -44,8 +44,8 @@ CEOP
       PARAMETER (
      &           sNx =  45,
      &           sNy =  20,
-     &           OLx =   3,
-     &           OLy =   3,
+     &           OLx =   2,
+     &           OLy =   2,
      &           nSx =   2,
      &           nSy =   2,
      &           nPx =   1,
