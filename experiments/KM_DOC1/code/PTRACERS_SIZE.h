7895e9540e6aba346d2c29a62ae41e576d859813
Katarina Merk <katarina_merk@brown.edu>
2024-08-22 10:31:12 -0400

    fixed bats package initilization error and modified print statement

---
+++ experiments/KM_DOC1/code/PTRACERS_SIZE.h
@@ -0,0 +1,19 @@
+#ifdef ALLOW_PTRACERS
+
+CBOP
+C    !ROUTINE: PTRACERS_SIZE.h
+C    !INTERFACE:
+C #include PTRACERS_SIZE.h
+ 
+C    !DESCRIPTION:
+C Contains passive tracer array size (number of tracers).
+
+C PTRACERS_num defines how many passive tracers are allocated/exist.
+C  and is set here (default 1)
+C
+C     Number of tracers
+      INTEGER PTRACERS_num
+      PARAMETER(PTRACERS_num = 36 )
+
+CEOP
+#endif /* ALLOW_PTRACERS */
