4e3c8a09bb0d0811f09a2d675ec7929d87302880
averdy <averdy@ucsd.edu>
2024-11-06 11:34:51 -0800

    Tidy up bling options

--- pkg/bling/BLING_OPTIONS.h
+++ pkg/bling/BLING_OPTIONS.h
@@ -12,10 +12,6 @@ C For the original 6-tracer model of Galbraith et al (2010),
 C define USE_BLING_V1 - but note the different order of tracers in data.ptracers
 #undef USE_BLING_V1
 
-C Use Liebig function instead of geometric mean of the
-C nutrient limitations to calculate maximum phyto growth rate (BLING_V1 only)
-#define MIN_NUT_LIM
-
 C Options for BLING+Nitrogen code:
 C SiBLING: add a 9th tracer for silica
 #undef USE_SIBLING
@@ -28,6 +24,10 @@ c #undef USE_SGS_SED
 
 C Prevents negative values in nutrient fields
 #define BLING_NO_NEG
+
+C Use Liebig function instead of geometric mean of the
+C nutrient limitations to calculate maximum phyto growth rate 
+#define MIN_NUT_LIM
 
 C Allow different phytoplankton groups to have different growth rates and
 C nutrient/light limitations. Parameters implemented have yet to be tuned
@@ -87,9 +87,6 @@ C  coeffs are used).
 
 C When calculating the fraction of sinking organic matter, use model biomass diagnostics.
 #define NEW_FRAC_EXP
-
-C Multiply nutrient limitation terms instead of using a minimum function (BLING_v1 only)
-#undef MULT_NUT_LIM
 
 #endif /* ALLOW_BLING */
 #endif /* BLING_OPTIONS_H */
