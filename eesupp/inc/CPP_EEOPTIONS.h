692891d5e10fb59af16bd219dcec0d02ea3db0f1
Jean-Michel Campin <jmc@mit.edu>
2024-11-08 17:59:49 -0500

    remove FAST_BYTESWAP code
    and add a stop in INI_MODEL_IO if defined

--- eesupp/inc/CPP_EEOPTIONS.h
+++ eesupp/inc/CPP_EEOPTIONS.h
@@ -76,10 +76,6 @@ C     a different file for each tile) and read are thread-safe.
 C--   Flag to turn off the writing of error message to ioUnit zero
 #undef DISABLE_WRITE_TO_UNIT_ZERO
 
-C--   Alternative formulation of BYTESWAP, faster than
-C     compiler flag -byteswapio on the Altix.
-#undef FAST_BYTESWAP
-
 C--   Flag to turn on old default of opening scratch files with the
 C     STATUS='SCRATCH' option. This method, while perfectly FORTRAN-standard,
 C     caused filename conflicts on some multi-node/multi-processor platforms
