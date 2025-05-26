f9e69ef36be7b400e15f5d0d2cae20ca524f58d1
mjlosch <Martin.Losch@awi.de>
2025-05-26 11:00:28 +0200

    store directive and code adjustment to avoid recomputations

--- pkg/bling/bling_ad_check_lev1_dir.h
+++ pkg/bling/bling_ad_check_lev1_dir.h
@@ -1,6 +1,7 @@
 #ifdef ALLOW_BLING
 
-CADJ STORE irr_mem           = comlev1, key = ikey_dynamics, kind=isbyte
-CADJ STORE chl               = comlev1, key = ikey_dynamics, kind=isbyte
+CADJ STORE pH      = comlev1, key = ikey_dynamics, kind = isbyte
+CADJ STORE irr_mem = comlev1, key = ikey_dynamics, kind = isbyte
+CADJ STORE chl     = comlev1, key = ikey_dynamics, kind = isbyte
 
 #endif /* ALLOW_BLING */
