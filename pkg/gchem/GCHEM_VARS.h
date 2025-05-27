7cac918bb4259418e081b30ddca3f36b00b54c83
mjlosch <Martin.Losch@awi.de>
2025-05-27 14:11:39 +0200

    merge two versions of solveaphe into gchem_solveaphe.F
    - the complication is that the parameters select* are part of the
      dic-namelist; for now they are awkwardly defined in GCHEM_VARS.h and
      set in dic/bling_readparams.F to preserve current setups, but we can
      decide to move the namelist parameters to data.gchem (TBD)

--- pkg/gchem/GCHEM_VARS.h
+++ pkg/gchem/GCHEM_VARS.h
@@ -62,94 +62,66 @@ C Fugacity Factor added by Val Bennington Nov. 2010
       _RL st         (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL bt         (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL Ksp_TP_Calc(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-#ifdef ALLOW_BLING
       _RL Ksp_TP_Arag(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-#endif
 
-CML#ifdef CARBONCHEM_SOLVESAPHE
-CMLC If using Solvesaphe routines (Munhoven, 2013) then in adittion,
-CMLC  cat  = total calcium concentration, Ref.: Culkin (1965)
-CMLC   akn = the dissociation constant of ammonium [H][NH3]/[NH4]
-CMLC           Ref.: Yao and Millero (1995)
-CMLC   akhs = the dissociation constant of hydrogen sulfide [H][HS]/[H2S]
-CMLC           Ref.: Millero et al. (1988)
-CMLC          (cited by Millero (1995) and Yao and Millero (1995))
-CMLC  aphscale = pH scale conversion factor ; convert from the total to the free
-CMLC          scale for solvesaphe calculations;  Ref.: Munhoven, 2013
-CMLC   Ksp_TP_Arag = solubility product for aragonite, Ref.: Mucci (1983)
-CMLC----
-CMLC  selectBTconst :: estimates borate concentration from salinity:
-CMLC     =1 :: use default formulation of Uppström (1974)(same as S/R CARBON_COEFFS)
-CMLC     =2 :: use new formulation from Lee et al (2010)
-CMLC
-CMLC  selectFTconst :: estimates fluoride concentration from salinity:
-CMLC     =1 :: use default formulation of Riley (1965) (same as S/R CARBON_COEFFS)
-CMLC     =2 :: use new formulation from Culkin (1965)
-CMLC
-CMLC  selectHFconst :: sets the first dissociation constant for hydrogen fluoride:
-CMLC     =1 :: use default  Dickson and Riley (1979) (same as S/R CARBON_COEFFS)
-CMLC     =2 :: use new formulation of Perez and Fraga (1987)
-CMLC
-CMLC  selectK1K2const :: sets the 1rst & 2nd dissociation constants of carbonic acid:
-CMLC     =1 :: use default formulation of Millero (1995) with data
-CMLC            from Mehrbach et al. (1973) (same as S/R CARBON_COEFFS)
-CMLC     =2 :: use formulation of Roy et al. (1993)
-CMLC     =3 :: use "combination" formulation of Millero (1995)
-CMLC     =4 :: use formulation of Luecker et al. (2000)
-CMLC     =5 :: use formulation of Millero (2010, Mar. Fresh Wat. Res.)
-CMLC     =6 :: use formulation of Waters, Millero, Woosley (2014, Mar. Chem.)
-CMLC  selectPHsolver :: sets the pH solver to use:
-CMLC     =1 :: use the GENERAL solver ;  =2 :: use SEC solver ;
-CMLC     =3 :: use FAST solver routine.
-CML
-CML       COMMON /CARBONCHEM_SOLVESAPHE_ARIANE/
-CMLcav     &                     cat, akn, akhs, aphscale, Ksp_TP_Arag,
-CML     &                     cat, akn, akhs, aphscale,
-CML     &                     at_maxniter,
-CML     &                     selectBTconst,selectFTconst,
-CML     &                     selectHFconst,selectK1K2const,
-CML     &                     selectPHsolver
-CML
-CML      _RL  cat(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-CML      _RL  akn(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-CML      _RL  akhs(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-CML      _RL  aphscale(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-CMLcav      _RL  Ksp_TP_Arag(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-CML
-CML      INTEGER at_maxniter
-CML      INTEGER selectBTconst
-CML      INTEGER selectFTconst
-CML      INTEGER selectHFconst
-CML      INTEGER selectK1K2const
-CML      INTEGER selectPHsolver
-CML#endif /* CARBONCHEM_SOLVESAPHE */
-CML
-CML       COMMON /OXYGEN_CHEM/
-CML     &                     oA0,oA1,oA2,oA3,oA4,oA5,
-CML     &                     oB0,oB1,oB2,oB3,
-CML     &                     oC0
-CML      _RL oA0,oA1,oA2,oA3,oA4,oA5
-CML      _RL oB0,oB1,oB2,oB3
-CML      _RL oC0
-CML
-CML       COMMON /GLOBAL_SURF_MEAN/
-CML     &                          permil,Pa2Atm,epsln
-CML
-CMLC      permil : is conversion factor for mol/m3 to mol/kg
-CMLC               assumes uniform (surface) density
-CMLC      Pa2Atm : for conversion of atmospheric pressure
-CMLC               when coming from atmospheric model
-CML
-CML      _RL  permil
-CML      _RL  Pa2Atm
-CML      _RL  epsln
-CML
-CML       COMMON /SCHMIDT_NO/
-CML     &                    sca1, sca2, sca3, sca4,
-CML     &                    sox1, sox2, sox3, sox4
-CML
-CMLC      Schmidt number coefficients
-CML
-CML      _RL  sca1,sca2,sca3,sca4
-CML      _RL  sox1,sox2,sox3,sox4
-CML
+#ifdef CARBONCHEM_SOLVESAPHE
+C If using Solvesaphe routines (Munhoven, 2013) then in adittion,
+C  cat  = total calcium concentration, Ref.: Culkin (1965)
+C   akn = the dissociation constant of ammonium [H][NH3]/[NH4]
+C           Ref.: Yao and Millero (1995)
+C   akhs = the dissociation constant of hydrogen sulfide [H][HS]/[H2S]
+C           Ref.: Millero et al. (1988)
+C          (cited by Millero (1995) and Yao and Millero (1995))
+C  aphscale = pH scale conversion factor ; convert from the total to the free
+C          scale for solvesaphe calculations;  Ref.: Munhoven, 2013
+C   Ksp_TP_Arag = solubility product for aragonite, Ref.: Mucci (1983)
+C----
+C  selectBTconst :: estimates borate concentration from salinity:
+C     =1 :: use default formulation of Uppström (1974)(same as S/R CARBON_COEFFS)
+C     =2 :: use new formulation from Lee et al (2010)
+C
+C  selectFTconst :: estimates fluoride concentration from salinity:
+C     =1 :: use default formulation of Riley (1965) (same as S/R CARBON_COEFFS)
+C     =2 :: use new formulation from Culkin (1965)
+C
+C  selectHFconst :: sets the first dissociation constant for hydrogen fluoride:
+C     =1 :: use default  Dickson and Riley (1979) (same as S/R CARBON_COEFFS)
+C     =2 :: use new formulation of Perez and Fraga (1987)
+C
+C  selectK1K2const :: sets the 1rst & 2nd dissociation constants of carbonic acid:
+C     =1 :: use default formulation of Millero (1995) with data
+C            from Mehrbach et al. (1973) (same as S/R CARBON_COEFFS)
+C     =2 :: use formulation of Roy et al. (1993)
+C     =3 :: use "combination" formulation of Millero (1995)
+C     =4 :: use formulation of Luecker et al. (2000)
+C     =5 :: use formulation of Millero (2010, Mar. Fresh Wat. Res.)
+C     =6 :: use formulation of Waters, Millero, Woosley (2014, Mar. Chem.)
+C  selectPHsolver :: sets the pH solver to use:
+C     =1 :: use the GENERAL solver ;  =2 :: use SEC solver ;
+C     =3 :: use FAST solver routine.
+
+       COMMON /CARBONCHEM_SOLVESAPHE/
+     &                     cat, akn, akhs, aphscale,
+#ifndef ALLOW_BLING
+     &                     Ksp_TP_Arag,
+#endif
+     &                     at_maxniter
+
+      _RL  cat(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  akn(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  akhs(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL  aphscale(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+
+      INTEGER at_maxniter
+
+      COMMON /CARBONCHEM_SOLVESAPHE_PARAMS/
+     &                     selectBTconst,selectFTconst,
+     &                     selectHFconst,selectK1K2const,
+     &                     selectPHsolver
+
+      INTEGER selectBTconst
+      INTEGER selectFTconst
+      INTEGER selectHFconst
+      INTEGER selectK1K2const
+      INTEGER selectPHsolver
+#endif /* CARBONCHEM_SOLVESAPHE */
