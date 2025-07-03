d4909ae0d7732f687018909a1b384c61d8779981
mjlosch <Martin.Losch@awi.de>
2025-07-03 15:31:33 +0200

    going around in circles: we do need these store directives
    basically reverting commit 08386ec

--- pkg/gchem/gchem_ad_check_lev2_dir.h
+++ pkg/gchem/gchem_ad_check_lev2_dir.h
@@ -1,22 +1,20 @@
-C     apparently we do not need these store directives anymore, but we
-C     keep them here for reference for now
-c#ifdef GCHEM_ALLOW_FFIELDS
-cCADJ STORE gchemapCO2  = tapelev2, key = ilev_2
-cCADJ STORE gchemapCO20 = tapelev2, key = ilev_2
-cCADJ STORE gchemapCO21 = tapelev2, key = ilev_2
-cCADJ STORE gchemAtmosp = tapelev2, key = ilev_2
-cCADJ STORE gchemApres0 = tapelev2, key = ilev_2
-cCADJ STORE gchemApres1 = tapelev2, key = ilev_2
-cCADJ STORE gchemFe     = tapelev2, key = ilev_2
-cCADJ STORE gchemFe0    = tapelev2, key = ilev_2
-cCADJ STORE gchemFe1    = tapelev2, key = ilev_2
-cCADJ STORE gchemIce    = tapelev2, key = ilev_2
-cCADJ STORE gchemIce0   = tapelev2, key = ilev_2
-cCADJ STORE gchemIce1   = tapelev2, key = ilev_2
-cCADJ STORE gchemSi     = tapelev2, key = ilev_2
-cCADJ STORE gchemSi0    = tapelev2, key = ilev_2
-cCADJ STORE gchemSi1    = tapelev2, key = ilev_2
-cCADJ STORE gchemWind   = tapelev2, key = ilev_2
-cCADJ STORE gchemWind0  = tapelev2, key = ilev_2
-cCADJ STORE gchemWind1  = tapelev2, key = ilev_2
-c#endif
+#ifdef GCHEM_ALLOW_FFIELDS
+CADJ STORE gchemapCO2  = tapelev2, key = ilev_2
+CADJ STORE gchemapCO20 = tapelev2, key = ilev_2
+CADJ STORE gchemapCO21 = tapelev2, key = ilev_2
+CADJ STORE gchemAtmosp = tapelev2, key = ilev_2
+CADJ STORE gchemApres0 = tapelev2, key = ilev_2
+CADJ STORE gchemApres1 = tapelev2, key = ilev_2
+CADJ STORE gchemFe     = tapelev2, key = ilev_2
+CADJ STORE gchemFe0    = tapelev2, key = ilev_2
+CADJ STORE gchemFe1    = tapelev2, key = ilev_2
+CADJ STORE gchemIce    = tapelev2, key = ilev_2
+CADJ STORE gchemIce0   = tapelev2, key = ilev_2
+CADJ STORE gchemIce1   = tapelev2, key = ilev_2
+CADJ STORE gchemSi     = tapelev2, key = ilev_2
+CADJ STORE gchemSi0    = tapelev2, key = ilev_2
+CADJ STORE gchemSi1    = tapelev2, key = ilev_2
+CADJ STORE gchemWind   = tapelev2, key = ilev_2
+CADJ STORE gchemWind0  = tapelev2, key = ilev_2
+CADJ STORE gchemWind1  = tapelev2, key = ilev_2
+#endif
