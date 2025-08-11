66afc456c2f26600952ec77f7fd873b19d7ba658
wangy0m <wangy0m@KW60844.KAUST.EDU.SA>
2025-08-11 19:34:00 +0300

    Enabling spatially varying (2D) Kd

--- pkg/bling/BLING_VARS.h
+++ pkg/bling/BLING_VARS.h
@@ -150,6 +150,13 @@ C      Schmidt number coefficients
 
 C ==========================================================
 C   Bling inputs (specified in data.bling)
+C   
+C   bling_k0_2dFile : Path to a file containing a 2D spatial field of
+C                     light attenuation coefficient (k0_2d, in m^-1).
+C                     This coefficient regulates underwater light
+C                     availability in the BLING model.
+C                     If not specified, a constant default value of 
+C                     k0 = 0.04 m^-1 is applied for entire domain.
 C ==========================================================
 
        COMMON /BLING_INPUTS/
