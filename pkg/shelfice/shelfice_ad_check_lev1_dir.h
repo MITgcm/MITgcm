3ff0d2a3fbaac682558eb018f475e53d0f9821e0
mjlosch <Martin.Losch@awi.de>
2025-10-22 11:21:20 +0200

    fix recomputations related to kpp/shelfice/bottom_control

--- pkg/shelfice/shelfice_ad_check_lev1_dir.h
+++ pkg/shelfice/shelfice_ad_check_lev1_dir.h
@@ -10,10 +10,10 @@ CADJ STORE shelficeForcingT = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE shelficeForcingS = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE cMeanSHIforT     = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE cMeanSHIforS     = comlev1, key=ikey_dynamics, kind=isbyte
-#endif /* ALLOW_SHELFICE */
-#ifdef ALLOW_STEEP_ICECAVITY
+c#endif /* ALLOW_SHELFICE */
+c#ifdef ALLOW_STEEP_ICECAVITY
 CADJ STORE shelficeFreshWaterFlux
 CADJ &                      = comlev1, key=ikey_dynamics, kind=isbyte
 CADJ STORE shelficeLoadAnomaly
 CADJ &                      = comlev1, key=ikey_dynamics, kind=isbyte
-#endif /* ALLOW_STEEP_ICECAVITY */
+#endif
