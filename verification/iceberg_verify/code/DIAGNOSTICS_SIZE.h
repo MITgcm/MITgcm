e381644cf6f56ded0c5dbce36e3387863cb79fc0
Paul Summers <somonesummers@gmail.com>
2025-11-05 12:13:16 -0500

    Medium size verification experiment for development purposes. Smaller verification will likely come later.

---
+++ verification/iceberg_verify/code/DIAGNOSTICS_SIZE.h
@@ -0,0 +1,30 @@
+C     Diagnostics Array Dimension 
+C     --------------------------- 
+C     ndiagMax   :: maximum total number of available diagnostics 
+C     numlists   :: maximum number of diagnostics list (in data.diagnostics) 
+C     numperlist :: maximum number of active diagnostics per list (data.diagnostics) 
+C     numLevels  :: maximum number of levels to write    (data.diagnostics) 
+C     numDiags   :: maximum size of the storage array for active 2D/3D diagnostics 
+C     nRegions   :: maximum number of regions (statistics-diagnostics) 
+C     sizRegMsk  :: maximum size of the regional-mask (statistics-diagnostics) 
+C     nStats     :: maximum number of statistics (e.g.: aver,min,max...) 
+C     diagSt_size:: maximum size of the storage array for statistics-diagnostics 
+C Note : may need to increase "numDiags" when using several 2D/3D diagnostics, 
+C  and "diagSt_size" (statistics-diags) since values here are deliberately small. 
+      INTEGER    ndiagMax 
+      INTEGER    numlists, numperlist, numLevels 
+      INTEGER    numDiags 
+      INTEGER    nRegions, sizRegMsk, nStats 
+      INTEGER    diagSt_size 
+      PARAMETER( ndiagMax = 500 ) 
+      PARAMETER( numlists = 4, numperlist = 6, numLevels=12 ) 
+      PARAMETER( numDiags = 4*12*5 ) 
+      PARAMETER( nRegions = 0 , sizRegMsk = 1 , nStats = 4 ) 
+      PARAMETER( diagSt_size = 9*Nr ) 
+ 
+
+ 
+
+CEH3 ;;; Local Variables: *** 
+CEH3 ;;; mode:fortran *** 
+ 
