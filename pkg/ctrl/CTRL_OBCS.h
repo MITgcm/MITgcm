0d7f4e786200d45fb1d72048e0f1359ea8f51469
Jean-Michel Campin <jmc@mit.edu>
2026-03-09 23:16:39 -0400

    change ordering: E,W,N.S
    and promote the 4 geographic direction to upper case

--- pkg/ctrl/CTRL_OBCS.h
+++ pkg/ctrl/CTRL_OBCS.h
@@ -19,100 +19,101 @@ C--
       PARAMETER ( nobcs = 4 )
 
 #ifdef ALLOW_COST
-      _RL  objf_obcsn(nSx,nSy), objf_obcss(nSx,nSy)
-      _RL  objf_obcsw(nSx,nSy), objf_obcse(nSx,nSy)
+      _RL  objf_obcsE(nSx,nSy), objf_obcsW(nSx,nSy)
+      _RL  objf_obcsN(nSx,nSy), objf_obcsS(nSx,nSy)
       _RL  objf_obcsvol, objf_ageos(nSx,nSy)
-      _RL  num_obcsn(nSx,nSy), num_obcss(nSx,nSy)
-      _RL  num_obcsw(nSx,nSy), num_obcse(nSx,nSy)
+      _RL  num_obcsE(nSx,nSy), num_obcsW(nSx,nSy)
+      _RL  num_obcsN(nSx,nSy), num_obcsS(nSx,nSy)
       _RL  num_obcsvol, num_ageos(nSx,nSy)
       COMMON /ctrl_cost_weights_obcs/
-     &     objf_obcsn, objf_obcss, objf_obcsw, objf_obcse,
+     &     objf_obcsE, objf_obcsW, objf_obcsN, objf_obcsS,
      &     objf_obcsvol, objf_ageos,
-     &     num_obcsn, num_obcss, num_obcsw, num_obcse,
+     &     num_obcsE, num_obcsW, num_obcsN, num_obcsS,
      &     num_obcsvol, num_ageos
 
       COMMON /ctrl_cost_mult_obcs/
-     &     mult_obcsn, mult_obcss, mult_obcsw, mult_obcse,
+     &     mult_obcsE, mult_obcsW, mult_obcsN, mult_obcsS,
      &     mult_obcsvol, mult_ageos
-      _RL  mult_obcsn, mult_obcss
-      _RL  mult_obcsw, mult_obcse
+      _RL  mult_obcsE, mult_obcsW
+      _RL  mult_obcsN, mult_obcsS
       _RL  mult_obcsvol, mult_ageos
 #endif
 
       COMMON /ih_modes/ modesv
       _RL modesv (Nr,Nr,Nr)
       COMMON /ctrl_dummy_obcs/
-     &                    xx_obcsn_dummy,
-     &                    xx_obcss_dummy,
-     &                    xx_obcsw_dummy,
-     &                    xx_obcse_dummy
-      _RL xx_obcsn_dummy
-      _RL xx_obcss_dummy
-      _RL xx_obcsw_dummy
-      _RL xx_obcse_dummy
+     &                    xx_obcsE_dummy,
+     &                    xx_obcsW_dummy,
+     &                    xx_obcsN_dummy,
+     &                    xx_obcsS_dummy
+      _RL xx_obcsE_dummy
+      _RL xx_obcsW_dummy
+      _RL xx_obcsN_dummy
+      _RL xx_obcsS_dummy
       COMMON /controlfiles_c_obcs/
-     &                      xx_obcsn_file,
-     &                      xx_obcss_file,
-     &                      xx_obcsw_file,
-     &                      xx_obcse_file
-      CHARACTER*(MAX_LEN_FNAM) xx_obcsn_file
-      CHARACTER*(MAX_LEN_FNAM) xx_obcss_file
-      CHARACTER*(MAX_LEN_FNAM) xx_obcsw_file
-      CHARACTER*(MAX_LEN_FNAM) xx_obcse_file
+     &                      xx_obcsE_file,
+     &                      xx_obcsW_file,
+     &                      xx_obcsN_file,
+     &                      xx_obcsS_file
+      CHARACTER*(MAX_LEN_FNAM) xx_obcsE_file
+      CHARACTER*(MAX_LEN_FNAM) xx_obcsW_file
+      CHARACTER*(MAX_LEN_FNAM) xx_obcsN_file
+      CHARACTER*(MAX_LEN_FNAM) xx_obcsS_file
       COMMON /controltimes_r_obcs/
-     &                        xx_obcsnperiod,
-     &                        xx_obcssperiod,
-     &                        xx_obcswperiod,
-     &                        xx_obcseperiod
-      _RL     xx_obcsnperiod
-      _RL     xx_obcssperiod
-      _RL     xx_obcswperiod
-      _RL     xx_obcseperiod
+     &                        xx_obcsEperiod,
+     &                        xx_obcsWperiod,
+     &                        xx_obcsNperiod,
+     &                        xx_obcsSperiod
+      _RL     xx_obcsEperiod
+      _RL     xx_obcsWperiod
+      _RL     xx_obcsNperiod
+      _RL     xx_obcsSperiod
       COMMON /controltimes_i_obcs/
-     &                        xx_obcsnstartdate1,
-     &                        xx_obcsnstartdate2,
-     &                        xx_obcssstartdate1,
-     &                        xx_obcssstartdate2,
-     &                        xx_obcswstartdate1,
-     &                        xx_obcswstartdate2,
-     &                        xx_obcsestartdate1,
-     &                        xx_obcsestartdate2,
-     &                        xx_obcsnstartdate,
-     &                        xx_obcssstartdate,
-     &                        xx_obcswstartdate,
-     &                        xx_obcsestartdate
-      INTEGER xx_obcsnstartdate1
-      INTEGER xx_obcsnstartdate2
-      INTEGER xx_obcssstartdate1
-      INTEGER xx_obcssstartdate2
-      INTEGER xx_obcswstartdate1
-      INTEGER xx_obcswstartdate2
-      INTEGER xx_obcsestartdate1
-      INTEGER xx_obcsestartdate2
-      INTEGER xx_obcsnstartdate(4)
-      INTEGER xx_obcssstartdate(4)
-      INTEGER xx_obcswstartdate(4)
-      INTEGER xx_obcsestartdate(4)
-      COMMON /controlvars_i_obcsn/
-     &                       nwetobcsn,
-     &                       nwetobcsnglo
-      INTEGER nwetobcsn     ( nSx,nSy,Nr,nobcs )
-      INTEGER nwetobcsnglo  ( Nr,nobcs )
-      COMMON /controlvars_i_obcss/
-     &                       nwetobcss,
-     &                       nwetobcssglo
-      INTEGER nwetobcss     ( nSx,nSy,Nr,nobcs )
-      INTEGER nwetobcssglo  ( Nr,nobcs )
-      COMMON /controlvars_i_obcsw/
-     &                       nwetobcsw,
-     &                       nwetobcswglo
-      INTEGER nwetobcsw     ( nSx,nSy,Nr,nobcs )
-      INTEGER nwetobcswglo  ( Nr,nobcs )
-      COMMON /controlvars_i_obcse/
-     &                       nwetobcse,
-     &                       nwetobcseglo
-      INTEGER nwetobcse     ( nSx,nSy,Nr,nobcs )
-      INTEGER nwetobcseglo  ( Nr,nobcs )
+     &                        xx_obcsEstartdate1,
+     &                        xx_obcsEstartdate2,
+     &                        xx_obcsWstartdate1,
+     &                        xx_obcsWstartdate2,
+     &                        xx_obcsNstartdate1,
+     &                        xx_obcsNstartdate2,
+     &                        xx_obcsSstartdate1,
+     &                        xx_obcsSstartdate2,
+     &                        xx_obcsEstartdate,
+     &                        xx_obcsWstartdate,
+     &                        xx_obcsNstartdate,
+     &                        xx_obcsSstartdate
+      INTEGER xx_obcsEstartdate1
+      INTEGER xx_obcsEstartdate2
+      INTEGER xx_obcsWstartdate1
+      INTEGER xx_obcsWstartdate2
+      INTEGER xx_obcsNstartdate1
+      INTEGER xx_obcsNstartdate2
+      INTEGER xx_obcsSstartdate1
+      INTEGER xx_obcsSstartdate2
+      INTEGER xx_obcsEstartdate(4)
+      INTEGER xx_obcsWstartdate(4)
+      INTEGER xx_obcsNstartdate(4)
+      INTEGER xx_obcsSstartdate(4)
+
+      COMMON /controlvars_i_obcsE/
+     &                       nwetobcsE,
+     &                       nwetobcsEglo
+      INTEGER nwetobcsE     ( nSx,nSy,Nr,nobcs )
+      INTEGER nwetobcsEglo  ( Nr,nobcs )
+      COMMON /controlvars_i_obcsW/
+     &                       nwetobcsW,
+     &                       nwetobcsWglo
+      INTEGER nwetobcsW     ( nSx,nSy,Nr,nobcs )
+      INTEGER nwetobcsWglo  ( Nr,nobcs )
+      COMMON /controlvars_i_obcsN/
+     &                       nwetobcsN,
+     &                       nwetobcsNglo
+      INTEGER nwetobcsN     ( nSx,nSy,Nr,nobcs )
+      INTEGER nwetobcsNglo  ( Nr,nobcs )
+      COMMON /controlvars_i_obcsS/
+     &                       nwetobcsS,
+     &                       nwetobcsSglo
+      INTEGER nwetobcsS     ( nSx,nSy,Nr,nobcs )
+      INTEGER nwetobcsSglo  ( Nr,nobcs )
 
 C     This is moved from ecco_local_params.h, because it is the only
 C     parameter used (by obcs ctrl parameters)
@@ -120,48 +121,48 @@ C     parameter used (by obcs ctrl parameters)
      &     obcs_data_errfile
       CHARACTER*(MAX_LEN_FNAM) obcs_data_errfile
 
-#ifdef ALLOW_OBCSN_CONTROL
-      COMMON /ctrl_cost_weights_obcsn/
-     &       wobcsn
-      _RL wobcsn     (                      Nr,nobcs)
-      COMMON /controlaux_obcsn_r/
-     &                      xx_obcsn0,
-     &                      xx_obcsn1
-      _RL xx_obcsn0 (1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
-      _RL xx_obcsn1 (1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
-#endif
-
-#ifdef ALLOW_OBCSS_CONTROL
-      COMMON /ctrl_cost_weights_obcss/
-     &       wobcss
-      _RL wobcss     (                      Nr,nobcs)
-      COMMON /controlaux_obcss_r/
-     &                      xx_obcss0,
-     &                      xx_obcss1
-      _RL xx_obcss0 (1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
-      _RL xx_obcss1 (1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
+#ifdef ALLOW_OBCSE_CONTROL
+      COMMON /ctrl_cost_weights_obcsE/
+     &     wobcsE
+      _RL wobcsE   (                      Nr,nobcs)
+      COMMON /controlaux_obcsE_r/
+     &                      xx_obcsE0,
+     &                      xx_obcsE1
+      _RL xx_obcsE0(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
+      _RL xx_obcsE1(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
 #endif
 
 #ifdef ALLOW_OBCSW_CONTROL
-      COMMON /ctrl_cost_weights_obcsw/
-     &       wobcsw
-      _RL wobcsw     (                      Nr,nobcs)
-      COMMON /controlaux_obcsw_r/
-     &                      xx_obcsw0,
-     &                      xx_obcsw1
-      _RL xx_obcsw0 (1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
-      _RL xx_obcsw1 (1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
+      COMMON /ctrl_cost_weights_obcsW/
+     &       wobcsW
+      _RL wobcsW   (                      Nr,nobcs)
+      COMMON /controlaux_obcsW_r/
+     &                      xx_obcsW0,
+     &                      xx_obcsW1
+      _RL xx_obcsW0(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
+      _RL xx_obcsW1(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
 #endif
 
-#ifdef ALLOW_OBCSE_CONTROL
-      COMMON /ctrl_cost_weights_obcse/
-     &     wobcse
-      _RL wobcse     (                      Nr,nobcs)
-      COMMON /controlaux_obcse_r/
-     &                      xx_obcse0,
-     &                      xx_obcse1
-      _RL xx_obcse0 (1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
-      _RL xx_obcse1 (1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
+#ifdef ALLOW_OBCSN_CONTROL
+      COMMON /ctrl_cost_weights_obcsN/
+     &       wobcsN
+      _RL wobcsN   (                      Nr,nobcs)
+      COMMON /controlaux_obcsN_r/
+     &                      xx_obcsN0,
+     &                      xx_obcsN1
+      _RL xx_obcsN0(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
+      _RL xx_obcsN1(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
+#endif
+
+#ifdef ALLOW_OBCSS_CONTROL
+      COMMON /ctrl_cost_weights_obcsS/
+     &       wobcsS
+      _RL wobcsS   (                      Nr,nobcs)
+      COMMON /controlaux_obcsS_r/
+     &                      xx_obcsS0,
+     &                      xx_obcsS1
+      _RL xx_obcsS0(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
+      _RL xx_obcsS1(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
 #endif
 
 #ifdef ALLOW_OBCS_CONTROL
@@ -169,10 +170,10 @@ C     parameter used (by obcs ctrl parameters)
 C     These fields are not used anywhere in the code so they are not
 C     defined by default.
       COMMON /ctrl_cost_weights2d/
-     &     wobcseLev, wobcswLev, wobcssLev, wobcsnLev
-      _RL wobcseLev(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
-      _RL wobcswLev(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
-      _RL wobcssLev(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
-      _RL wobcsnLev(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
+     &     wobcsELev, wobcsWLev, wobcsNLev, wobcsSLev
+      _RL wobcsELev(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
+      _RL wobcsWLev(1-OLy:sNy+OLy,Nr,nSx,nSy,nobcs)
+      _RL wobcsNLev(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
+      _RL wobcsSLev(1-OLx:sNx+OLx,Nr,nSx,nSy,nobcs)
 # endif
 #endif
