2d8bb0a635a13a3b1ce356a3389d34633333b0ce
Jean-Michel Campin <jmc@mit.edu>
2026-03-24 15:34:09 -0400

    add proper #ifdef to avoid compilation error
    - add #ifdef ALLOW_DIC and #ifdef ALLOW_SHELFICE to enable to compile
      without `'pkg/dic` and `pkg/shelfice`
    - move comments related to CPP options in COST_OPTIONS.h
    - more consistent use of ALLOW_COST_SHELFICE

--- pkg/cost/COST_OPTIONS.h
+++ pkg/cost/COST_OPTIONS.h
@@ -38,8 +38,11 @@ C       >>> Cost function contributions
 #undef ALLOW_COST_TRACER
 
 C   List these options here:
+C-  User needs to provide "cost_temp.F"  code before defining following option:
 #undef ALLOW_COST_TEMP
+C-  User needs to provide "cost_hflux.F" code before defining following option:
 #undef ALLOW_COST_HFLUXM
+C-  The following option contains some hacks (reset cost-function):
 #undef ALLOW_DIC_COST
 #undef ALLOW_THSICE_COST_TEST
 #undef ALLOW_COST_STREAMICE
