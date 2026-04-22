baeb35eec0a3d4c4cb78e1f49cc3c86784145d23
Jean-Michel Campin <jmc@mit.edu>
2026-04-22 17:32:25 -0400

    update description

--- pkg/layers/LAYERS_P2SHARE.h
+++ pkg/layers/LAYERS_P2SHARE.h
@@ -1,16 +1,16 @@
 CBOP
-C    !ROUTINE: LAYERS_SWITCH.h
+C    !ROUTINE: LAYERS_P2SHARE.h
 C    !INTERFACE:
-C    include LAYERS_SWITCH.h
+C    include LAYERS_P2SHARE.h
 C    !DESCRIPTION: \bv
 C     *==========================================================*
-C     | LAYERS_SWITCH.h
-C     | o variables for switching on/off some calculations
-C     |   Note: intended to be included also outside pkg/layers
+C     | LAYERS_P2SHARE.h
+C     | o Header file for pkg "layers" parameters that are used
+C     |   outside pkg/layers (and need to be shared).
 C     *==========================================================*
 C     \ev
 CEOP
 C     layers_useThermo :: true if using LAYERS_THERMODYNAMICS code
       LOGICAL layers_useThermo
-      COMMON / LAYERS_SWITCH_L / layers_useThermo
+      COMMON / LAYERS_PARAMS_TO_SHARE_L / layers_useThermo
 
